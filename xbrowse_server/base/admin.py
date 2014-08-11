b4bf12af5183308bc617a6e5f2247cc221ef185e
Brett Thomas <brettpthomas@gmail.com>
2014-08-11 10:00:31 -0400

    updating admin so I can select family image slides

--- xbrowse_server/base/admin.py
+++ xbrowse_server/base/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from xbrowse_server.base.models import *
+from django import forms
 
 admin.site.register(Project)
 admin.site.register(Family)
-admin.site.register(FamilyImageSlide)
 admin.site.register(Cohort)
 admin.site.register(Individual)
 admin.site.register(DiseaseGeneList)
@@ -15,3 +15,24 @@ admin.site.register(FamilySearchFlag)
 
 admin.site.register(VCFFile)
 admin.site.register(FamilyGroup)
+
+
+class FamilyModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "%s - %s" % (obj.project.project_id, obj.family_id)
+
+class FamilyImageSlideAdminForm(forms.ModelForm):
+    family = FamilyModelChoiceField(queryset=Family.objects.all())
+    class Meta:
+        model = FamilyImageSlide
+
+
+class FamilyImageSlideAdmin(admin.ModelAdmin):
+    form = FamilyImageSlideAdminForm
+
+
+
+
+
+
+admin.site.register(FamilyImageSlide, FamilyImageSlideAdmin)
