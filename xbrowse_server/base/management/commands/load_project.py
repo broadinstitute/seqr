3c792148072fd78e7867bdf42d1849eb2ce44f28
Brett Thomas <brettpthomas@gmail.com>
2014-11-02 21:44:33 -0800

    Including --force-clean option to load_project

--- xbrowse_server/base/management/commands/load_project.py
+++ xbrowse_server/base/management/commands/load_project.py
@@ -8,14 +8,19 @@ class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
         make_option('--force-annotations', action="store_true", dest='force_annotations', default=False),
-        make_option('--all', action="load_all", dest='load_all', default=False),
+        make_option('--force-clean', action="store_true", dest='force_clean', default=False),
+        make_option('--all', action="store_true", dest='load_all', default=False),
     )
 
     def handle(self, *args, **options):
         force_annotations = options.get('force_annotations')
+        force_clean = options.get('force_clean')
         load_all = options.get('load_all')
         if load_all:
-            for project in Project.objects.all().order_by('-last_accessed_date'):
-                xbrowse_controls.load_project(project.project_id, force_annotations=force_annotations)
-        for project_id in args:
+            project_ids = [p.project_id for p in Project.objects.all().order_by('-last_accessed_date')]
+        else:
+            project_ids = args
+        for project_id in project_ids:
+            if force_clean:
+                xbrowse_controls.clean_project(project_id)
             xbrowse_controls.load_project(project_id, force_annotations=force_annotations)
