4000c210e7a575ee21eb91f1431fedaf6da8d01c
Brett Thomas <brettpthomas@gmail.com>
2014-11-02 21:36:27 -0800

    Adding --all option to load_project, which loads all projects in order of most recently accessed.

--- xbrowse_server/base/management/commands/load_project.py
+++ xbrowse_server/base/management/commands/load_project.py
@@ -1,15 +1,21 @@
 from optparse import make_option
 from xbrowse_server import xbrowse_controls
 from django.core.management.base import BaseCommand
+from xbrowse_server.base.models import Project
 
 
 class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
         make_option('--force-annotations', action="store_true", dest='force_annotations', default=False),
+        make_option('--all', action="load_all", dest='load_all', default=False),
     )
 
     def handle(self, *args, **options):
         force_annotations = options.get('force_annotations')
+        load_all = options.get('load_all')
+        if load_all:
+            for project in Project.objects.all().order_by('-last_accessed_date'):
+                xbrowse_controls.load_project(project.project_id, force_annotations=force_annotations)
         for project_id in args:
             xbrowse_controls.load_project(project_id, force_annotations=force_annotations)
