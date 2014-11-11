d1a31462c78dfb66da68e8a8c23ea86ad1470f5f
Brett Thomas <brettpthomas@gmail.com>
2014-11-11 10:05:10 -0800

    Fixing this command line script - haven't used it in a while; wasn't working

--- scripts/freqs_from_vcf.py
+++ scripts/freqs_from_vcf.py
@@ -1,11 +1,11 @@
 import sys
 from xbrowse.parsers.vcf_stuff import iterate_vcf
-from xbrowse.utils import get_aaf
+from xbrowse.utils import get_aaf, compressed_file
 
 
 if __name__ == '__main__':
 
-    vcf_file = open(sys.argv[1])
+    vcf_file = compressed_file(sys.argv[1])
     for variant in iterate_vcf(vcf_file, genotypes=True):
         print '\t'.join([
             str(variant.xpos),
