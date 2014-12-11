75b297381d8cc7c98c998b084310068812f24096
Brett Thomas <brettpthomas@gmail.com>
2014-12-11 10:45:03 -0800

    Updating replace ID command

--- xbrowse_server/base/management/commands/replace_sample_ids.py
+++ xbrowse_server/base/management/commands/replace_sample_ids.py
@@ -1,21 +1,20 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
-from xbrowse_server import mall
 from xbrowse_server.base.models import Project, Individual
 
 
 class Command(BaseCommand):
+    """
+    Replace sample IDs in a project. Takes project ID and a 2 col TSV file with columns old_id, new_id
+    """
     def handle(self, *args, **options):
         project = Project.objects.get(project_id=args[0])
         sample_map = dict(l.strip('\n').split('\t') for l in open(args[1]).readlines())
 
-        datastore_db = mall.get_datastore()._db
-
         for old_id, new_id in sample_map.iteritems():
-            # change actual IDs
-            indiv = Individual.objects.get(project=project, indiv_id=old_id)
+            try:
+                indiv = Individual.objects.get(project=project, indiv_id=old_id)
+            except ObjectDoesNotExist:
+                continue
             indiv.indiv_id = new_id
             indiv.save()
-
-            # datastore
-            if indiv.family:
-                mall.get_datastore().delete_family(project.project_id, indiv.family.family_id)
