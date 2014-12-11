e7cd120c88d6402b894aa861718b15d0674cd48b
Brett Thomas <brettpthomas@gmail.com>
2014-12-11 11:51:53 -0800

    #51 - forgot to add the JS file

---
+++ xbrowse_server/staticfiles/js/family/family_variant_view.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function() {
+
+    window.hbc = new HeadBallCoach();
+    window.family = new Family(FAMILY);
+
+    var variant_view = new BasicVariantView({
+        hbc: hbc,
+        variant: VARIANT,
+        family: family,
+        show_genotypes: true,
+        individuals: family.individuals_with_variant_data(),
+    });
+
+    $('#variant-container').html(variant_view.render().el);
+
+    Backbone.history.start();
+
+});
