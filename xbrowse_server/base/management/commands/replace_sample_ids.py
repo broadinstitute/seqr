e8d4aabb680f7e4c3350da7b7b907014494fca96
Brett Thomas <brettpthomas@gmail.com>
2014-12-12 08:57:19 -0800

    Updating replace sample ID commands to fix pedigrees too

--- xbrowse_server/base/management/commands/replace_sample_ids.py
+++ xbrowse_server/base/management/commands/replace_sample_ids.py
@@ -11,6 +11,7 @@ class Command(BaseCommand):
         project = Project.objects.get(project_id=args[0])
         sample_map = dict(l.strip('\n').split('\t') for l in open(args[1]).readlines())
 
+        # replace the actual sample IDs
         for old_id, new_id in sample_map.iteritems():
             try:
                 indiv = Individual.objects.get(project=project, indiv_id=old_id)
@@ -18,3 +19,12 @@ class Command(BaseCommand):
                 continue
             indiv.indiv_id = new_id
             indiv.save()
+
+        # replace the maternal and paternal IDs
+        for individual in project.get_individuals():
+            if individual.paternal_id in sample_map:
+                individual.paternal_id = sample_map[individual.paternal_id]
+                individual.save()
+            if individual.maternal_id in sample_map:
+                individual.maternal_id = sample_map[individual.maternal_id]
+                individual.save()
