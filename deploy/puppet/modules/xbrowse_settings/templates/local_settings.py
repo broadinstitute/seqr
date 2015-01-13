04d48254116e3ac1616af327d9f34b621d569069
Brett Thomas <brettpthomas@gmail.com>
2015-01-13 10:49:24 -0500

    local_settings.py is part of provisioning now and should not be ignored

---
+++ deploy/puppet/modules/xbrowse_settings/templates/local_settings.py
@@ -0,0 +1,84 @@
+from xbrowse.reference import Reference
+from xbrowse.annotation import VariantAnnotator
+from xbrowse.datastore.population_datastore import PopulationDatastore
+from xbrowse.coverage import CoverageDatastore
+from xbrowse.datastore import MongoDatastore
+from xbrowse.cnv import CNVStore
+
+import os
+import pymongo
+import imp
+
+#from xbrowse_server.xbrowse_annotation_controls import CustomAnnotator
+
+# django stuff
+
+DEBUG = True
+#COMPRESS_ENABLED = False
+BASE_URL = 'http://192.168.50.101/'
+URL_PREFIX = '/'
+
+GENERATED_FILES_DIR = os.path.join(os.path.dirname(__file__), 'generated_files')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), 'database.sqlite'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+ALLOWED_HOSTS = ['*']
+
+MEDIA_ROOT = GENERATED_FILES_DIR + '/media/'
+STATIC_ROOT = GENERATED_FILES_DIR + '/static_root/'
+
+STATICFILES_DIRS = (
+    os.path.dirname(os.path.realpath(__file__)) + '/xbrowse_server/staticfiles/',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+#
+# xbrowse stuff
+#
+COMMON_SNP_FILE = "/vagrant/xbrowse-laptop-downloads/markers.txt"
+
+REFERENCEDATA_DIR = os.path.dirname(os.path.realpath(__file__)) + '/'
+HGMD_OMIM_FILE = '/vagrant/xbrowse-laptop-downloads/hgmd_omim_genes.txt'
+
+REFERENCE_SETTINGS = imp.load_source(
+    'reference_settings',
+    os.path.dirname(os.path.realpath(__file__)) + '/reference_settings.py'
+)
+CUSTOM_ANNOTATOR_SETTINGS = None
+
+ANNOTATOR_SETTINGS = imp.load_source(
+    'annotator_settings',
+    os.path.dirname(os.path.realpath(__file__)) + '/annotator_settings.py'
+)
+
+_conn = pymongo.Connection()
+DATASTORE_DB = _conn['xbrowse_datastore']
+POPULATION_DATASTORE_DB = _conn['xbrowse_pop_datastore']
+
+DEFAULT_CONTROL_COHORT = 'controls'
+CONTROL_COHORTS = [
+    {
+        'slug': 'controls',
+        'vcf': '',
+    },
+]
+
+COVERAGE_DB = _conn['xbrowse_coverage']
+
+PROJECT_DATASTORE_DB = _conn['xbrowse_proj_store']
+
+CNV_STORE_DB_NAME = 'xbrowse_cnvs'
+
+CUSTOM_POPULATIONS_DB = _conn['xcustom_refpops']
+
