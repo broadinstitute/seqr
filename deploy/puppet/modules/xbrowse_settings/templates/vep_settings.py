729e2b4bd968f559b501de2a0958ec0298c4a365
Brett Thomas <brettpthomas@gmail.com>
2015-01-23 19:02:48 -0800

    #85

--- deploy/puppet/modules/xbrowse_settings/templates/vep_settings.py
+++ deploy/puppet/modules/xbrowse_settings/templates/vep_settings.py
@@ -1,3 +1,3 @@
-vep_perl_path = '${execution_dir}/variant_effect_predictor/variant_effect_predictor.pl'
-vep_cache_dir = '${raw_data_dir}'
+vep_perl_path = '<%= raw_data_dir %>/variant_effect_predictor/variant_effect_predictor.pl'
+vep_cache_dir = '<%= raw_data_dir %>'
 vep_batch_size = 25000
