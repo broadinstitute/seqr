0aca7591c3c4521e621577a9602e380c1311a8ef
Brett Thomas <brettpthomas@gmail.com>
2015-02-12 10:24:47 -0800

    Separating VEP and Perl into two packages, and removing VEP from the installation by default

--- deploy/puppet/modules/vep/manifests/init.pp
+++ deploy/puppet/modules/vep/manifests/init.pp
@@ -23,25 +23,25 @@ class vep {
         require => Exec[ 'cpanmin' ],
     }
 
-    file { 'variant_effect_predictor.tar.gz':
-        path => "${execution_dir}/variant_effect_predictor.tar.gz",
-        ensure => present,
-        source => "${raw_data_dir}/variant_effect_predictor.tar.gz",
-        require => Exec[ 'extract-CGI-time' ],
-    }
-
-    exec { 'untar-variant-predictor':
-        command => "tar -xzf ${execution_dir}/variant_effect_predictor.tar.gz -C ${execution_dir}",
-        creates => "${execution_dir}/variant_effect_predictor/",
-        require => File[ "${execution_dir}/variant_effect_predictor.tar.gz" ],
-    }
-
-    exec { 'perl-install-variant':
-        command => "perl ${execution_dir}/variant_effect_predictor/INSTALL.pl",
-        cwd => "${execution_dir}/variant_effect_predictor",
-        require => Exec[ 'untar-variant-predictor' ],
-        creates => "${execution_dir}/variant_effect_predictor/Bio",
-    }
+#    file { 'variant_effect_predictor.tar.gz':
+#        path => "${execution_dir}/variant_effect_predictor.tar.gz",
+#        ensure => present,
+#        source => "${raw_data_dir}/variant_effect_predictor.tar.gz",
+#        require => Exec[ 'extract-CGI-time' ],
+#    }
+#
+#    exec { 'untar-variant-predictor':
+#        command => "tar -xzf ${execution_dir}/variant_effect_predictor.tar.gz -C ${execution_dir}",
+#        creates => "${execution_dir}/variant_effect_predictor/",
+#        require => File[ "${execution_dir}/variant_effect_predictor.tar.gz" ],
+#    }
+#
+#    exec { 'perl-install-variant':
+#        command => "perl ${execution_dir}/variant_effect_predictor/INSTALL.pl",
+#        cwd => "${execution_dir}/variant_effect_predictor",
+#        require => Exec[ 'untar-variant-predictor' ],
+#        creates => "${execution_dir}/variant_effect_predictor/Bio",
+#    }
 
 }
 
