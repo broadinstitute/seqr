a61269f2ed5165f29b5345dfc16908397d730c5f
Brett Thomas <brettpthomas@gmail.com>
2015-02-12 10:27:36 -0800

    updating provisioner

--- deploy/ec2/ec2_provision.pp
+++ deploy/ec2/ec2_provision.pp
@@ -5,14 +5,15 @@ node default {
     $server_type = 'ec2'
     $hostname = 'xbrowse'
     $fqdn = 'xbrowse.local'
+    $base_url = 'http://xbrowse.broadinstitute.org'
     $user = 'root'
     $gunicorn_num_workers = '4'
     $enable_ssl = true
 
-    $xbrowse_repo_dir = '/mnt/xbrowse-puppet/xbrowse'
-    $provisioning_base_dir = '/mnt/xbrowse-puppet'
+    $xbrowse_repo_dir = '/mnt/code/xbrowse'
+    $provisioning_base_dir = '/mnt'
     $execution_dir = '/mnt'
-    $raw_data_dir = '/mnt/xbrowse-laptop-downloads'
+    $raw_data_dir = '/mnt/data'
     $mongodb_dbpath = '/mnt/mongodb'
 
 #    # Drop firewall as we don't need it.
