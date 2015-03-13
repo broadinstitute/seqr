b8b66d9c9f016ad5e15f2c600cc4cc4678e6ea35
bw2 <weisburd@broadinstitute.org>
2015-03-13 03:45:14 -0400

    Get rid of external dbSNP data. Will now load dbSNP rsIDs from dbNSFP

--- deploy/puppet/modules/xbrowse_settings/templates/custom_annotator_settings.py
+++ deploy/puppet/modules/xbrowse_settings/templates/custom_annotator_settings.py
@@ -3,7 +3,6 @@ import pymongo
 db = pymongo.Connection()['x_custom_annots']
 
 reference_data_dir = '<%= raw_data_dir %>/'
-dbsnp_vcf_file = reference_data_dir + '00-All.vcf'
 dbnsfp_dir = reference_data_dir + 'dbNSFP/'
 #esp_target_file = reference_data_dir + 'esp_target.interval_list'
 
