5bd3620bd778711b243315b1e84ce2c810e18428
bw2 <ben.weisburd@gmail.com>
2015-03-23 10:36:53 -0400

    added parse error handling

--- xbrowse/parsers/fam_stuff.py
+++ xbrowse/parsers/fam_stuff.py
@@ -10,32 +10,34 @@ def get_individuals_from_fam_file(fam_file, project_id='.'):
     individuals = []
 
     for line in fam_file:
+        try: 
+            # ignore these rows
+            if line == '' or line.startswith('#'): continue
+            
+            fields = line.strip('\n').split('\t')
+            
+            indiv_id = slugify.slugify(fields[1])
+            family_id = slugify.slugify(fields[0])
+        
+            paternal_id = slugify.slugify(fields[2])
+            if paternal_id == "0": paternal_id = "."
 
-        # ignore these rows
-        if line == '' or line.startswith('#'): continue
+            maternal_id = slugify.slugify(fields[3])
+            if maternal_id == "0": maternal_id = "."
 
-        fields = line.strip('\n').split('\t')
+            gender = 'unknown'
+            if fields[4] == '2':
+                gender = 'female'
+            elif fields[4] == '1':
+                gender = 'male'
 
-        indiv_id = slugify.slugify(fields[1])
-        family_id = slugify.slugify(fields[0])
-
-        paternal_id = slugify.slugify(fields[2])
-        if paternal_id == "0": paternal_id = "."
-
-        maternal_id = slugify.slugify(fields[3])
-        if maternal_id == "0": maternal_id = "."
-
-        gender = 'unknown'
-        if fields[4] == '2':
-            gender = 'female'
-        elif fields[4] == '1':
-            gender = 'male'
-
-        affected_status = 'unknown'
-        if fields[5] == '2':
-            affected_status = 'affected'
-        elif fields[5] == '1':
-            affected_status = 'unaffected'
+            affected_status = 'unknown'
+            if fields[5] == '2':
+                affected_status = 'affected'
+            elif fields[5] == '1':
+                affected_status = 'unaffected'
+        except:
+            raise ValueError("Couldn't parse line: %s" % str(fields))
 
         indiv = Individual(
             indiv_id,
