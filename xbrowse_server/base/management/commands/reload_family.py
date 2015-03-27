8e51af2081774ef6758a9c095a4aea0c71c5d3b1
bw2 <weisburd@broadinstitute.org>
2015-03-27 14:50:42 -0400

    refactored to reload per-family variant db for specific families

--- xbrowse_server/base/management/commands/reload_family.py
+++ xbrowse_server/base/management/commands/reload_family.py
@@ -1,34 +1,46 @@
 from optparse import make_option
 
 from django.core.management.base import BaseCommand
+from xbrowse_server import xbrowse_controls
 
 from xbrowse_server.base.models import Project
-import tasks
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from xbrowse_server import xbrowse_controls
+
+from xbrowse_server.base.models import Project
+from xbrowse_server.mall import get_datastore
 
 
 class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
-        make_option('--project-id'),
-        make_option('--family-id'),
-        make_option('--no-async', action="store_true", dest='no_async'),
+        make_option('-p', "--project-id"),
     )
 
     def handle(self, *args, **options):
 
-        if not options.get('project_id'):
-            raise Exception
+        project_id = options["project_id"]
+        family_ids = args
+        project = Project.objects.get(project_id=project_id)
 
-        if 'project_id' in options and not options.get('family_id'):
-            project = Project.objects.get(project_id=options.get('project_id'))
-            if options.get('no_async'):
-                tasks.reload_project_variants(project.project_id)
-            else:
-                tasks.reload_project_variants.delay(project.project_id)
+        for vcf_file, families in project.families_by_vcf().items():
+            families_to_load = []
+            for family in families:
+                family_id = family.family_id
+                print("Checking id: " + family_id)
+                if not family_ids or family.family_id not in family_ids:
+                    continue
 
-        if 'project_id' in options and options.get('family_id'):
-            if options.get('no_async'):
-                tasks.reload_family_variants(options.get('project_id'), options.get('family_id'))
-            else:
-                tasks.reload_family_variants.delay(options.get('project_id'), options.get('family_id'))
+                # delete this family
+                get_datastore().delete_family(project_id, family_id)
+
+                families_to_load.append(family)
+                # reload family
+
+            print("Loading %(project_id)s %(families_to_load)s" % locals())
+            xbrowse_controls.load_variants_for_family_list(project, families_to_load, vcf_file)
+
+
 
