63a5d641e307211eae1913caa626599d4b92b3b6
bw2 <weisburd@broadinstitute.org>
2015-04-02 16:16:19 -0400

    Add dbnsfp loading to load_resources command

--- xbrowse_server/base/management/commands/load_resources.py
+++ xbrowse_server/base/management/commands/load_resources.py
@@ -2,9 +2,15 @@ from django.conf import settings
 from django.core.management import BaseCommand
 from xbrowse_server import mall
 from xbrowse_server.mall import get_reference
+from xbrowse_server.xbrowse_annotation_controls import CustomAnnotator
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        print("Load dbNSFP.. ")
+        custom_annotator = CustomAnnotator(settings.CUSTOM_ANNOTATOR_SETTINGS)
+        custom_annotator.load()
+
         get_reference().load()
         mall.get_annotator().load()
+
