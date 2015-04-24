2ad792bd34b417f02d488e511f47f48a8335e3f6
bw2 <ben.weisburd@gmail.com>
2015-04-23 21:17:49 -0400

    better logging for lock contention fix

--- xbrowse/datastore/mongo_datastore.py
+++ xbrowse/datastore/mongo_datastore.py
@@ -330,10 +330,10 @@ class MongoDatastore(datastore.Datastore):
         number_of_families = len(family_info_list)
         sys.stderr.write("Loading variants for %(number_of_families)d families %(family_info_list)s from %(vcf_file_path)s\n" % locals())
 
-        for family in family_info_list:
-            print(date.strftime(datetime.now(), "%m/%d/%Y %H:%M:%S  -- indexing family: " + str(family)))
-            collection = collections[family['family_id']]
-            collection.ensure_index([('xpos', 1), ('ref', 1), ('alt', 1)])
+        #for family in family_info_list:
+        #    print("Indexing family: " + str(family))
+        #    collection = collections[family['family_id']]
+        #    collection.ensure_index([('xpos', 1), ('ref', 1), ('alt', 1)])
 
         vcf_file = compressed_file(vcf_file_path)
         size = os.path.getsize(vcf_file_path)
@@ -356,7 +356,8 @@ class MongoDatastore(datastore.Datastore):
                 if len(buff[family_id]) == 0:
                     del buff[family_id]  # if no more variants for this family, delete it
 
-
+        vcf_rows_counter = 0
+        variants_buffered_counter = 0
         family_id_to_variant_list = defaultdict(list)  # will accumulate variants to be inserted all at once
         for variant in vcf_stuff.iterate_vcf(vcf_file, genotypes=True, indiv_id_list=indiv_id_list, vcf_id_map=vcf_id_map):
             progress.update(vcf_file.tell_progress())
@@ -366,6 +367,7 @@ class MongoDatastore(datastore.Datastore):
                 sys.stderr.write("WARNING: " + str(e) + "\n")
                 continue
 
+            vcf_rows_counter += 1
             for family in family_info_list:
                 # TODO: can we move this inside the if relevant clause below?
                 family_variant = variant.make_copy(restrict_to_genotypes=family['individuals'])
@@ -378,11 +380,12 @@ class MongoDatastore(datastore.Datastore):
                         variants_buffered_counter += 1
 
             if variants_buffered_counter > 10000:
-                print(date.strftime(datetime.now(), "%m/%d/%Y %H:%M:%S  -- inserting %d variants for %s families" % (variants_buffered_counter, len(family_id_to_variant_list))))
+                print(date.strftime(datetime.now(), "%m/%d/%Y %H:%M:%S") + "-- inserting %d family-variants from %d vcf rows into %s families" % (variants_buffered_counter, vcf_rows_counter, len(family_id_to_variant_list)))
 
                 insert_all_variants_in_buffer(family_id_to_variant_list, collections)
 
                 assert len(family_id_to_variant_list) == 0
+                vcf_rows_counter = 0
                 variants_buffered_counter = 0
 
                 #collection.update(
