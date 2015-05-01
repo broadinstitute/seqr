57a14584981e363c04c47990cc67dc65c053dd8e
bw2 <weisburd@broadinstitute.org>
2015-05-01 17:43:48 -0400

    Add options for AN, AC, AF keys to support ExAC population specific and popmax custom populations

--- xbrowse_server/base/management/commands/load_custom_populations.py
+++ xbrowse_server/base/management/commands/load_custom_populations.py
@@ -3,10 +3,14 @@ from xbrowse_server import xbrowse_controls
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from xbrowse_server.base.models import ReferencePopulation
+import sys
 
 class Command(BaseCommand):
+    # The 1st positional command line arg should be a population id (which was created using the create_custom_population command)
     option_list = BaseCommand.option_list + (
-        make_option('--some-option'),
+        make_option('--AF-key', help="The VCF info field key corresponding the to AF"),
+        make_option('--AC-key', help="The VCF info field key corresponding the to AC"),
+        make_option('--AN-key', help="The VCF info field key corresponding the to AN"),
     )
 
 
@@ -18,13 +22,22 @@ class Command(BaseCommand):
         else:
             pop_store = mall.get_custom_population_store()
             pop_store._ensure_indices()
-            print("Loading population: " + args[0])
 
-            requested_slug = args[0]
-            projects = [p for p in settings.ANNOTATOR_REFERENCE_POPULATIONS if p["slug"] == requested_slug] + \
-                [p.to_dict() for p in ReferencePopulation.objects.all() if p.slug == requested_slug]
+            population_id = args[0]
+            print("Loading population: " + population_id)
+
+            projects = [p for p in settings.ANNOTATOR_REFERENCE_POPULATIONS if p["slug"] == population_id] + \
+                       [p.to_dict() for p in ReferencePopulation.objects.all() if p.slug == population_id]
 
             assert len(projects) == 1
+
+            if options.af_key:
+                print(options.af_key)
+            elif options.ac_key and options.an_key:
+                print(options.ac_key, options.an_key)
+            else:
+                sys.exit("Must specify either --AF-key or both --AC-key and --AN-key")
+
             pop_store.load_population(projects[0])
 
 
