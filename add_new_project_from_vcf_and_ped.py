8de6b9c0ffbed3e42e2b60f742b033cf0e6546b5
bw2 <ben.weisburd@gmail.com>
2015-06-26 17:51:58 -0400

    Allow adding vcf without also adding a ped file

--- add_new_project_from_vcf_and_ped.py
+++ add_new_project_from_vcf_and_ped.py
@@ -9,7 +9,7 @@ from datetime import datetime, date
 p = argparse.ArgumentParser("")
 p.add_argument("-i", "--project-id", help="Project id", required=True)
 p.add_argument("-v", "--vcf", help="The vcf file", required=True)
-p.add_argument("-p", "--ped", help="The ped file", required=True)
+p.add_argument("-p", "--ped", help="The ped file. If not specified, the sample ids from the VCF will be used instead of the ped file.")
 p.add_argument("-f", "--force", help="Force annotation", action="store_true")
 p.add_argument("-r", dest="run", action="store_true", help="Actually run the commands")
 opts = p.parse_args()
@@ -20,14 +20,14 @@ project_id = opts.project_id
 
 if not os.path.isfile(vcf):
     p.error("Invalid vcf: " + vcf)
-if not os.path.isfile(ped):
+if ped and not os.path.isfile(ped):
     p.error("Invalid ped: " + ped)
 
 
 commands = [
     "kill `pgrep -f continuously_reload_all_projects_daemon.sh`",
     "python2.7 -u manage.py add_project %(project_id)s", 
-    "python2.7 -u manage.py add_individuals_to_project %(project_id)s --ped %(ped)s", 
+    "python2.7 -u manage.py add_individuals_to_project %(project_id)s " +  ("--ped %(ped)s" if ped else "--vcf %(vcf)s"), 
     "python2.7 -u manage.py add_vcf_to_project %(project_id)s %(vcf)s", 
     "python2.7 -u manage.py load_project %(project_id)s" + (" --force-annotations --force-clean " if opts.force else ""), 
     "python2.7 -u manage.py load_project_datastore %(project_id)s",
