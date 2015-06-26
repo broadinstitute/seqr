aa65464c86c562a690ba42901fa9dc24f17ba714
bw2 <ben.weisburd@gmail.com>
2015-06-26 17:53:53 -0400

    Print error if dot in project ids

--- xbrowse_server/base/management/commands/add_project.py
+++ xbrowse_server/base/management/commands/add_project.py
@@ -1,11 +1,14 @@
 from django.core.management.base import BaseCommand
 from xbrowse_server.base.models import Project
-
+import sys
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         project_id = args[0]
+        if "." in project_id:
+            sys.exit("ERROR: A '.' in the project ID is not supported")
+
         if Project.objects.filter(project_id=project_id).exists():
             raise Exception("Project exists :(")
         Project.objects.create(project_id=project_id)
