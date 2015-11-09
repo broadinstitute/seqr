666aa0ad8fb8967ce1d8352b8de240ccacbd2ed6
harindra <harindra@wm81f-a51.broadinstitute.org>
2015-11-09 15:43:57 -0500

    cleaning up this code and adding in an option to create patients in phenotips

--- xbrowse_server/base/management/commands/add_individuals_to_project.py
+++ xbrowse_server/base/management/commands/add_individuals_to_project.py
@@ -1,6 +1,6 @@
 from optparse import make_option
 import gzip
-
+import sys
 from django.core.management.base import BaseCommand
 
 from xbrowse_server.base.models import Project
@@ -14,9 +14,22 @@ class Command(BaseCommand):
         make_option('--sample-list'),
         make_option('--vcf'),
         make_option('--ped'),
+        make_option('--create_phenotips_patients'),
+        make_option('--phenotips_username'),
+        make_option('--phenotips_password'),
     )
 
     def handle(self, *args, **options):
+      
+        if options['vcf'] is None or options['ped'] is None or len(args)==0:
+          print '\n\nPlease enter a VCF file (--vcf), a PED file (--ped), and a project ID (first positional argument)'
+          print 'for example: python manage.py add_individuals_to_project  myProjectId  --vcf myVcf.vcf  --ped myPed.ped\n'
+          print 'If you also wish to create patients in phenotips, please use the --create_phenotips_patients,--phenotips_username, --phenotips_password to do so. ALL of them are REQUIRED.\n'
+          sys.exit()
+          
+        if options['create_phenotips_patients'] is not None and (options['phenotips_username'] is None or options['phenotips_password'] is None):
+          print '\n\nplease note if you use the --create_phenotips_patients option, both --phenotips_username and --phenotips_password options are REQUIRED.\n\n'
+          sys.exit()
 
         project_id = args[0]
         project = Project.objects.get(project_id=project_id)
@@ -48,5 +61,7 @@ class Command(BaseCommand):
     #given a list of individuals add them to phenotips
     def __add_individuals_to_phenotips(self,individuals):
       '''given a list of individuals add them to phenotips '''
-      print individuals
+      for individual in  individuals:
+        print individual
+        
       
