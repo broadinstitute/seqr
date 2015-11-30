e3fe6f3a27576d49be867aefa2ba9d5d65b09906
harindra <harindra@wm81f-a51.broadinstitute.org>
2015-11-30 09:53:16 -0500

    creating patients in phenotips via vcf and ped files

--- xbrowse_server/phenotips/utilities.py
+++ xbrowse_server/phenotips/utilities.py
@@ -9,9 +9,10 @@ logger = logging.getLogger(__name__)
 
 
 #create a patient record in phenotips
-def create_patient_record(self,individual_id):
+def create_patient_record(individual_id):
   '''make a patient record'''
-  uri = os.path.join(settings.PHENOPTIPS_HOST_NAME,'/bin/PhenoTips/OpenPatientRecord?create=true&eid=' + individual_id)
+  uri = settings.PHENOPTIPS_HOST_NAME + '/bin/PhenoTips/OpenPatientRecord?create=true&eid=' + individual_id
+  do_authenticated_call_to_phenotips(uri)
   
       
 #authenticates to phenotips, fetches (GET) given results and returns that
