c436d0bb4aa63098071bf3aa780154b124d69644
harindra <harindra@wm81f-a51.broadinstitute.org>
2015-12-18 12:42:15 -0500

    Switching to common styling by using doc strings and putting the method comments into the doc string

--- xbrowse_server/phenotips/utilities.py
+++ xbrowse_server/phenotips/utilities.py
@@ -8,11 +8,16 @@ import requests
 logger = logging.getLogger(__name__)
 import sys
 
-#create a patient record in phenotips
-#by convention username and password are project_id,project_idproject_id
-#authentication is protected by access to machine/localhost
+
 def create_patient_record(individual_id,project_id,patient_details=None):
-  '''make a patient record'''
+  '''
+  make a patient record:
+  
+  create a patient record in phenotips
+  by convention username and password are project_id,project_idproject_id
+  authentication is protected by access to machine/localhost  
+  
+  '''
   uri = settings.PHENOPTIPS_HOST_NAME + '/bin/PhenoTips/OpenPatientRecord?create=true&eid=' + individual_id
   if patient_details is not None:
     uri += '&gender='+patient_details['gender']
@@ -28,9 +33,12 @@ def create_patient_record(individual_id,project_id,patient_details=None):
       print 'error creating patient',individual_id,':',result
   
       
-#authenticates to phenotips, fetches (GET) given results and returns that
+
 def do_authenticated_call_to_phenotips(url,uname,pwd):
-  '''authenticates to phenotips, fetches (GET) given results and returns that'''
+  '''
+  authenticates to phenotips, fetches (GET) given results and returns that
+  '''
+
   try:
     password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
     request = urllib2.Request(url)
@@ -42,7 +50,7 @@ def do_authenticated_call_to_phenotips(url,uname,pwd):
     return e
     
 
-#to help process a translation of internal id to external id
+
 def convert_internal_id_to_external_id(int_id,project_phenotips_uname,project_phenotips_pwd):
   '''to help process a translation of internal id to external id '''
   try:
@@ -55,24 +63,33 @@ def convert_internal_id_to_external_id(int_id,project_phenotips_uname,project_ph
     logger.error('phenotips.views:'+str(e) + ' : ' + str(result))
     return {'mapping':None,'error':str(e)}
   
-#return the username and password for this project
+
 def get_uname_pwd_for_project(project_name):
+  '''
+  return the username and password for this project
+  '''
   uname=project_name
   pwd=project_name+project_name
   return uname,pwd
 
 
-#return the generic collaborator username and password for this project
+
 def get_generic_collaborator_uname_pwd_for_project(project_name):
+  '''
+  return the generic collaborator username and password for this project
+  '''
   uname=project_name+ '_view'
   pwd=project_name+project_name
   return uname,pwd
 
 
 
-#generates a new user in phenotips
+
 def add_new_user_to_phenotips(new_user_first_name, new_user_last_name,new_user_name,email_address,new_user_pwd):
-  #we need to put this password in a non-checkin file
+  '''
+  TBD: we need to put this password in a non-checkin file:
+  generates a new user in phenotips
+  '''
   admin_uname='Admin'
   admin_pwd='admin'
   headers={"Content-Type": "application/x-www-form-urlencoded"}
@@ -89,9 +106,12 @@ def add_new_user_to_phenotips(new_user_first_name, new_user_last_name,new_user_n
   do_authenticated_POST(admin_uname,admin_pwd,url,data,headers)
   
   
-#adds a collaborator to an existing patient. Requires an existing collaborator username, patient_eid (PXXXX..)
+
 def add_read_only_collaborator_phenotips_patient(collaborator_username,patient_eid):
-  #we need to put this password in a non-checkin file
+  '''
+  we need to put this password in a non-checkin file:
+  adds a collaborator to an existing patient. Requires an existing collaborator username, patient_eid (PXXXX..)
+  '''
   admin_uname='Admin'
   admin_pwd='admin'
   headers={"Content-Type": "application/x-www-form-urlencoded"}
@@ -104,16 +124,22 @@ def add_read_only_collaborator_phenotips_patient(collaborator_username,patient_e
   do_authenticated_POST(admin_uname,admin_pwd,url,data,headers)
   
   
-#do a PUT call to phenotips
+
 def do_authenticated_PUT(uname,pwd,url,data,headers):
+  '''
+  do a PUT call to phenotips
+  '''
   try:
     request=requests.put(url,data=data,auth=(uname,pwd),headers=headers)
   except Exception as e:
     print 'error in do_authenticated_PUT:',e,
     return e
   
-#do a PUT call to phenotips
+
 def do_authenticated_POST(uname,pwd,url,data,headers):
+  '''
+  do a POST call to phenotips
+  '''
   try:
     request=requests.post(url,data=data,auth=(uname,pwd),headers=headers)
   except Exception as e:
