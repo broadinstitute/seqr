c140920b58161008336ecbf49e4dd71fe246d08f
harindra <harindra@broadinstitute.org>
2016-01-21 15:13:50 -0500

    minor updates

--- settings.py
+++ settings.py
@@ -226,8 +226,8 @@ _client = MongoClient('localhost', 27017)
 _db = _client['phenotips_edit_audit']
 PHENOTIPS_EDIT_AUDIT = _db['phenotips_audit_record']
 PHENOTIPS_SUPPORTED_PROJECTS = (
-                       '1kg',
-                       'MYOSEQ_v16',
+                       '1000_Genomes',
+                       'Pierce',
                        )
 PHENOTIPS_ADMIN_UNAME='Admin'
 PHENOTIPS_ADMIN_PWD='admin'
