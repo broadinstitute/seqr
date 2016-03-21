680b5a37251480df19e0f56a8f9e23e9c8c3dff1
bw2 <ben.weisburd@gmail.com>
2016-03-21 02:32:03 -0400

    run collectstatic with --no-post-process . Postprocessing breaks on bootstrap and fontawesome dependencies

--- collect_static.sh
+++ collect_static.sh
@@ -1,2 +1,4 @@
 # script for deploying static files in production
-python manage.py collectstatic --link -i 'font-awesome*' -i 'igv.css' -i 'DT_bootstrap.css' -i 'jquery-ui-1.*'
+
+#python manage.py collectstatic --no-input --clear --link  -i 'font-awesome*' -i 'igv.css' -i 'DT_bootstrap.css' -i 'jquery-ui-1.*'
+python manage.py collectstatic --no-input --clear --link --no-post-process
