451388ab363786f1ef73d043d80c2b96be7465d5
bw2 <ben.weisburd@gmail.com>
2016-03-21 14:57:18 -0400

    Allow static file caching

--- settings.py
+++ settings.py
@@ -25,8 +25,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
             os.path.dirname(os.path.realpath(__file__)) + '/xbrowse_server/staticfiles/',
@@ -39,11 +39,11 @@ STATICFILES_FINDERS = (
 )
 
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-    }
-}
+#CACHES = {
+#    'default': {
+#        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+#    }
+#}
 
 TEMPLATES = [
     {
