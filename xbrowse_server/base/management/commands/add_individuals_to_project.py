0ba0329212e1d2874781f750a10bf4ef8cbdae62
bw2 <ben.weisburd@gmail.com>
2016-03-22 01:13:17 -0400

    Added basic consistency checks for ped files. Adjusted parsing to match latest xls format.

--- xbrowse_server/base/management/commands/add_individuals_to_project.py
+++ xbrowse_server/base/management/commands/add_individuals_to_project.py
@@ -4,6 +4,9 @@ from django.core.management.base import BaseCommand
 from xbrowse_server.base.models import Project
 from xbrowse_server import sample_management
 from xbrowse.parsers import vcf_stuff
+from xbrowse.utils import slugify
+
+
 try:
     from openpyxl import load_workbook
 except ImportError:
@@ -11,7 +14,6 @@ except ImportError:
 
 
 class Command(BaseCommand):
-
 
     def add_arguments(self, parser):
         parser.add_argument('--sample-list',
@@ -27,17 +29,17 @@ class Command(BaseCommand):
         parser.add_argument('--xls',
                     help=('An Excel spreadsheet with the following columns: '
                           'Collaborator Family ID, Collaborator Sample ID, Collaborator Father Sample ID, '
-                          'Collaborator Mother Sample ID, Sex, Affected?')
+                          'Collaborator Mother Sample ID, Sex, Affected Status. '
+                          'For example: ["NR", "NR_0", "NR_1", "NR_2", "Male", "Affected"]')
                     )
         parser.add_argument('args', nargs='*')
 
 
     def handle(self, *args, **options):
 
-        if len(args)==0 or options['vcf'] is None and options['ped'] is None:
-          print '\n\nPlease enter a VCF file (--vcf), OR IDEALLY A PED file (--ped), and a project ID (first positional argument).'
+        if len(args)==0 or options['vcf'] is None and options['ped'] is None and options['xls'] is None:
+          print '\n\nPlease enter a VCF file (--vcf), OR IDEALLY A PED or XLS file (using --ped or --xls), and a project ID (first positional argument).'
           print 'For example: python manage.py add_individuals_to_project  myProjectId --ped myPed.ped\n'
-          print 'If you also wish to create patients in phenotips, please use the --create_phenotips_patients\n'
           sys.exit()
           
         project_id = args[0]
@@ -69,59 +71,50 @@ class Command(BaseCommand):
 
         if options.get('xls'):
             xls_file = options.get('xls')
-            title_row, ped_rows = parse_xl_workbook(xls_file)
-            print(ped_rows)
+            title_row, xl_rows = parse_xl_workbook(xls_file)
+
             temp_ped_filename = 'temp.ped'
-            process_ped_rows(temp_ped_filename, ped_rows)
-            individual_details = sample_management.update_project_from_fam(project, temp_ped_filename)
-            for j in individual_details:
-                print("Adding %s: %s" % (j['indiv_id'], j))
-            #os.remove(temp_ped_filename)
+            write_xl_rows_to_ped(temp_ped_filename, xl_rows)
+            individual_details = sample_management.update_project_from_fam(project, open(temp_ped_filename))
+            
+            #for j in individual_details:
+            #    print("Adding %s: %s" % (j['indiv_id'], j))
 
 
+def write_xl_rows_to_ped(ped_filename, xl_rows):
+    """Writes the given rows to a ped file with the given filename
 
-def process_ped_rows(out_name,ped_rows):
-    '''
-    Process a PED and transform it into RDAP friendly format
-    Inputs:
-    1. A list of PED rows (each row is a tuple of ped columns)
-    Outputs:
-    1. True/False on success
-    '''
-    out=open(out_name,'w')
+    Args:
+        ped_filename: output filename 
+        xl_rows: a list of tuples where each tuple has 6 elements: family_id, sample_id, paternal_id, maternal_id, sex, affected
+    """
 
-    for row in ped_rows:
-        for i,col in enumerate(row):
-            if i==0:
-                writef(extract_family_id(col),out)
-                writef('\t',out)
-                writef(col.rstrip(),out),
+    with open(ped_filename, 'w') as out:
+        for i, row in enumerate(xl_rows):
+            assert len(row) == 6, "Unexpectd number of columns in row #%(i)s: %(row)s" % locals()
+
+            for _id in filter(None, row[0:4]):
+                assert slugify(_id) == _id, "row %(i)s has unexpected characters in id: '%(_id)s'. Only a-Z0-9 and - or _ are allowed" % locals()
+                
+            family_id, sample_id, paternal_id, maternal_id, sex, affected = row
+
+            assert family_id and sample_id, "family_id or sample_id not specified in row: %(row)s" % locals()
+
+            paternal_id = '.' if paternal_id is None else paternal_id
+            maternal_id = '.' if maternal_id is None else maternal_id
+
+            if sex is not None:
+                sex = {'M': '1', 'F': '2'}.get(sex[0].upper())
             else:
-                if col is not None:
-                    adj=col
-                    if i==3:
-                        if col=='F':
-                            adj='2'
-                        if col=='M':
-                            adj='1'
-                    if i==4:
-                        if col=='Unaffected':
-                            adj='1'
-                        if col=='Affected':
-                            adj='2'
-                    writef(adj,out)
-                else:
-                    if i==4:
-                        print '----WARN: found a missing Affected/Unaffected status, designating as -9'
-                        writef('-9',out)
-                    else:
-                        writef('.',out)
-            if i<len(row)-1:
-                writef('\t',out)
-        writef('\n',out)
-    out.close()
-    return True
+                sex ='.'
+                
+            if affected is not None:
+                affected = {'unaffected': '1', 'affected': '2'}.get(affected.lower())
+            else:
+                affected = '-9'
 
+            out.write('\t'.join([family_id, sample_id, paternal_id, maternal_id, sex, affected]) + '\n')
+                   
 
 def extract_family_id(patient_id):
     '''
@@ -147,6 +140,7 @@ def parse_xl_workbook(xl_file_name,has_title=True):
     '''
     ped=[]
     title_row=()
+    print("Loading Excel file: %s" % xl_file_name)
     wb = load_workbook(filename =xl_file_name)
     ws = wb.active
     for i,row in enumerate(ws.rows):
@@ -157,7 +151,7 @@ def parse_xl_workbook(xl_file_name,has_title=True):
             title_row=(tuple(r))
         else:
             ped.append(tuple(r))
-    return title_row,ped
+    return title_row, ped
 
 
 def writef(t,handle):
@@ -165,5 +159,3 @@ def writef(t,handle):
       Write text to file
     '''
     handle.write(t.encode('utf8'))
-
-
