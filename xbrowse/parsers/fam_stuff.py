5c5eb6835e582c8fc3749fb6411540342e7e3167
bw2 <ben.weisburd@gmail.com>
2016-03-23 07:07:07 -0400

    better ped file validation

--- xbrowse/parsers/fam_stuff.py
+++ xbrowse/parsers/fam_stuff.py
@@ -3,47 +3,35 @@ import itertools
 from xbrowse import Family, Individual
 from xbrowse.utils import slugify
 
+
 def get_individuals_from_fam_file(fam_file, project_id='.'):
     """
     Returns a list of individuals from a FAM file
     """
     individuals = []
 
-    # used for validating
-    indiv_to_pat_id = {}
-    indiv_to_mat_id = {}
-    indiv_to_family_id = {}
-
     for line in fam_file:
-        try: 
+        try:
             # ignore these rows
             if line == '' or line.startswith('#'): continue
-            
+
             fields = line.strip('\n').split('\t')
-            
+
             indiv_id = slugify(fields[1], separator='_')
             family_id = slugify(fields[0], separator='_')
-        
+
             paternal_id = slugify(fields[2], separator='_')
             if paternal_id == "0": paternal_id = "."
 
             maternal_id = slugify(fields[3], separator='_')
             if maternal_id == "0": maternal_id = "."
 
-            
-            assert indiv_id not in indiv_to_family_id, "duplicate individual_id: %(indiv_id)s" % locals()
-            indiv_to_family_id[indiv_id] = family_id
-            if maternal_id and maternal_id != '.': 
-                indiv_to_mat_id[indiv_id] = maternal_id
-            if paternal_id and paternal_id != '.': 
-                indiv_to_pat_id[indiv_id] = paternal_id
-                
             gender = 'unknown'
             if fields[4] == '2' or fields[4] == 'F':
                 gender = 'female'
             elif fields[4] == '1' or fields[4] == 'M':
                 gender = 'male'
-            
+
             affected_status = 'unknown'
             if fields[5] == '2':
                 affected_status = 'affected'
@@ -63,21 +51,58 @@ def get_individuals_from_fam_file(fam_file, project_id='.'):
         )
         individuals.append(indiv)
 
-    print("Loaded %d individuals in %d families" % (len(indiv_to_family_id), len(set(indiv_to_family_id.values()))))
+    return individuals
+
+
+def validate_fam_file(fam_file):
+    """
+    Reads in and does basic consistency checks on the given fam file.
+
+    Args:
+        fam_file: An open fam file id
+    """
+
+    individuals = get_individuals_from_fam_file(fam_file)
+
+    # used for validating
+    indiv_to_sex = {}
+    indiv_to_pat_id = {}
+    indiv_to_mat_id = {}
+    indiv_to_family_id = {}
+    for i in individuals:
+        assert i.indiv_id not in indiv_to_family_id, "duplicate individual_id: %(indiv_id)s" % locals()
+
+        indiv_to_family_id[i.indiv_id] = i.family_id
+        if i.maternal_id and i.maternal_id != '.':
+            indiv_to_mat_id[i.indiv_id] = i.maternal_id
+        if i.paternal_id and i.paternal_id != '.':
+            indiv_to_pat_id[i.indiv_id] = i.paternal_id
+        indiv_to_sex[i.indiv_id] = i.gender
+
+    print("Validating %d individuals in %d families" % (len(indiv_to_family_id), len(set(indiv_to_family_id.values()))))
+
+
     # run basic consistency checks
     for indiv_id, family_id in indiv_to_family_id.items():
+        
         for label, indiv_to_parent_id_map in (('maternal', indiv_to_mat_id), ('paternal', indiv_to_pat_id)):
             if indiv_id not in indiv_to_parent_id_map:
                 # parent not specified
                 continue
             
-            parent_id = indiv_to_parent_id_map[indiv_id]
-            assert parent_id in indiv_to_family_id, "%(indiv_id)s's %(label)s id: %(parent_id)s not found among individual ids: %(indiv_to_family_id)s" % locals()
+            parent_id = indiv_to_parent_id_map[indiv_id]            
+            if parent_id not in indiv_to_sex:
+                print("WARNING: %(indiv_id)s's %(label)s id: %(parent_id)s not found among individual ids: %(indiv_to_family_id)s" % locals())
+                continue
+
+            parent_sex = indiv_to_sex[parent_id]
+            if (label=='maternal' and parent_sex == 'male') or (label=='paternal' and parent_sex == 'female'):
+                raise ValueError("ERROR: %(parent_id)s is marked as %(label)s for %(indiv_id)s but has sex == %(parent_sex)s" % locals())
+                
             parent_family_id = indiv_to_family_id[parent_id]
             assert parent_family_id == family_id,  "%(indiv_id)s's family id: %(family_id)s does't match %(label)s family id: %(parent_family_id)s" % locals()
 
-    
-    return individuals
+
 
 def get_families_from_individuals(individuals, project_id='.'):
     """
@@ -116,5 +141,4 @@ def write_individuals_to_ped_file(fam_file, individuals):
         affected = affected_map[i.affected]
         fields = [family_id, i.indiv_id, i.paternal_id or ".", i.maternal_id or ".", gender, affected]
         fam_file.write("\t".join(fields) + "\n")
-
 
