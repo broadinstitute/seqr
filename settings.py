fe4248444e64e305fdaf8f8fae809d2cd6925eae
bw2 <ben.weisburd@gmail.com>
2016-04-11 23:19:51 -0400

    added username, passwd for HTTP bam server

--- settings.py
+++ settings.py
@@ -220,17 +220,7 @@ PHENOTIPS_ADMIN_PWD='admin'
 
 # when set to None, this *disables* the PhenoTips interface for all projects. If set to a list of project ids, it will
 # enable the PhenoTips interface for *all* projects except those in the list.
-PROJECTS_WITHOUT_PHENOTIPS = [
-"BC-S47-RNA",
-"Pierce_OGI497",
-"Pierce_OGI497_2",
-"MYOSEQ_v16",
-"MYOSEQ_v15",
-"INMR_v9",
-"INMR_exomes",
-"INMR_genomes",
-"INMR_rna",
-]
+PROJECTS_WITHOUT_PHENOTIPS = None
 
 
 from local_settings import *
