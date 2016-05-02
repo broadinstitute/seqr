229c45b632fc4191eaaa43d02dca7969d4767b8d
bw2 <weisburd@broadinstitute.org>
2016-05-01 22:36:51 -0400

    initial iteration of user dashboard page, a replacement for the all-projects page

---
+++ assets/pages/dashboard/components/bread-crumbs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+module.exports = React.createClass({
+
+    render: function () {
+        return <div className="ui large breadcrumb">
+            <div className="active section">Projects</div>
+        </div>
+    }
+});
