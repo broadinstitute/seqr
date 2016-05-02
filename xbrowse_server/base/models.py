f4ebcf602c58dacd4fbadbc87b7c43117883bee1
bw2 <ben.weisburd@gmail.com>
2016-05-02 02:37:34 -0400

    removed red and pink tag colors since these blend in with gene list tags and the background color for clinvar variants

--- xbrowse_server/base/models.py
+++ xbrowse_server/base/models.py
@@ -998,8 +998,6 @@ class ProjectTag(models.Model):
                 '#1f78b4',
                 '#b2df8a',
                 '#33a02c',
-                '#fb9a99',
-                '#e31a1c',
                 '#fdbf6f',
                 '#ff7f00',
                 '#cab2d6',
