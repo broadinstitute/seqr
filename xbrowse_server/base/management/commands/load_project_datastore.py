a02aa41203bb02ad5a9b011f8cfef35df94a446e
bw2 <ben.weisburd@gmail.com>
2016-05-04 12:08:02 -0400

    Added --start-from-chrom arg

--- xbrowse_server/base/management/commands/load_project_datastore.py
+++ xbrowse_server/base/management/commands/load_project_datastore.py
@@ -6,7 +6,8 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('args', nargs='*')
+        parser.add_argument('-s', '--start-from-chrom', help="start loading from this chromosome")
 
     def handle(self, *args, **options):
         project_id = args[0]
-        xbrowse_controls.load_project_datastore(project_id)
+        xbrowse_controls.load_project_datastore(project_id, start_from_chrom=options['start_from_chrom'])
