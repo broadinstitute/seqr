612732814c494c992f59950f1ca82859b9291e11
harindra <harindra@broadinstitute.org>
2016-05-11 22:11:52 -0400

    Adding a hash to represent MME ID

--- xbrowse_server/matchmaker/views.py
+++ xbrowse_server/matchmaker/views.py
@@ -6,6 +6,7 @@ from xbrowse_server.base.models import Project
 from django.conf import settings
 from xbrowse_server.server_utils import JSONResponse
 from xbrowse_server.reports.utilities import fetch_project_single_individual_data
+import hashlib
 
 @login_required
 @log_request('matchmaker_add')
@@ -17,8 +18,13 @@ def add_individual(request, project_id, individual_id):
     else:          
         family_data,variant_data,phenotype_entry_counts,family_statuses = fetch_project_single_individual_data(project_id, individual_id)
     
-    #MME_ID
-    id=individual_id
+    #make a unique hash to represent individual in MME for MME_ID
+    h = hashlib.md5()
+    h.update(individual_id)
+    id=h.hexdigest()
+    #map to put into mongo
+    map={"individual_id":individual_id,
+         "mme_id":id}
     
     
     #for testing only, should return a success/fail message
