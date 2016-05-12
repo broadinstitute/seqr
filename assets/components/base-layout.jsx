f7e2224fb7a0659fc9b99e9bf172a09ad2a20aea
bw2 <weisburd@broadinstitute.org>
2016-05-12 12:05:36 -0400

    just formatting

--- assets/components/base-layout.jsx
+++ assets/components/base-layout.jsx
@@ -9,11 +9,11 @@ module.exports = React.createClass({
                 <Header/>
 
                 <div className="ui grid" style={{minHeight:'calc(100% - 40px)'}}>
-                    <div className="two wide column"></div>
-                    <div className="twelve wide column" style={{ marginTop: "30px"}}>
+                    <div className="one wide column"></div>
+                    <div className="fourteen wide column" style={{ marginTop: "30px"}}>
                         {this.props.children}
                     </div>
-                    <div className="two wide column"></div>
+                    <div className="one wide column"></div>
                 </div>
             <Footer/>
         </div>
