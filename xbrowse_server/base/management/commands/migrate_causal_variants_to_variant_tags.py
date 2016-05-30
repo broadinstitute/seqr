defdd0cc6f34f19c69ed117d483577947be18bf5
bw2 <ben.weisburd@gmail.com>
2016-05-30 04:33:33 -0400

    Don't auto-create any tags besides 'Causal Variant'

--- xbrowse_server/base/management/commands/migrate_causal_variants_to_variant_tags.py
+++ xbrowse_server/base/management/commands/migrate_causal_variants_to_variant_tags.py
@@ -70,12 +70,12 @@ class Command(BaseCommand):
                     project_tag.save()
 
             # create the new ProjectTags
-            get_or_create_project_tag(project, new_tag_name="Strong Candidate", description="plausible candidate gene or variant",
-                                      keywords=["good", "strong"], color='#33a02c')
-            get_or_create_project_tag(project, new_tag_name="Excluded Variant", description="variant considered but excluded",
-                                      keywords=["exclude"], color='#ff0000')
-            get_or_create_project_tag(project, new_tag_name="Review", description="variant looks interesting but requires additional review at gene and variant level",
-                                      keywords=["review"], color='#ffbf00')
+            #get_or_create_project_tag(project, new_tag_name="Strong Candidate", description="plausible candidate gene or variant",
+            #                          keywords=["good", "strong"], color='#33a02c')
+            #get_or_create_project_tag(project, new_tag_name="Excluded Variant", description="variant considered but excluded",
+            #                          keywords=["exclude"], color='#ff0000')
+            #get_or_create_project_tag(project, new_tag_name="Review", description="variant looks interesting but requires additional review at gene and variant level",
+            #                          keywords=["review"], color='#ffbf00')
 
             causal_variant_tag = get_or_create_project_tag(project, new_tag_name="Causal Variant", description="proven causal variant or gene", keywords=["causal"], color='#1f78b4' )
             
