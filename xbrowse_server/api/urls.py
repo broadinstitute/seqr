6137524bca9147ecc45c907f282e64b60b870930
bw2 <weisburd@broadinstitute.org>
2016-06-10 17:08:08 -0400

    delete-note url

--- xbrowse_server/api/urls.py
+++ xbrowse_server/api/urls.py
@@ -47,6 +47,7 @@ urlpatterns = [
     url(r'^autocomplete/gene$', xbrowse_server.api.views.gene_autocomplete, name='gene_autocomplete'),
 
     url(r'^add-variant-note', xbrowse_server.api.views.add_variant_note, name='add_variant_note'),
+    url(r'^delete-variant-note/(?P<note_id>[\d]+)$', xbrowse_server.api.views.delete_variant_note, name='delete_variant_note'),
     url(r'^edit-variant-tags', xbrowse_server.api.views.edit_variant_tags, name='edit_variant_tags'),
     
     #phenotips related
