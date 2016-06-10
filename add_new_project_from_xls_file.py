3d2560d18d7ff19e4aa37cd6a6095ad7bb04eead
bw2 <ben.weisburd@gmail.com>
2016-06-10 11:06:07 -0400

    renamed 'generate_pedigree_images_for_project' to 'generate_pedigree_images'

--- add_new_project_from_xls_file.py
+++ add_new_project_from_xls_file.py
@@ -29,7 +29,7 @@ commands = [
     "python2.7 -u manage.py add_individuals_to_project %(project_id)s --ped temp.ped ",
     "python2.7 -u manage.py add_project_to_phenotips %(project_id)s '%(project_name)s' ",
     "python2.7 -u manage.py add_individuals_to_phenotips %(project_id)s --ped temp.ped ",
-    "python2.7 -u manage.py generate_pedigree_images_for_project %(project_id)s",
+    "python2.7 -u manage.py generate_pedigree_images %(project_id)s",
 ]
 commands = map(lambda s: s % globals(), commands )
 
