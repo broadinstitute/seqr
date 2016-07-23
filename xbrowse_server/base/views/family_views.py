58dac9beb5a8cf520e99a21a1af4d2500410b232
harindra <harindra@broadinstitute.org>
2016-07-22 20:48:49 -0400

    project report table that has phenotypes entered by individual and turning off MME features in readyness for a quick release

--- xbrowse_server/base/views/family_views.py
+++ xbrowse_server/base/views/family_views.py
@@ -55,10 +55,12 @@ def family_home(request, project_id, family_id):
         phenotips_supported=True
         if settings.PROJECTS_WITHOUT_PHENOTIPS is not None and project_id in settings.PROJECTS_WITHOUT_PHENOTIPS:
           phenotips_supported=False
-          
-        matchmaker_supported=True
-        if settings.PROJECTS_WITHOUT_MATCHMAKER is not None and project_id in settings.PROJECTS_WITHOUT_MATCHMAKER:
-          matchmaker_supported=False
+         
+        #COMMENTING PENDING TESTING
+        matchmaker_supported=False
+        #matchmaker_supported=True
+        #if settings.PROJECTS_WITHOUT_MATCHMAKER is not None and project_id in settings.PROJECTS_WITHOUT_MATCHMAKER:
+        #  matchmaker_supported=False
 
         analysis_status_json = family.get_analysis_status_json()
         analysis_status_choices = dict(ANALYSIS_STATUS_CHOICES)
