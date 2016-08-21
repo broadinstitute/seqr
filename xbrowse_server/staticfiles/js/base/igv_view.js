fd43921d0f3f73cf8701190a2561028fb097a82a
bw2 <ben.weisburd@gmail.com>
2016-08-20 23:44:05 -0400

    IGV bed tracks

--- xbrowse_server/staticfiles/js/base/igv_view.js
+++ xbrowse_server/staticfiles/js/base/igv_view.js
@@ -15,11 +15,30 @@ window.IgvView = Backbone.View.extend({
         });
 
         for (var i = 0; i < this.individuals.length; i += 1) {
+	    var indiv = this.individuals[i];
+	    var cnv_bed_url = null;
+	    if(indiv.indiv_id.startsWith("MAN_")) {
+		cnv_bed_url = '/static/igv/cnvs/manton_array_cnv_bed_files/'+ indiv.indiv_id + '.bed';
+	    } else if(indiv.indiv_id.startsWith("PIE_")) {
+		cnv_bed_url = '/static/igv/cnvs/pierce_array_cnv_bed_files/'+ indiv.indiv_id + '.bed';
+	    }
+
+	    if(cnv_bed_url) {
+		console.log("Adding " + cnv_bed_url);
+	        tracks.push({
+		    url: cnv_bed_url,
+		    indexed: false,
+		    name: '<i style="font-family: FontAwesome; font-style: normal; font-weight: normal;" class="' + utils.get_pedigree_icon(indiv) + '"></i> ' + indiv.indiv_id + ' CNVs',
+	        });		
+	    }
+	}
+
+        for (var i = 0; i < this.individuals.length; i += 1) {
             var indiv = this.individuals[i];
             if (!indiv.has_bam_file_path) {
                 continue;
             }
-
+	 
             tracks.push({
                 url: "/project/" + indiv.project_id + "/igv-track/" + indiv.indiv_id,
                 type: 'bam',
@@ -38,6 +57,7 @@ window.IgvView = Backbone.View.extend({
             locus: this.locus,
             showKaryo: false,
             tracks: tracks,
+	    showVerticalLine: true,
         };
 
         igv.createBrowser(this.el, this.options);
