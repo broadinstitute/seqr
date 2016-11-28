5fd087b453a256dbaa9b1edf1d161f2cbe002a2e
bw2 <weisburd@broadinstitute.org>
2016-11-27 22:35:46 -0500

    indents

--- assets/pages/search/actions/searchResults.js
+++ assets/pages/search/actions/searchResults.js
@@ -5,8 +5,8 @@ export const SET_SEARCH_RESULT = 'SET_SEARCH_RESULT'
 
 // action creators
 export const setSearchResults = (results) => {
-    return {
-        type: SET_SEARCH_RESULT,
-        results: results
-    }
+  return {
+    type: SET_SEARCH_RESULT,
+    results: results,
+  }
 }
