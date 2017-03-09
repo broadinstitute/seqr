36f2848901b963a9d9c959a6cce0341c89264340
bw2 <weisburd@broadinstitute.org>
2017-03-09 06:02:33 -0500

    added FAMILY_ANALYSIS_STATUS_OPTIONS

--- ui/pages/Dashboard/constants.js
+++ ui/pages/Dashboard/constants.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multi-spaces */
 
 //possible values
 export const SHOW_ALL = 'SHOW_ALL'
@@ -14,7 +15,6 @@ export const SORT_BY_ANALYSIS = 'SORT_BY_ANALYSIS'
 
 //export const SORT_BY_DATE_ACCESSED = 'SORT_BY_DATE_ACCESSED'
 
-
 // modal
 export const EDIT_NAME_MODAL = 'EDIT_NAME'
 export const EDIT_DESCRIPTION_MODAL = 'EDIT_DESCRIPTION'
@@ -25,3 +25,18 @@ export const DELETE_PROJECT_MODAL = 'DELETE_PROJECT'
 export const MODAL_SAVING = 'MODAL_SAVING'
 export const MODAL_SAVE_SUCCEEDED = 'MODAL_SAVE_SUCCEEDED'
 export const MODAL_SAVE_ERROR = 'MODAL_SAVE_ERROR'
+
+
+export const FAMILY_ANALYSIS_STATUS_OPTIONS = [
+  { key: 'S',         color: '#4CAF50', name: 'Solved' },
+  { key: 'S_kgfp',    color: '#4CAF50', name: 'Solved - known gene for phenotype' },
+  { key: 'S_kgdp',    color: '#4CAF50', name: 'Solved - gene linked to different phenotype' },
+  { key: 'S_ng',      color: '#4CAF50', name: 'Solved - novel gene' },
+  { key: 'Sc_kgfp',   color: '#CDDC39', name: 'Strong candidate - known gene for phenotype' },
+  { key: 'Sc_kgdp',   color: '#CDDC39', name: 'Strong candidate - gene linked to different phenotype' },
+  { key: 'Sc_ng',     color: '#CDDC39', name: 'Strong candidate - novel gene' },
+  { key: 'Rcpc',      color: '#CDDC39', name: 'Reviewed, currently pursuing candidates' },
+  { key: 'Rncc',      color: '#EF5350', name: 'Reviewed, no clear candidate' },
+  { key: 'I',         color: '#4682B4', name: 'Analysis in Progress' },
+  { key: 'Q',         color: '#AAAAAF', name: 'Waiting for data' },  //#FFC107
+]
