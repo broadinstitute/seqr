24f809a9ff67d63b0fec9d26da8742219bde3ec4
harindra <harindra@broadinstitute.org>
2017-03-15 18:56:25 -0400

    -Fixed a counting bug in finding genes in seqr and matchbox, where the code was incrementing gene count when the gene may have appeared in other patients as well. -Changed raw value of number of variants matchbox and seqr have in common to only a percentage -Added in an error modal for when database update fails -Changed the "hit return to save" to a button to save

--- xbrowse_server/api/views.py
+++ xbrowse_server/api/views.py
@@ -1375,10 +1375,11 @@ def get_matchbox_metrics(request):
         seqr__gene_info = gather_all_annotated_genes_in_seqr()
         seqr_metrics={"genes_in_seqr":len(seqr__gene_info),
                       "genes_found_in_matchbox":0}
+        unique_genes=[]
         for gene_ids,proj in seqr__gene_info.iteritems():
             if gene_ids[0] in genes_in_matchbox:
-                seqr_metrics['genes_found_in_matchbox'] +=1
-        
+                unique_genes.append(gene_ids[0])
+        seqr_metrics['genes_found_in_matchbox'] = len(set(unique_genes))
         seqr_metrics["submission_info"]=find_projects_with_families_in_matchbox()
                        
         return JSONResponse({"from_matchbox":r.json(),
