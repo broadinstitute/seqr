10a7304a0dacc0f37e5700f98c2f143b415628aa
bw2 <weisburd@broadinstitute.org>
2017-03-22 10:53:38 -0400

    minor formatting and layout changes

--- seqr/views/pages/staff/users_page.py
+++ seqr/views/pages/staff/users_page.py
@@ -21,7 +21,7 @@ TEMPLATE = Template("""
                 targets: 0,
             } ],
 
-        "order": [[ 1, 'asc' ]]
+            order: [[ 1, 'asc' ]]
         });
 
         // index column (https://datatables.net/examples/api/counter_columns.html)
@@ -32,42 +32,46 @@ TEMPLATE = Template("""
         } ).draw();
     });
     </script>
+    <style>
+        .nowrap {
+            white-space: nowrap
+        }
+    </style>
 </head>
 <body>
+    <a href="/">seqr</a>  &nbsp; <a href="/dashboard">dashboard</a>
     <table id="userTable" class="display">
     <thead>
         <tr>
             <th> </th>
-            <th>login</th>
-            <th>email</th>
-            <th>username</th>
-            <th data-s-type="date">date joined</th>
-            <th data-s-type="date">last login</th>
-            <th>name</th>
-            <th>password</th>
-            <th>staff</th>
-            <th>superuser</th>
-            <th>id</th>
+            <th width="80%">email</th>
+            <th class="nowrap">username</th>
+            <th class="nowrap" data-s-type="date">date joined</th>
+            <th class="nowrap" data-s-type="date">last login</th>
+            <th class="nowrap">name</th>
+            <th class="nowrap">staff</th>
+            <th class="nowrap">superuser</th>
+            <th class="nowrap">id</th>
+            <th class="nowrap">login as</th>
         </tr>
     </thead>
     {% for user in users %}
         <tr>
             <td> </td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.username }}</td>
+            <td>{{ user.date_joined|date:'Y-m-d' }}</td>
+            <td>{{ user.last_login|date:'Y-m-d' }}</td>
+            <td>{{ user.first_name }} {{ user.last_name }}</td>
+            <td>{% if user.is_staff %} &#x2714; {% endif %}</td>
+            <td>{% if user.is_superuser %} &#x2714; {% endif %}</td>
+            <td>{{ user.id }}</td>
             <td>
                 <form action="/hijack/{{ user.id }}/" method="post">
                     {% csrf_token %}
                     <button type="submit">Log in</button>
                 </form>
             </td>
-            <td>{{ user.email }}</td>
-            <td>{{ user.username }}</td>
-            <td>{{ user.date_joined|date:'Y-m-d' }}</td>
-            <td>{{ user.last_login|date:'Y-m-d' }}</td>
-            <td>{{ user.first_name }} {{ user.last_name }}</td>
-            <td>{{ user.password }}</td>
-            <td>{% if user.is_staff %} &#x2714; {% endif %}</td>
-            <td>{% if user.is_superuser %} &#x2714; {% endif %}</td>
-            <td>{{ user.id }}</td>
         </tr>
     {% endfor %}
     </table>
@@ -78,6 +82,7 @@ TEMPLATE = Template("""
 
 @staff_member_required
 def users_template(request):
+    # generate HttpResponse. Use RequestContext instead of Context so that csrf_token is handled properly
     c = RequestContext(request, {
         'users': User.objects.all().order_by('email')
     })
