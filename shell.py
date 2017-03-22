0120ae79564d79481b13b88b0a3541f6fea3fa30
bw2 <weisburd@broadinstitute.org>
2017-03-22 07:44:27 -0400

    added User and UserProfile to context

--- shell.py
+++ shell.py
@@ -12,25 +12,37 @@ from xbrowse.coverage import CoverageDatastore
 from xbrowse.datastore import MongoDatastore
 import reference_settings
 import annotator_settings
-from xbrowse_server.base.models import Project, Family, Individual, ProjectTag, VariantTag
-reference_db = pymongo.MongoClient('xbrowse_reference')
-reference = Reference(reference_settings) 
+from seqr.models import Project as SeqrProject, Family as SeqrFamily, Individual as SeqrIndividual, VariantTagType, VariantTag
+from django.contrib.auth.models import User, UserProfile
+from xbrowse_server.base.models import Project as BaseProject, Family as BaseFamily, Individual as BaseIndividual
 
-annotator_db = pymongo.MongoClient('xbrowse_annotator')
-annotator = VariantAnnotator(annotator_settings, reference)
-annotator._ensure_indices()
+annotator = datastore = None
+try:
+    reference_db = pymongo.MongoClient('xbrowse_reference')
+    reference = Reference(reference_settings) 
 
-datastore_db = pymongo.MongoClient('xbrowse_datastore')
-datastore = MongoDatastore(datastore_db, annotator)
+    annotator_db = pymongo.MongoClient('xbrowse_annotator')
+    annotator = VariantAnnotator(annotator_settings, reference)
+    annotator._ensure_indices()
+
+    datastore_db = pymongo.MongoClient('xbrowse_datastore')
+    datastore = MongoDatastore(datastore_db, annotator)
+except Exception as e:
+    print("Error while connecting to mongo: " + str(e))
 
 user_ns = {
     'annotator': annotator, 
     'datastore': datastore,
-    'Project': Project,
-    'Family': Family,
-    'Individual': Individual,
+    'BaseProject': BaseProject,
+    'BaseFamily': BaseFamily,
+    'BaseIndividual': BaseIndividual,
+    'SeqrProject': SeqrProject,
+    'SeqrFamily': SeqrFamily,
+    'SeqrIndividual': SeqrIndividual,
     'VariantTag': VariantTag,
-    'ProjectTag': ProjectTag,
+    'VariantTagType': VariantTagType,
+    'User': User,
+    'UserProfile': UserProfile,
 }
 
 import IPython
