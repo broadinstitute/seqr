0f0e986697cb1269aa2094c8ca8e982f4e4eecc1
bw2 <weisburd@broadinstitute.org>
2017-03-31 14:39:06 -0400

    output logs to stdout/stderr

--- deploy/docker/seqr/config/gunicorn_config.py
+++ deploy/docker/seqr/config/gunicorn_config.py
@@ -4,7 +4,6 @@ workers = 1
 loglevel = 'info'
 pythonpath='/seqr'
 timeout = 120   # seconds (default is 30)
-
-# errorlog = '${INSTALL_DIR}/logs/gunicorn-error.log'
-# accesslog = '${INSTALL_DIR}/logs/gunicorn-access.log'
+errorlog = '-'  # '${INSTALL_DIR}/logs/gunicorn-error.log'
+accesslog = '-' # '${INSTALL_DIR}/logs/gunicorn-access.log'
 #worker_tmp_dir = '/tmp'
