b49581e0e5ed4450a99d5a608e996a1e542b2962
bw2 <weisburd@broadinstitute.org>
2017-04-17 01:39:20 -0400

    refactored secrets - particularly passwords

--- deploy/docker/phenotips/entrypoint.sh
+++ deploy/docker/phenotips/entrypoint.sh
@@ -5,6 +5,20 @@ trap './stop.sh' SIGTERM
 
 env
 
+
+echo Current Directory: $(pwd)
+
+# update config files
+sed -i s/connection.url\"\>jdbc\:postgresql\:xwiki/connection.url\"\>jdbc:postgresql:\\\/\\\/postgres-svc:5432\\\/xwiki/g  ./webapps/phenotips/WEB-INF/hibernate.cfg.xml
+
+if [ -n "$POSTGRES_USERNAME" ]; then
+    sed -i s/connection.username\"\>postgres\</connection.username\"\>$(echo $POSTGRES_USERNAME)\</g  ./webapps/phenotips/WEB-INF/hibernate.cfg.xml
+fi
+
+if [ -n "$POSTGRES_PASSWORD" ]; then
+    sed -i s/connection.password\"\>\</connection.password\"\>$(echo $POSTGRES_PASSWORD)\</g  ./webapps/phenotips/WEB-INF/hibernate.cfg.xml
+fi
+
 # turn on debugging
 #if [ "$PT_DEBUG" = "true" ]; then
 #   export START_OPTS="$START_OPTS -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
--- deploy/docker/postgres/002_init.sh
+++ deploy/docker/postgres/002_init.sh
@@ -4,3 +4,4 @@ echo "002_init.sh is running"
 
 env
 
+echo ALTER USER postgres WITH PASSWORD \'${POSTGRES_PASSWORD}\' | psql -U postgres
