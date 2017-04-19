56e6d7aa249f38a7f8a9cb9a1e7f5efba325e088
bw2 <weisburd@broadinstitute.org>
2017-04-19 10:49:20 -0400

    updated matchbox deploy script

--- deploy/kubernetes/utils/constants.py
+++ deploy/kubernetes/utils/constants.py
@@ -16,4 +16,17 @@ PORTS = {
     'cockpit':   [9090],
 }
 
+
+DEPLOYMENT_SCRIPTS = [
+    'scripts/deploy_init.sh',
+    'scripts/deploy_nginx.sh',
+    'scripts/deploy_postgres.sh',
+    'scripts/deploy_mongo.sh',
+    'scripts/deploy_phenotips.sh',
+    'scripts/deploy_cockpit.sh',
+    'scripts/deploy_matchbox.sh',
+    'scripts/deploy_seqr.sh',
+]
+
+
 WEB_SERVER_COMPONENTS = ['seqr', 'phenotips', 'cockpit']
