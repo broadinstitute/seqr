1db21fab50fdecd8846852092557fd017f3a0f41
bw2 <weisburd@broadinstitute.org>
2017-04-25 23:50:22 -0400

    separate command to load allele freqs. from commnand to load other reference data

--- xbrowse_server/base/management/commands/load_resources.py
+++ xbrowse_server/base/management/commands/load_resources.py
@@ -1,12 +1,6 @@
-from django.conf import settings
 from django.core.management import BaseCommand
-from xbrowse_server import mall
 from xbrowse_server.mall import get_reference
-from xbrowse_server.xbrowse_annotation_controls import CustomAnnotator
-
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         get_reference().load()
-        mall.get_annotator().load()
-
