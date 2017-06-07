20633f4b743c715694a6d05cfb1c55f4cafa1a26
bw2 <weisburd@broadinstitute.org>
2017-06-07 08:13:12 -0400

    added 5e-4, 1e-4, 0 positions to AF filters

--- xbrowse_server/staticfiles/js/base/utils.js
+++ xbrowse_server/staticfiles/js/base/utils.js
@@ -81,24 +81,27 @@ window.utils = {
 	},
 	
 	freqIndex: function(val) {
-
-		if (val == .001) return 1;
-		else if (val == .005) return 2;
-		else if (val == .01) return 3;
-		else if (val == .05) return 4;
-		else if (val == .1) return 5;
-		else return 6;
+		if (val == 0) return 1;
+    else if (val == .0001) return 2;
+    else if (val == .0005) return 3;
+    else if (val == .001) return 4;
+		else if (val == .005) return 5;
+    else if (val == .01) return 6;
+    else if (val == .05) return 7;
+		else if (val == .1) return 8;
+		else return 11;
 	}, 
 
 	freqInverse: function(position) {
-
-		if (position == 1) return Number(.001).toExponential();
-		else if (position == 2) return Number(.005).toExponential();
-		else if (position == 3) return .01;
-		else if (position == 4) return .05;
-		else if (position == 5) return .1;
+    if (position == 1) return 0;
+    else if (position == 2) return Number(.0001).toExponential();
+    else if (position == 3) return Number(.0005).toExponential();
+    else if (position == 4) return Number(.001).toExponential();
+		else if (position == 5) return Number(.005).toExponential();
+		else if (position == 6) return .01;
+		else if (position == 7) return .05;
+		else if (position == 8) return .1;
 		else return 1;
-
 	}, 
 
 	formatFrequency: _formatFrequency, 
