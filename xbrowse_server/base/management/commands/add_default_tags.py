28cf64f7129204856d5fdcf5bb632bb3f484ba36
bw2 <weisburd@broadinstitute.org>
2017-06-07 19:01:13 -0400

    updated variant tags

--- xbrowse_server/base/management/commands/add_default_tags.py
+++ xbrowse_server/base/management/commands/add_default_tags.py
@@ -85,18 +85,19 @@ class Command(BaseCommand):
 
             get_or_create_project_tag(project, order=9, category="CMG Discovery Tags", tag_name="Known gene for phenotype", color='#030A75', description="The gene overlapping the variant has been previously associated with the same phenotype presented by the patient")
 
-            get_or_create_project_tag(project, order=10, category="Collaboration", original_names=["Review", "Interesting"], tag_name="Review - variant and/or gene of interest", description="", color='#668FE3')
+            get_or_create_project_tag(project, order=10, category="Collaboration", original_names=["Review", "Interesting"], tag_name="Review", description="To mark a variant and/or gene of interest for further review.", color='#668FE3')
+            get_or_create_project_tag(project, order=10.5, category="Collaboration", tag_name="Excluded", description="To mark a variant and/or gene you previously reviewed but do not think it contributing to the phenotype in this case. To help other members of your team (and yourself), please consider also adding a note with details of why you reprioritized this variant.", color='#555555')
 
             get_or_create_project_tag(project, order=11, category="ACMG Variant Classification", tag_name="Pathogenic", description="", color='#B92732')  # red
             get_or_create_project_tag(project, order=15, category="ACMG Variant Classification", tag_name="Benign", description="", color='#2971B1')  # green6
 
             get_or_create_project_tag(project, order=12, category="ACMG Variant Classification", original_names=["LP"], tag_name="Likely Pathogenic", description="", color='#E48065')  # light red
-            get_or_create_project_tag(project, order=13, category="ACMG Variant Classification", tag_name="VUS", description="", color='#FACCB4')  # gray
+            get_or_create_project_tag(project, order=13, category="ACMG Variant Classification", tag_name="VUS", description="Variant of uncertain significance", color='#FACCB4')  # gray
             get_or_create_project_tag(project, order=14, category="ACMG Variant Classification", original_names=["LB"], tag_name="Likely Benign", description="", color='#6BACD0')  # light green
 
             get_or_create_project_tag(project, order=16, category="ACMG Variant Classification", original_names=["Incidental Finding"], tag_name="Secondary finding", color="#FED82F", description="The variant was found during the course of searching for candidate disease genes and can be described as pathogenic or likely pathogenic according to ACMG criteria and overlaps a gene known to cause a disease that differs from the patient's primary indication for sequencing.")
 
             get_or_create_project_tag(project, order=17, category="Data Sharing", original_names=["MME"], tag_name="MatchBox (MME)", description="Gene, variant, and phenotype to be submitted to Matchmaker Exchange", color='#531B86')
-            get_or_create_project_tag(project, order=18, category="Data Sharing", original_names=["clinvar"], tag_name="Submit to Clinvar", description="", color='#8A62AE')
+            get_or_create_project_tag(project, order=18, category="Data Sharing", original_names=["clinvar"], tag_name="Submit to Clinvar", description="By selecting this tag, you are notifying CMG staff that this variant should be submitted to ClinVar. Generally, this is for pathogenic or likely pathogenic variants in known disease genes or for any benign or likely benign variants that are incorrectly annotated in ClinVar. Please also add a note that describes supporting evidence for how you interpreted this variant.", color='#8A62AE')
 
         print("Done")
