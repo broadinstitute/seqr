9b5fce5b9a48d69a126d6e058ec6b959bedaf3ae
bw2 <weisburd@broadinstitute.org>
2017-06-08 20:13:43 -0400

    Fixing directory name

--- ui/pages/Project/components/table-header/TableHeaderRow.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import { TableHeaderRowComponent } from './TableHeaderRow'
-
-test('shallow-render without crashing', () => {
-  shallow(<TableHeaderRowComponent />)
-})
