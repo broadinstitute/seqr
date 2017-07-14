fa18f8af7d54cbae0898d099dc9654e01363d2f4
harindra <harindra@broadinstitute.org>
2017-07-14 19:00:58 -0400

    adjusting matchbox docker deployment files for a faster build

--- deploy/docker/matchbox/entrypoint.sh
+++ deploy/docker/matchbox/entrypoint.sh
@@ -6,6 +6,6 @@ env
 
 cd /matchbox_deployment
 
-java -jar -Dexomiser.data-directory=/root/reference_data/data matchbox-0.1.0.jar &
+java -jar -Dexomiser.data-directory=/Exomiser/matchbox/data/data matchbox-0.1.0.jar &
 
 sleep 10000000000
