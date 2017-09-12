28eac614f1ce42fc8fc32cb630af4c8906980971
bw2 <weisburd@broadinstitute.org>
2017-09-11 20:25:02 -0400

    updated path of gene name file. Matchbox is now running!

--- deploy/docker/matchbox/entrypoint.sh
+++ deploy/docker/matchbox/entrypoint.sh
@@ -10,7 +10,7 @@ then
     mkdir -p /mounted-bucket/exomiser/exomiser-cli-8.0.0/data/phenix
 fi
 
-cd /matchbox_deployment/config/
+cd /
 wget https://storage.googleapis.com/matchbox-mounted-bucket/gene_symbol_to_ensembl_id_map.txt
 
 cd /matchbox_deployment
@@ -21,6 +21,7 @@ java -jar \
     -Dspring.data.mongodb.host=$MONGO_SERVICE_HOSTNAME \
     -Dspring.data.mongodb.port=$MONGO_SERVICE_PORT \
     -Dspring.data.mongodb.database=$MONGODB_DATABASE \
+    -Dmatchbox.gene-symbol-to-id-mappings=/gene_symbol_to_ensembl_id_map.txt \
     matchbox-0.1.0.jar &
 
 sleep 10000000000
