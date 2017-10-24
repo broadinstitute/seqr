4e49bed6d0e6f796ebcd6fba9128c6c7e8b19292
bw2 <weisburd@broadinstitute.org>
2017-10-24 06:49:00 -0400

    updated elasticsearch version

--- deploy/docker/kibana/entrypoint.sh
+++ deploy/docker/kibana/entrypoint.sh
@@ -7,7 +7,7 @@ env
 su kibana -c "/usr/local/kibana-${KIBANA_VERSION}-linux-x86_64/bin/kibana \
     --host=0.0.0.0 \
     --port=${KIBANA_SERVICE_PORT} \
-    --elasticsearch.url=http://elasticsearch:${ELASTICSEARCH_SERVICE_PORT}" &
+    --elasticsearch.url=http://${ELASTICSEARCH_SERVICE_HOSTNAME}:${ELASTICSEARCH_SERVICE_PORT}" &
 
 echo Kibana started on port ${KIBANA_SERVICE_PORT}!!
 
