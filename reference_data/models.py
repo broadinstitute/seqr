db0b54f9638f3497948e2916b853757912a13efb
bw2 <weisburd@broadinstitute.org>
2017-10-25 22:27:38 -0400

    added lookup for HPO categories

--- reference_data/models.py
+++ reference_data/models.py
@@ -9,6 +9,32 @@ GENOME_VERSION_CHOICES = [
 ]
 
 
+HPO_CATEGORY_NAMES = {
+    'HP:0000119': 'Genitourinary System',
+    'HP:0000152': 'Head or Neck',
+    'HP:0000478': 'Eye Defects',
+    'HP:0000598': 'Ear Defects',
+    'HP:0000707': 'Nervous System',
+    'HP:0000769': 'Breast',
+    'HP:0000818': 'Endocrine System',
+    'HP:0000924': 'Skeletal System',
+    'HP:0001197': 'Prenatal development or birth',
+    'HP:0001507': 'Growth Abnormality',
+    'HP:0001574': 'Integument',
+    'HP:0001608': 'Voice',
+    'HP:0001626': 'Cardiovascular System',
+    'HP:0001871': 'Blood',
+    'HP:0001939': 'Metabolism/Homeostasis',
+    'HP:0002086': 'Respiratory',
+    'HP:0002664': 'Neoplasm',
+    'HP:0002715': 'Immune System',
+    'HP:0003011': 'Musculature',
+    'HP:0003549': 'Connective Tissue',
+    'HP:0025031': 'Digestive System',
+    'HP:0040064': 'Limbs',
+    'HP:0045027': 'Thoracic Cavity',
+}
+
 class HumanPhenotypeOntology(models.Model):
     """Human Phenotype Ontology table contains one record per phenotype term parsed from the hp.obo
     file at http://human-phenotype-ontology.github.io/downloads.html
