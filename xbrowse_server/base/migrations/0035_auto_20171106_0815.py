ed736557c83a2d214c08ae7b7910503a82fb2e21
bw2 <weisburd@broadinstitute.org>
2017-11-28 17:41:31 -0500

    fixed migrations

--- xbrowse_server/base/migrations/0035_auto_20171106_0815.py
+++ xbrowse_server/base/migrations/0035_auto_20171106_0815.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0034_auto_20171102_0000'),
+        ('base', '0033_merge_20170912_0857'),
     ]
 
     operations = [
