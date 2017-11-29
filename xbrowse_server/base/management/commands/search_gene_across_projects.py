c9930a9ed1305416c3461cf6fca164db2239e97b
bw2 <weisburd@broadinstitute.org>
2017-11-29 15:43:29 -0500

    added -gl gene list support

--- xbrowse_server/base/management/commands/search_gene_across_projects.py
+++ xbrowse_server/base/management/commands/search_gene_across_projects.py
@@ -1,20 +1,19 @@
-from optparse import make_option
-import sys
-import os
 from django.core.management.base import BaseCommand
-import json
-import time
-import datetime
+
 from pprint import pprint
+
+from django.db.models.query_utils import Q
+from slugify import slugify
 
 from xbrowse.reference.clinvar import get_clinvar_variants
 from xbrowse_server.analysis import project as project_analysis
 from xbrowse.core.variant_filters import get_default_variant_filter
 from xbrowse_server.api.utils import add_extra_info_to_variants_family, add_extra_info_to_variants_project
+from xbrowse_server.gene_lists.models import GeneList, GeneListItem
 from xbrowse_server.mall import get_reference, get_project_datastore
 from xbrowse_server.analysis.project import get_knockouts_in_gene
 from django.conf import settings
-from xbrowse_server.base.models import Project
+from xbrowse_server.base.models import Project, Individual
 from xbrowse.utils.basic_utils import get_gene_id_from_str
 from xbrowse_server import mall
 import csv
@@ -30,90 +29,106 @@ class Command(BaseCommand):
         group.add_argument('-g', '--gene_id', dest='gene_id', help='Searches for this gene id.')
         group.add_argument('-gl', '--gene_list', dest='gene_list', help='Searches all genes in this gene list.', action="append")
 
-
     def handle(self, *args, **options):
-      '''
-        Search for a gene across a project
-        Args:
-       		1. Gene name (Required)- the name of the gene to search for
-       		2. A list of project name to search for (Optional- if not given will search in all projects (Warn: Computationally Expensive!))
+        if options['gene_id']:
+            gene_id = options['gene_id']
+            gene_ids = [gene_id]
+            output_filename = 'results_%s.tsv' % gene_id
+        elif options['gene_list']:
+            gene_list = options['gene_list']
+            matching_gene_lists = GeneList.objects.filter(Q(slug__icontains=gene_list) | Q(name__icontains=gene_list))
+            if not matching_gene_lists:
+                raise ValueError("'%s' gene list not found" % gene_list)
+            if len(matching_gene_lists) > 1:
+                raise ValueError("matched multiple gene lists: %s" % ", ".join([l.slug for l in matching_gene_lists]))
 
-      '''
-      if options['gene_id']:
-          gene_ids = [options['gene_id']]
-      elif options['gene_list']:
-          gene_list = options['gene_list']
-          print(gene_list)
-      else:
-          raise ValueError("Must specifcy either -g or -gl arg")
+            gene_ids = [i.gene_id for i in GeneListItem.objects.filter(gene_list=matching_gene_lists[0])]
 
-      self.search_for_gene(gene_ids, args, max_af=options['max_af'])
-      
-      
-    def search_for_gene(self, gene_ids, project_id_list, max_af=0.01):
-      '''
+            output_filename = 'results_%s.tsv' % slugify(gene_list)
+        else:
+            raise ValueError("Must specifcy either -g or -gl arg")
+
+        self.search_for_genes(gene_ids, args, max_af=options['max_af'])
+
+    def search_for_genes(self, gene_ids, project_id_list, output_filename, max_af=0.01):
+        """
         Search for a gene across project(s)
+
         Args:
-            gene_ids: Gene IDs to search for
-            project_id_list: An optional list of projects to narrow down the search
-      '''
-      output_filename = 'results_'+search_gene_id + '.tsv'
-      outfile = open(output_filename,'w')
-      
-      header = ["project_id","gene", "chr", "pos", "ref", "alt", "rsID", "filter", "impact",
+            gene_ids (list): 'ENSG..' gene id strings.
+            project_id_list (list): (optional) project ids to narrow down the search
+            output_filename (string): output file name
+            max_af (float): AF filter
+        """
+
+        outfile = open(output_filename, 'w')
+
+        header = ["project_id","gene", "chr", "pos", "ref", "alt", "rsID", "filter", "impact",
                 "HGVS.c", "HGVS.p", "sift", "polyphen", "muttaster", "fathmm", "clinvar_id", "clinvar_clinical_sig",
                 "freq_1kg_wgs_phase3", "freq_1kg_wgs_phase3_popmax",
                 "freq_exac_v3", "freq_exac_v3_popmax",
                 "all_genotypes"]
-      
-      writer = csv.writer(outfile, delimiter='\t')
-      writer.writerow(header)
 
-      for gene_id in gene_ids:
-          gene_id = get_gene_id_from_str(search_gene_id, get_reference())
-                  
-          gene = get_reference().get_gene(gene_id)
-            
-          print("Staring gene search for: %s %s in projects: %s\n" % (search_gene_id, gene['gene_id'], ", ".join(project_id_list)))
-          print("Max AF threshold: %s" % max_af)
+        writer = csv.writer(outfile, delimiter='\t')
+        writer.writerow(header)
 
-          # all rare coding variants
-          variant_filter = get_default_variant_filter('all_coding', mall.get_annotator().reference_population_slugs)
-          print("All Filters: ")
-          pprint(variant_filter.toJSON())
+        print("Staring gene search for:\n  %s\n  in projects: %s\n" % (", ".join(gene_ids), ", ".join(project_id_list)))
+        print("Max AF threshold: %s" % max_af)
 
-          if project_id_list: 
-              for project_id in project_id_list:
-                  project = Project.objects.filter(project_id=project_id)[0]  # TODO validate
-          else:
-              project_id_list = [p.project_id for p in Project.objects.all()]
-      
-          for project_id in project_id_list:
-              project = Project.objects.filter(project_id=project_id)[0]
-              if get_project_datastore(project_id).project_collection_is_loaded(project_id):
-                  print("Running on project %s" % project_id)
-              else:
-                  print("Skipping project %s - gene search is not enabled for this project" % project_id)
-                  continue
-              
-              for variant in project_analysis.get_variants_in_gene(project, gene_id, variant_filter=variant_filter):
-                  if max(variant.annotation['freqs'].values()) >= max_af:
-                      continue
-                  #pprint(variant.toJSON())
-                  add_extra_info_to_variants_project(get_reference(), project, [variant])
+        # all rare coding variants
+        variant_filter = get_default_variant_filter('all_coding', mall.get_annotator().reference_population_slugs)
+        print("All Filters: ")
+        pprint(variant_filter.toJSON())
 
-                  worst_annotation_idx = variant.annotation["worst_vep_index_per_gene"][gene_id]
-                  worst_annotation = variant.annotation["vep_annotation"][worst_annotation_idx]
-                  all_genotypes_list = []
-                  pass_filter = "N/A"
-                  for indiv_id, genotype in variant.genotypes.items():
-                      pass_filter = genotype.filter  # filter value is stored in the genotypes even though it's the same for all individuals
-                      if genotype.num_alt > 0:
-                          all_genotypes_list.append("%s[gt:%s GQ:%s AB:%0.3f]" % (indiv_id, ">".join(genotype.alleles), genotype.gq, genotype.ab if genotype.ab is not None else float('NaN')))
+        if project_id_list:
+            projects = [Project.objects.get(project_id=project_id) for project_id in project_id_list]
+        else:
+            projects = Project.objects.all()
 
-                  measureset_id, clinvar_significance = get_clinvar_variants().get(variant.unique_tuple(), ("", ""))
-                  row = map(str,
-                            [project_id, 
+        indiv_id_cache = {}
+        for project in projects:
+            project_id = project.project_id
+
+            for gene_id in gene_ids:
+                gene_id = get_gene_id_from_str(gene_id, get_reference())
+
+                gene = get_reference().get_gene(gene_id)
+
+                if get_project_datastore(project_id).project_collection_is_loaded(project_id):
+                    print("Running on project %s" % project_id)
+                else:
+                    print("Skipping project %s - gene search is not enabled for this project" % project_id)
+                    continue
+
+                for variant in project_analysis.get_variants_in_gene(project, gene_id, variant_filter=variant_filter):
+                    if max(variant.annotation['freqs'].values()) >= max_af:
+                        continue
+
+                    add_extra_info_to_variants_project(get_reference(), project, [variant])
+
+                    worst_annotation_idx = variant.annotation["worst_vep_index_per_gene"][gene_id]
+                    worst_annotation = variant.annotation["vep_annotation"][worst_annotation_idx]
+                    all_genotypes_list = []
+                    pass_filter = "N/A"
+                    for indiv_id, genotype in variant.genotypes.items():
+                        individual = indiv_id_cache.get('indiv_id')
+                        if individual is None:
+                            individual = Individual.objects.get(project=project, indiv_id=indiv_id)
+                            indiv_id_cache[indiv_id] = individual
+
+                        pass_filter = genotype.filter  # filter value is stored in the genotypes even though it's the same for all individuals
+                        if genotype.num_alt > 0:
+                            all_genotypes_list.append("%s%s[gt:%s GQ:%s AB:%0.3f]" % (
+                                indiv_id,
+                                "[Aff]" if individual.affected == "A" else ("[-]" if individual.affected == "N" else "[?]"),
+                                ">".join(genotype.alleles),
+                                genotype.gq,
+                                genotype.ab if genotype.ab is not None else float('NaN')
+                            ))
+
+                    measureset_id, clinvar_significance = get_clinvar_variants().get(variant.unique_tuple(), ("", ""))
+                    row = map(str, [
+                        project_id,
                              gene["symbol"],
                              variant.chr,
                              variant.pos,
@@ -135,14 +150,8 @@ class Command(BaseCommand):
                              variant.annotation["freqs"].get("exac_v3", ""),
                              variant.annotation["freqs"].get("exac_v3_popmax", ""),
                              ", ".join(all_genotypes_list),
-                         ])
-                  writer.writerow(row)
-      
-      outfile.close()
-      print("Wrote out %s" % output_filename)
-      
-          
+                        ])
+                    writer.writerow(row)
 
-      	 
-  
-        
+        outfile.close()
+        print("Wrote out %s" % output_filename)
