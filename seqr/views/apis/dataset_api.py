7320942d6d7cfabaebcaef1a1bea996b9f475a04
bw2 <weisburd@broadinstitute.org>
2017-12-04 02:02:37 -0500

    stating to dataset_api

--- seqr/views/apis/dataset_api.py
+++ seqr/views/apis/dataset_api.py
@@ -1,12 +1,64 @@
 import datetime
+import json
 import logging
 import os
 
-from django.core.exceptions import ObjectDoesNotExist
-from seqr.models import Dataset, ElasticsearchDataset
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+from django.views.decorators.csrf import csrf_exempt
+
+from seqr.models import Dataset, ElasticsearchDataset, CAN_EDIT, Project
 from seqr.utils.file_utils import get_file_stats
+from seqr.views.apis.auth_api import API_LOGIN_REQUIRED_URL
+from seqr.views.utils.json_utils import create_json_response
+from seqr.views.utils.orm_to_json_utils import _get_json_for_dataset
+from seqr.views.utils.permissions_utils import get_project_and_check_permissions
 
 logger = logging.getLogger(__name__)
+
+
+@login_required(login_url=API_LOGIN_REQUIRED_URL)
+@csrf_exempt
+def add_dataset_handler(request, project_guid):
+    """Update project metadata - including one or more of these fields: name, description
+
+    Args:
+        project_guid (string): GUID of the project that should be updated
+
+    HTTP POST
+        Request body - should contain the following json structure:
+        {
+            'form' : {
+                'name':  <project name>,
+                'description': <project description>,
+            }
+        }
+
+        Response body - will contain the following structure, representing the updated project:
+            {
+                'projectsByGuid':  {
+                    <projectGuid1> : { ... <project key-value pairs> ... }
+                }
+            }
+
+    """
+
+    project = get_project_and_check_permissions(project_guid, request.user, permission_level=CAN_EDIT)
+
+    request_json = json.loads(request.body)
+    if 'form' not in request_json:
+        return create_json_response({}, status=400, reason="Invalid request: 'form' not in request_json")
+
+    form_data = request_json['form']
+
+    logger.info("add_dataset_handler", form_data)
+
+    return create_json_response({
+        'errors': ['this is it'],
+        'projectsByGuid': {
+            project.guid: None #_get_json_for_dataset(dataset, request.user)
+        },
+    })
 
 
 def get_dataset_by_id(dataset_id):
@@ -169,4 +221,5 @@ def create_elasticsearch_dataset(
 def link_dataset_to_sample_records(dataset, sample_records):
     for sample in sample_records:
         dataset.samples.add(sample)
+
 
