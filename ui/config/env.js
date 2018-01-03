d68af1f996bce3090f2862719704bcf080a7b707
bw2 <weisburd@broadinstitute.org>
2018-01-02 19:26:18 -0500

    updated build scripts to match the latest create-react-app

--- ui/config/env.js
+++ ui/config/env.js
@@ -53,6 +53,8 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
   .map(folder => path.resolve(appDirectory, folder))
   .join(path.delimiter);
 
+console.log('NODE_PATH: ', process.env.NODE_PATH)
+
 // Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
 const REACT_APP = /^REACT_APP_/i;
--- ui/config/webpackDevServer.config.js
+++ ui/config/webpackDevServer.config.js
@@ -1,16 +1,15 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable */
 
-/* eslint-disable object-shorthand */
+const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
+const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
+const path = require('path');
+const config = require('./webpack.config.dev');
+const paths = require('./paths');
 
-const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware')
-const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware')
-const config = require('./webpack.config.dev')
-const paths = require('./paths')
+const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
+const host = process.env.HOST || '0.0.0.0';
 
-const protocol = process.env.HTTPS === 'true' ? 'https' : 'http'
-const host = process.env.HOST || '0.0.0.0'
-
-module.exports = function (proxy, allowedHost) {
+module.exports = function(proxy, allowedHost) {
   return {
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
@@ -73,8 +72,15 @@ module.exports = function (proxy, allowedHost) {
     ],
     // Reportedly, this avoids CPU overload on some systems.
     // https://github.com/facebookincubator/create-react-app/issues/293
+    // src/node_modules is not ignored to support absolute imports
+    // https://github.com/facebookincubator/create-react-app/issues/1065
     watchOptions: {
-      ignored: /node_modules/,
+      ignored: new RegExp(
+        `^(?!${path
+          .normalize(paths.appSrc + '/')
+          .replace(/[\\]+/g, '\\\\')}).+[\\\\/]node_modules[\\\\/]`,
+        'g'
+      ),
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === 'https',
@@ -86,15 +92,15 @@ module.exports = function (proxy, allowedHost) {
       disableDotRule: true,
     },
     public: allowedHost,
-    setup(app) {
+    before(app) {
       // This lets us open files from the runtime error overlay.
-      app.use(errorOverlayMiddleware())
+      app.use(errorOverlayMiddleware());
       // This service worker file is effectively a 'no-op' that will reset any
       // previous service worker registered for the same host:port combination.
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
       // https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
-      app.use(noopServiceWorkerMiddleware())
+      app.use(noopServiceWorkerMiddleware());
     },
-  }
-}
+  };
+};
