d74fa8e0f367642a81d78d7c957a9511a6522894
bw2 <weisburd@broadinstitute.org>
2018-01-08 06:40:14 -0500

    added comment - cant switch from xhyve to hyperkit yet because it doesn't have sufficient support for local drive mapping

--- deploy/start_minikube.sh
+++ deploy/start_minikube.sh
@@ -1,1 +1,3 @@
-minikube start --disk-size=100g --memory 8000 --cpus 8 --vm-driver=xhyve
+minikube start --disk-size=100g --memory 9000 --cpus 8 --vm-driver=xhyve
+
+#hyperkit - waiting for a way to map local drives (eg. ~/code/seqr) into the VM
