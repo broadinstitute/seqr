107c78a660158d21a99b33d62594c44e3f9382da
bw2 <weisburd@broadinstitute.org>
2018-02-06 12:45:34 -0500

    update legacy models when adding a new vcf

--- seqr/views/utils/dataset/dataset_utils.py
+++ seqr/views/utils/dataset/dataset_utils.py
@@ -7,6 +7,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from seqr.models import Dataset
 from seqr.utils.file_utils import get_file_stats
 
+from xbrowse_server.base.models import Individual as BaseIndividual, VCFFile
+
 logger = logging.getLogger()
 
 
@@ -162,3 +164,21 @@ def link_dataset_to_sample_records(dataset, sample_records):
     for sample in sample_records:
         dataset.samples.add(sample)
 
+    _deprecated_update_base_individuals(dataset, sample_records)
+
+
+def _deprecated_update_base_individuals(dataset, sample_records):
+    if not dataset.is_loaded:
+        return
+
+    vcf_file, created = VCFFile.objects.get_or_create(file_path=dataset.source_file_path)
+    for sample in sample_records:
+        base_individual = BaseIndividual.objects.get(
+            project__project_id=dataset.project.deprectated_project_id,
+            indiv_id=sample.individual.individual_id)
+        base_individual.vcf_files.add(vcf_file)
+
+        family = base_individual.family
+        if family.analysis_status == "Q":
+            family.analysis_status = 'I'
+            family.save()
