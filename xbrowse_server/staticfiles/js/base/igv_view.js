bf7deb91946b394c4f2899d38e5b1b9cec2576b5
bw2 <weisburd@broadinstitute.org>
2018-02-08 08:46:19 -0500

    adding cram support

--- xbrowse_server/staticfiles/js/base/igv_view.js
+++ xbrowse_server/staticfiles/js/base/igv_view.js
@@ -5,41 +5,68 @@ window.IgvView = Backbone.View.extend({
     initialize: function (options) {
         this.individuals = options.individuals;
 
-        //initialize IGV.js browser
         var tracks = [];
-        tracks.push({
-          url: 'https://storage.googleapis.com/seqr-reference-data/GRCh37/gencode/gencode.v27lift37.annotation.sorted.gtf.gz',
-          name: "gencode v27",
-          //displayMode: "EXPANDED",
-          displayMode: "SQUISHED",
-        });
 
+        var has_cram_files = false
         for (var i = 0; i < this.individuals.length; i += 1) {
             var indiv = this.individuals[i];
             if (!indiv.read_data_is_available) {
                 continue;
             }
 
-            tracks.push({
+            var alignmentTrack = {
                 url: "/project/" + indiv.project_id + "/igv-track/" + indiv.indiv_id,
                 type: 'bam',
                 indexed: true,
                 alignmentShading: 'strand',
-                name: '<i style="font-family: FontAwesome; font-style: normal; font-weight: normal;" class="' + utils.get_pedigree_icon(indiv) + '"></i> ' + indiv.indiv_id,
+                name: '<i style="font-family: FontAwesome; font-style: normal; font-weight: normal;" class="' +
+                utils.get_pedigree_icon(indiv) + '"></i> ' + indiv.indiv_id,
                 height: 300,
                 minHeight: 300,
                 autoHeight: false,
-            });
+            }
+
+            if (indiv.read_data_format == 'cram') {
+                alignmentTrack.sourceType = 'pysam'
+                alignmentTrack.url = "/project/" + indiv.project_id + "/igv-track/" + indiv.indiv_id
+                alignmentTrack.alignmentFile = alignmentTrack.url
+                alignmentTrack.referenceFile = alignmentTrack.url
+            }
+
+            tracks.push(alignmentTrack);
         }
 
+        //initialize IGV.js browser
+        if (options.genome == "hg38" || options.genome == "GRCh38") {
+            if (!options.gencodeUrl) {
+                options.gencodeVersion = "gencode GRCh38v27";
+                options.gencodeUrl = 'https://storage.googleapis.com/seqr-reference-data/GRCh37/gencode/gencode.v27.annotation.sorted.gtf.gz';
+            }
+        } else {
+            if (!options.genome) {
+                options.genome = "hg19"
+            }
+            if (!options.gencodeUrl) {
+                options.gencodeVersion = "gencode GRCh37/v27";
+                options.gencodeUrl = 'https://storage.googleapis.com/seqr-reference-data/GRCh37/gencode/gencode.v27lift37.annotation.sorted.gtf.gz';
+            }
+        }
+
+        tracks.push({
+            url: options.gencodeUrl,
+            name: options.gencodeVersion,
+            //displayMode: "EXPANDED",
+            displayMode: "SQUISHED",
+        });
+
         this.options = {
-          showCommandBar: true,
-          genome: 'hg19',
-          locus: options.locus,
-          showKaryo: false,
-          tracks: tracks,
-          showCenterGuide: true,
-          showCursorTrackingGuide: true,
+            showCommandBar: true,
+            genome: options.genome,
+            locus: options.locus,
+            showKaryo: false,
+            tracks: tracks,
+            showCenterGuide: true,
+            showCursorTrackingGuide: true,
         };
 
         igv.createBrowser(this.el, this.options);
