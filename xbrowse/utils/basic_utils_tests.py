8dbb882b6f38724e03876fcc19f2c0cadfb7ff63
bw2 <weisburd@broadinstitute.org>
2018-03-07 18:07:12 -0500

    added more test cases

--- xbrowse/utils/basic_utils_tests.py
+++ xbrowse/utils/basic_utils_tests.py
@@ -6,16 +6,22 @@ class BasicUtilsTest(TestCase):
 
     def test_encode_decode_name(self):
 
-        # test all ascii chars
+        # test special cases
         for test_string in [
             "SPECIAL_CHARS_TEST1_.,#*$[]{}()_1_.,#*$[]{}()/\\",
             "SPECIAL_CHARS_TEST2_..,,##**$$[[]]{{}}(())////\\\\",
             "SPECIAL_CHARS_TEST3__$dot$__$comma$__$hash$___$star$__$lp$__$rp$__$lsb$__$rsb$__$lcb$__$rcb$_",
+            ".SPECIAL_START_CHAR",
+            "*SPECIAL_START_CHAR",
+            "__SPECIAL_START_CHAR",
+            "__$SPECIAL_START_CHAR",
+            "_$SPECIAL_START_CHAR",
         ]:
 
             decoded_test_string = _decode_name(_encode_name(test_string))
             self.assertEqual(test_string, decoded_test_string)
 
+        # test all ascii chars
         for acii_code in range(32, 127):
             test_char = chr(acii_code)
 
