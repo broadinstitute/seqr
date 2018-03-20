e3f2b8da23c37d4b427ac9fb4efa5abfb0f342a3
Hana Snow <hsnow@broadinstitute.org>
2018-03-20 10:58:36 -0400

    modal owns state

--- ui/shared/components/modal/Modal.jsx
+++ ui/shared/components/modal/Modal.jsx
@@ -3,24 +3,55 @@ import PropTypes from 'prop-types'
 
 import { Modal, Icon } from 'semantic-ui-react'
 
-const CustomModal = ({ title, handleClose, children, size = 'small' }) =>
-  <Modal open onClose={handleClose} size={size}>
-    <Modal.Header>
-      <span style={{ fontSize: '15px' }}>{title}</span>
-      <a role="button" tabIndex="0" style={{ float: 'right', cursor: 'pointer' }} onClick={handleClose}>
-        <Icon name="remove" style={{ fontSize: '15px', color: '#A3A3A3' }} />
-      </a>
-    </Modal.Header>
-    <Modal.Content style={{ textAlign: 'center' }}>
-      {children}
-    </Modal.Content>
-  </Modal>
 
-CustomModal.propTypes = {
-  title: PropTypes.string.isRequired,
-  handleClose: PropTypes.func.isRequired,
-  size: PropTypes.oneOf(['small', 'large', 'fullscreen']),
-  children: PropTypes.node,
+class CustomModal extends React.Component
+{
+  static propTypes = {
+    trigger: PropTypes.node,
+    title: PropTypes.string.isRequired,
+    handleClose: PropTypes.func,
+    confirmClose: PropTypes.func,
+    size: PropTypes.oneOf(['small', 'large', 'fullscreen']),
+    children: PropTypes.node,
+  }
+
+  defaultProps = {
+    size: 'small',
+    confirmClose: () => true,
+  }
+
+  state = {
+    modalOpen: !this.props.trigger,
+  }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => {
+    if (this.props.confirmClose()) {
+      if (this.props.handleClose) {
+        this.props.handleClose()
+      } else {
+        this.setState({ modalOpen: false })
+      }
+    }
+  }
+
+  render() {
+    const trigger = this.props.trigger ? React.cloneElement(this.props.trigger, { onClick: this.handleOpen }) : null
+    return (
+      <Modal open={this.state.modalOpen} trigger={trigger} onClose={this.handleClose} size={this.props.size}>
+        <Modal.Header>
+          <span style={{ fontSize: '15px' }}>{this.props.title}</span>
+          <a role="button" tabIndex="0" style={{ float: 'right', cursor: 'pointer' }} onClick={this.handleClose}>
+            <Icon name="remove" style={{ fontSize: '15px', color: '#A3A3A3' }} />
+          </a>
+        </Modal.Header>
+        <Modal.Content style={{ textAlign: 'center' }}>
+          {this.props.children}
+        </Modal.Content>
+      </Modal>
+    )
+  }
 }
 
 export default CustomModal
