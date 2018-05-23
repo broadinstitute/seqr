fced427e661552fd0f48e9148f496dc61d11e452
bw2 <weisburd@broadinstitute.org>
2018-05-23 05:45:23 -0400

    fixed lint errors

--- ui/pages/Project/components/FamilyTable/CaseReviewStatusDropdown.jsx
+++ ui/pages/Project/components/FamilyTable/CaseReviewStatusDropdown.jsx
@@ -4,28 +4,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
-import { Checkbox } from 'semantic-ui-react'
 
 import ReduxFormWrapper from 'shared/components/form/ReduxFormWrapper'
 import EditTextButton from 'shared/components/buttons/EditTextButton'
-import { VerticalSpacer } from 'shared/components/Spacers'
 import { updateIndividual } from 'redux/rootReducer'
 
 import {
-  CASE_REVIEW_STATUS_ACCEPTED,
   CASE_REVIEW_STATUS_MORE_INFO_NEEDED,
   CASE_REVIEW_STATUS_OPTIONS,
-  CASE_REVIEW_STATUS_ACCEPTED_FOR_OPTIONS,
 } from 'shared/constants/caseReviewConstants'
 
 const StatusContainer = styled.span`
   display: inline-block;
   whitespace: nowrap;
   min-width: 220px;
-`
-
-const AcceptedForCheckbox = styled(Checkbox)`
-  padding: 3px 10px 5px 5px;
 `
 
 const STATUS_FORM_FIELDS = [{
@@ -55,59 +47,9 @@ CaseReviewStatusForm.propTypes = {
   updateIndividual: PropTypes.func.isRequired,
 }
 
-const CaseReviewAcceptedForForm = (props) => {
-  const initialValues = { caseReviewStatusAcceptedFor: props.individual.caseReviewStatusAcceptedFor }
-  const fields = CASE_REVIEW_STATUS_ACCEPTED_FOR_OPTIONS.map((option, k) => {
-    if (option === '---') {
-      return { component: 'br', name: k, displayOnly: true }
-    }
-
-    return ({
-      key: option.value,
-      name: 'caseReviewStatusAcceptedFor',
-      component: ({ value, onChange }) => //eslint-disable-line react/prop-types
-        <AcceptedForCheckbox
-          defaultChecked={props.individual.caseReviewStatusAcceptedFor !== null && props.individual.caseReviewStatusAcceptedFor.includes(option.value)}
-          label={option.name}
-          value={option.value}
-          onChange={(e, result) => {
-            if (result.checked) {
-              value += result.value
-            } else {
-              value = value.replace(result.value, '')
-            }
-            onChange(value)
-          }}
-        />,
-    })
-  })
-  return (
-    <ReduxFormWrapper
-      onSubmit={props.updateIndividual}
-      form={`editCaseReviewStatusAcceptedFor-${props.individual.individualGuid}`}
-      initialValues={initialValues}
-      closeOnSuccess={false}
-      submitOnChange
-      fields={fields}
-    />
-  )
-}
-
-CaseReviewAcceptedForForm.propTypes = {
-  individual: PropTypes.object.isRequired,
-  updateIndividual: PropTypes.func.isRequired,
-}
-
 const CaseReviewStatusDropdown = props =>
   <StatusContainer>
     <CaseReviewStatusForm {...props} />
-    {
-      props.individual.caseReviewStatus === CASE_REVIEW_STATUS_ACCEPTED &&
-        <div>
-          <VerticalSpacer height={5} />
-          <CaseReviewAcceptedForForm {...props} />
-        </div>
-    }
     {/* edit case review discussion for individual: */}
     <div>
       {
