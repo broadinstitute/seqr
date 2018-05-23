1622bd318a6c4b48b6395ff36f2417b4adf2d81a
bw2 <weisburd@broadinstitute.org>
2018-05-23 00:19:56 -0400

    fixed xbrowse models admin settings

--- xbrowse_server/base/admin.py
+++ xbrowse_server/base/admin.py
@@ -1,62 +1,55 @@
 from django.contrib import admin
 from xbrowse_server.base.models import *
-from django import forms
 
-class SharedAdminSettings:
-    save_on_top = True
-    list_per_page = 2000
-    
 
 @admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin, SharedAdminSettings):
+class UserProfileAdmin(admin.ModelAdmin):
     search_fields = ['user__email', 'user__first_name', 'user__last_name', 'display_name']
     list_filter = ['user__is_staff', 'user__is_superuser']
+    save_on_top = True
+    list_per_page = 1000
+
 
 @admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin, SharedAdminSettings):
-    list_display = ['project_id', 'project_name', 'description', 'disease_area', 'is_functional_data_enabled', 'disable_staff_access']
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['project_id', 'project_name', 'description', 'disease_area', 'is_functional_data_enabled']
     search_fields = ['project_id', 'project_name', 'description', 'disease_area']
-    list_filter = ['is_functional_data_enabled', 'disable_staff_access']
+    list_filter = ['is_functional_data_enabled']
+    save_on_top = True
+    list_per_page = 1000
+
 
 @admin.register(Family)
-class FamilyAdmin(admin.ModelAdmin, SharedAdminSettings):
-    pass
-
+class FamilyAdmin(admin.ModelAdmin):
+    search_fields = ['family_id', 'family_name']
+    list_display = search_fields + ['analysis_status']
+    save_on_top = True
+    list_per_page = 1000
+    
 
 @admin.register(Individual)
-class IndividualAdmin(admin.ModelAdmin, SharedAdminSettings):
-    pass
+class IndividualAdmin(admin.ModelAdmin):
+    search_fields = ['indiv_id']
+    list_display = search_fields + ['maternal_id', 'paternal_id', 'gender', 'affected', 'other_notes']
+    save_on_top = True
+    list_per_page = 1000
 
 
 @admin.register(VCFFile)
-class VCFFileAdmin(admin.ModelAdmin, SharedAdminSettings):
-    pass
-
+class VCFFileAdmin(admin.ModelAdmin):
+    search_fields = ['file_path']
+    list_display = search_fields + ['project', 'sample_type', 'dataset_type', 'elasticsearch_index', 'loaded_date']
+    save_on_top = True
+    list_per_page = 1000
+    
 
 @admin.register(ReferencePopulation)
-class ReferencePopulationAdmin(admin.ModelAdmin, SharedAdminSettings):
-    pass
-
+class ReferencePopulationAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_per_page = 1000
+    
 
 @admin.register(FamilyGroup)
-class FamilyGroupAdmin(admin.ModelAdmin, SharedAdminSettings):
-    pass
-
-
-class FamilyModelChoiceField(forms.ModelChoiceField):
-    def label_from_instance(self, obj):
-        return "%s - %s" % (obj.project.project_id, obj.family_id)
-
-
-class FamilyImageSlideAdminForm(forms.ModelForm):
-    family = FamilyModelChoiceField(queryset=Family.objects.all())
-    class Meta:
-        model = FamilyImageSlide
-        exclude = []
-
-
-class FamilyImageSlideAdmin(admin.ModelAdmin, SharedAdminSettings):
-    form = FamilyImageSlideAdminForm
-
-
-admin.site.register(FamilyImageSlide, FamilyImageSlideAdmin)
+class FamilyGroupAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_per_page = 1000
