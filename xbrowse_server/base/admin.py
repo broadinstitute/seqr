c8fe2936bcc8f4e7f35e3c08deac2823a203c9a0
bw2 <weisburd@broadinstitute.org>
2018-05-22 21:22:44 -0400

    improve seqr admin site

--- xbrowse_server/base/admin.py
+++ xbrowse_server/base/admin.py
@@ -2,23 +2,51 @@ from django.contrib import admin
 from xbrowse_server.base.models import *
 from django import forms
 
-admin.site.register(Project)
-admin.site.register(Family)
-admin.site.register(Cohort)
-admin.site.register(Individual)
-admin.site.register(ReferencePopulation)
-admin.site.register(ProjectPhenotype)
-admin.site.register(UserProfile)
+class SharedAdminSettings:
+    save_on_top = True
+    list_per_page = 2000
+    
 
-admin.site.register(FamilySearchFlag)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin, SharedAdminSettings):
+    search_fields = ['user__email', 'user__first_name', 'user__last_name', 'display_name']
+    list_filter = ['user__is_staff', 'user__is_superuser']
 
-admin.site.register(VCFFile)
-admin.site.register(FamilyGroup)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin, SharedAdminSettings):
+    list_display = ['project_id', 'project_name', 'description', 'disease_area', 'is_functional_data_enabled', 'disable_staff_access']
+    search_fields = ['project_id', 'project_name', 'description', 'disease_area']
+    list_filter = ['is_functional_data_enabled', 'disable_staff_access']
+
+@admin.register(Family)
+class FamilyAdmin(admin.ModelAdmin, SharedAdminSettings):
+    pass
+
+
+@admin.register(Individual)
+class IndividualAdmin(admin.ModelAdmin, SharedAdminSettings):
+    pass
+
+
+@admin.register(VCFFile)
+class VCFFileAdmin(admin.ModelAdmin, SharedAdminSettings):
+    pass
+
+
+@admin.register(ReferencePopulation)
+class ReferencePopulationAdmin(admin.ModelAdmin, SharedAdminSettings):
+    pass
+
+
+@admin.register(FamilyGroup)
+class FamilyGroupAdmin(admin.ModelAdmin, SharedAdminSettings):
+    pass
 
 
 class FamilyModelChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
         return "%s - %s" % (obj.project.project_id, obj.family_id)
+
 
 class FamilyImageSlideAdminForm(forms.ModelForm):
     family = FamilyModelChoiceField(queryset=Family.objects.all())
@@ -26,12 +54,9 @@ class FamilyImageSlideAdminForm(forms.ModelForm):
         model = FamilyImageSlide
         exclude = []
 
-class FamilyImageSlideAdmin(admin.ModelAdmin):
+
+class FamilyImageSlideAdmin(admin.ModelAdmin, SharedAdminSettings):
     form = FamilyImageSlideAdminForm
-
-
-
-
 
 
 admin.site.register(FamilyImageSlide, FamilyImageSlideAdmin)
