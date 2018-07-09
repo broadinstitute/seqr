6b68ff406fbdbed132f46aedbfe5c19b14b3b5c7
bw2 <weisburd@broadinstitute.org>
2018-07-09 11:06:24 -0400

    file utils for processing reference datasets

---
+++ reference_data/management/commands/file_utils.py
@@ -0,0 +1,47 @@
+import logging
+import os
+import re
+import subprocess
+import tempfile
+
+from django.core.validators import URLValidator
+
+logger = logging.getLogger(__name__)
+
+
+def download_remote_file(url, to_dir=tempfile.gettempdir()):
+    """Uses wget to download the given file and returns its local path.
+
+    Args:
+        url (string): HTTP or FTP url
+
+    Returns:
+        string: local file path
+    """
+    if not url or (not url.startswith("http://") and not url.startswith("ftp://")):
+        raise ValueError("Invalid url: {}".format(url))
+
+
+    local_file_path = os.path.join(to_dir, os.path.basename(url))
+    remote_file_size = get_remote_file_size(url)
+
+    if os.path.isfile(local_file_path) and os.path.getsize(local_file_path) == remote_file_size:
+        return local_file_path
+
+    logger.info("Downloading {} to local_file_path".format(url, local_file_path))
+
+    os.system("wget %s -O %s" % (url, local_file_path))
+
+    return local_file_path
+
+
+def get_remote_file_size(url):
+    URLValidator()(url)  # make sure it's a valid url, to avoid security issues with shell execution
+
+    curl_output = subprocess.check_output("curl -s --head '{}'".format(url), shell=True)
+
+    match = re.search("Content-Length:[ ](\d+)", curl_output)
+    if match:
+        return int(match.group(1))
+
+    return None
