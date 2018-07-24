6fadef0257bcb4538413088d2e5bdb31d12e86a3
harindra <harindra@broadinstitute.org>
2018-07-24 16:06:26 -0400

    Adjusting url processing to allow for spaces in individual names, and style adjustments for large indel displays

--- xbrowse_server/api/urls.py
+++ xbrowse_server/api/urls.py
@@ -72,12 +72,12 @@ urlpatterns = [
 
 
     #matchmaker related URLs
-    url(r'^matchmaker/candidate/project/(?P<project_id>[\w|-]+)/family/(?P<family_id>[\w|-]+)/individual/(?P<indiv_id>[\w|-]+)$', xbrowse_server.api.views.get_submission_candidates, name='get_submission_candidates'),
+    url(r'^matchmaker/candidate/project/(?P<project_id>[\w|-]+)/family/(?P<family_id>[\w|-]+)/individual/(?P<indiv_id>[\w\s|-]+)$', xbrowse_server.api.views.get_submission_candidates, name='get_submission_candidates'),
     url(r'^matchmaker/add$', xbrowse_server.api.views.add_individual, name='add_individual'),
     url(r'^matchmaker/last_submission/project/(?P<project_id>[\w|-]+)/family/(?P<family_id>[\w|-]+)$', xbrowse_server.api.views.get_family_submissions, name='get_family_submissions'),
-    url(r'^matchmaker/match_internally_and_externally/project/(?P<project_id>[\w|-]+)/individual/(?P<indiv_id>[\w|-]+)$', xbrowse_server.api.views.match_internally_and_externally, name='match_internally_and_externally'),
+    url(r'^matchmaker/match_internally_and_externally/project/(?P<project_id>[\w|-]+)/individual/(?P<indiv_id>[\w\s|-]+)$', xbrowse_server.api.views.match_internally_and_externally, name='match_internally_and_externally'),
     url(r'^matchmaker/match_in_open_mme_sources/project/(?P<project_id>[\w|-]+)/individual/(?P<indiv_id>[\w|-]+)$', xbrowse_server.api.views.match_in_open_mme_sources, name='match_in_open_mme_sources'),
-    url(r'^matchmaker/project/(?P<project_id>[\w|-]+)/individual/(?P<indiv_id>[\w|-]+)/delete$', xbrowse_server.api.views.delete_individual, name='delete_individual'),
+    url(r'^matchmaker/project/(?P<project_id>[\w|-]+)/individual/(?P<indiv_id>[\w\s|-]+)/delete$', xbrowse_server.api.views.delete_individual, name='delete_individual'),
 
     url(r'^matchmaker/translate/matchbox_id/(?P<matchbox_id>[\w|-]+)$', xbrowse_server.api.views.get_matchbox_id_details, name='get_matchbox_id_details'),
     url(r'^matchmaker/metrics$', xbrowse_server.api.views.get_matchbox_metrics, name='get_matchbox_metrics'),
