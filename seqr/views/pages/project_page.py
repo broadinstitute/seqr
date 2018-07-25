5da31c20d2d7a75a805f1e24bb9c500dae3185a6
Hana Snow <hsnow@broadinstitute.org>
2018-07-25 13:30:52 -0400

    clean up

--- seqr/views/pages/project_page.py
+++ seqr/views/pages/project_page.py
@@ -5,7 +5,6 @@ APIs used by the project page
 import itertools
 import logging
 import json
-from collections import defaultdict
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
