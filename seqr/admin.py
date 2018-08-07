98863c7c5aab8a1dc7d0c64044d1b3bbd64b43ac
Hana Snow <hsnow@broadinstitute.org>
2018-08-07 15:19:40 -0400

    add model to admin

--- seqr/admin.py
+++ seqr/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from seqr.models import Project, Family, Individual, Sample, LocusList, LocusListGene, LocusListInterval, VariantNote, \
-    VariantTag, VariantTagType, VariantFunctionalData, SavedVariant
+    VariantTag, VariantTagType, VariantFunctionalData, SavedVariant, GeneNote, AnalysisGroup, ProjectCategory, \
+    FamilyAnalysedBy
 
 for model_class in [
     Project,
@@ -15,6 +16,10 @@ for model_class in [
     VariantTagType,
     VariantFunctionalData,
     SavedVariant,
+    GeneNote,
+    AnalysisGroup,
+    ProjectCategory,
+    FamilyAnalysedBy,
 ]:
 
     @admin.register(model_class)
@@ -22,6 +27,12 @@ for model_class in [
         search_fields = [field.name for field in model_class._meta.get_fields() if field.name in set([
             'guid', 'name', 'display_name', 'deprecated_project_id', 'family_id', 'individual_id', 'description'
         ])]
-        list_display = search_fields + ['created_date']
+        list_display = model_class._meta.json_fields if hasattr(model_class._meta, 'json_fields') else search_fields
+        if hasattr(model_class._meta, 'internal_json_fields'):
+            list_display = model_class._meta.internal_json_fields + list_display
+        if 'created_date' not in list_display:
+            list_display.append('created_date')
+        if 'last_modified_date' not in list_display:
+            list_display.append('last_modified_date')
         save_on_top = True
         list_per_page = 2000
