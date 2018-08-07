c973d3aabf5468cb19d7e2f580a6d343825acee2
bw2 <weisburd@broadinstitute.org>
2018-08-07 09:20:15 -0400

    add --reset option to 1st clear saved_variant_json fields so that it's easy to see which ones have been updated. Also, save variants immediately after retrieving them from elasticsearch - so that if a tunnel to ES times-out in the middle of a large project, most of it can still be updated

--- seqr/management/commands/reload_saved_variant_json.py
+++ seqr/management/commands/reload_saved_variant_json.py
@@ -1,7 +1,8 @@
 import logging
 from django.core.management.base import BaseCommand
+from django.db.models.query_utils import Q
 from tqdm import tqdm
-
+import traceback
 from seqr.models import Project, SavedVariant
 from seqr.utils.model_sync_utils import retrieve_saved_variants_json, update_saved_variant_json
 
@@ -12,6 +13,7 @@ class Command(BaseCommand):
     help = 'Transfer projects to the new seqr schema'
 
     def add_arguments(self, parser):
+        parser.add_argument('--reset', help='Reset saved variants.', action="store_true")
         parser.add_argument('projects', nargs="*", help='Project(s) to transfer. If not specified, defaults to all projects.')
 
     def handle(self, *args, **options):
@@ -19,7 +21,7 @@ class Command(BaseCommand):
         projects_to_process = options['projects']
 
         if projects_to_process:
-            projects = Project.objects.filter(name__in=projects_to_process)
+            projects = Project.objects.filter(Q(name__in=projects_to_process) | Q(guid__in=projects_to_process))
             logging.info("Processing %s projects" % len(projects))
         else:
             projects = Project.objects.filter(deprecated_project_id__isnull=False)
@@ -31,20 +33,28 @@ class Command(BaseCommand):
             logger.info("Project: " + project.name)
 
             saved_variants = SavedVariant.objects.filter(project=project, family__isnull=False).select_related('family')
+            if options['reset']:
+                for saved_variant in saved_variants:
+                    saved_variant.saved_variant_json = None
+                    saved_variant.save()
+                logger.info('Reset {0} variants in {1}'.format(len(saved_variants), project.name))
+
             saved_variants_map = {(v.xpos_start, v.ref, v.alt, v.family.family_id): v for v in saved_variants}
 
-            try:
-                variants_json = retrieve_saved_variants_json(project, saved_variants_map.keys())
-            except Exception as e:
-                logger.error('Error in project {0}: {1}'.format(project.name, e))
-                error[project.name] = e
-                continue
+            for saved_variant in saved_variants_map.keys():
+                try:
+                    variants_json = retrieve_saved_variants_json(project, [saved_variant])
+                except Exception as e:
+                    logger.error('Error in project {0}: {1}'.format(project.name, e))
+                    traceback.print_exc()
+                    error[project.name] = e
+                    continue
 
-            for var in variants_json:
-                saved_variant = saved_variants_map[(var['xpos'], var['ref'], var['alt'], var['extras']['family_id'])]
-                update_saved_variant_json(saved_variant, var)
-            success[project.name] = len(variants_json)
-            logger.info('Updated {0} variants for project {1}'.format(len(variants_json), project.name))
+                for var in variants_json:
+                    saved_variant = saved_variants_map[(var['xpos'], var['ref'], var['alt'], var['extras']['family_id'])]
+                    update_saved_variant_json(saved_variant, var)
+                success[project.name] = len(variants_json)
+                logger.info('Updated {0} variants for project {1}'.format(len(variants_json), project.name))
 
         logger.info("Done")
         logger.info("Summary: ")
