da26f1fcf9511ef0c495242afe887f704a0d18d9
Hana Snow <hsnow@broadinstitute.org>
2018-08-14 13:32:53 -0400

    show anaysis groups in awesomebar

--- seqr/views/apis/awesomebar_api.py
+++ seqr/views/apis/awesomebar_api.py
@@ -8,12 +8,11 @@ from django.db.models import Q
 from django.db.models.functions import Length
 
 from django.views.decorators.http import require_GET
-from guardian.shortcuts import get_objects_for_user
 
 from seqr.views.apis.auth_api import API_LOGIN_REQUIRED_URL
-from seqr.views.utils.json_utils import create_json_response
+from seqr.views.utils.json_utils import create_json_response, _to_title_case
 from reference_data.models import GeneInfo
-from seqr.models import Project, Family, Individual, CAN_VIEW
+from seqr.models import Project, Family, Individual, AnalysisGroup
 
 logger = logging.getLogger(__name__)
 
@@ -21,8 +20,8 @@ MAX_RESULTS_PER_CATEGORY = 8
 MAX_STRING_LENGTH = 100
 
 
-def _get_matching_projects(user, query):
-    """Returns projects that match the given query string, and that the user can view.
+def _get_matching_objects(user, query, object_cls, filter_fields, get_title, get_href, get_description=None, project_field=''):
+    """Returns objects that match the given query string, and that the user can view, for the given object criteria.
 
     Args:
         user: Django user
@@ -30,95 +29,69 @@ def _get_matching_projects(user, query):
     Returns:
         Sorted list of matches where each match is a dictionary of strings
     """
-    project_filter = Q(deprecated_project_id__icontains=query) | Q(name__icontains=query)
+    project_field_prefix = '{}__'.format(project_field) if project_field else ''
+    object_filter = Q()
+    for field in filter_fields:
+        object_filter |= Q(**{'{}__icontains'.format(field): query})
     if not user.is_superuser:
         if user.is_staff:
-            project_filter &= Q(can_view_group__user=user) | Q(disable_staff_access=False)
+            object_filter &= Q(**{'{}can_view_group__user'.format(project_field_prefix): user}) | Q(**{'{}disable_staff_access'.format(project_field_prefix): False})
         else:
-            project_filter &= Q(can_view_group__user=user)
+            object_filter &= Q(**{'{}can_view_group__user'.format(project_field_prefix): user})
 
-    matching_projects = Project.objects.filter(project_filter).distinct()
+    matching_objects = getattr(object_cls, 'objects').filter(object_filter).distinct()
+    if project_field:
+        matching_objects = matching_objects.select_related(project_field)
 
-    projects_result = []
-    for p in matching_projects[:MAX_RESULTS_PER_CATEGORY]:
-        title = p.name or p.guid  # TODO make sure all projects & families have a name
-        projects_result.append({
-            'key': p.guid,
-            'title': title[:MAX_STRING_LENGTH],
-            'description': '',  # '('+p.description+')' if p.description else '',
-            'href': '/project/%s/project_page' % p.guid,
-        })
+    results = [{
+        'key': obj.guid,
+        'title': get_title(obj)[:MAX_STRING_LENGTH],
+        'description': u'({})'.format(get_description(obj)) if get_description else '',
+        'href': get_href(obj),
+    } for obj in matching_objects[:MAX_RESULTS_PER_CATEGORY]]
 
-    projects_result.sort(key=lambda f: len(f.get('title', '')))
+    results.sort(key=lambda f: len(f.get('title', '')))
 
-    return projects_result
+    return results
+
+
+def _get_matching_projects(user, query):
+    return _get_matching_objects(
+        user, query, Project,
+        filter_fields=['deprecated_project_id', 'name'],
+        get_title=lambda p: p.name,
+        get_href=lambda p: '/project/{}/project_page'.format(p.guid),
+    )
 
 
 def _get_matching_families(user, query):
-    """Returns families that match the given query string, and that the user can view.
+    return _get_matching_objects(
+        user, query, Family,
+        filter_fields=['family_id', 'display_name'],
+        get_title=lambda f: f.display_name or f.family_id,
+        get_href=lambda f: '/project/{}/family_page/{}'.format(f.project.guid, f.guid),
+        get_description=lambda f: f.project.name,
+        project_field='project')
 
-    Args:
-        user: Django user
-        query: String typed into the awesomebar
-    Returns:
-        Sorted list of matches where each match is a dictionary of strings
-    """
-    family_filter = Q(family_id__icontains=query) | Q(display_name__icontains=query)
-    if not user.is_superuser:
-        if user.is_staff:
-            family_filter &= Q(project__can_view_group__user=user) | Q(project__disable_staff_access=False)
-        else:
-            family_filter &= Q(project__can_view_group__user=user)
 
-    matching_families = Family.objects.select_related('project').filter(family_filter).distinct()
-
-    families_result = []
-    for f in matching_families[:MAX_RESULTS_PER_CATEGORY]:
-        title = f.display_name or f.family_id or f.guid
-        families_result.append({
-            'key': f.guid,
-            'title': title[:MAX_STRING_LENGTH],
-            'description': ('(%s)' % f.project.name) if f.project else '',
-            'href': '/project/'+f.project.deprecated_project_id+'/family/'+f.family_id,
-        })
-
-    families_result.sort(key=lambda f: len(f.get('title', '')))
-
-    return families_result
+def _get_matching_analysis_groups(user, query):
+    return _get_matching_objects(
+        user, query, AnalysisGroup,
+        filter_fields=['name'],
+        get_title=lambda f: f.name,
+        get_href=lambda f: '/project/{}/analysis_group/{}'.format(f.project.guid, f.guid),
+        get_description=lambda f: f.project.name,
+        project_field='project')
 
 
 def _get_matching_individuals(user, query):
-    """Returns individuals that match the given query string, and that the user can view.
-
-    Args:
-        user: Django user
-        query: String typed into the awesomebar
-    Returns:
-        Sorted list of matches where each match is a dictionary of strings
-    """
-    individual_filter = Q(individual_id__icontains=query) | Q(display_name__icontains=query)
-    if not user.is_superuser:
-        if user.is_staff:
-            individual_filter &= Q(family__project__can_view_group__user=user) | Q(family__project__disable_staff_access=False)
-        else:
-            individual_filter &= Q(family__project__can_view_group__user=user)
-
-    matching_individuals = Individual.objects.select_related('family__project').filter(individual_filter).distinct()
-
-    individuals_result = []
-    for i in matching_individuals[:MAX_RESULTS_PER_CATEGORY]:
-        title = i.display_name or i.individual_id or i.guid
-        f = i.family
-        individuals_result.append({
-            'key': i.guid,
-            'title': title[:MAX_STRING_LENGTH],
-            'description': ('(%s : family %s)' % (f.project.name, f.display_name)) if f.project else '',
-            'href': '/project/'+f.project.deprecated_project_id+'/family/'+f.family_id,
-        })
-
-    individuals_result.sort(key=lambda i: len(i.get('title', '')))
-
-    return individuals_result
+    return _get_matching_objects(
+        user, query, Individual,
+        filter_fields=['individual_id', 'display_name'],
+        get_title=lambda i: i.display_name or i.individual_id,
+        get_href=lambda i: '/project/{}/family_page/{}'.format(i.family.project.guid, i.family.guid),
+        get_description=lambda i: '{}: family {}'.format(i.family.project.name, (i.family.display_name or i.family.family_id)),
+        project_field='family__project')
 
 
 def _get_matching_genes(user, query):
@@ -151,6 +124,15 @@ def _get_matching_genes(user, query):
     return result
 
 
+CATEGORY_MAP = {
+    'projects': _get_matching_projects,
+    'families': _get_matching_families,
+    'analysis_groups': _get_matching_analysis_groups,
+    'individuals': _get_matching_individuals,
+    'genes': _get_matching_genes,
+}
+
+
 @login_required(login_url=API_LOGIN_REQUIRED_URL)
 @require_GET
 def awesomebar_autocomplete_handler(request):
@@ -160,25 +142,11 @@ def awesomebar_autocomplete_handler(request):
     if query is None:
         raise ValueError("missing ?q=<prefix> url arg")
 
-    categories = request.GET.get('categories', '').split(',')
+    categories = request.GET.get('categories').split(',') if request.GET.get('categories') else CATEGORY_MAP.keys()
 
-    results = collections.OrderedDict()
-    if len(query) > 0:
-        projects = _get_matching_projects(request.user, query) if 'projects' in categories else None
-        if projects:
-            results['projects'] = {'name': 'Projects', 'results': projects}
+    results = {
+        category: {'name': _to_title_case(category), 'results': CATEGORY_MAP[category](request.user, query)}
+        for category in categories
+    }
 
-        families = _get_matching_families(request.user, query) if 'families' in categories else None
-        if families:
-            results['families'] = {'name': 'Families', 'results': families}
-
-        individuals = _get_matching_individuals(request.user, query) if 'individuals' in categories else None
-        if individuals:
-            results['individuals'] = {'name': 'Individuals', 'results': individuals}
-
-        genes = _get_matching_genes(request.user, query) if 'genes' in categories else None
-        if genes:
-            results['genes'] = {'name': 'Genes', 'results': genes}
-
-
-    return create_json_response({'matches': results})
+    return create_json_response({'matches': {k: v for k, v in results.items() if v['results']}})
