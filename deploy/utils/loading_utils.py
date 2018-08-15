d6727637b235c53a444c7683ae61a964aad5aead
bw2 <weisburd@broadinstitute.org>
2018-08-15 15:17:33 -0400

    pr feedback, more comments, updates to update_all_reference_data, load_project, load_example_project

--- deploy/utils/loading_utils.py
+++ deploy/utils/loading_utils.py
@@ -3,17 +3,20 @@ import os
 
 from deploy.utils.kubectl_utils import get_pod_name, run_in_pod
 from deploy.utils.servctl_utils import check_kubernetes_context
+from seqr.models import Sample
 
 logger = logging.getLogger(__name__)
 
 
-def load_project(deployment_target, project_id="1kg", genome_version="37", vcf=None, ped=None):
+def load_project(deployment_target, project_id, genome_version, sample_type, dataset_type, vcf, ped):
     """Load example project
 
     Args:
         deployment_target (string):
         project_id (string): project id
         genome_version (string): reference genome version - either "37" or "38"
+        sample_type (string): "WES", "WGS", etc.
+        dataset_type (string): "VARIANTS", "SV", etc.
         vcf (string): VCF path
         ped (string): PED path
     """
@@ -22,7 +25,9 @@ def load_project(deployment_target, project_id="1kg", genome_version="37", vcf=N
 
     pod_name = get_pod_name('pipeline-runner', deployment_target=deployment_target)
     if not pod_name:
-        raise ValueError("No 'seqr' pods found. Is the kubectl environment configured in this terminal? and has this type of pod been deployed?" % locals())
+        pod_name = get_pod_name('seqr', deployment_target=deployment_target)
+    if not pod_name:
+        raise ValueError("No 'pipeline-runner' or 'seqr' pods found. Is the kubectl environment configured in this terminal? and have either of these pods been deployed?" % locals())
 
     if not project_id:
         raise ValueError("project_id not specified")
@@ -46,9 +51,18 @@ def load_project(deployment_target, project_id="1kg", genome_version="37", vcf=N
     run_in_pod(pod_name, "python2.7 -u manage.py generate_pedigree_images -f '%(project_id)s'" % locals(), verbose=True)
     run_in_pod(pod_name, "python2.7 -u manage.py add_default_tags '%(project_id)s'" % locals(), verbose=True)
 
-    # TODO fix this 
-    #run_in_pod(pod_name, "python2.7 -u manage.py load_project '%(project_id)s'" % locals(), verbose=True)
-    #run_in_pod(pod_name, "python2.7 -u manage.py load_project_datastore '%(project_id)s'" % locals(), verbose=True)
+    run_in_pod(pod_name, """
+        cd /hail-elasticsearch-pipelines/; \
+        python2.7 ./gcloud_dataproc/submit.py \
+            --run-locally hail_scripts/v01/load_dataset_to_es.py \
+            --genome-version %(genome_version)s \
+            --project-guid %(project_id)s \
+            --sample-type %(sample_type)s \
+            --dataset-type %(dataset_type)s \
+            --host elasticsearch \
+            --vep-block-size 30 \
+            %(vcf)s
+        """ % locals(), verbose=True)
 
 
 def load_example_project(deployment_target, genome_version="37"):
@@ -76,31 +90,32 @@ def load_example_project(deployment_target, genome_version="37"):
     vcf = "https://storage.googleapis.com/seqr-public/test-projects/1kg-exomes/%(vcf_filename)s" % locals()
     ped = "https://storage.googleapis.com/seqr-public/test-projects/1kg-exomes/1kg.ped"
 
-    load_project(deployment_target, project_id=project_id, genome_version=genome_version, vcf=vcf, ped=ped)
+    load_project(
+        deployment_target,
+        project_id=project_id,
+        genome_version=genome_version,
+        sample_type=Sample.SAMPLE_TYPE_WES,
+        dataset_type=Sample.DATASET_TYPE_VARIANT_CALLS,
+        vcf=vcf,
+        ped=ped)
 
 
-def load_reference_data(deployment_target, genome_version="37"):
+def update_reference_data(deployment_target):
     """Load reference data
 
     Args:
         deployment_target (string):
-        genome_version (string): reference genome version - either "37" or "38"
     """
 
     check_kubernetes_context(deployment_target)
 
-    pod_name = get_pod_name('seqr', deployment_target=deployment_target)
+    pod_name = get_pod_name('pipeline-runner', deployment_target=deployment_target)
     if not pod_name:
-        raise ValueError("No 'seqr' pods found. Is the kubectl environment configured in this terminal? and has this type of pod been deployed?" % locals())
+        pod_name = get_pod_name('seqr', deployment_target=deployment_target)
+    if not pod_name:
+        raise ValueError("No 'pipeline-runner' or 'seqr' pods found. Is the kubectl environment configured in this terminal? and have either of these pods been deployed?" % locals())
 
-    #run_in_pod(pod_name, "mkdir -p /data/reference_data/" % locals())
-    #run_in_pod(pod_name, "wget -N https://storage.googleapis.com/seqr-public/reference-data/seqr-resource-bundle.GRCh%(genome_version)s.tar.gz -P /data/reference_data/" % locals(), verbose=True)
-    #run_in_pod(pod_name, "tar -xzf /data/reference_data/seqr-resource-bundle.GRCh%(genome_version)s.tar.gz --directory /data/reference_data/" % locals(), verbose=True)
-    #run_in_pod(pod_name, "python2.7 -u manage.py load_resources" % locals(), verbose=True)
-
-    run_in_pod(pod_name, "python2.7 -u manage.py update_all_rerence_data --omim-key $OMIM_KEY" % locals(), verbose=True)
-
-    run_in_pod(pod_name, "/usr/local/bin/restart_server.sh" % locals(), verbose=True)
+    run_in_pod(pod_name, "python2.7 -u manage.py update_all_reference_data --omim-key '$OMIM_KEY'" % locals(), verbose=True, print_command=True)
 
 
 def create_user(deployment_target, email=None, password=None):
