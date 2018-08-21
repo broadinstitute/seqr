827cbbdf7285bd34019f33b8b7ab3c7af2e1791b
bw2 <weisburd@broadinstitute.org>
2018-08-21 09:08:02 -0400

    cleaned up and refactored Dockerfiles

--- deploy/docker/pipeline-runner/entrypoint.sh
+++ deploy/docker/pipeline-runner/entrypoint.sh
@@ -24,10 +24,6 @@ cd /seqr
 
 pip install --upgrade -r requirements.txt
 
-git checkout dev
-git pull
-python -u manage.py load_resources
-
 python -u manage.py makemigrations
 python -u manage.py migrate
 python -u manage.py check
@@ -38,7 +34,6 @@ cd /seqr_settings
 #python manage.py runserver 0.0.0.0:8000 &
 
 # launch jupyter notebook in background
-
 mkdir /ipython_notebooks
 cd /ipython_notebooks
 nohup jupyter notebook --ip=0.0.0.0 --port=30005 --allow-root --NotebookApp.token='' &
