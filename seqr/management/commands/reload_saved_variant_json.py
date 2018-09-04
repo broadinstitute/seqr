778cf37b7bd0f3468573fb06f841fd3101aa3f2b
Hana Snow <hsnow@broadinstitute.org>
2018-09-04 11:15:29 -0400

    reload saved variants by  family

--- seqr/management/commands/reload_saved_variant_json.py
+++ seqr/management/commands/reload_saved_variant_json.py
@@ -2,7 +2,7 @@ import logging
 from django.core.management.base import BaseCommand
 from django.db.models.query_utils import Q
 from tqdm import tqdm
-from seqr.models import Project
+from seqr.models import Project, Family
 from seqr.views.utils.variant_utils import update_project_saved_variant_json
 
 logger = logging.getLogger(__name__)
@@ -13,10 +13,12 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('projects', nargs="*", help='Project(s) to transfer. If not specified, defaults to all projects.')
+        parser.add_argument('--family-id', help='optional family to reload variants for')
 
     def handle(self, *args, **options):
         """transfer project"""
         projects_to_process = options['projects']
+        family_id = options['family_id']
 
         if projects_to_process:
             projects = Project.objects.filter(Q(name__in=projects_to_process) | Q(guid__in=projects_to_process))
@@ -30,7 +32,7 @@ class Command(BaseCommand):
         for project in tqdm(projects, unit=" projects"):
             logger.info("Project: " + project.name)
             try:
-                variants_json = update_project_saved_variant_json(project)
+                variants_json = update_project_saved_variant_json(project, family_id=family_id)
                 success[project.name] = len(variants_json)
                 logger.info('Updated {0} variants for project {1}'.format(len(variants_json), project.name))
             except Exception as e:
