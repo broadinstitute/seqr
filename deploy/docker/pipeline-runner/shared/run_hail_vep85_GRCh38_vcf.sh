9c15f70685c8dac7983e981425d4e4800a14784a
bw2 <weisburd@broadinstitute.org>
2018-09-17 14:18:43 -0400

    copy vep-gcloud-grch3*.properties, /vep/run_hail_vep85_GRCh3*_vcf.sh, and 1var.vcf files from /hail-elasticsearch-pipelines instead of keeping local copies. TODO delete local copies

--- deploy/docker/pipeline-runner/shared/run_hail_vep85_GRCh38_vcf.sh
+++ deploy/docker/pipeline-runner/shared/run_hail_vep85_GRCh38_vcf.sh
@@ -16,5 +16,5 @@ perl /vep/variant_effect_predictor/variant_effect_predictor.pl \
   --fasta /vep/homo_sapiens/85_GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz \
   --minimal \
   --assembly GRCh38 \
-  --plugin LoF,human_ancestor_fa:/vep/loftee_data_grch38/human_ancestor.fa.gz,filter_position:0.05,min_intron_size:15 \
+  --plugin LoF,human_ancestor_fa:/vep/loftee_data_grch38/loftee_data/human_ancestor.fa.gz,filter_position:0.05,min_intron_size:15 \
   -o STDOUT
