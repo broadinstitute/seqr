01da8e943a2ee5d1b806014d98b55017788d1769
bw2 <weisburd@broadinstitute.org>
2018-09-25 11:53:23 -0400

    reduced seqr memory reqs.

--- deploy/step2.install_and_start_elasticsearch.sh
+++ deploy/step2.install_and_start_elasticsearch.sh
@@ -8,7 +8,7 @@ ELASTICSEARCH_VERSION=elasticsearch-6.4.0
 curl -L http://artifacts.elastic.co/downloads/elasticsearch/${ELASTICSEARCH_VERSION}.tar.gz -o ${ELASTICSEARCH_VERSION}.tar.gz
 tar xzf ${ELASTICSEARCH_VERSION}.tar.gz
 
-echo "ES_JAVA_OPTS='-Xms5000m -Xmx5000m' ./${ELASTICSEARCH_VERSION}/bin/elasticsearch -E network.host=0.0.0.0" | tee start_elasticsearch.sh
+echo "ES_JAVA_OPTS='-Xms3900m -Xmx3900m' ./${ELASTICSEARCH_VERSION}/bin/elasticsearch -E network.host=0.0.0.0" | tee start_elasticsearch.sh
 chmod 777 ./start_elasticsearch.sh
 
 set +x
