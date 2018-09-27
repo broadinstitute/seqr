7aec82c8b5d814e8d581d48d88557530a7bb00d1
bw2 <weisburd@broadinstitute.org>
2018-09-26 20:49:37 -0400

    don't update to latest seqr code when the pod starts. This way the docker image tag can accurately identify the git commit that was used in the image.

--- deploy/docker/seqr/entrypoint.sh
+++ deploy/docker/seqr/entrypoint.sh
@@ -27,7 +27,7 @@ fi
 # launch django dev server in background
 cd /seqr
 
-git pull
+# git pull
 git checkout $SEQR_GIT_BRANCH
 pip install --upgrade -r requirements.txt  # doublecheck that requirements are up-to-date
 python -u manage.py makemigrations
