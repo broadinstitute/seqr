74db6ee37b82e3e5ffcb8f62051670a53667eb0c
bw2 <weisburd@broadinstitute.org>
2018-10-08 14:52:40 -0400

    switch back to seqr master branch. improve elasticsearch start script

--- deploy/step2.install_and_start_elasticsearch.sh
+++ deploy/step2.install_and_start_elasticsearch.sh
@@ -4,11 +4,14 @@ echo ==== Install and start elasticsearch =====
 set -x
 
 ELASTICSEARCH_VERSION=elasticsearch-6.4.0
+CURRENT_DIR=$(pwd)
 
 curl -L http://artifacts.elastic.co/downloads/elasticsearch/${ELASTICSEARCH_VERSION}.tar.gz -o ${ELASTICSEARCH_VERSION}.tar.gz
 tar xzf ${ELASTICSEARCH_VERSION}.tar.gz
 
-echo "ES_JAVA_OPTS='-Xms3900m -Xmx3900m' ./${ELASTICSEARCH_VERSION}/bin/elasticsearch -E network.host=0.0.0.0" | tee start_elasticsearch.sh
+echo 'cd "'$CURRENT_DIR'";
+ES_JAVA_OPTS="-Xms3900m -Xmx3900m" nohup ./${ELASTICSEARCH_VERSION}/bin/elasticsearch -E network.host=0.0.0.0 | tee elasticsearch.log
+' | tee start_elasticsearch.sh
 chmod 777 ./start_elasticsearch.sh
 
 set +x
