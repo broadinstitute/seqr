d1c773e4ca8e5b47cb44fa4e8bee10969eddd1bd
bw2 <weisburd@broadinstitute.org>
2018-10-17 01:17:05 -0700

    renumbered scripts

--- deploy/install_local.all_steps.sh
+++ deploy/install_local.all_steps.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "$SEQR_DIR"  ]; then
+    echo "SEQR_DIR environment variable not set. Please run previous install step."
+    exit 1
+fi
+
+cd ${SEQR_DIR}
 ./seqr/deploy/install_local.step1.install_mongo.sh
 ./seqr/deploy/install_local.step2.install_postgres.sh
 ./seqr/deploy/install_local.step3.elasticsearch.sh
