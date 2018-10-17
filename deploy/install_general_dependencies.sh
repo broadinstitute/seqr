daa90d32df587dfaaccc064fbaddc4ceac5a4202
bw2 <weisburd@broadinstitute.org>
2018-10-17 01:25:09 -0700

    fixed spacing

--- deploy/install_general_dependencies.sh
+++ deploy/install_general_dependencies.sh
@@ -130,14 +130,14 @@ elif [ $PLATFORM = "macos" ]; then
 elif [ $PLATFORM = "centos" ]; then
 
     sudo tee /etc/yum.repos.d/google-cloud-sdk.repo << EOM
-    [google-cloud-sdk]
-    name=Google Cloud SDK
-    baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
-    enabled=1
-    gpgcheck=1
-    repo_gpgcheck=1
-    gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-           https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOM
     sudo yum install -y google-cloud-sdk
 
