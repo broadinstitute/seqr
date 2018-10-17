44ca47c3a63755fa19f5f15199f902e7f2c476d4
bw2 <weisburd@broadinstitute.org>
2018-10-17 00:57:12 -0700

    renumbered scripts

--- deploy/install_local.all_steps.sh
+++ deploy/install_local.all_steps.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/macarthur-lab/seqr/deploy_code_hail_elasticsearch_repo_integration/deploy/install_local.step0.set_env_vars.sh
-source ./install_local.step0.set_env_vars.sh
-
-wget https://raw.githubusercontent.com/macarthur-lab/seqr/deploy_code_hail_elasticsearch_repo_integration/deploy/install_local.step1.clone_seqr_repo.sh
-./install_local.step1.clone_seqr_repo.sh
-chmod 777 ./install_local.step1.clone_seqr_repo.sh
-
-./seqr/deploy/install_local.step2.install_mongo.sh
-./seqr/deploy/install_local.step3.install_postgres.sh
-./seqr/deploy/install_local.step4.elasticsearch.sh
-./seqr/deploy/install_local.step5.kibana.sh
-./seqr/deploy/install_local.step6.install_redis.sh
-./seqr/deploy/install_local.step7.install_seqr.sh
-./seqr/deploy/install_local.step8.install_phenotips.sh
-./seqr/deploy/install_local.step9.install_pipeline_runner.sh
+./seqr/deploy/install_local.step1.install_mongo.sh
+./seqr/deploy/install_local.step2.install_postgres.sh
+./seqr/deploy/install_local.step3.elasticsearch.sh
+./seqr/deploy/install_local.step4.kibana.sh
+./seqr/deploy/install_local.step5.install_redis.sh
+./seqr/deploy/install_local.step6.install_seqr.sh
+./seqr/deploy/install_local.step7.install_phenotips.sh
+./seqr/deploy/install_local.step8.install_pipeline_runner.sh
---
+++ deploy/install_local.step7.install_phenotips.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+echo "==== Installing Phenotips ===="
+set -x
+
+export PT_VERSION="1.2.6"
+wget https://nexus.phenotips.org/nexus/content/repositories/releases/org/phenotips/phenotips-standalone/${PT_VERSION}/phenotips-standalone-${PT_VERSION}.zip \
+    && unzip phenotips-standalone-${PT_VERSION}.zip \
+    && rm phenotips-standalone-${PT_VERSION}.zip
+
+cd phenotips-standalone-${PT_VERSION}
+wget https://jdbc.postgresql.org/download/postgresql-42.1.4.jar -O ./webapps/phenotips/WEB-INF/lib/postgresql-42.1.4.jar
+
+cp ${SEQR_DIR}/deploy/docker/phenotips/config/${PT_VERSION}/xwiki.cfg ./webapps/phenotips/WEB-INF/xwiki.cfg
+cp ${SEQR_DIR}/deploy/docker/phenotips/config/${PT_VERSION}/hibernate.cfg.xml ./webapps/phenotips/WEB-INF/hibernate.cfg.xml
+sed -i 's/<property.name="connection.url">.*<\/property>/<property name="connection.url">jdbc:postgresql:\/\/localhost:5432\/xwiki<\/property>/' ./webapps/phenotips/WEB-INF/hibernate.cfg.xml
+
+rm -rf data/extension data/jobs
+cp -r ${SEQR_DIR}/deploy/docker/phenotips/init/${PT_VERSION}/extension ./data/extension
+cp -r ${SEQR_DIR}/deploy/docker/phenotips/init/${PT_VERSION}/jobs ./data/jobs
+
+# init PhenoTips db
+psql -U postgres postgres -c "create role xwiki with CREATEDB LOGIN PASSWORD 'xwiki'"
+psql -U postgres postgres -c 'grant all privileges on database xwiki to xwiki'
+psql -U xwiki postgres -c 'create database xwiki'
+psql -U xwiki xwiki -f ${SEQR_DIR}/deploy/docker/phenotips/init/${PT_VERSION}/init_phenotips_db.sql
+
+echo 'cd '$(pwd)'
+LOG_FILE=$(pwd)/phenotips.log
+(nohup ./start.sh >& ${LOG_FILE}) &
+echo "PhenoTips started in background. See ${LOG_FILE}"
+' | tee start_phenotips.sh
+chmod 777 ./start_phenotips.sh
+
+echo "PhenoTips started in background. See ${LOG_FILE}"
+
+cd ..
+set +x
