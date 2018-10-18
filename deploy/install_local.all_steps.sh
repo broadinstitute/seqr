c1e0d9f77bf8b3829c48b46979ed273cc443f71b
bw2 <weisburd@broadinstitute.org>
2018-10-17 18:37:25 -0700

    fixed paths

--- deploy/install_local.all_steps.sh
+++ deploy/install_local.all_steps.sh
@@ -6,11 +6,11 @@ if [ -z "$SEQR_DIR"  ]; then
 fi
 
 cd ${SEQR_DIR}
-./seqr/deploy/install_local.step1.install_mongo.sh
-./seqr/deploy/install_local.step2.install_postgres.sh
-./seqr/deploy/install_local.step3.elasticsearch.sh
-./seqr/deploy/install_local.step4.kibana.sh
-./seqr/deploy/install_local.step5.install_redis.sh
-./seqr/deploy/install_local.step6.install_seqr.sh
-./seqr/deploy/install_local.step7.install_phenotips.sh
-#./seqr/deploy/install_local.step8.install_pipeline_runner.sh
+./deploy/install_local.step1.install_mongo.sh
+./deploy/install_local.step2.install_postgres.sh
+./deploy/install_local.step3.elasticsearch.sh
+./deploy/install_local.step4.kibana.sh
+./deploy/install_local.step5.install_redis.sh
+./deploy/install_local.step6.install_seqr.sh
+./deploy/install_local.step7.install_phenotips.sh
+#./deploy/install_local.step8.install_pipeline_runner.sh
