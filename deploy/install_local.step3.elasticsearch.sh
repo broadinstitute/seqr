29e58ba16bc6c8e28f867244afe8e177c5b44283
bw2 <bw2@users.noreply.github.com>
2018-10-24 12:19:15 -0400

    Update install_local.step3.elasticsearch.sh
    added sleep before curl

--- deploy/install_local.step3.elasticsearch.sh
+++ deploy/install_local.step3.elasticsearch.sh
@@ -19,6 +19,7 @@ echo '
 cd '$(pwd)'
 LOG_FILE=$(pwd)/elasticsearch.log
 (ES_JAVA_OPTS="-Xms3900m -Xmx3900m" nohup ./bin/elasticsearch -E network.host=0.0.0.0 >& ${LOG_FILE}) &
+sleep 7;
 curl http://localhost:9200
 echo "Elasticsearch started in background. See ${LOG_FILE}"
 ' | tee start_elasticsearch.sh
