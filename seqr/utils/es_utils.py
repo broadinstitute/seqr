f89d81a6cb8701220b1c0a39ef79302062951632
Hana Snow <hsnow@broadinstitute.org>
2019-01-02 11:34:29 +0100

    fix add dataset timeout

--- seqr/utils/es_utils.py
+++ seqr/utils/es_utils.py
@@ -5,5 +5,5 @@ import settings
 VARIANT_DOC_TYPE = 'variant'
 
 
-def get_es_client():
-    return elasticsearch.Elasticsearch(host=settings.ELASTICSEARCH_SERVICE_HOSTNAME)
+def get_es_client(timeout=10):
+    return elasticsearch.Elasticsearch(host=settings.ELASTICSEARCH_SERVICE_HOSTNAME, timeout=timeout)
