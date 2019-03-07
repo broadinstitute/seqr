597337fdeb5f65fe2dd8f5c0e7d003a3b2fc2380
Hana Snow <hsnow@broadinstitute.org>
2019-03-07 16:26:47 -0500

    fix test runner

--- settings.py
+++ settings.py
@@ -428,17 +428,19 @@ SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_HTTPONLY = True
 # SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'seqrdb',
-    'USER': '',
-    'PASSWORD': '',
-    'HOST': '',
-    'PORT': '',
-    'TEST': {
-        'NAME': 'seqr_testing_db',
+if len(sys.argv) >= 2 and sys.argv[1] == 'test':
+    # use in-memory database for running tests
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'seqrdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'seqr_test_db'
+        }
     }
-}
 
 logger.info("MONGO_SERVICE_HOSTNAME: " + MONGO_SERVICE_HOSTNAME)
 logger.info("PHENOTIPS_SERVICE_HOSTNAME: " + PHENOTIPS_SERVICE_HOSTNAME)
