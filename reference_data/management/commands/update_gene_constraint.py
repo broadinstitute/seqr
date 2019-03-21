050dfcf524d0a5eee302aa7356a7e42b58c7724a
Hana Snow <hsnow@broadinstitute.org>
2019-03-21 16:58:47 -0400

    restructure refeence data loading gene constraint

--- reference_data/management/commands/update_gene_constraint.py
+++ reference_data/management/commands/update_gene_constraint.py
@@ -1,88 +1,53 @@
 import logging
-import os
-from tqdm import tqdm
-from django.core.management.base import BaseCommand, CommandError
-from reference_data.management.commands.utils.download_utils import download_file
+from django.core.management.base import CommandError
+from reference_data.management.commands.utils.update_utils import GeneCommand, ReferenceDataHandler, update_records
 from reference_data.models import TranscriptInfo, GeneConstraint
 
 logger = logging.getLogger(__name__)
 
-GENE_CONSTRAINT_SCORES_URL = "http://storage.googleapis.com/seqr-reference-data/gene_constraint/gnomad.v2.1.1.lof_metrics.by_gene.txt"
 
+class GeneConstraintReferenceDataHandler(ReferenceDataHandler):
 
-class Command(BaseCommand):
-    help = "Loads gene constraint data"
+    model_cls = GeneConstraint
+    url = "http://storage.googleapis.com/seqr-reference-data/gene_constraint/gnomad.v2.1.1.lof_metrics.by_gene.txt"
 
-    def add_arguments(self, parser):
-        parser.add_argument('gene_constraint_path', nargs="?",
-            help="local path of gene constraint file",
-            default=os.path.join('resource_bundle', os.path.basename(GENE_CONSTRAINT_SCORES_URL)))
-
-    def handle(self, *args, **options):
-        update_gene_constraint(gene_constraint_path=options.get('gene_constraint_path'))
-
-
-def update_gene_constraint(gene_constraint_path=None):
-    """
-    Args:
-        gene_constraint_path (str): optional local constraint table path. If not specified, or the path doesn't exist,
-            the table will be downloaded.
-    """
     if TranscriptInfo.objects.count() == 0:
         raise CommandError("TranscriptInfo table is empty. Run './manage.py update_gencode' before running this command.")
 
-    if not gene_constraint_path or not os.path.isfile(gene_constraint_path):
-        gene_constraint_path = download_file(GENE_CONSTRAINT_SCORES_URL)
+    gene_reference = {t.transcript_id: t for t in TranscriptInfo.objects.all().prefetch_related('gene')}
 
-    logger.info("Deleting {} existing GeneConstraint records".format(GeneConstraint.objects.count()))
-    GeneConstraint.objects.all().delete()
+    @staticmethod
+    def parse_record(record):
+        yield {
+            'transcript_id': record['transcript'].split(".")[0],
+            'mis_z': float(record['mis_z']),
+            'pLI': float(record['pLI']) if record['pLI'] != 'NA' else 0,
+        }
 
-    constraint_records = parse_gene_constraint_table(gene_constraint_path)
-
-    # add _rank fields
-    for field in ['mis_z', 'pLI']:
-        for i, record in enumerate(sorted(constraint_records, key=lambda record: -1*record[field])):
-            record['{}_rank'.format(field)] = i
-
-    logger.info("Creating {} GeneConstraint records".format(len(constraint_records)))
-    skip_counter = 0
-    models = []
-
-    transcripts_by_id = {t.transcript_id: t for t in TranscriptInfo.objects.all().prefetch_related('gene')}
-
-    for record in tqdm(constraint_records, unit=" records"):
+    @classmethod
+    def get_gene_for_record(cls, record):
         transcript_id = record.pop("transcript_id")
-        transcript = transcripts_by_id.get(transcript_id)
+        transcript = cls.gene_reference.get(transcript_id)
         if not transcript:
-            skip_counter += 1
-            logger.warn(("transcript id '{}' not found in TranscriptInfo table. "
-                         "Running ./manage.py update_gencode to update the gencode version might fix this. "
-                         "Full record: {}").format(transcript_id, record))
-            continue
+            raise ValueError('transcript id "{}" not found in TranscriptInfo table'.format(transcript_id))
 
-        record['gene'] = transcript.gene
-        models.append(GeneConstraint(**record))
+        return transcript.gene
 
-    GeneConstraint.objects.bulk_create(models)
-
-    logger.info("Done")
-    logger.info("Loaded {} GeneConstraint records from {}. Skipped {} records with unrecognized transcript id.".format(
-        GeneConstraint.objects.count(), gene_constraint_path, skip_counter))
+    @staticmethod
+    def post_process_models(models):
+        # add _rank fields
+        for field in ['mis_z', 'pLI']:
+            for i, model in enumerate(sorted(models, key=lambda model: -1 * getattr(model, field))):
+                setattr(model, '{}_rank'.format(field), i)
 
 
-def parse_gene_constraint_table(gene_constraint_path):
-    """Parse the genemap2 table, and yield a dictionary representing each gene-phenotype pair."""
+class Command(GeneCommand):
+    reference_data_handler = GeneConstraintReferenceDataHandler
 
-    with open(gene_constraint_path) as f:
-        header_fields = next(f).rstrip('\n\r').split('\t')
 
-        constraint_records = []
-        for line in f:
-            record = dict(zip(header_fields, line.rstrip('\r\n').split('\t')))
-            constraint_records.append({
-                'transcript_id': record['transcript'].split(".")[0],
-                'mis_z': float(record['mis_z']),
-                'pLI': float(record['pLI']) if record['pLI'] != 'NA' else 0,
-            })
-
-        return constraint_records
+def update_gene_constraint(**kwargs):
+    """
+    Args:
+        file_path (str): optional local file path. If not specified, or the path doesn't exist, the table will be downloaded.
+    """
+    update_records(GeneConstraint, **kwargs)
