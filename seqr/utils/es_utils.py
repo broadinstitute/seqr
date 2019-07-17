b28f79b3f9375849f69955e85dc1a04c58489558
Hana Snow <hsnow@broadinstitute.org>
2019-07-17 14:54:34 -0400

    search by variant ids

--- seqr/utils/es_utils.py
+++ seqr/utils/es_utils.py
@@ -7,6 +7,7 @@ import logging
 from pyliftover.liftover import LiftOver
 from sys import maxint
 import redis
+import re
 
 import settings
 from reference_data.models import GENOME_VERSION_GRCh38, GENOME_VERSION_GRCh37, Omim, GeneConstraint
@@ -143,14 +144,17 @@ def _get_es_variants_for_search(search_model, es_search_cls, process_previous_re
     genes, intervals, invalid_items = parse_locus_list_items(search.get('locus', {}))
     if invalid_items:
         raise Exception('Invalid genes/intervals: {}'.format(', '.join(invalid_items)))
+    rs_ids, variant_ids, invalid_items = _parse_variant_items(search.get('locus', {}))
+    if invalid_items:
+        raise Exception('Invalid variants: {}'.format(', '.join(invalid_items)))
 
     es_search = es_search_cls(search_model.families.all(), previous_search_results=previous_search_results, skip_unaffected_families=search.get('inheritance'))
 
     if sort:
         es_search.sort(sort)
 
-    if genes or intervals:
-        es_search.filter(_location_filter(genes, intervals, search['locus']))
+    if genes or intervals or rs_ids or variant_ids:
+        es_search.filter(_location_filter(genes, intervals, rs_ids, variant_ids, search['locus']))
 
     # Pathogencicity and transcript consequences act as "OR" filters instead of the usual "AND"
     pathogenicity_filter = _pathogenicity_filter(search.get('pathogenicity', {}))
@@ -954,7 +958,7 @@ def _family_genotype_inheritance_filter(inheritance_mode, inheritance_filter, sa
     return samples_q
 
 
-def _location_filter(genes, intervals, location_filter):
+def _location_filter(genes, intervals, rs_ids, variant_ids, location_filter):
     q = None
     if intervals:
         q = _build_or_filter('range', [{
@@ -971,10 +975,56 @@ def _location_filter(genes, intervals, location_filter):
         else:
             q = gene_q
 
+    if rs_ids:
+        rs_id_q = Q('terms', rsid=rs_ids)
+        if q:
+            q |= rs_id_q
+        else:
+            q = rs_id_q
+
+    if variant_ids:
+        variant_id_q = Q('terms', variantId=variant_ids)
+        if q:
+            q |= variant_id_q
+        else:
+            q = variant_id_q
+
     if location_filter.get('excludeLocations'):
         return ~q
     else:
         return q
+
+
+def _parse_variant_items(search_json):
+    raw_items = search_json.get('rawVariantItems')
+    if not raw_items:
+        return None, None, None
+
+    invalid_items = []
+    variant_ids = []
+    rs_ids = []
+    for item in raw_items.replace(',', ' ').split():
+        if item.startswith('rs'):
+            rs_ids.append(item)
+        else:
+            var_match = re.match('(?P<chrom>\w+):(?P<pos>\d+)-(?P<ref>\w+)/(?P<alt>\w+)', item)
+            if var_match:
+                variant = var_match.groupdict()
+                try:
+                    chrom = variant['chrom'].lstrip('chr')
+                    pos = int(variant['pos'])
+                    ref = variant['ref']
+                    alt = variant['alt']
+                    get_xpos(chrom, pos)
+                    variant_ids.append('{}-{}-{}-{}'.format(chrom, pos, ref, alt))
+                except (KeyError, ValueError):
+                    invalid_items.append('chr{chrom}:{pos}-{ref}/{alt}'.format(
+                        chrom=variant.get('chrom'), pos=variant.get('start'), ref=variant.get('ref'), alt=variant.get('alt')
+                    ))
+            else:
+                invalid_items.append(item)
+
+    return rs_ids, variant_ids, invalid_items
 
 
 CLINVAR_SIGNFICANCE_MAP = {
