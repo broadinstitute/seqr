158fce7c83c6e1768aa672ed1558c58dbf042c9f
zhuang <zhuang@broadinstitute.org>
2019-08-07 10:31:16 -0400

    rm setting for redux dev tool

--- ui/redux/utils/configureStore.js
+++ ui/redux/utils/configureStore.js
@@ -19,38 +19,10 @@ const persistStoreMiddleware = store => next => (action) => {
   return result
 }
 
-// const enhancer = compose(
-//   applyMiddleware(thunkMiddleware, persistStoreMiddleware),
-//   reduxSearchEnhancer,
-// )
-//
-//
-// /**
-//  * Initialize the Redux store
-//  * @param rootReducer
-//  * @param initialState
-//  * @returns {*}
-//  */
-// export const configureStore = (
-//   rootReducer = state => state,
-//   initialState = {},
-// ) => {
-//
-//   PERSISTING_STATE.forEach((key) => { initialState[key] = loadState(key) })
-//
-//   console.log('Creating store with initial state:')
-//   console.log(initialState)
-//
-//   return createStore(rootReducer, initialState, enhancer)
-// }
-
-/* eslint-disable no-underscore-dangle */
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const enhancer = composeEnhancers(
+const enhancer = compose(
   applyMiddleware(thunkMiddleware, persistStoreMiddleware),
   reduxSearchEnhancer,
 )
-/* eslint-enable */
 
 
 /**
@@ -69,6 +41,5 @@ export const configureStore = (
   console.log('Creating store with initial state:')
   console.log(initialState)
 
-  return createStore(rootReducer, initialState, /* preloadedState, */ enhancer)
+  return createStore(rootReducer, initialState, enhancer)
 }
-
