a260731387e02690be72f3dc61546130dcc449f7
bw2 <weisburd@broadinstitute.org>
2019-11-06 16:54:42 -0500

    fix docker install

--- deploy/install_local.step8.install_pipeline_runner.sh
+++ deploy/install_local.step8.install_pipeline_runner.sh
@@ -97,7 +97,7 @@ elif [ $PLATFORM = "ubuntu" ]; then
 
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
     sudo apt-key fingerprint 0EBFCD88
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
