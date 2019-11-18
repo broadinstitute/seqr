12114e90b3f68bcf940addcf4266da943374f792
Hana Snow <hsnow@broadinstitute.org>
2019-11-18 12:22:43 -0500

    update manage commands

--- seqr/management/commands/run_postgres_database_backup.py
+++ seqr/management/commands/run_postgres_database_backup.py
@@ -30,10 +30,9 @@ class Command(BaseCommand):
         for db_name in ['seqrdb', 'xwiki']:
             backup_filename = "{db_name}_{deployment_type}_backup_{timestamp}.txt.gz".format(
                 db_name=db_name, deployment_type=options['deployment_type'], timestamp=timestamp)
-            exclude_table_arg = "--exclude-table='reference_*'" if db_name == "seqrdb" else ""
 
-            run("/usr/bin/pg_dump -U postgres {exclude_table_arg} --host {postgres_host} {db_name} | gzip -c - > {backup_dir}/{backup_filename}".format(
-                exclude_table_arg=exclude_table_arg, postgres_host=options['postgres_host'], db_name=db_name, backup_dir=backup_dir,
+            run("/usr/bin/pg_dump -U postgres --host {postgres_host} {db_name} | gzip -c - > {backup_dir}/{backup_filename}".format(
+               postgres_host=options['postgres_host'], db_name=db_name, backup_dir=backup_dir,
                 backup_filename=backup_filename
             ))
             run("gsutil mv {backup_dir}/{backup_filename} gs://{bucket}/postgres/{backup_filename}".format(
