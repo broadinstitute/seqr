f28cd588449b6bff0df0451891eb7f1d16c67290
Hana Snow <hsnow@broadinstitute.org>
2020-01-14 16:48:59 -0500

    proxy local files for igv

--- seqr/utils/file_utils.py
+++ seqr/utils/file_utils.py
@@ -9,6 +9,8 @@ from seqr.utils.local.local_file_utils import is_local_file_path, get_local_file
 
 logger = logging.getLogger(__name__)
 
+BYTE_CHUNK_SIZE = 1024*64
+
 
 def does_file_exist(file_path):
     if is_google_bucket_file_path(file_path):
@@ -19,14 +21,20 @@ def does_file_exist(file_path):
         raise ValueError("This type of file path is not supported: %(file_path)" % locals())
 
 
-def file_iter(file_path):
+def file_iter(file_path, byte_range=None):
     if is_google_bucket_file_path(file_path):
-        for line in google_bucket_file_iter(file_path):
+        for line in google_bucket_file_iter(file_path, byte_range=byte_range):
             yield line
     elif is_local_file_path(file_path):
         with open(file_path) as f:
-            for line in f:
-                yield line
+            if byte_range:
+                f.seek(byte_range[0])
+                while f.tell() < byte_range[1]:
+                    for line in f:
+                        yield line
+            else:
+                for line in f:
+                    yield line
     else:
         raise ValueError("This type of file path is not supported: %(file_path)" % locals())
 
