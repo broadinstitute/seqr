2ac095ac4b79b0eb26766fad0f81186c60d805ac
Hana Snow <hsnow@broadinstitute.org>
2020-02-12 22:18:14 -0500

    remove dev toolbar

--- settings.py
+++ settings.py
@@ -73,7 +73,7 @@ ALLOWED_HOSTS = ['*']
 CSRF_COOKIE_HTTPONLY = True
 
 # django-debug-toolbar settings
-ENABLE_DJANGO_DEBUG_TOOLBAR = True
+ENABLE_DJANGO_DEBUG_TOOLBAR = False
 if ENABLE_DJANGO_DEBUG_TOOLBAR:
     MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
     INSTALLED_APPS = ['debug_toolbar'] + INSTALLED_APPS
