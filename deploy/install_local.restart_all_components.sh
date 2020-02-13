8d622bc464fd16f9bd6e8ed187ad9541649516c5
Romeo Solano <rsolano1@uw.edu>
2020-02-13 09:29:56 -0800

    no reason for that line to exist, causes problems if you remove the seqr account from sudo because you don't want the service account having any root access

--- deploy/install_local.restart_all_components.sh
+++ deploy/install_local.restart_all_components.sh
@@ -2,8 +2,6 @@
 
 # this script restarts all server components needed for seqr
 
-sudo systemctl start postgresql-9.6    # though postgres should start up automatically after reboot
-
 cd ${SEQR_DIR}/..
 ./elasticsearch-6.4.0/start_elasticsearch.sh
 ./kibana-6.4.0-linux-x86_64/start_kibana.sh
