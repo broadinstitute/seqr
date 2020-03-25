a9fe3acdbe3245d1dc22abdca8b49a6003c8e519
Shifa Zhang <zhangshifa07504@gmail.com>
2020-03-25 16:08:00 -0400

    Updated the tests for the edit and delete family apis.

--- seqr/views/apis/family_api_tests.py
+++ seqr/views/apis/family_api_tests.py
@@ -10,6 +10,7 @@ from seqr.views.apis.family_api import update_family_pedigree_image, update_fami
 from seqr.views.utils.test_utils import _check_login
 
 FAMILY_GUID = 'F000001_1'
+FAMILY_GUID2 = 'F000002_2'
 
 PROJECT_GUID = 'R0001_1kg'
 EMPTY_PROJECT_GUID = 'R0002_empty'
@@ -30,9 +31,9 @@ class ProjectAPITest(TestCase):
         # send request with a "families" attribute
         req_values = {
             'families': [
-                {'familyGuid': FAMILY_GUID, 'description': 'Test'},
-                {PREVIOUS_FAMILY_ID_FIELD: '1', 'description': 'Test'},
-                {FAMILY_ID_FIELD: '13', 'description': 'Test'}
+                {'familyGuid': FAMILY_GUID, 'description': 'Test description 1'},
+                {PREVIOUS_FAMILY_ID_FIELD: '2', 'description': 'Test description 2'},
+                {FAMILY_ID_FIELD: '13', 'description': 'Test description 13'}
             ]
         }
         response = self.client.post(url, content_type='application/json',
@@ -41,25 +42,32 @@ class ProjectAPITest(TestCase):
         response_json = response.json()
 
         self.assertListEqual(response_json.keys(), ['familiesByGuid'])
-        self.assertEqual(response_json['familiesByGuid'][FAMILY_GUID]['description'], 'Test')
+        self.assertEqual(response_json['familiesByGuid'][FAMILY_GUID]['description'], 'Test description 1')
+        self.assertEqual(response_json['familiesByGuid'][FAMILY_GUID2]['description'], 'Test description 2')
+        self.assertEqual(response_json['familiesByGuid']['F000013_13']['description'], 'Test description 13')
 
     def test_delete_families_handler(self):
         url = reverse(delete_families_handler, args=[PROJECT_GUID])
         _check_login(self, url)
 
-        # send request with a "families" attribute
+        # send request with a "families" attribute to provide a list of families
         req_values = {
             'families': [
-                {'familyGuid': FAMILY_GUID}
+                {'familyGuid': FAMILY_GUID},
+                {'familyGuid': FAMILY_GUID2}
             ]
         }
         response = self.client.post(url, content_type='application/json',
                                     data=json.dumps(req_values))
         self.assertEqual(response.status_code, 200)
         response_json = response.json()
-
         self.assertListEqual(response_json.keys(), ['individualsByGuid', 'familiesByGuid'])
         self.assertIsNone(response_json['familiesByGuid'][FAMILY_GUID])
+        self.assertIsNone(response_json['familiesByGuid'][FAMILY_GUID2])
+
+        response = self.client.post(url, content_type='application/json',
+                                    data=json.dumps({'families': None}))
+        self.assertEqual(response.status_code, 400)
 
     def test_update_family_analysed_by(self):
         url = reverse(update_family_analysed_by, args=[FAMILY_GUID])
@@ -89,16 +97,16 @@ class ProjectAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
         response_json = response.json()
 
-        self.assertListEqual(response_json.keys(), ['F000001_1'])
-        self.assertRegex(response_json['F000001_1']['pedigreeImage'], '/media/pedigree_images/new_ped_image_.+\.png')
+        self.assertListEqual(response_json.keys(), [FAMILY_GUID])
+        self.assertRegex(response_json[FAMILY_GUID]['pedigreeImage'], '/media/pedigree_images/new_ped_image_.+\.png')
 
         # send valid delete request
         response = self.client.post(url)
         self.assertEqual(response.status_code, 200)
         response_json = response.json()
 
-        self.assertListEqual(response_json.keys(), ['F000001_1'])
-        self.assertIsNone(response_json['F000001_1']['pedigreeImage'])
+        self.assertListEqual(response_json.keys(), [FAMILY_GUID])
+        self.assertIsNone(response_json[FAMILY_GUID]['pedigreeImage'])
 
     def test_update_family_assigned_analyst(self):
         url = reverse(update_family_assigned_analyst, args=[FAMILY_GUID])
@@ -116,16 +124,16 @@ class ProjectAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
         response_json = response.json()
 
-        self.assertListEqual(response_json.keys(), ['F000001_1'])
-        self.assertEqual(response_json['F000001_1']['assignedAnalyst']['email'], 'test_user@test.com')
-        self.assertEqual(response_json['F000001_1']['assignedAnalyst']['fullName'], 'Test User')
+        self.assertListEqual(response_json.keys(), [FAMILY_GUID])
+        self.assertEqual(response_json[FAMILY_GUID]['assignedAnalyst']['email'], 'test_user@test.com')
+        self.assertEqual(response_json[FAMILY_GUID]['assignedAnalyst']['fullName'], 'Test User')
 
         # unassign analyst
         response = self.client.post(url, content_type='application/json', data=json.dumps({}))
         self.assertEqual(response.status_code, 200)
         response_json = response.json()
-        self.assertListEqual(response_json.keys(), ['F000001_1'])
-        self.assertIsNone(response_json['F000001_1']['assignedAnalyst'])
+        self.assertListEqual(response_json.keys(), [FAMILY_GUID])
+        self.assertIsNone(response_json[FAMILY_GUID]['assignedAnalyst'])
 
     def test_update_success_story_types(self):
         url = reverse(update_family_fields_handler, args=[FAMILY_GUID])
@@ -136,4 +144,4 @@ class ProjectAPITest(TestCase):
                                     data=json.dumps({'successStoryTypes': ['O', 'D']}))
         self.assertEqual(response.status_code, 200)
         response_json = response.json()
-        self.assertListEqual(response_json['F000001_1']['successStoryTypes'], ['O', 'D'])
+        self.assertListEqual(response_json[FAMILY_GUID]['successStoryTypes'], ['O', 'D'])
