dabba494ecd5d2ab45d3fa8554fe17e8fa3ade34
Shifa Zhang <zhangshifa07504@gmail.com>
2020-03-26 00:00:29 -0400

    Added test result checking.

--- seqr/views/apis/project_categories_api_tests.py
+++ seqr/views/apis/project_categories_api_tests.py
@@ -4,15 +4,22 @@ from django.urls.base import reverse
 from seqr.views.apis.project_categories_api import update_project_categories_handler
 from seqr.views.utils.test_utils import _check_login
 
+PROJECT_GUID = 'R0001_1kg'
+
 
 class ProjectCategoriesAPITest(TestCase):
     fixtures = ['users', '1kg_project']
 
     def test_project_categories_api(self):
-        url = reverse(update_project_categories_handler, args=['R0001_1kg'])
+        url = reverse(update_project_categories_handler, args=[PROJECT_GUID])
         _check_login(self, url)
 
         response = self.client.post(url, content_type='application/json', data=json.dumps({
             'categories': ['PC000002_categry_with_unicde', 'PC000003_categry_with_unicde']
         }))
         self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+
+        self.assertListEqual(response_json.keys(), ['projectsByGuid', 'projectCategoriesByGuid'])
+        self.assertListEqual(response_json['projectsByGuid'].keys(), [PROJECT_GUID])
+
