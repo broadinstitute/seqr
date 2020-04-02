0847044007c0802357d29cf626d5d21f6862d0a0
Shifa Zhang <zhangshifa07504@gmail.com>
2020-04-02 18:12:13 -0400

    Added staff apis test other than elasticsearch and anvil.

--- seqr/views/apis/staff_api_tests.py
+++ seqr/views/apis/staff_api_tests.py
@@ -4,10 +4,15 @@ import mock
 from django.test import TestCase
 from django.urls.base import reverse
 
-from seqr.views.apis.staff_api import elasticsearch_status
+from seqr.views.apis.staff_api import elasticsearch_status, mme_details, seqr_stats, get_projects_for_category, discovery_sheet , success_story
 from seqr.views.utils.test_utils import _check_login
 
-ES_CAT_ALLOCATION = [{
+PROJECT_GUID = 'R0001_1kg'
+PROJECT_EMPTY_GUID = 'R0002_empty'
+
+PROJECT_CATEGRORY_NAME = 'test category name'
+
+ES_CAT_ALLOCATION=[{
     u'node': u'node-1',
     u'disk.used': u'67.2gb',
     u'disk.avail': u'188.6gb',
@@ -198,7 +203,7 @@ EXPECTED_ERRORS = [
 
 
 class StaffAPITest(TestCase):
-    fixtures = ['users', '1kg_project']
+    fixtures = ['users', '1kg_project', 'reference_data', 'variant_searches']
     multi_db = True
 
     @mock.patch('elasticsearch_dsl.index.Index.get_mapping')
@@ -231,3 +236,48 @@ class StaffAPITest(TestCase):
                                                       h="index,docs.count,store.size,creation.date.string")
         mock_es_client.cat.aliases.assert_called_with(format="json", h="alias,index")
         mock_get_mapping.assert_called_with(doc_type='variant,structural_variant')
+
+    def test_mme_details(self):
+        url = reverse(mme_details)
+        _check_login(self, url)
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertListEqual(response_json.keys(), ['metrics', 'genesById', 'submissions'])
+
+    def test_seqr_stats(self):
+        url = reverse(seqr_stats)
+        _check_login(self, url)
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertListEqual(response_json.keys(), ['individualCount', 'familyCount', 'sampleCountByType'])
+
+    def test_get_projects_for_category(self):
+        url = reverse(get_projects_for_category, args=[PROJECT_CATEGRORY_NAME])
+        _check_login(self, url)
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertListEqual(response_json.keys(), ['projectGuids'])
+
+    def test_discovery_sheet(self):
+        url = reverse(discovery_sheet, args=[PROJECT_GUID])
+        _check_login(self, url)
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertListEqual(response_json.keys(), ['rows', 'errors'])
+
+    def test_success_story(self):
+        url = reverse(success_story, args=['all'])
+        _check_login(self, url)
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertListEqual(response_json.keys(), ['rows'])
