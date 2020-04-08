34f312dc47b1d3378bd6844136741a39e541dfd1
Shifa Zhang <zhangshifa07504@gmail.com>
2020-04-07 20:53:07 -0400

    Added test cases for the add_project_tag command.

--- seqr/management/commands/add_project_tag_tests.py
+++ seqr/management/commands/add_project_tag_tests.py
@@ -1,8 +1,12 @@
 from io import StringIO
 from django.core.management import call_command
+from django.core.management.base import CommandError
 from django.test import TestCase
 
-TEST_ARGUMENTS = {
+from seqr.models import Project, VariantTagType
+from django.db.models.query_utils import Q
+
+TAG_ARGUMENTS = {
     "project": "Test Project",
     "name": "Test tag - Novel gene and phenotype",
     "order": 22.0,
@@ -17,11 +21,62 @@ class AddProjectTagTest(TestCase):
 
     def test_normal_command(self):
         out = StringIO()
-        call_command('add_project_tag', '--project={}'.format(TEST_ARGUMENTS["project"]),
-            '--name={}'.format(TEST_ARGUMENTS["name"]), '--order={}'.format(TEST_ARGUMENTS["order"]),
-            '--category={}'.format(TEST_ARGUMENTS["category"]),
-            '--description={}'.format(TEST_ARGUMENTS["description"]),
-            '--color={}'.format(TEST_ARGUMENTS["color"]), stdout = out)
+        call_command('add_project_tag', '--project={}'.format(TAG_ARGUMENTS["project"]),
+            '--name={}'.format(TAG_ARGUMENTS["name"]), '--order={}'.format(TAG_ARGUMENTS["order"]),
+            '--category={}'.format(TAG_ARGUMENTS["category"]),
+            '--description={}'.format(TAG_ARGUMENTS["description"]),
+            '--color={}'.format(TAG_ARGUMENTS["color"]), stdout = out)
 
-        expectOutput = out.getvalue()
-        self.assertIn('', expectOutput)
+        self.assertIn('', out.getvalue())
+
+        project = Project.objects.get(Q(name = TAG_ARGUMENTS["project"]))
+        variantTagType = VariantTagType.objects.get(name__iexact=TAG_ARGUMENTS["name"], project=project)
+        tag_info = {
+            "project": variantTagType.project.name,
+            "name": variantTagType.name,
+            "order": variantTagType.order,
+            "category": variantTagType.category,
+            "description": variantTagType.description,
+            "color": variantTagType.color
+        }
+        self.assertDictEqual(tag_info, TAG_ARGUMENTS)
+
+    def test_missing_required_args(self):
+        out = StringIO()
+        error = ''
+        try:
+            call_command('add_project_tag',
+                stdout = out)
+        except CommandError as err:
+            error = err.message
+        self.assertEqual(error, 'Error: argument --project is required')
+
+        try:
+            call_command('add_project_tag',
+                '--project={}'.format(TAG_ARGUMENTS["project"]),
+                stdout = out)
+        except CommandError as err:
+            error = err.message
+        self.assertEqual(error, 'Error: argument --name is required')
+
+        try:
+            call_command('add_project_tag',
+                '--project={}'.format(TAG_ARGUMENTS["project"]),
+                '--name={}'.format(TAG_ARGUMENTS["name"]),
+                stdout = out)
+        except CommandError as err:
+            error = err.message
+        self.assertEqual(error, 'Error: argument --order is required')
+
+    def test_bad_argument_value(self):
+        out = StringIO()
+        error = ''
+        try:
+            call_command('add_project_tag', '--project={}'.format(TAG_ARGUMENTS["project"]),
+                '--name=Tier 1 - Novel gene and phenotype', '--order={}'.format(TAG_ARGUMENTS["order"]),
+                '--category={}'.format(TAG_ARGUMENTS["category"]),
+                '--description={}'.format(TAG_ARGUMENTS["description"]),
+                '--color={}'.format(TAG_ARGUMENTS["color"]), stdout = out)
+        except CommandError as err:
+            error = err.message
+        self.assertEqual(error, 'Tag "Tier 1 - Novel gene and phenotype" already exists for project Test Project')
