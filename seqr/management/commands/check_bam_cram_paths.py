2ad56cfc72f2e2692397a84b385ba5fe55d0b208
Shifa Zhang <zhangshifa07504@gmail.com>
2020-04-14 11:03:50 -0400

    Replaced print() with self.stdout.write() as suggested by Django guides.

--- seqr/management/commands/check_bam_cram_paths.py
+++ seqr/management/commands/check_bam_cram_paths.py
@@ -21,8 +21,8 @@ class Command(BaseCommand):
             except Exception as e:
                 individual_id = sample.individual.individual_id
                 failed.append(individual_id)
-                print('Error at {} (Individual: {}): {} '.format(sample.file_path, individual_id, e.message))
+                self.stdout.write('Error at {} (Individual: {}): {} '.format(sample.file_path, individual_id, e.message))
 
-        print('---- DONE ----')
-        print('Checked {} samples'.format(len(samples)))
-        print('{} failed samples: {}'.format(len(failed), ', '.join(failed)))
+        self.stdout.write('---- DONE ----')
+        self.stdout.write('Checked {} samples'.format(len(samples)))
+        self.stdout.write('{} failed samples: {}'.format(len(failed), ', '.join(failed)))
