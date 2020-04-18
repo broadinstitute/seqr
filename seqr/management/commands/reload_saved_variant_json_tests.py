2fb2c63c3e9e1d99b537cc149c3398ec67a59578
Shifa Zhang <zhangshifa07504@gmail.com>
2020-04-18 12:10:27 -0400

    Updated the mock classes/methods' name, used the mock_reset(), and changed tests for using a family id.

--- seqr/management/commands/reload_saved_variant_json_tests.py
+++ seqr/management/commands/reload_saved_variant_json_tests.py
@@ -17,19 +17,30 @@ class ReloadSavedVariantJsonTest(TestCase):
 
     @mock.patch('logging.getLogger')
     @mock.patch('seqr.views.utils.variant_utils.update_project_saved_variant_json')
-    def test_with_param_command(self, mock_update_json, mock_getLogger):
+    def test_with_param_command(self, mock_update_json, mock_get_logger):
         mock_update_json.return_value = SAVED_VARIANT_GUIDS
-        logger = mock_getLogger.return_value
+        mock_logger = mock_get_logger.return_value
 
-        # Test with specific projects and a family id.
+        # Test with a specific project and a family id.
         call_command('reload_saved_variant_json',
-                     PROJECT_NAME, PROJECT_GUID2,
-                     '--family-id=1'.format(FAMILY_ID))
+                     PROJECT_NAME,
+                     '--family-id={}'.format(FAMILY_ID))
 
-        projects_to_process = [PROJECT_NAME, PROJECT_GUID2]
+        projects_to_process = [PROJECT_NAME]
         projects = Project.objects.filter(Q(name__in=projects_to_process) | Q(guid__in = projects_to_process))
         calls = [mock.call(project, family_id=FAMILY_ID) for project in projects]
         mock_update_json.assert_has_calls(calls, any_order = True)
+
+        loggerInfoCalls = [
+            mock.call(u'Project: 1kg project n\xe5me with uni\xe7\xf8de'),
+            mock.call(u'Updated 2 variants for project 1kg project n\xe5me with uni\xe7\xf8de'),
+            mock.call('Done'),
+            mock.call('Summary: '),
+            mock.call(u'  1kg project n\xe5me with uni\xe7\xf8de: Updated 2 variants')
+        ]
+        mock_logger.info.assert_has_calls(loggerInfoCalls)
+        mock_update_json.reset_mock()
+        mock_logger.reset_mock()
 
         # Test for all projects and no specific family ids
         call_command('reload_saved_variant_json')
@@ -38,26 +49,7 @@ class ReloadSavedVariantJsonTest(TestCase):
         calls = [mock.call(project, family_id=None) for project in projects]
         mock_update_json.assert_has_calls(calls, any_order = True)
 
-        # Test with an exception.
-        mock_update_json.side_effect = Exception("Database error.")
-        call_command('reload_saved_variant_json',
-                     PROJECT_GUID,
-                     '--family-id=1'.format(FAMILY_ID))
-
-        projects_to_process = [PROJECT_GUID]
-        projects = Project.objects.filter(Q(name__in=projects_to_process) | Q(guid__in = projects_to_process))
-        calls = [mock.call(project, family_id=FAMILY_ID) for project in projects]
-        mock_update_json.assert_has_calls(calls, any_order = True)
-
         loggerInfoCalls = [
-            mock.call(u'Project: 1kg project n\xe5me with uni\xe7\xf8de'),
-            mock.call(u'Updated 2 variants for project 1kg project n\xe5me with uni\xe7\xf8de'),
-            mock.call(u'Project: Test Project'),
-            mock.call(u'Updated 2 variants for project Test Project'),
-            mock.call('Done'),
-            mock.call('Summary: '),
-            mock.call(u'  1kg project n\xe5me with uni\xe7\xf8de: Updated 2 variants'),
-            mock.call(u'  Test Project: Updated 2 variants'),
             mock.call(u'Project: 1kg project n\xe5me with uni\xe7\xf8de'),
             mock.call(u'Updated 2 variants for project 1kg project n\xe5me with uni\xe7\xf8de'),
             mock.call(u'Project: Empty Project'),
@@ -68,16 +60,33 @@ class ReloadSavedVariantJsonTest(TestCase):
             mock.call('Summary: '),
             mock.call(u'  1kg project n\xe5me with uni\xe7\xf8de: Updated 2 variants'),
             mock.call(u'  Empty Project: Updated 2 variants'),
-            mock.call(u'  Test Project: Updated 2 variants'),
+            mock.call(u'  Test Project: Updated 2 variants')
+        ]
+        mock_logger.info.assert_has_calls(loggerInfoCalls)
+        mock_update_json.reset_mock()
+        mock_logger.reset_mock()
+
+        # Test with an exception.
+        mock_update_json.side_effect = Exception("Database error.")
+        call_command('reload_saved_variant_json',
+                     PROJECT_GUID,
+                     '--family-id={}'.format(FAMILY_ID))
+
+        projects_to_process = [PROJECT_GUID]
+        projects = Project.objects.filter(Q(name__in=projects_to_process) | Q(guid__in = projects_to_process))
+        calls = [mock.call(project, family_id=FAMILY_ID) for project in projects]
+        mock_update_json.assert_has_calls(calls, any_order = True)
+
+        loggerInfoCalls = [
             mock.call(u'Project: 1kg project n\xe5me with uni\xe7\xf8de'),
             mock.call('Done'),
             mock.call('Summary: '),
             mock.call(u'1 failed projects'),
             mock.call(u'  1kg project n\xe5me with uni\xe7\xf8de: Database error.')
         ]
-        logger.info.assert_has_calls(loggerInfoCalls)
+        mock_logger.info.assert_has_calls(loggerInfoCalls)
 
         loggerErrorCalls = [
             mock.call(u'Error in project 1kg project n\xe5me with uni\xe7\xf8de: Database error.')
         ]
-        logger.error.assert_has_calls(loggerErrorCalls)
+        mock_logger.error.assert_has_calls(loggerErrorCalls)
