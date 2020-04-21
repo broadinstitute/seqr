50e249c876be5d4e201e108350702ebfb7e170e8
Simon Sadedin <ssadedin@gmail.com>
2020-04-21 23:05:14 +1000

    add command to copy tags from one project to another

---
+++ seqr/management/commands/copy_project_tags.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models.query_utils import Q
+
+from seqr.models import Project, VariantTagType
+from seqr.model_utils import create_seqr_model
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--source', help='Project to copy tags from', required=True)
+        parser.add_argument('--target', help='Project to copy tags to', required=True)
+
+    def handle(self, *args, **options):
+
+        source_project_name = options['source']
+        target_project_name = options['target']
+
+        source_project = Project.objects.get(Q(name=source_project_name) | Q(guid=source_project_name))
+
+        target_project = Project.objects.get(Q(name=target_project_name) | Q(guid=target_project_name))
+        
+        tags = VariantTagType.objects.filter(project=source_project)
+        
+        for tag in tags:
+            tag.pk = None
+            tag.id = None
+            tag.project = target_project
+            tag.save()
+            print 'Saved tag %s (new id = %d)' % (tag.name, tag.id)
+
