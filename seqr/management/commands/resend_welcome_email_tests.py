39e45cfc46a4458e752f4b13837038c95b371e71
Shifa Zhang <zhangshifa07504@gmail.com>
2020-04-27 14:16:43 -0400

    Removed the mock of Django model User.

--- seqr/management/commands/resend_welcome_email_tests.py
+++ seqr/management/commands/resend_welcome_email_tests.py
@@ -6,23 +6,22 @@ from django.test import TestCase
 
 from django.core.management.base import CommandError
 
+from django.contrib.auth.models import User
+
 TEST_USER_EMAIL = 'test_user@test.com'
 REFERRER_EMAIL = 'test_user_no_staff@test.com'
 
 
 class ResendWelcomeEmailTest(TestCase):
+    fixtures = ['users']
+
     @mock.patch('seqr.utils.communication_utils.send_welcome_email')
-    @mock.patch('django.contrib.auth.models.User')
-    def test_command(self, mock_user, mock_send_welcome_email):
-        mock_user.objects.get.side_effect = ['user', 'referrer']
+    def test_command(self, mock_send_welcome_email):
         call_command('resend_welcome_email', '--email-address={}'.format(TEST_USER_EMAIL),
                      '--referrer={}'.format(REFERRER_EMAIL))
-        calls = [
-            mock.call(email__iexact=TEST_USER_EMAIL),
-            mock.call(email__iexact = REFERRER_EMAIL),
-        ]
-        mock_user.objects.get.assert_has_calls(calls)
-        mock_send_welcome_email.assert_called_with('user', 'referrer')
+        user = User.objects.get(email__iexact=TEST_USER_EMAIL)
+        referrer = User.objects.get(email__iexact=REFERRER_EMAIL)
+        mock_send_welcome_email.assert_called_with(user, referrer)
 
     def test_command_exceptions(self):
         with self.assertRaises(CommandError) as ce:
