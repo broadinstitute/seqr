cba967e94d159d9d2dc6093eead57324284b05f1
Shifa Zhang <zhangshifa07504@gmail.com>
2020-05-04 11:05:30 -0400

    Updated the mocks of the handler classes.

--- reference_data/management/tests/update_all_reference_data_tests.py
+++ reference_data/management/tests/update_all_reference_data_tests.py
@@ -4,30 +4,19 @@ from django.core.management import call_command
 from django.core.management.base import CommandError
 from django.test import TestCase
 
-def mock_dbnsfp_gene_handler():
-    return "dbnsfp_gene"
 
-def mock_gene_constraint_handler():
-    return "gene_constraint"
+def omim_exception(omim_key):
+    raise Exception('Omim exception, key: '+omim_key)
 
-def mock_primate_ai_handler():
+
+def primate_ai_exception():
     raise Exception('Primate_AI failed')
 
-def mock_mgi_handler():
+
+def mgi_exception():
     raise Exception('MGI failed')
 
-MOCK_REFERENCE_DATA_SOURCES = [
-            ("dbnsfp_gene", mock_dbnsfp_gene_handler),
-            ("gene_constraint", mock_gene_constraint_handler),
-            ("primate_ai", mock_primate_ai_handler),
-            ("mgi", mock_mgi_handler),
-            ("hpo", None)
-        ]
 
-def raise_exception(msg):
-    raise Exception('Bad ' + msg)
-
-@mock.patch('reference_data.management.commands.update_all_reference_data.REFERENCE_DATA_SOURCES')
 @mock.patch('reference_data.management.commands.update_all_reference_data.logger')
 @mock.patch('reference_data.management.commands.update_all_reference_data.update_records')
 @mock.patch('reference_data.management.commands.update_all_reference_data.update_hpo')
@@ -36,17 +25,23 @@ def raise_exception(msg):
 class UpdateAllReferenceDataTest(TestCase):
     fixtures = ['users', 'reference_data']
 
-    def test_update_all_reference_data_command(self, mock_omim, mock_update_gencode, mock_update_hpo, mock_update_records, mock_logger, mock_sources):
+    @mock.patch('reference_data.management.commands.update_dbnsfp_gene.DbNSFPReferenceDataHandler')
+    @mock.patch('reference_data.management.commands.update_gene_constraint.GeneConstraintReferenceDataHandler')
+    @mock.patch('reference_data.management.commands.update_primate_ai.PrimateAIReferenceDataHandler')
+    @mock.patch('reference_data.management.commands.update_mgi.MGIReferenceDataHandler')
+    def test_update_all_reference_data_command(self, mock_mgi_handler, mock_primate_ai_handler, mock_gene_constraint_handler, mock_dbnsfp_gene_handler, mock_omim, mock_update_gencode, mock_update_hpo, mock_update_records, mock_logger):
 
-        # Test missing required argument
+        # Test missing required arguments
         with self.assertRaises(CommandError) as err:
             call_command('update_all_reference_data')
         self.assertEqual(err.exception.message, u'Error: one of the arguments --omim-key --skip-omim is required')
 
-        # Test update all gencode, no any skips
+        # Test update all gencode, no skips, fail primate_ai and mgi
         mock_omim.return_value = 'omim'
-        mock_sources.keys.return_value = ['dbnsfp_gene', 'gene_constraint', 'primate_ai', 'mgi', 'hpo']
-        mock_sources.items.return_value = MOCK_REFERENCE_DATA_SOURCES
+        mock_dbnsfp_gene_handler.return_value = "dbnsfp_gene"
+        mock_gene_constraint_handler.return_value = "gene_constraint"
+        mock_primate_ai_handler.side_effect = primate_ai_exception
+        mock_mgi_handler.side_effect = mgi_exception
         call_command('update_all_reference_data', '--omim-key=test_key')
 
         calls = [
@@ -83,34 +78,24 @@ class UpdateAllReferenceDataTest(TestCase):
         mock_logger.error.assert_has_calls(calls)
 
         # Test skipping all
-    def test_update_none_reference_data_command(self, mock_omim, mock_update_gencode, mock_update_hpo, mock_update_records, mock_logger, mock_sources):
-        mock_sources.keys.return_value = ['dbnsfp_gene', 'gene_constraint', 'primate_ai', 'mgi', 'hpo']
-        mock_sources.items.return_value = MOCK_REFERENCE_DATA_SOURCES
+    def test_update_none_reference_data_command(self, mock_omim, mock_update_gencode, mock_update_hpo, mock_update_records, mock_logger):
         call_command('update_all_reference_data', '--skip-gencode', '--skip-omim', '--skip-dbnsfp-gene', '--skip-gene-constraint', '--skip-primate-ai', '--skip-mgi', '--skip-hpo')
 
         mock_update_gencode.assert_not_called()
-
         mock_omim.assert_not_called()
-
         mock_update_records.assert_not_called()
-
         mock_update_hpo.assert_not_called()
-
         mock_logger.info.assert_called_with("Done")
 
         # Test omim exception
-    def test_update_exceptions(self, mock_omim, mock_update_gencode, mock_update_hpo, mock_update_records, mock_logger, mock_sources):
-        mock_omim.side_effect = raise_exception
-        mock_sources.keys.return_value = ['dbnsfp_gene', 'gene_constraint', 'primate_ai', 'mgi', 'hpo']
-        mock_sources.items.return_value = MOCK_REFERENCE_DATA_SOURCES
+    def test_update_exceptions(self, mock_omim, mock_update_gencode, mock_update_hpo, mock_update_records, mock_logger):
+
+        mock_omim.side_effect = omim_exception
         call_command('update_all_reference_data', '--skip-gencode', '--omim=test_key', '--skip-dbnsfp-gene', '--skip-gene-constraint', '--skip-primate-ai', '--skip-mgi', '--skip-hpo')
 
         mock_update_gencode.assert_not_called()
-
         mock_omim.assert_called_with('test_key')
-
         mock_update_records.assert_not_called()
-
         mock_update_hpo.assert_not_called()
 
         calls = [
@@ -119,4 +104,5 @@ class UpdateAllReferenceDataTest(TestCase):
         ]
         mock_logger.info.assert_has_calls(calls)
 
-        mock_logger.error.assert_called_with("unable to update omim: Bad test_key")
+        mock_logger.error.assert_called_with("unable to update omim: Omim exception, key: test_key")
+
