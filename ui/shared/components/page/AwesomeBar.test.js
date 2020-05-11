0d027740c78b52e1f3f957dcd3ee41736c4e172e
Hana Snow <hsnow@broadinstitute.org>
2020-05-11 11:50:43 -0400

    client side tests

--- ui/shared/components/page/AwesomeBar.test.js
+++ ui/shared/components/page/AwesomeBar.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import toJson from 'enzyme-to-json'
 import { shallow, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { AwesomeBarComponent } from './AwesomeBar'
+import { AwesomeBarComponent, AwesomeBarFormInput } from './AwesomeBar'
 
 configure({ adapter: new Adapter() })
 
@@ -10,4 +10,8 @@ test('shallow-render without crashing', () => {
   const wrapper = shallow(<AwesomeBarComponent />)
 
   expect(toJson(wrapper)).toMatchSnapshot()
+})
+
+test('shallow-render AwesomeBarFormInput  without crashing', () => {
+  shallow(<AwesomeBarFormInput />)
 })
