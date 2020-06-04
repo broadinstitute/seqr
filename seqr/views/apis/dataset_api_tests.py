a15e6fbadb14b4f0735815564fc9a4c82b3cee68
Shifa Zhang <zhangshifa07504@gmail.com>
2020-06-04 16:39:46 -0400

    Updated the convertion of dataset_api.

--- seqr/views/apis/dataset_api_tests.py
+++ seqr/views/apis/dataset_api_tests.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import json
 import mock
 import subprocess
@@ -194,12 +196,12 @@ class DatasetAPITest(AuthenticationTestCase):
         self.assertSetEqual(set(response_json.keys()), {'samplesByGuid', 'individualsByGuid', 'familiesByGuid'})
         sv_sample_guid = 'S1234567_NA19675_1'
         self.assertDictEqual(response_json['familiesByGuid'], {})
-        self.assertListEqual(response_json['samplesByGuid'].keys(), [sv_sample_guid])
+        self.assertListEqual(list(response_json['samplesByGuid'].keys()), [sv_sample_guid])
         self.assertEqual(response_json['samplesByGuid'][sv_sample_guid]['datasetType'], 'SV')
         self.assertTrue(response_json['samplesByGuid'][sv_sample_guid]['isActive'])
-        self.assertDictEqual(response_json['individualsByGuid'], {
-            'I000001_na19675': {'sampleGuids': [sv_sample_guid, existing_index_sample_guid]},
-        })
+        self.assertSetEqual(set(response_json['individualsByGuid']['I000001_na19675']['sampleGuids']),
+                            set([sv_sample_guid, existing_index_sample_guid]))
+
         # Regular variant sample should still be active
         sample_models = Sample.objects.filter(individual__guid='I000001_na19675')
         self.assertEqual(len(sample_models), 2)
@@ -222,17 +224,16 @@ class DatasetAPITest(AuthenticationTestCase):
         self.assertDictEqual(response.json(), {'errors': ['The following Individual IDs do not exist: NA19675']})
 
         # Send valid request
-        f = SimpleUploadedFile('samples.csv', "NA19675_1,/readviz\xe2/NA19675.cram\nNA19679,gs://readviz\xe3/NA19679.bam".encode('utf-8'))
+        f = SimpleUploadedFile('samples.csv', "NA19675_1,/readviz/NA19675.cram\nNA19679,gs://readviz/NA19679.bam".encode('utf-8'))
         response = self.client.post(url, data={'f': f})
         self.assertEqual(response.status_code, 200)
 
         self.assertDictEqual(response.json(), {
             'uploadedFileId': mock.ANY,
             'errors': [],
-            'info': ['Parsed 2 rows from samples.csv'],
+            'info': ['Parsed 2 rows from samples.csv', 'No change detected for 1 individuals'],
             'updatesByIndividualGuid': {
-                'I000001_na19675': '/readviz\xe2/NA19675.cram',
-                'I000003_na19679': 'gs://readviz\xe3/NA19679.bam',
+                'I000003_na19679': 'gs://readviz/NA19679.bam',
             },
         })
 
@@ -258,7 +259,7 @@ class DatasetAPITest(AuthenticationTestCase):
         self.assertListEqual(response.json()['info'], ['Parsed 2 rows from samples.xlsx'])
 
     @mock.patch('seqr.utils.file_utils.subprocess.Popen')
-    @mock.patch('seqr.utils.file_utils.does_google_bucket_file_exist')
+    @mock.patch('seqr.utils.file_utils.os.path.isfile')
     def test_add_alignment_sample(self, mock_local_file_exists, mock_subprocess):
         url = reverse(update_individual_igv_sample, args=['I000001_na19675'])
         self.check_manager_login(url)
@@ -309,11 +310,11 @@ class DatasetAPITest(AuthenticationTestCase):
 
         self.assertSetEqual(set(response_json.keys()), {'igvSamplesByGuid', 'individualsByGuid'})
         self.assertEqual(len(response_json['igvSamplesByGuid']), 1)
-        sample_guid = response_json['igvSamplesByGuid'].keys()[0]
+        sample_guid = next(iter(response_json['igvSamplesByGuid']))
         self.assertDictEqual(response_json['igvSamplesByGuid'][sample_guid], {
             'projectGuid': PROJECT_GUID, 'individualGuid': 'I000003_na19679', 'sampleGuid': sample_guid,
             'filePath': 'gs://readviz/NA19679.bam'})
-        self.assertListEqual(response_json['individualsByGuid'].keys(), ['I000003_na19679'])
+        self.assertListEqual(list(response_json['individualsByGuid'].keys()), ['I000003_na19679'])
         self.assertSetEqual(
             set(response_json['individualsByGuid']['I000003_na19679']['igvSampleGuids']),
             {sample_guid}
