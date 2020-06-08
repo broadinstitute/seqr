5d31124519adc09e5c84fbece8a14f745aebad19
Hana Snow <hsnow@broadinstitute.org>
2020-06-08 16:57:36 -0400

    update omim account deacitvated error handling

--- reference_data/management/commands/update_omim.py
+++ reference_data/management/commands/update_omim.py
@@ -88,7 +88,7 @@ class OmimReferenceDataHandler(ReferenceDataHandler):
                 break
             elif not line or line.startswith("#"):
                 continue
-            elif line.startswith('This account is inactive') or line.startswith('This account has expired'):
+            elif 'account is inactive' in line or 'account has expired' in line:
                 raise Exception(line)
             elif header_fields is None:
                 raise ValueError("Header row not found in genemap2 file before line {}: {}".format(i, line))
