5f0b4c4cb4c1b4fb17f3a727bda4699f8f08e48a
Shifa Zhang <zhangshifa07504@gmail.com>
2020-06-18 09:44:01 -0400

    Converting the seqr view utils.

--- seqr/views/utils/export_utils_2_3_tests.py
+++ seqr/views/utils/export_utils_2_3_tests.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from builtins import str
 
 from django.test import TestCase
 
 from openpyxl import load_workbook
-from StringIO import StringIO
+from io import BytesIO
 import mock
 
 from seqr.views.utils.export_utils import export_table, export_multiple_files
@@ -14,28 +16,29 @@ class ExportTableUtilsTest(TestCase):
 
     def test_export_table(self):
         header = ['column1', 'column2']
-        rows = [['row1_v1', 'row1_v2'], ['row2_v1', 'row2_v2']]
+        rows = [['row1_v1\xe2', 'row1_v2'], ['row2_v1', 'row2_v2']]
 
         # test tsv format
         response = export_table('test_file', header, rows, file_format='tsv')
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('content-disposition'), 'attachment; filename="test_file.tsv"')
-        self.assertEqual(response.content, '\n'.join(['\t'.join(row) for row in [header]+rows]) + '\n')
+        self.assertEqual(response.content.decode('utf-8', errors = 'ignore'),
+                         '\n'.join(['\t'.join(row) for row in [header]+rows]) + '\n')
 
         # test Excel format
         response = export_table('test_file', header, rows, file_format='xls')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('content-disposition'), 'attachment; filename="test_file.xlsx"')
-        wb = load_workbook(StringIO(response.content))
+        wb = load_workbook(BytesIO(response.content))
         worksheet = wb.active
 
-        self.assertListEqual([cell.value for cell in worksheet['A']], ['Column1', 'row1_v1', 'row2_v1'])
+        self.assertListEqual([cell.value for cell in worksheet['A']], ['Column1', 'row1_v1\xe2', 'row2_v1'])
         self.assertListEqual([cell.value for cell in worksheet['B']], ['Column2', 'row1_v2', 'row2_v2'])
         self.assertEqual([cell.value for cell in worksheet['C']], [None, None, None])
 
         # test unknown format
-        self.assertRaisesRegexp(ValueError, '.*format.*',
+        self.assertRaisesRegex(ValueError, '.*format.*',
             lambda: export_table('test_file', header, rows, file_format='unknown_format')
         )
 
@@ -43,7 +46,7 @@ class ExportTableUtilsTest(TestCase):
     def test_export_multiple_files(self, mock_zip):
         mock_zip_content = {}
         def mock_write_zip(file, content):
-            mock_zip_content[file] = unicode(content)
+            mock_zip_content[file] = content
         mock_zip.return_value.__enter__.return_value.writestr.side_effect = mock_write_zip
 
         header1 = ['col1', 'col2']
@@ -56,8 +59,8 @@ class ExportTableUtilsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('content-disposition'), 'attachment; filename="zipfile.zip"')
         self.assertDictEqual(mock_zip_content, {
-            'file1.csv': 'col1,col2\nrow1_v1,row1_v2\nrow2_v1,',
-            'file2.csv': 'col1\nrow1_v1\nrow2_v1',
+            'file1.csv': 'col1,col2\nrow1_v1\xe2,row1_v2\nrow2_v1,',
+            'file2.csv': 'col1\nrow1_v1\xe2\nrow2_v1',
         })
         mock_zip_content = {}
 
@@ -66,8 +69,8 @@ class ExportTableUtilsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('content-disposition'), 'attachment; filename="zipfile.zip"')
         self.assertDictEqual(mock_zip_content, {
-            'file1.tsv': 'col1\tcol2\nrow1_v1\trow1_v2\nrow2_v1\t',
-            'file2.tsv': 'col1\nrow1_v1\nrow2_v1',
+            'file1.tsv': 'col1\tcol2\nrow1_v1\xe2\trow1_v2\nrow2_v1\t',
+            'file2.tsv': 'col1\nrow1_v1\xe2\nrow2_v1',
         })
         mock_zip_content = {}
 
@@ -76,12 +79,12 @@ class ExportTableUtilsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get('content-disposition'), 'attachment; filename="zipfile.zip"')
         self.assertDictEqual(mock_zip_content, {
-            'file1.csv': 'col1,01-col2\nrow1_v1,row1_v2\nrow2_v1,X',
-            'file2.csv': 'col2,01-col3,02-col1\nrow1_v2,X,row1_v1\nX,X,row2_v1',
+            'file1.csv': 'col1,01-col2\nrow1_v1\xe2,row1_v2\nrow2_v1,X',
+            'file2.csv': 'col2,01-col3,02-col1\nrow1_v2,X,row1_v1\xe2\nX,X,row2_v1',
         })
         mock_zip_content = {}
 
         # test unknown format
         with self.assertRaises(ValueError) as cm:
             export_multiple_files([['file1', header1, rows], ['file2', header2, rows]], 'zipfile', file_format='foo')
-        self.assertEqual(cm.exception.message, 'Invalid file_format: foo')
+        self.assertEqual(str(cm.exception), 'Invalid file_format: foo')
