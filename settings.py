927035a0b9f7de4955de161ae493cc74aad1288f
Shifa Zhang <zhangshifa07504@gmail.com>
2020-07-29 09:20:15 -0400

    Updated Site ID.

--- settings.py
+++ settings.py
@@ -190,7 +190,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SITE_ID = 0
+SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
