5846d9525fd41d75598d6d57780418f4c0e7c8f4
bw2 <weisburd@broadinistitute.org>
2020-09-08 16:29:46 -0400

    check elasticsearch connection from inside the seqr container. elasticsearch is no longer visible on the host

--- test_local_deployment.sh
+++ test_local_deployment.sh
@@ -6,7 +6,7 @@ docker-compose up -d seqr
 docker-compose logs postgres
 docker-compose logs elasticsearch
 docker-compose logs redis
-curl localhost:9200
+docker-compose exec seqr curl elasticsearch:9200
 sleep 30
 docker-compose logs seqr
 echo -ne 'testpassword\n' docker-compose exec seqr python manage.py createsuperuser --username test --email test@test.com
