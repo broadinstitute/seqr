046c91e1a74e346f18abfb48926a54a0f1be5b68
Shifa Zhang <zhangshifa07504@gmail.com>
2020-10-08 14:16:54 -0400

    Removed the Google login button.

--- ui/pages/Login/components/Login.jsx
+++ ui/pages/Login/components/Login.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Divider } from 'semantic-ui-react'
-import GoogleButton from 'react-google-button'
 
 import { validators } from 'shared/components/form/ReduxFormWrapper'
 import { login } from '../reducers'
@@ -24,8 +23,7 @@ const Login = ({ onSubmit }) =>
   >
     <Link to="/users/forgot_password">Forgot Password?</Link>
     <Divider />
-    <GoogleButton href="/google_login" />
-    <a href="/google_login">Google Login</a>
+    <a href="/google_login">Sign in with Google</a>
   </UserFormLayout>
 
 Login.propTypes = {
