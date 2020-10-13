5738a00bfab387404f4f593683e0383016e296bf
Hana Snow <hana.snow@gmail.com>
2020-10-13 16:32:43 -0400

    debug travis

--- test_local_deployment.sh
+++ test_local_deployment.sh
@@ -2,21 +2,22 @@
 
 set -ex
 
-cp docker-compose.yml ..
-cd ..
-mkdir ./data
-chmod +rw ./data
+#cp docker-compose.yml ..
+#cd ..
+#mkdir ./data
+#chmod +rw ./data
+echo "$USER"
 
 docker-compose up -d elasticsearch
-docker-compose exec elasticsearch chmod +rw ./data
+docker-compose exec elasticsearch echo "$USER"
 sleep 30
 docker-compose logs elasticsearch
 
-docker-compose up -d seqr
-docker-compose logs postgres
-docker-compose logs elasticsearch
-docker-compose logs redis
-docker-compose exec seqr curl elasticsearch:9200
-sleep 30
-docker-compose logs seqr
-echo -ne 'testpassword\n' docker-compose exec seqr python manage.py createsuperuser --username test --email test@test.com
+#docker-compose up -d seqr
+#docker-compose logs postgres
+#docker-compose logs elasticsearch
+#docker-compose logs redis
+#docker-compose exec seqr curl elasticsearch:9200
+#sleep 30
+#docker-compose logs seqr
+#echo -ne 'testpassword\n' docker-compose exec seqr python manage.py createsuperuser --username test --email test@test.com
