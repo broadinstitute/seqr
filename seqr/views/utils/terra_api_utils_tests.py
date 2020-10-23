3ba89e36e7204895d8e7381e0afa86f7d85ebfc7
Shifa Zhang <zhangshifa07504@gmail.com>
2020-10-23 13:00:03 -0400

    Moved Google Auth URL constances to test utils.

--- seqr/views/utils/terra_api_utils_tests.py
+++ seqr/views/utils/terra_api_utils_tests.py
@@ -6,11 +6,7 @@ from django.contrib.auth.models import User
 
 from seqr.views.utils.terra_api_utils import get_anvil_billing_projects, get_anvil_profile, list_anvil_workspaces, sa_get_workspace_acl
 from settings import TERRA_API_ROOT_URL
-
-GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token'
-AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/auth'
-ACCESS_TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
-REVOKE_TOKEN_URL = 'https://accounts.google.com/o/oauth2/revoke'
+from seqr.views.utils.test_utils import GOOGLE_API_TOKEN_URL
 
 
 @mock.patch('seqr.views.utils.terra_api_utils.time')
@@ -24,7 +20,7 @@ class TerraApiUtilsCase(TestCase):
     @responses.activate
     def test_get_billing_projects(self, mock_time):
         content = b'{"access_token":"ya29.c.EXAMPLE","expires_in":3599,"token_type":"Bearer"}'
-        responses.add(responses.POST, GOOGLE_TOKEN_URL, status = 200, body = content)
+        responses.add(responses.POST, GOOGLE_API_TOKEN_URL, status = 200, body = content)
         mock_time.time.return_value = self.social.extra_data['auth_time'] + 10
 
         url = TERRA_API_ROOT_URL + 'api/profile/billing'
@@ -43,7 +39,7 @@ class TerraApiUtilsCase(TestCase):
     @responses.activate
     def test_get_anvil_profile(self, mock_time):
         content = b'{"access_token":"ya29.c.EXAMPLE","expires_in":3599,"token_type":"Bearer"}'
-        responses.add(responses.POST, GOOGLE_TOKEN_URL, status = 200, body = content)
+        responses.add(responses.POST, GOOGLE_API_TOKEN_URL, status = 200, body = content)
         mock_time.time.return_value = self.social.extra_data['auth_time'] + 10
 
         url = TERRA_API_ROOT_URL + 'register'
@@ -62,7 +58,7 @@ class TerraApiUtilsCase(TestCase):
     @responses.activate
     def test_list_workspaces(self, mock_time):
         content = b'{"access_token":"ya29.c.EXAMPLE","expires_in":3599,"token_type":"Bearer"}'
-        responses.add(responses.POST, GOOGLE_TOKEN_URL, status = 200, body = content)
+        responses.add(responses.POST, GOOGLE_API_TOKEN_URL, status = 200, body = content)
         mock_time.time.return_value = self.social.extra_data['auth_time'] + 10
 
         url = TERRA_API_ROOT_URL + 'api/workspaces'
@@ -75,7 +71,7 @@ class TerraApiUtilsCase(TestCase):
         self.assertDictEqual(workspaces[0], {"accessLevel": "PROJECT_OWNER", "public": False, "workspace": {"attributes": {"description": "Workspace for seqr project"}, "authorizationDomain": [], "bucketName": "fc-237998e6-663d-40b9-bd13-57c3bb6ac593", "createdBy": "test1@test.com", "createdDate": "2020-09-09T15:10:32.816Z", "isLocked": False, "lastModified": "2020-09-09T15:10:32.818Z", "name": "1000 Genomes Demo", "namespace": "my-seqr-billing", "workflowCollectionName": "237998e6-663d-40b9-bd13-57c3bb6ac593", "workspaceId": "237998e6-663d-40b9-bd13-57c3bb6ac593" }, "workspaceSubmissionStats": {"runningSubmissionsCount": 0}})
 
         responses.reset()
-        responses.add(responses.POST, GOOGLE_TOKEN_URL, status = 200, body = content)
+        responses.add(responses.POST, GOOGLE_API_TOKEN_URL, status = 200, body = content)
         responses.add(responses.GET, url+'?fields=accessLevel,workspace.name,workspace.namespace,workspace.workspaceId', status = 200,
             body = '[{"accessLevel": "PROJECT_OWNER", "workspace": {"name": "1000 Genomes Demo", "namespace": "my-seqr-billing", "workspaceId": "237998e6-663d-40b9-bd13-57c3bb6ac593" }},'
                    '{"accessLevel": "READER","workspace": {"name": "degenome","namespace": "degenome", "workspaceId": "2706d493-5fce-4fb2-9993-457c30364a06"}},'
@@ -93,7 +89,7 @@ class TerraApiUtilsCase(TestCase):
     @responses.activate
     def test_get_workspace_acl(self, mock_time):
         content = b'{"access_token":"ya29.c.EXAMPLE","expires_in":3599,"token_type":"Bearer"}'
-        responses.add(responses.POST, GOOGLE_TOKEN_URL, status = 200, body = content)
+        responses.add(responses.POST, GOOGLE_API_TOKEN_URL, status = 200, body = content)
 
         url = TERRA_API_ROOT_URL + 'api/workspaces/my-seqr-billing/my-seqr-workspace/acl'
         responses.add(responses.GET, url, status = 200, body = '{"acl": {"test1@test1.com": {"accessLevel": "OWNER","canCompute": true,"canShare": true,"pending": false},"sf-seqr@my-seqr.iam.gserviceaccount.com": {"accessLevel": "OWNER","canCompute": true,"canShare": true,"pending": false},"test2@test2.org": {"accessLevel": "OWNER","canCompute": true,"canShare": true,"pending": false},"test3@test3.com": {"accessLevel": "READER","canCompute": false,"canShare": false,"pending": false}}}')
