413e850969446ddee7c5c0aff9272b71ae23881a
Hana Snow <hana.snow@gmail.com>
2020-10-26 17:32:39 -0400

    fix codacy

--- seqr/models.py
+++ seqr/models.py
@@ -102,7 +102,6 @@ class ModelWithGUID(models.Model):
 
     def delete_model(self, user, user_can_delete=False):
         """Helper delete method that logs the deletion"""
-
         db_entity = type(self).__name__
         entity_id = self.guid
         if not (user_can_delete or user.is_staff or self.created_by == user):
@@ -115,7 +114,6 @@ class ModelWithGUID(models.Model):
     @classmethod
     def bulk_create(cls, user, new_models):
         """Helper bulk create method that logs the creation"""
-
         for model in new_models:
             model.created_by = user
         models = cls.objects.bulk_create(new_models)
@@ -129,7 +127,6 @@ class ModelWithGUID(models.Model):
     @classmethod
     def bulk_update(cls, user, update_json, queryset=None, **filter_kwargs):
         """Helper bulk update method that logs the update"""
-
         if queryset is None:
             queryset = cls.objects.filter(**filter_kwargs)
         entity_ids = [o.guid for o in queryset]
@@ -146,7 +143,6 @@ class ModelWithGUID(models.Model):
     @classmethod
     def bulk_delete(cls, user, queryset=None, **filter_kwargs):
         """Helper bulk delete method that logs the deletion"""
-
         if queryset is None:
             queryset = cls.objects.filter(**filter_kwargs)
         entity_ids = [o.guid for o in queryset]
@@ -155,7 +151,6 @@ class ModelWithGUID(models.Model):
         logger.info('delete {} {}s'.format(len(entity_ids), db_entity), extra={'user': user, 'db_update': {
             'dbEntity': db_entity, 'entityIds': entity_ids, 'updateType': 'bulk_delete',
         }})
-
 
 
 class Project(ModelWithGUID):
