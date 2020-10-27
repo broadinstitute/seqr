8f568453a77a72650c73a53c9b06e3b7fd3d1ccc
Hana Snow <hana.snow@gmail.com>
2020-10-27 11:39:24 -0400

    travis matrix

--- install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-pip install -r requirements.txt --upgrade
-pip install -r requirements-dev.txt --upgrade
-cd ui/
-echo -ne "\n \n \3033[1B\nsemantic/\n" | npm install
-cd ..
