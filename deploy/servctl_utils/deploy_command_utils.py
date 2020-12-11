9a04217b3dc79d494489dad30fbed9b25f32b118
Shifa Zhang <zhangshifa07504@gmail.com>
2020-12-11 09:26:20 -0500

    Add Google client ID to the secrect.

--- deploy/servctl_utils/deploy_command_utils.py
+++ deploy/servctl_utils/deploy_command_utils.py
@@ -74,7 +74,8 @@ SECRETS = {
     'matchbox': ['{deploy_to}/config.json'],
     'nginx': ['{deploy_to}/tls.key', '{deploy_to}/tls.crt'],
     'seqr': [
-        'omim_key', 'postmark_server_token', 'slack_token', 'airtable_key', 'django_key', 'seqr_es_password', 'google_client_secret'
+        'omim_key', 'postmark_server_token', 'slack_token', 'airtable_key', 'django_key', 'seqr_es_password',
+        'google_client_id',  'google_client_secret'
     ],
 }
 
