33d3735a1bde4ffc4c04f5e7c584bc8bb7c90c53
bw2 <weisburd@broadinistitute.org>
2021-03-02 15:04:40 -0500

    attempt to fix  hadoop_is_file mock

--- seqr/management/tests/check_bam_cram_paths_tests.py
+++ seqr/management/tests/check_bam_cram_paths_tests.py
@@ -5,7 +5,12 @@ from django.test import TestCase
 
 from seqr.models import IgvSample
 
-@mock.patch('hail.hadoop_is_file', lambda path: False)
+
+def hadoop_is_file_mock(path: str) -> bool:
+    return False
+
+
+@mock.patch('hail.hadoop_is_file', hadoop_is_file_mock)
 class CheckBamCramPathsTest(TestCase):
     fixtures = ['users', '1kg_project']
 
