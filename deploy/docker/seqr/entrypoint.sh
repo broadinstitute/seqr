63a0d5cc529373f72e283f8daf374895a40c967f
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-06 22:51:05 +1000

    Always install GSA_KEY

--- deploy/docker/seqr/entrypoint.sh
+++ deploy/docker/seqr/entrypoint.sh
@@ -8,11 +8,9 @@ echo SHELL: $SHELL
 echo PATH: $PATH
 echo PYTHONPATH: $PYTHONPATH
 
-if [ $GSA_KEY ]; then
-    echo $GSA_KEY > /tmp/gsa-key.json
-    gcloud auth activate-service-account --key-file /tmp/gsa-key.json
-    rm /tmp/gsa-key.json
-fi
+echo $GSA_KEY > /tmp/gsa-key.json
+gcloud auth activate-service-account --key-file /tmp/gsa-key.json
+rm /tmp/gsa-key.json
 
 # link to persistent disk dir with static files
 mkdir -p /seqr_static_files/generated_files
