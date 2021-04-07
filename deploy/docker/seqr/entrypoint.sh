2a782634a67cd34f5875ef62b6b45add84a7e5c6
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-07 21:49:55 +1000

    Restore working directory

--- deploy/docker/seqr/entrypoint.sh
+++ deploy/docker/seqr/entrypoint.sh
@@ -13,13 +13,14 @@ rm /tmp/gsa-key.json
 # link to persistent disk dir with static files
 mkdir -p /seqr_static_files/generated_files
 
+cd /seqr
+
 # init seqrdb unless it already exists
 if ! psql --host $POSTGRES_SERVICE_HOSTNAME -U postgres -l | grep seqrdb; then
   psql --host $POSTGRES_SERVICE_HOSTNAME -U postgres -c 'CREATE DATABASE reference_data_db';
   psql --host $POSTGRES_SERVICE_HOSTNAME -U postgres reference_data_db <  <(curl -s $REFERENCE_DATA_DB_INIT_URL | gunzip -c -);
 
   psql --host $POSTGRES_SERVICE_HOSTNAME -U postgres -c 'CREATE DATABASE seqrdb';
-  cd /seqr
   python -u manage.py makemigrations
   python -u manage.py migrate
   python -u manage.py check
