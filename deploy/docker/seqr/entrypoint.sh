1618cd25d15e61e307e1d831156c0deeef3231a8
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-04-13 14:47:39 +1000

    Update deploy yaml to build UI in GH actions

--- deploy/docker/seqr/entrypoint.sh
+++ deploy/docker/seqr/entrypoint.sh
@@ -7,5 +7,4 @@ rm /tmp/gsa-key.json
 # link to persistent disk dir with static files
 mkdir -p /seqr_static_files/generated_files
 
-cd /seqr
 gunicorn -w $GUNICORN_WORKER_THREADS -c deploy/docker/seqr/config/gunicorn_config.py wsgi:application
