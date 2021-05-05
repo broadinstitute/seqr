6dc8f6082eadd0d471cfc2460d071ff0b21a9cc8
bw2 <weisburd@broadinistitute.org>
2021-05-05 14:31:12 -0400

    PR feedback

--- seqr/management/commands/check_bam_cram_paths.py
+++ seqr/management/commands/check_bam_cram_paths.py
@@ -55,14 +55,10 @@ class Command(BaseCommand):
                 logger.info('   {} in {}'.format(c, project_name))
 
             # post to slack
-            safe_post_to_slack(
-                SEQR_SLACK_DATA_ALERTS_NOTIFICATION_CHANNEL,
-                'Found {} broken bam/cram path(s)'.format(sum(missing_counter.values())),
-            )
-            slack_message = ""
+            slack_message = 'Found {} broken bam/cram path(s)\n'.format(sum(missing_counter.values()))
             for project_name, missing_paths_list in project_name_to_missing_paths.items():
-                slack_message += "In project {}\n".format(project_name)
+                slack_message += "\nIn project {}:\n".format(project_name)
                 slack_message += "\n".join([
                     "  {}   {}".format(individual_id, path) for individual_id, path in missing_paths_list
                 ])
-                safe_post_to_slack(SEQR_SLACK_DATA_ALERTS_NOTIFICATION_CHANNEL, slack_message)
+            safe_post_to_slack(SEQR_SLACK_DATA_ALERTS_NOTIFICATION_CHANNEL, slack_message)
