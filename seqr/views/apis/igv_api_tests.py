f2d0a518e2d2b2eac9307f81a8e3e0751c529589
Hana Snow <hana.snow@gmail.com>
2021-05-19 16:25:33 -0400

    updae unit test

--- seqr/views/apis/igv_api_tests.py
+++ seqr/views/apis/igv_api_tests.py
@@ -169,13 +169,26 @@ class IgvAPITest(AuthenticationTestCase):
 
     @responses.activate
     def test_igv_genomes_proxyy(self):
-        url = reverse(igv_genomes_proxy)
+        url_path = 'org.genomes/foo?guery=true'
+        url = reverse(igv_genomes_proxy, args=[url_path])
 
         expected_body = {'genes': ['GENE1', 'GENE2']}
         responses.add(
-            responses.GET, 'https://s3.amazonaws.com/igv.org.genomes/genomes.json', status=200,
+            responses.GET, 'https://s3.amazonaws.com/igv.org.genomes/foo?guery=true', match_querystring=True,
             content_type='application/json', body=json.dumps(expected_body))
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertDictEqual(json.loads(response.content), expected_body)
+        self.assertIsNone(responses.calls[0].request.headers.get('Range'))
+
+        # test with range header proxy
+        expected_content = 'test file content'
+        responses.replace(
+            responses.GET, 'https://s3.amazonaws.com/igv.org.genomes/foo?guery=true', match_querystring=True,
+            body=expected_content)
+
+        response = self.client.get(url, HTTP_RANGE='bytes=100-200')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), expected_content)
+        self.assertEqual(responses.calls[1].request.headers.get('Range'), 'bytes=100-200')
