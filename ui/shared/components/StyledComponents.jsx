009d4105ac238ed3aa02ea232a5fc4104bf8cb1d
Hana Snow <hana.snow@gmail.com>
2021-07-12 17:17:35 -0400

    update ped js settings

--- ui/shared/components/StyledComponents.jsx
+++ ui/shared/components/StyledComponents.jsx
@@ -96,3 +96,115 @@ export const SectionHeader = styled.div`
   font-weight: 300;
   font-size: 18px; 
 `
+
+
+// Map font-awesome icons to semantic-ui icons
+export const FontAwesomeIconsContainer = styled.div`
+  i.fa {
+    display: inline-block;
+    opacity: 1;
+    margin: 0em 0.25rem 0em 0em;
+    width: 1.18em;
+    height: 1em;
+    font-family: 'Icons';
+    font-style: normal;
+    font-weight: normal;
+    text-decoration: inherit;
+    text-align: center;
+    speak: none;
+    font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+  
+  i.fa:before {
+    background: none !important;
+  }
+  
+  i.fa-fw {
+    width: 1.2857142857142858em;
+    text-align: center;
+  }
+  
+  i.fa-lg {
+    line-height: 1;
+    vertical-align: middle;
+    font-size: 1.5em;
+  }
+  
+  i.fa-2x {
+    font-size: 2em;
+  }
+  
+  i.fa-spin  {
+    height: 1em;
+    line-height: 1;
+    -webkit-animation: icon-loading 2s linear infinite;
+    animation: icon-loading 2s linear infinite;
+  }
+  
+  i.fa-check:before {
+    content: "\\f00c";
+  }
+  i.fa-square:before {
+    content: "\\f098";
+  } 
+  i.fa-search:before {
+    content: "\\f002";
+  }
+  i.fa-minus-circle:before {
+    content: "\\f056";
+  } 
+  i.fa-plus-circle:before  {
+    content: "\\f055";
+  }
+  i.fa-times:before  {
+    content: "\\f00d";
+  }  
+  i.fa-times-circle:before  {
+    content: "\\f057";
+  } 
+  i.fa-gear:before  {
+    content: "\\f013";
+  }
+  i.fa-exclamation-triangle:before  {
+    content: "\\f071";
+  }  
+  i.fa-spinner:before  {
+    content: "\\f110";
+  }
+  i.fa-undo:before  {
+    content: "TODO";
+  }  
+  i.fa-repeat:before  {
+    content: "TODO";
+  } 
+  i.fa-refresh:before  {
+    content: "TODO";
+  } 
+  i.fa-arrows-alt:before  {
+    content: "TODO";
+  } 
+  i.fa-circle:before  {
+    content: "TODO";
+  } 
+  i.fa-crosshairs:before  {
+    content: "TODO";
+  } 
+  i.fa-unspecified:before  {
+    content: "TODO";
+  } 
+  i.fa-angle-up:before  {
+    content: "TODO";
+  }  
+  i.fa-caret-up:before  {
+    content: "TODO";
+  }
+
+  .igv-zoom-widget i {
+    line-height: 24px;
+  }
+  
+`
--- ui/shared/components/panel/view-pedigree-image/PedigreeImagePanel.jsx
+++ ui/shared/components/panel/view-pedigree-image/PedigreeImagePanel.jsx
@@ -3,13 +3,20 @@ import PropTypes from 'prop-types'
 import { Icon, Segment, Table } from 'semantic-ui-react'
 import styled from 'styled-components'
 import { build as buildPedigeeJs } from 'pedigreejs/es/pedigree'
+import { scale_to_fit as scalePedigreeToFit } from 'pedigreejs/es/zoom'
 
 import Modal from '../../modal/Modal'
-import { NoBorderTable } from '../../StyledComponents'
+import { NoBorderTable, FontAwesomeIconsContainer } from '../../StyledComponents'
 import { EditPedigreeImageButton, DeletePedigreeImageButton } from './PedigreeImageButtons'
 
+const PED_IMAGE_SIZES = {
+  small: { height: 35, width: 50 },
+  medium: { height: 150, width: 250 },
+  large: { height: 250, width: 350 },
+}
+
 const UploadedPedigreeImage = styled.img.attrs({ alt: 'pedigree' })`
-  max-height: ${props => props.height}px;
+  max-height: ${props => PED_IMAGE_SIZES[props.size].height}px;
   max-width: 225px;
   vertical-align: top;
   cursor: ${props => (props.disablePedigreeZoom ? 'auto' : 'zoom-in')};
@@ -21,42 +28,47 @@ class PedigreeJs extends React.PureComponent {
 
   static propTypes = {
     family: PropTypes.object,
+    size: PropTypes.string,
+    disablePedigreeZoom: PropTypes.bool,
+    isEditable: PropTypes.bool,
   }
 
   constructor(props) {
     super(props)
-    this.containerId = `pedigreeJS-${props.family.familyGuid}`
+    this.containerId = `pedigreeJS-${props.family.familyGuid}-${props.size}`
   }
 
   render() {
-    return <div id={this.containerId} />
+    // TODO fix display in family label hover (i.e. saved variant page)
+    return (
+      <FontAwesomeIconsContainer>
+        <div id={`${this.containerId}-buttons`} />
+        <div id={this.containerId} />
+      </FontAwesomeIconsContainer>)
   }
 
   componentDidMount() {
+    const { size, disablePedigreeZoom, isEditable } = this.props
     const dataset = [
       { name: 'm21', sex: 'M', top_level: true },
       { name: 'f21', sex: 'F', top_level: true },
-      { name: 'ch1', sex: 'F', mother: 'f21', father: 'm21', breast_cancer: true, proband: true },
+      { name: 'ch1', sex: 'F', mother: 'f21', father: 'm21', affected: true },
     ]
     const opts = {
       dataset,
       targetDiv: this.containerId,
-      height: this.props.height,
-      width: this.props.height * 1.5,
-      // zoomOut: 3,
-      symbol_size: 30,
-      // 'btn_target': 'pedigree_history',
-      // 'width': 450,
-      // 'height': 320,
-      // 'symbol_size': 35,
-      // 'store_type': 'array',
-      // 'diseases': [],
-      // labels: ['age', 'yob'],
-      // font_size: '.75em',
-      // font_family: 'Helvetica',
-      // font_weight: 700,
+      btn_target: `${this.containerId}-buttons`,
+      edit: !!(disablePedigreeZoom && isEditable),
+      background: '#fff',
+      diseases: [{ type: 'affected', colour: '#111' }],
+      labels: ['name'],
+      zoomIn: 5,
+      zoomOut: 5,
+      store_type: 'array', // TODO remove?
+      ...PED_IMAGE_SIZES[size],
     }
-    buildPedigeeJs(opts)
+    const builtOpts = buildPedigeeJs(opts)
+    scalePedigreeToFit(builtOpts)
   }
 }
 
@@ -74,7 +86,7 @@ const PedigreeImagePanel = React.memo(({ family, isEditable, compact, disablePed
   const image = <PedigreeImage
     family={family}
     disablePedigreeZoom={disablePedigreeZoom}
-    height={compact ? 35 : 150}
+    size={compact ? 'small' : 'medium'}
   />
   if (disablePedigreeZoom) {
     return image
@@ -93,7 +105,7 @@ const PedigreeImagePanel = React.memo(({ family, isEditable, compact, disablePed
       }
     >
       <Segment basic textAlign="center">
-        <PedigreeImage family={family} disablePedigreeZoom height={250} /><br />
+        <PedigreeImage family={family} disablePedigreeZoom isEditable={isEditable} size="large" /><br />
       </Segment>
       <NoBorderTable basic="very" compact="very" collapsing>
         <Table.Body>
