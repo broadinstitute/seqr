296765b4767aa378edce24a5fe7cd822b68a9473
Hana Snow <hana.snow@gmail.com>
2021-08-02 18:48:49 -0400

    pr feedback

--- ui/shared/components/form/Inputs.jsx
+++ ui/shared/components/form/Inputs.jsx
@@ -239,7 +239,7 @@ export class SearchInput extends React.PureComponent {
   }
 }
 
-const YEAR_OPTIONS = [...[...Array(130).keys()].map(i => ({ value: i + 1900 }))]
+const YEAR_OPTIONS = [...Array(130).keys()].map(i => ({ value: i + 1900 }))
 const YEAR_OPTIONS_UNKNOWN = [{ value: 0, text: 'Unknown' }, ...YEAR_OPTIONS]
 const YEAR_OPTIONS_ALIVE = [{ value: -1, text: 'Alive' }, ...YEAR_OPTIONS_UNKNOWN]
 const yearOptions = (includeAlive, includeUnknown) => {
