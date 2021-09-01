95f920f5ef43634438e7553e61a0d6de4dcf7888
Hana Snow <hana.snow@gmail.com>
2021-09-01 16:52:48 -0400

    fix unit test

--- ui/redux/selectors.test.js
+++ ui/redux/selectors.test.js
@@ -13,11 +13,10 @@ test('getVariantTagNotesByByFamilyVariants', () => {
     STATE_WITH_2_FAMILIES,
   )
   expect(Object.keys(tagsNotesByGuid).length).toEqual(2)
-  expect(Object.keys(tagsNotesByGuid.F011652_1).length).toEqual(3)
+  expect(Object.keys(tagsNotesByGuid.F011652_1).length).toEqual(2)
   expect(tagsNotesByGuid.F011652_1['22-45919065-TTTC-T'].notes.length).toEqual(1)
   expect(tagsNotesByGuid.F011652_1['22-45919065-TTTC-T'].tags.length).toEqual(1)
   expect(tagsNotesByGuid.F011652_1['1-248367227-TC-T'].tags.length).toEqual(2)
-  expect(tagsNotesByGuid.F011652_1['batch_123_DEL'].tags).toEqual(undefined)
   expect(Object.keys(tagsNotesByGuid.F011652_2).length).toEqual(3)
   expect(tagsNotesByGuid.F011652_2['22-248367227-C-T']).toEqual({ variantGuids: 'SV0000003_2246859832_r0390_100'})
   expect(tagsNotesByGuid.F011652_2['22-248367228-C-T']).toEqual({ variantGuids: 'SV0000005_2246859833_r0390_100'})
