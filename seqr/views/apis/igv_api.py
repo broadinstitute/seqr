546b4936847e2f27392c16fdafd4f7ca455cbb09
Shifa Zhang <zhangshifa07504@gmail.com>
2021-09-07 10:35:30 -0400

    Updated the Redis key.

--- seqr/views/apis/igv_api.py
+++ seqr/views/apis/igv_api.py
@@ -17,7 +17,7 @@ from seqr.views.utils.permissions_utils import get_project_and_check_permissions
 from seqr.utils.redis_utils import safe_redis_get_json, safe_redis_set_json
 
 EXPIRATION_TIME_IN_SECONDS = 3600 - 5
-ACCESS_TOKEN_CACHE_KEY = 'GOOGLE_ACCESS_TOKEN_REDIS_CACHE_KEY'
+ACCESS_TOKEN_CACHE_KEY = 'GOOGLE_ACCOUNT_REDIS_CACHE_KEY'
 
 
 @pm_or_data_manager_required
