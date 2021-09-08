80049f62278eb9a86fcdc25cb96e3661fe2fce18
sjahl <sjahl@broadinstitute.org>
2021-09-08 14:49:21 -0400

    adds a new dockerfile and beginnings of new image build structure

--- deploy/docker/seqr/entrypoint.sh
+++ deploy/docker/seqr/entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+# XXX: this is a security issue
 env
 
 echo SHELL: $SHELL
@@ -9,6 +10,7 @@ echo PATH: $PATH
 echo PYTHONPATH: $PYTHONPATH
 
 # init gcloud
+# Revisit: can/should we switch seqr to using workload identity instead of manually associating the pod?
 if [ "$GCLOUD_PROJECT" ]; then
     gcloud config set project "$GCLOUD_PROJECT"
 fi
@@ -30,19 +32,24 @@ mkdir -p /seqr_static_files/generated_files
 # launch django dev server in background
 cd /seqr
 
+# XXX: we want the image to be based on the version of the code rather than dynamically checking it out
 if [ "$SEQR_GIT_BRANCH" ]; then
   git pull
   git checkout "$SEQR_GIT_BRANCH"
 fi
 
+# XXX: versioned docker images should preclude the need for this
 pip install --upgrade -r requirements.txt  # doublecheck that requirements are up-to-date
 
 # allow pg_dump and other postgres command-line tools to run without having to enter a password
+# Revisit: see if we can either take advantage of workload identity with the cloudsql proxy, or use env vars
 echo "*:*:*:*:$POSTGRES_PASSWORD" > ~/.pgpass
 chmod 600 ~/.pgpass
 cat ~/.pgpass
 
 # init seqrdb unless it already exists
+# Revisit: Helm/Argo give us deployment hooks that could handle migrations, but these will all
+# need to be idempotent actions
 if ! psql --host "$POSTGRES_SERVICE_HOSTNAME" -U postgres -l | grep seqrdb; then
 
   psql --host "$POSTGRES_SERVICE_HOSTNAME" -U postgres -c 'CREATE DATABASE reference_data_db';
@@ -60,6 +67,7 @@ fi
 # launch django server in background
 /usr/local/bin/start_server.sh
 
+# XXX: we should use k8s native facilities for running periodic jobs, rather than running cron in the image
 if [ "$RUN_CRON_JOBS" ]; then
     # set up cron jobs
     echo 'SHELL=/bin/bash
@@ -79,4 +87,7 @@ fi
 
 
 # sleep to keep image running even if gunicorn is killed / restarted
+# XXX: we should allow k8s to restart the pod if the process crashes. A health-check should help us avoid killing the 
+# pod in the case of short restarts of the app
+# Revisit: understand why we want to do this.
 sleep 1000000000000
