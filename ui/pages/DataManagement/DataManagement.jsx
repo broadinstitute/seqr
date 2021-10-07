c04fb8a993d1273327a4b3be3b3007e9e4a54879
Hana Snow <hana.snow@gmail.com>
2021-10-07 14:11:03 -0400

    fix a11y rules

--- ui/pages/DataManagement/DataManagement.jsx
+++ ui/pages/DataManagement/DataManagement.jsx
@@ -14,7 +14,10 @@ const IFRAME_STYLE = { position: 'fixed', left: '0', top: '95px' }
 
 export const DATA_MANAGEMENT_PAGES = [
   { path: 'elasticsearch_status', component: ElasticsearchStatus },
-  { path: 'kibana', component: () => <iframe width="100%" height="100%" style={IFRAME_STYLE} src="/app/kibana" /> },
+  {
+    path: 'kibana',
+    component: () => <iframe width="100%" height="100%" title="Kibana" style={IFRAME_STYLE} src="/app/kibana" />,
+  },
   { path: 'sample_qc', component: SampleQc },
   { path: 'users', component: Users },
 ]
