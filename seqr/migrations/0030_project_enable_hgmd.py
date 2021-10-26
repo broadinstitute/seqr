482728aea5118fb044e0f18dc828aafbb74ec02f
Hana Snow <hana.snow@gmail.com>
2021-10-26 14:34:56 -0400

    fix migration

--- seqr/migrations/0030_project_enable_hgmd.py
+++ seqr/migrations/0030_project_enable_hgmd.py
@@ -10,9 +10,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='project',
-            name='enable_hgmd',
-            field=models.BooleanField(default=False),
-        ),
+        # migrations.AddField(
+        #     model_name='project',
+        #     name='enable_hgmd',
+        #     field=models.BooleanField(default=False),
+        # ),
     ]
