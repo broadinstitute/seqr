060328623843a6654d7db1441930f95866f769f6
Hana Snow <hana.snow@gmail.com>
2021-10-26 17:22:42 -0400

    disable source maps

--- ui/config/webpack.config.prod.js
+++ ui/config/webpack.config.prod.js
@@ -59,9 +59,6 @@ module.exports = {
   mode: 'production',
   // Don't attempt to continue if there are any errors.
   bail: true,
-  // We generate sourcemaps in production. This is slow but gives good results.
-  // You can exclude the *.map files from the build during deployment.
-  devtool: 'source-map',
 
   entry: {
     app: [
