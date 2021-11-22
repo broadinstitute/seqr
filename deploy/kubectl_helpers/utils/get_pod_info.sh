c6f0aa7af87f6a60bb3cea4fd0b499ddd00534a2
Hana Snow <hana.snow@gmail.com>
2021-11-22 17:31:49 -0500

    working prototype seqr deployment

--- deploy/kubectl_helpers/utils/get_pod_info.sh
+++ deploy/kubectl_helpers/utils/get_pod_info.sh
@@ -6,5 +6,14 @@ DEPLOYMENT_TARGET=$1
 COMPONENT=$2
 JSON_PATH=$3
 
+case ${DEPLOYMENT_TARGET} in
+  dev)
+    DEPLOYMENT_TARGET=gcloud-${DEPLOYMENT_TARGET}
+    ;;
+  prod)
+    DEPLOYMENT_TARGET=gcloud-${DEPLOYMENT_TARGET}
+    ;;
+esac
 
-kubectl get pod -l "name=${COMPONENT},deployment=gcloud-${DEPLOYMENT_TARGET}" -o "jsonpath=${JSON_PATH}"
+
+kubectl get pod -l "name=${COMPONENT},deployment=${DEPLOYMENT_TARGET}" -o "jsonpath=${JSON_PATH}"
