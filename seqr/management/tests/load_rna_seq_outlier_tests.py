ed92812140f75872e1c34b50939eda12797d2bdd
Hana Snow <hana.snow@gmail.com>
2022-02-01 13:41:04 -0500

    rename existing command

--- seqr/management/tests/load_rna_seq_outlier_tests.py
+++ seqr/management/tests/load_rna_seq_outlier_tests.py
@@ -14,8 +14,8 @@ class LoadRnaSeqTest(TestCase):
     fixtures = ['users', '1kg_project', 'reference_data']
 
     @mock.patch('seqr.views.apis.data_manager_api.ANALYST_PROJECT_CATEGORY', 'analyst-projects')
-    @mock.patch('seqr.management.commands.load_rna_seq.logger.info')
-    @mock.patch('seqr.management.commands.load_rna_seq.open')
+    @mock.patch('seqr.management.commands.load_rna_seq_outlier.logger.info')
+    @mock.patch('seqr.management.commands.load_rna_seq_outlier.open')
     @mock.patch('seqr.views.apis.data_manager_api.gzip.open')
     def test_command(self, mock_gzip_open, mock_open, mock_logger):
         mock_gzip_file = mock_gzip_open.return_value.__enter__.return_value
@@ -30,14 +30,14 @@ class LoadRnaSeqTest(TestCase):
         mock_open.return_value.__enter__.return_value.__iter__.return_value = ['NA19675_D4\tNA19678']
 
         with self.assertRaises(ValueError) as e:
-            call_command('load_rna_seq', RNA_FILE_ID)
+            call_command('load_rna_seq_outlier', RNA_FILE_ID)
         self.assertEqual(str(e.exception), 'Unable to find matches for the following samples: NA19675_D3, NA19675_D4')
 
         with self.assertRaises(ValueError) as e:
-            call_command('load_rna_seq', RNA_FILE_ID, '--mapping-file', 'map.tsv')
+            call_command('load_rna_seq_outlier', RNA_FILE_ID, '--mapping-file', 'map.tsv')
         self.assertEqual(str(e.exception), 'Unable to find matches for the following samples: NA19675_D3')
 
-        call_command('load_rna_seq', RNA_FILE_ID, '--ignore-extra-samples')
+        call_command('load_rna_seq_outlier', RNA_FILE_ID, '--ignore-extra-samples')
 
         rna_samples = Sample.objects.filter(individual_id=1, sample_id='NA19675_D2', sample_type='RNA')
         self.assertEqual(len(rna_samples), 1)
