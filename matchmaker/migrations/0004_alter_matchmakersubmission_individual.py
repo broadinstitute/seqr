777342a3c5bede5877ad7bfc632cbde3002a68d7
Hana Snow <hana.snow@gmail.com>
2022-03-15 16:02:24 -0400

    fix migration dependency

--- matchmaker/migrations/0004_alter_matchmakersubmission_individual.py
+++ matchmaker/migrations/0004_alter_matchmakersubmission_individual.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('seqr', '0039_project_is_demo'),
         ('matchmaker', '0003_auto_20201123_2111'),
     ]
 
