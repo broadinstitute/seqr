332e46ae2fcf81218a373b13093e2c6ef4dc26b1
Hana Snow <hana.snow@gmail.com>
2022-03-30 14:08:26 -0400

    add analysis status to case review

--- ui/pages/Project/components/CaseReview.jsx
+++ ui/pages/Project/components/CaseReview.jsx
@@ -10,6 +10,7 @@ import {
   FAMILY_FIELD_INTERNAL_SUMMARY,
   FAMILY_FIELD_CODED_PHENOTYPE,
   FAMILY_FIELD_ASSIGNED_ANALYST,
+  FAMILY_FIELD_ANALYSIS_STATUS,
 } from 'shared/utils/constants'
 import { CASE_REVIEW_TABLE_NAME } from '../constants'
 import { getCaseReviewStatusCounts } from '../selectors'
@@ -17,6 +18,7 @@ import FamilyTable from './FamilyTable/FamilyTable'
 
 const FIELDS = [
   { id: FAMILY_FIELD_DESCRIPTION },
+  { id: FAMILY_FIELD_ANALYSIS_STATUS },
   { id: FAMILY_FIELD_ASSIGNED_ANALYST },
   { id: FAMILY_FIELD_CASE_NOTES },
   { id: FAMILY_FIELD_ANALYSIS_NOTES },
