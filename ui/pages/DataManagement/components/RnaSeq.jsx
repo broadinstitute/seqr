cf6455ee661a86c3aec8406303701c6ccd177cf4
Hana Snow <hana.snow@gmail.com>
2022-05-06 13:31:08 -0400

    add rna type to loading ui

--- ui/pages/DataManagement/components/RnaSeq.jsx
+++ ui/pages/DataManagement/components/RnaSeq.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 
 import { validators } from 'shared/components/form/FormHelpers'
 import FileUploadField from 'shared/components/form/XHRUploaderField'
-import { BooleanCheckbox } from 'shared/components/form/Inputs'
+import { BooleanCheckbox, Select } from 'shared/components/form/Inputs'
 import UploadFormPage from 'shared/components/page/UploadFormPage'
 
 import { getRnaSeqUploadStats } from '../selectors'
@@ -17,14 +17,20 @@ const mapStateToProps = state => ({
       validate: validators.required,
     },
     {
-      name: 'mappingFile',
-      component: FileUploadField,
-      dropzoneLabel: 'Drag-drop or click here to upload an optional file that maps Sample Ids (column 1) to their corresponding Seqr Individual Ids (column 2)',
+      name: 'dataType',
+      label: 'Data Type',
+      component: Select,
+      options: ['Outlier', 'TPM'].map(text => ({ text, value: text.toLowerCase() })),
     },
     {
       name: 'ignoreExtraSamples',
       component: BooleanCheckbox,
       label: 'Ignore extra samples',
+    },
+    {
+      name: 'mappingFile',
+      component: FileUploadField,
+      dropzoneLabel: 'Drag-drop or click here to upload an optional file that maps Sample Ids (column 1) to their corresponding Seqr Individual Ids (column 2)',
     },
   ],
   uploadStats: getRnaSeqUploadStats(state),
