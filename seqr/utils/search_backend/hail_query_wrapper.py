20cbd5a43946347880df64d803b3164a59cbf73a
Hana Snow <hana.snow@gmail.com>
2022-06-08 15:57:24 -0400

    hail syntax

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -690,7 +690,7 @@ class GcnvHailTableQuery(BaseHailTableQuery):
         # TODO override interval/ genes for sample-specific SV size
         'pos': lambda r: r.interval.start.position,
         'end': lambda r: r.interval.end.position,
-        'rg37LocusEnd': lambda r: r.rg37_locus_end,
+        'rg37LocusEnd': lambda r: hl.struct(contig=r.rg37_locus_end.contig, position=r.rg37_locus_end.position),
         'svType': lambda r: r.svType.replace('^gCNV_', ''),
     }
     BASE_ANNOTATION_FIELDS.update(BaseHailTableQuery.BASE_ANNOTATION_FIELDS)
