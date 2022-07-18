6d1d766c360ad79e0f2a37d1ee49181dd189ef35
Hana Snow <hana.snow@gmail.com>
2022-07-18 18:00:58 -0400

    fix typo

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -148,7 +148,7 @@ class BaseHailTableQuery(object):
                 reference_genome=self._genome_version, invalid_missing=True)
             ) for interval in intervals]
             invalid_intervals = [raw_intervals[i] for interval, i in enumerate(intervals) if interval is None]
-            if invalid_interval_indices:
+            if invalid_intervals:
                 raise InvalidSearchException(f'Invalid intervals: {", ".join(invalid_intervals)}')
         return intervals
 
