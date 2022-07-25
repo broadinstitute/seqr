1d85ef49fe1e3a9a1c0e066667b34a3981de1a33
Hana Snow <hana.snow@gmail.com>
2022-07-25 14:22:13 -0400

    migrate servctl to python 3

--- deploy/servctl_utils/yaml_settings_utils.py
+++ deploy/servctl_utils/yaml_settings_utils.py
@@ -45,7 +45,7 @@ def load_settings(settings_file_paths, settings=None):
         yaml_string = _parse_jinja_template(settings_path, template_variables=settings)
 
         try:
-            settings_from_this_file = yaml.load(yaml_string) # nosec
+            settings_from_this_file = yaml.load(yaml_string, Loader=yaml.Loader) # nosec
         except yaml.parser.ParserError:
             raise ValueError('Unable to parse yaml file %(settings_path)s: %(e)s' % locals())
 
