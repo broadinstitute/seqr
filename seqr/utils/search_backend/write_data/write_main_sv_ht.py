f51512542d8f21ac2146e00f2f5cf20a8fd44065
Hana Snow <hana.snow@gmail.com>
2022-08-01 12:03:42 -0400

    write main sv ht

---
+++ seqr/utils/search_backend/write_data/write_main_sv_ht.py
@@ -0,0 +1,58 @@
+import argparse
+import hail as hl
+
+CHROMOSOMES = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21',
+    '22', 'X', 'Y', 'M',
+]
+CHROM_NUMBER_TO_CHROM = hl.literal({i: chrom for i, chrom in enumerate(CHROMOSOMES)})
+
+SEQR_FIELDS = [
+    'filters', 'rg37_locus', 'rg37_locus_end', 'sortedTranscriptConsequences', 'svType', 'variantId', 'xpos',
+]
+RENAME_FIELDS = {
+    'bothsides_support': 'bothsidesSupport', 'cpx_intervals': 'cpxIntervals', 'sv_type_detail': 'svTypeDetail',
+}
+CALLSET_FIELDS = {'AF': 'sf', 'AC': 'sc', 'AN': 'sn', 'Het': 'sv_callset_Het', 'Hom': 'sv_callset_Hom'}
+
+
+def write_main_ht(file):
+    ht = hl.read_table(f'gs://hail-backend-datasets/{file}.ht')
+
+    ht = ht.annotate(
+        end_chrom=CHROM_NUMBER_TO_CHROM[hl.int(ht.xstop / 1e9) - 1],
+        end_pos=hl.int(ht.xstop % int(1e9)),
+    )
+    ht = ht.annotate(has_end2=(ht.contig != ht.end_chrom) | (ht.end != ht.end_pos))
+
+    variant_annotations = {
+        'algorithms': hl.str(',').join(ht.algorithms),
+        'gnomad_svs': hl.struct(AF=ht.gnomad_svs_AF, ID=ht.gnomad_svs_ID),
+        'interval': hl.interval(
+            hl.locus(hl.format('chr%s', ht.contig), ht.start, reference_genome='GRCh38'),
+            hl.if_else(
+                ht.has_end2 & (ht.svType != 'INS') & (
+                    # This is to handle a bug in the SV pipeline, should not go to production
+                    (ht.svType != 'CPX') | (hl.is_valid_locus(hl.format('chr%s', ht.end_chrom), ht.end_pos, 'GRCh38'))
+                ),
+                hl.locus(hl.format('chr%s', ht.end_chrom), ht.end_pos, reference_genome='GRCh38'),
+                hl.locus(hl.format('chr%s', ht.contig), ht.end, reference_genome='GRCh38')
+            ),
+        ),
+        'strvctvre': hl.struct(score=ht.StrVCTVRE_score),
+        'sv_callset': hl.struct(**{key: ht[field] for key, field in CALLSET_FIELDS.items()}),
+        'svSourceDetail': hl.or_missing(ht.has_end2 & (ht.svType == 'INS'), hl.struct(chrom=ht.end_chrom))
+    }
+    ht = ht.annotate(**variant_annotations)
+    ht = ht.rename(RENAME_FIELDS)
+    ht = ht.select_globals().select(*variant_annotations.keys(), *RENAME_FIELDS.values(), *SEQR_FIELDS)
+
+    ht.write(f'gs://hail-backend-datasets/{file}_parsed.ht')
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument('file')
+    args = p.parse_args()
+
+    write_main_ht(args.file)
---
+++ seqr/utils/search_backend/write_data/write_project_sv_sample_hts.py
@@ -0,0 +1,24 @@
+import argparse
+import hail as hl
+
+from seqr.utils.search_backend.write_data.write_project_samples_utils import write_project_samples_hts
+
+
+def _read_table(file, subset_ht=None):
+    mt = hl.read_matrix_table(f'gs://hail-backend-datasets/{file}.mt').select_globals().select_rows()
+    mt = mt.semi_join_cols(subset_ht)
+    return mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
+
+
+def _get_sample_table(mt, sample_id):
+    sample_ht = mt.filter_cols(mt.s == sample_id).key_cols_by().entries()
+    return sample_ht.select('GQ', 'GT', 'CN')
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument('file')
+    p.add_argument('project')
+    args = p.parse_args()
+
+    write_project_samples_hts(args.file, args.project, _read_table, _get_sample_table)
