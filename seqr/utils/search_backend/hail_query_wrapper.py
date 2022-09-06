c66a50153c11475d833cb82c37dd2d7f722691b4
Hana Snow <hana.snow@gmail.com>
2022-09-06 14:50:27 -0400

    use struct for poulations

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -77,7 +77,7 @@ class BaseHailTableQuery(object):
     def annotation_fields(self):
         annotation_fields = {
             'populations': lambda r: hl.dict({
-                population: hl.dict({
+                population: hl.struct(**{
                     response_key: hl.or_else(r[population][field], '' if response_key == 'id' else 0)
                     for response_key, field in pop_config.items() if field is not None
                 }) for population, pop_config in self.populations_configs.items()
