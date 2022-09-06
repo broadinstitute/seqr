f0acdf74b0900675536779fa909a41c06c9d95ca
Hana Snow <hana.snow@gmail.com>
2022-09-06 15:49:09 -0400

    fix data source handling

--- seqr/utils/search_backend/hail_search.py
+++ seqr/utils/search_backend/hail_search.py
@@ -55,7 +55,7 @@ class HailSearch(object):
 
         if data_type == Sample.DATASET_TYPE_SV_CALLS:
             data_type = None
-            data_sources_by_type = {k: v for k, v in data_sources_by_type.items() if f.startswith(Sample.DATASET_TYPE_SV_CALLS)}
+            data_sources_by_type = {k: v for k, v in data_sources_by_type.items() if k.startswith(Sample.DATASET_TYPE_SV_CALLS)}
 
         if not data_type and len(data_sources_by_type) == 1:
             data_type = list(data_sources_by_type.keys())[0]
