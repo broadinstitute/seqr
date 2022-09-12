58e371f56b93e616aa58a5c5cd687b786343a34f
Shifa Zhang <zhangshifa07504@gmail.com>
2022-09-12 14:23:55 -0400

    Update per review comments.

--- seqr/utils/vcf_utils.py
+++ seqr/utils/vcf_utils.py
@@ -9,41 +9,29 @@ BLOCK_SIZE = 65536
 
 EXPECTED_META_FIELDS ={
     'INFO': {
-        'AC': {
-            'Number': 'A',
-            'Type': 'Integer',
-        },
-        'AN': {
-            'Number': '1',
-            'Type': 'Integer',
-        },
-        'AF': {
-            'Number': 'A',
-            'Type': 'Float',
-        }
+        'AC': {'Number': 'A', 'Type': 'Integer'},
+        'AN': {'Number': '1', 'Type': 'Integer'},
+        'AF': {'Number': 'A', 'Type': 'Float'}
     },
     'FORMAT': {
-        'GQ': {
-            'Number': '1',
-            'Type': 'Integer',
-        },
-        'GT': {
-            'Number': '1',
-            'Type': 'String',
-        }
+        'AD': {'Number': '.', 'Type': 'Integer'},
+        'DP': {'Number': '1', 'Type': 'Integer'},
+        'GQ': {'Number': '1', 'Type': 'Integer'},
+        'GT': {'Number': '1', 'Type': 'String'}
     }
 }
 
 REQUIRED_HEADERS = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT']
 
 
-def _validate_vcf(header, meta, first_line):
-
+def _validate_vcf_header(header):
     missing_headers = [h for h in REQUIRED_HEADERS if h not in header ]
     if missing_headers:
         missing_fields = ','.join(missing_headers)
         raise ErrorsWarningsException([f'Required VCF header fields {missing_fields} are missing.'], [])
 
+
+def _validate_vcf_meta(meta):
     for field, sub_field_meta in EXPECTED_META_FIELDS.items():
         for sub_field, metadata in sub_field_meta.items():
             for key, value in metadata.items():
@@ -51,14 +39,18 @@ def _validate_vcf(header, meta, first_line):
                     error = f'VCF header field {field}.{sub_field} and meta information {key}={value} is expected.'
                     raise ErrorsWarningsException([error], [])
 
+
+def _validate_vcf_data_row(first_line):
+    # Has required INFO fields?
     row_data = first_line.split('\t')
-    info_keys = [k.split('=')[0] for k in row_data[7].split(';')]
-    missing_info_fields = [f for f in EXPECTED_META_FIELDS['INFO'].keys() if f not in info_keys ]
+    info_keys = [k.split('=')[0] for k in row_data[REQUIRED_HEADERS.index('INFO')].split(';')]
+    missing_info_fields = [f for f in EXPECTED_META_FIELDS['INFO'].keys() if f not in info_keys]
     if missing_info_fields:
         missing_fields = ','.join(missing_info_fields)
         raise ErrorsWarningsException([f'Missing INFO field(s) {missing_fields} in the row data'], [])
 
-    format_keys = [k for k in row_data[8].split(':')]
+    # Has required FORMAT fields?
+    format_keys = [k for k in row_data[REQUIRED_HEADERS.index('FORMAT')].split(':')]
     missing_format_fields = [f for f in EXPECTED_META_FIELDS['FORMAT'].keys() if f not in format_keys]
     if missing_format_fields:
         missing_fields = ','.join(missing_format_fields)
@@ -85,13 +77,15 @@ def validate_vcf_and_get_samples(vcf_filename):
     meta = defaultdict(dict)
     for line in file_iter(vcf_filename, byte_range=byte_range):
         if line[0] != '#':
-            _validate_vcf(header[0:9], meta, line)
+            _validate_vcf_header(header[0:len(REQUIRED_HEADERS)])
+            _validate_vcf_meta(meta)
+            _validate_vcf_data_row(line)
             break
         field, sub_field_meta = _get_vcf_meta_data(line)
         if field:
             meta[field].update(sub_field_meta)
         if line.startswith('#CHROM'):
             header = line.rstrip().split('\t')
-            samples = set(header[9:])
+            samples = set(header[len(REQUIRED_HEADERS):])
 
     return samples
