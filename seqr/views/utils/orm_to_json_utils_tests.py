dfc88e4b13d1286c7a5b99ccf11c66d77621d93e
Hana Snow <hana.snow@gmail.com>
2022-09-14 13:56:50 -0400

    codacy fixes

--- seqr/views/utils/orm_to_json_utils_tests.py
+++ seqr/views/utils/orm_to_json_utils_tests.py
@@ -7,7 +7,7 @@ from seqr.models import Project, Family, Individual, Sample, IgvSample, SavedVar
 from seqr.views.utils.orm_to_json_utils import get_json_for_user, _get_json_for_project, _get_json_for_family, \
     _get_json_for_individual, get_json_for_sample, get_json_for_saved_variant, get_json_for_variant_tags, \
     get_json_for_variant_functional_data_tags, get_json_for_variant_note, get_json_for_locus_list, \
- get_json_for_saved_search, get_json_for_saved_variants_with_tags, get_json_for_current_user
+    get_json_for_saved_search, get_json_for_saved_variants_with_tags, get_json_for_current_user
 from seqr.views.utils.test_utils import PROJECT_FIELDS, FAMILY_FIELDS, INTERNAL_FAMILY_FIELDS, \
     INDIVIDUAL_FIELDS, INTERNAL_INDIVIDUAL_FIELDS, INDIVIDUAL_FIELDS_NO_FEATURES, SAMPLE_FIELDS, SAVED_VARIANT_FIELDS,  \
     FUNCTIONAL_FIELDS, SAVED_SEARCH_FIELDS, LOCUS_LIST_DETAIL_FIELDS, PA_LOCUS_LIST_FIELDS, IGV_SAMPLE_FIELDS, \
