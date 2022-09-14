5a952a4485fbbb38ed46665ed34704fa3d27b480
Shifa Zhang <zhangshifa07504@gmail.com>
2022-09-14 10:12:23 -0400

    Update the header parsing order and add tests.

--- seqr/utils/vcf_utils.py
+++ seqr/utils/vcf_utils.py
@@ -27,8 +27,8 @@ REQUIRED_HEADERS = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO
 def _validate_vcf_header(header):
     missing_headers = [h for h in REQUIRED_HEADERS if h not in header ]
     if missing_headers:
-        missing_fields = ','.join(missing_headers)
-        raise ErrorsWarningsException([f'Required VCF header fields {missing_fields} are missing.'], [])
+        missing_fields = ', '.join(missing_headers)
+        raise ErrorsWarningsException([f'Missing required VCF header field(s) {missing_fields}.'], [])
 
 
 def _validate_vcf_meta(meta):
@@ -49,19 +49,20 @@ def _get_vcf_meta_data(line):
 
 def validate_vcf_and_get_samples(vcf_filename):
     byte_range = None if vcf_filename.endswith('.vcf') else (0, BLOCK_SIZE)
-    samples = {}
+    samples = set()
     header = []
     meta = defaultdict(dict)
     for line in file_iter(vcf_filename, byte_range=byte_range):
-        if line[0] != '#':
-            return {}
-        field, sub_field_type = _get_vcf_meta_data(line)
-        if field:
-            meta[field].update(sub_field_type)
-        if line.startswith('#CHROM'):
-            header = line.rstrip().split('\t')
-            samples = set(header[len(REQUIRED_HEADERS):])
-            break;
+        if line.startswith('#'):
+            if line.startswith('#CHROM'):
+                header = line.rstrip().split('\t')
+                samples = set(header[len(REQUIRED_HEADERS):])
+            else:
+                field, sub_field_type = _get_vcf_meta_data(line)
+                if field:
+                    meta[field].update(sub_field_type)
+        else:
+            break
 
     _validate_vcf_header(header[0:len(REQUIRED_HEADERS)])
     _validate_vcf_meta(meta)
