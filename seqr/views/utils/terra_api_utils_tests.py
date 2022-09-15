ed92c77dd162f0b8fd21718be6a88cae2cde24c6
Hana Snow <hana.snow@gmail.com>
2022-09-15 14:47:35 -0400

    test get user groups

--- seqr/views/utils/terra_api_utils_tests.py
+++ seqr/views/utils/terra_api_utils_tests.py
@@ -11,7 +11,7 @@ from seqr.views.utils.test_utils import TEST_TERRA_API_ROOT_URL, GOOGLE_TOKEN_RE
 from seqr.views.utils.terra_api_utils import list_anvil_workspaces, user_get_workspace_acl,\
     anvil_call, user_get_workspace_access_level, TerraNotFoundException, TerraAPIException, \
     TerraRefreshTokenFailedException,  is_anvil_authenticated, is_google_authenticated, remove_token, \
-    add_service_account, has_service_account_access, get_anvil_group_members
+    add_service_account, has_service_account_access, get_anvil_group_members, user_get_anvil_groups
 
 GET_WORKSPACE_PATH = 'api/workspaces?fields=public,workspace.name,workspace.namespace'
 AUTH_EXTRA_DATA = {"expires": 3599, "auth_time": TOKEN_AUTH_TIME, "token_type": "Bearer", "access_token": "ya29.EXAMPLE"}
@@ -333,4 +333,40 @@ class TerraApiUtilsCallsCase(TestCase):
         self.assertListEqual(members, cached_members)
         mock_logger.info.assert_called_with('Terra API cache hit for: GET api/groups/TGG_USERS test_user', user)
         mock_redis.return_value.get.assert_called_with('terra_req__test_user__api/groups/TGG_USERS')
+        responses.assert_call_count(url, 0)  # no call to the Terra API
+
+    @responses.activate
+    @mock.patch('seqr.utils.redis_utils.redis.StrictRedis')
+    @mock.patch('seqr.views.utils.terra_api_utils.logger')
+    def test_user_get_anvil_groups(self, mock_logger, mock_redis):
+        user = User.objects.get(username='test_user')
+
+        url = f'{TEST_TERRA_API_ROOT_URL}api/groups'
+        mock_redis.return_value.get.return_value = None
+        responses.add(responses.GET, url, status=200, body=json.dumps([
+            {'groupEmail': 'TGG_Users@firecloud.org', 'groupName': 'TGG_Users', 'role': 'Admin'},
+            {'groupEmail': 'External_Users@firecloud.org', 'groupName': 'External_Users', 'role': 'Member'},
+        ]))
+        groups = user_get_anvil_groups(user)
+        self.assertListEqual(groups, ['TGG_Users', 'External_Users'])
+        mock_logger.info.assert_called_with('GET https://terra.api/api/groups 200 183', user)
+        self.assertEqual(len(mock_logger.method_calls), 1)
+        responses.assert_call_count(url, 1)
+        mock_redis.return_value.set.assert_called_with('terra_req__test_user__api/groups', json.dumps(groups))
+        mock_redis.return_value.expire.assert_called_with('terra_req__test_user__api/groups', 300)
+
+        mock_redis.return_value.get.return_value = None
+        responses.add(responses.GET, url, status=401)
+        with self.assertRaises(TerraAPIException) as ec:
+            user_get_anvil_groups(user)
+        self.assertEqual(str(ec.exception),
+            'Error: called Terra API: GET /api/groups got status: 401 with a reason: Unauthorized')
+
+        responses.reset()
+        cached_groups = ['foo', 'bar', 'baz']
+        mock_redis.return_value.get.return_value = json.dumps(cached_groups)
+        groups = user_get_anvil_groups(user)
+        self.assertListEqual(groups, cached_groups)
+        mock_logger.info.assert_called_with('Terra API cache hit for: GET api/groups test_user', user)
+        mock_redis.return_value.get.assert_called_with('terra_req__test_user__api/groups')
         responses.assert_call_count(url, 0)  # no call to the Terra API
