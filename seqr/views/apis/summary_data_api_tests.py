94d8b520901b39b5ebb85881cda0dc1ccd2c6f98
Hana Snow <hana.snow@gmail.com>
2022-10-04 15:07:56 -0400

    fix tests

--- seqr/views/apis/summary_data_api_tests.py
+++ seqr/views/apis/summary_data_api_tests.py
@@ -25,6 +25,7 @@ EXPECTED_MME_DETAILS_METRICS = {
 SAVED_VARIANT_RESPONSE_KEYS = {
     'projectsByGuid', 'locusListsByGuid', 'savedVariantsByGuid', 'variantFunctionalDataByGuid', 'genesById',
     'variantNotesByGuid', 'individualsByGuid', 'variantTagsByGuid', 'familiesByGuid', 'familyNotesByGuid',
+    'mmeSubmissionsByGuid',
 }
 
 
