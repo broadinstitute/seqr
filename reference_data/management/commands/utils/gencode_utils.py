13dcd8a319241413b0ff92913dfdad41159df744
Hana Snow <hana.snow@gmail.com>
2022-11-02 16:47:20 -0400

    fix codacy complexity error

--- reference_data/management/commands/utils/gencode_utils.py
+++ reference_data/management/commands/utils/gencode_utils.py
@@ -96,6 +96,39 @@ def _parse_line(line, i, new_genes, new_transcripts,  existing_gene_ids, existin
         return
 
     # parse info field
+    _parse_record(record)
+
+    if len(record["chrom"]) > 2:
+        return  # skip super-contigs
+
+    if record['feature_type'] == 'gene':
+        if record["gene_id"] in existing_gene_ids:
+            counters["genes_skipped"] += 1
+            return
+
+        new_genes[record['gene_id']].update(_parse_gene_record(record, genome_version, gencode_release))
+    elif record['feature_type'] == 'transcript':
+        if record["transcript_id"] in existing_transcript_ids:
+            counters["transcripts_skipped"] += 1
+            return
+
+        new_transcripts[record['transcript_id']].update(_parse_transcript_record(record, genome_version))
+    elif record['feature_type'] == 'CDS':
+        if record["transcript_id"] in existing_transcript_ids:
+            return
+
+        coding_region_size_field_name = "coding_region_size_grch{}".format(genome_version)
+        # add + 1 because GTF has 1-based coords. (https://useast.ensembl.org/info/website/upload/gff.html)
+        transcript_size = record["end"] - record["start"] + 1
+        transcript_size += new_transcripts[record['transcript_id']].get(coding_region_size_field_name, 0)
+        new_transcripts[record['transcript_id']][coding_region_size_field_name] = transcript_size
+
+        if record['gene_id'] not in existing_gene_ids and \
+                transcript_size > new_genes[record['gene_id']].get(coding_region_size_field_name, 0):
+            new_genes[record['gene_id']][coding_region_size_field_name] = transcript_size
+
+
+def _parse_record(record):
     info_fields = [x.strip().split() for x in record['info'].split(';') if x != '']
     info_dict = {}
     for k, v in info_fields:
@@ -115,53 +148,31 @@ def _parse_line(line, i, new_genes, new_transcripts,  existing_gene_ids, existin
     record['start'] = int(record['start'])
     record['end'] = int(record['end'])
 
-    if len(record["chrom"]) > 2:
-        return  # skip super-contigs
 
-    if record['feature_type'] == 'gene':
-        if record["gene_id"] in existing_gene_ids:
-            counters["genes_skipped"] += 1
-            return
+def _parse_gene_record(record, genome_version, gencode_release):
+    return {
+        "gene_id": record["gene_id"],
+        "gene_symbol": record["gene_name"],
 
-        new_genes[record['gene_id']].update({
-            "gene_id": record["gene_id"],
-            "gene_symbol": record["gene_name"],
+        "chrom_grch{}".format(genome_version): record["chrom"],
+        "start_grch{}".format(genome_version): record["start"],
+        "end_grch{}".format(genome_version): record["end"],
+        "strand_grch{}".format(genome_version): record["strand"],
 
-            "chrom_grch{}".format(genome_version): record["chrom"],
-            "start_grch{}".format(genome_version): record["start"],
-            "end_grch{}".format(genome_version): record["end"],
-            "strand_grch{}".format(genome_version): record["strand"],
+        "gencode_gene_type": record["gene_type"],
+        "gencode_release": int(gencode_release),
+    }
 
-            "gencode_gene_type": record["gene_type"],
-            "gencode_release": int(gencode_release),
-        })
 
-    elif record['feature_type'] == 'transcript':
-        if record["transcript_id"] in existing_transcript_ids:
-            counters["transcripts_skipped"] += 1
-            return
-
-        new_transcripts[record['transcript_id']].update({
-            "gene_id": record["gene_id"],
-            "transcript_id": record["transcript_id"],
-            "chrom_grch{}".format(genome_version): record["chrom"],
-            "start_grch{}".format(genome_version): record["start"],
-            "end_grch{}".format(genome_version): record["end"],
-            "strand_grch{}".format(genome_version): record["strand"],
-        })
-        if 'MANE_Select' in record.get('tag', []):
-            new_transcripts[record['transcript_id']]['is_mane_select'] = True
-
-    elif record['feature_type'] == 'CDS':
-        if record["transcript_id"] in existing_transcript_ids:
-            return
-
-        coding_region_size_field_name = "coding_region_size_grch{}".format(genome_version)
-        # add + 1 because GTF has 1-based coords. (https://useast.ensembl.org/info/website/upload/gff.html)
-        transcript_size = record["end"] - record["start"] + 1
-        transcript_size += new_transcripts[record['transcript_id']].get(coding_region_size_field_name, 0)
-        new_transcripts[record['transcript_id']][coding_region_size_field_name] = transcript_size
-
-        if record['gene_id'] not in existing_gene_ids and \
-                transcript_size > new_genes[record['gene_id']].get(coding_region_size_field_name, 0):
-            new_genes[record['gene_id']][coding_region_size_field_name] = transcript_size
+def _parse_transcript_record(record, genome_version):
+    transcript = {
+        "gene_id": record["gene_id"],
+        "transcript_id": record["transcript_id"],
+        "chrom_grch{}".format(genome_version): record["chrom"],
+        "start_grch{}".format(genome_version): record["start"],
+        "end_grch{}".format(genome_version): record["end"],
+        "strand_grch{}".format(genome_version): record["strand"],
+    }
+    if 'MANE_Select' in record.get('tag', []):
+        transcript['is_mane_select'] = True
+    return transcript
