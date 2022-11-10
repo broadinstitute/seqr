6e49f6898978d42c677a1bb709a99ff5d836e4f4
Hana Snow <hana.snow@gmail.com>
2022-11-10 14:58:08 -0500

    fix sv snp search

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -805,10 +805,6 @@ class BaseSvHailTableQuery(BaseHailTableQuery):
         return families_expr
 
 
-def _is_gcnv_variant(r):
-    return hl.is_defined(r.svType) & r.svType.startswith('gCNV_')
-
-
 class GcnvHailTableQuery(BaseSvHailTableQuery):
 
     GENOTYPE_FIELDS = {
@@ -826,7 +822,7 @@ class GcnvHailTableQuery(BaseSvHailTableQuery):
     })
     COMPUTED_ANNOTATION_FIELDS = {
         'transcripts': lambda self, r: hl.if_else(
-            ~_is_gcnv_variant(r) & _no_genotype_override(r.genotypes, 'geneIds'), r.transcripts, hl.bind(
+            _no_genotype_override(r.genotypes, 'geneIds'), r.transcripts, hl.bind(
                 lambda gene_ids: hl.dict(r.transcripts.items().filter(lambda t: gene_ids.contains(t[0]))),
                 r.genotypes.values().flatmap(lambda g: g.geneIds)
             ),
@@ -1005,10 +1001,14 @@ class MultiDataTypeHailTableQuery(object):
         return {}
 
 
+def _is_gcnv_variant(r):
+    return hl.is_defined(r.svType) & r.svType.startswith('gCNV_')
+
+
 class AllSvHailTableQuery(MultiDataTypeHailTableQuery, BaseSvHailTableQuery):
 
     COMPUTED_ANNOTATION_FIELDS = {
-        k: lambda self, r: hl.or_else(v(self, r), r[k])
+        k: lambda self, r: hl.if_else(_is_gcnv_variant(mt), hl.or_else(v(self, r), r[k]),  r[k])
         for k, v in GcnvHailTableQuery.COMPUTED_ANNOTATION_FIELDS.items()
     }
     INITIAL_ENTRY_ANNOTATIONS = {
