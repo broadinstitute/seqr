1104b9708b185ba3b0a5da54dcdacb0752ff3633
Hana Snow <hana.snow@gmail.com>
2022-11-10 17:40:02 -0500

    handle missing transcripts

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -86,13 +86,9 @@ class BaseHailTableQuery(object):
             'predictions': lambda r: hl.struct(**{
                 prediction: r[path[0]][path[1]] for prediction, path in self.PREDICTION_FIELDS_CONFIG.items()
             }),
-            'transcripts': lambda r: hl.if_else(
-                hl.is_defined(r.sortedTranscriptConsequences),
-                r.sortedTranscriptConsequences.map(
-                    lambda t: hl.struct(**{_to_camel_case(k): t[k] for k in self.TRANSCRIPT_FIELDS})).group_by(
-                    lambda t: t.geneId),
-                hl.dict({}),
-            ),
+            'transcripts': lambda r: hl.or_else(r.sortedTranscriptConsequences, hl.array([])).map(
+                lambda t: hl.struct(**{_to_camel_case(k): t[k] for k in self.TRANSCRIPT_FIELDS})).group_by(
+                lambda t: t.geneId),
         }
         annotation_fields.update(self.BASE_ANNOTATION_FIELDS)
         if self._genome_version == GENOME_VERSION_LOOKUP[GENOME_VERSION_GRCh38]:
