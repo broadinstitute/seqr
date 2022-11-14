50c17f11868c18fc3a48561e4d624e607ad3760d
Hana Snow <hana.snow@gmail.com>
2022-11-14 17:25:51 -0500

    debug

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -124,13 +124,18 @@ class BaseHailTableQuery(object):
 
     def _load_table(self, data_source, samples, intervals=None, **kwargs):
         ht = self.import_filtered_ht(data_source, samples, intervals=self._parse_intervals(intervals), **kwargs)
+        logger.info(f'{data_source}: {ht.count()}')
         mt = ht.to_matrix_table_row_major(list(self._individuals_by_sample_id.keys()), col_field_name='s')
         mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
+        logger.info(f'{data_source}: {mt.count()}')
         mt = mt.unfilter_entries()
+        logger.info(f'{data_source}: {mt.count()}')
         if self.INITIAL_ENTRY_ANNOTATIONS:
             mt = mt.annotate_entries(**{k: v(mt) for k, v in self.INITIAL_ENTRY_ANNOTATIONS.items()})
+        logger.info(f'{data_source}: {mt.count()}')
         if self._filtered_genes:
             mt = self._filter_gene_ids(mt, self._filtered_genes)
+        logger.info(f'{data_source}: {mt.count()}')
         return mt
 
     @classmethod
