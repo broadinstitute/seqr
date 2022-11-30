c22b6c5cac75465b94b04a03ae22c58de1d01d04
Hana Snow <hana.snow@gmail.com>
2022-11-30 14:32:15 -0500

    codacy fix

--- seqr/urls.py
+++ seqr/urls.py
@@ -271,9 +271,9 @@ api_endpoints = {
 
     'matchmaker/get_mme_matches/(?P<submission_guid>[\w.|-]+)': get_individual_mme_matches,
     'matchmaker/get_mme_nodes': get_mme_nodes,
-    'matchmaker/search_local_mme_matches/(?P<submission_guid>[\w.|-]+)': search_local_individual_mme_matches,
-    'matchmaker/search_mme_matches/(?P<submission_guid>[\w.|-]+)/(?P<node>.+)': search_individual_mme_matches,
-    'matchmaker/finalize_mme_search/(?P<submission_guid>[\w.|-]+)': finalize_mme_search,
+    'matchmaker/search_local_mme_matches/(?P<submission_guid>[^/]+)': search_local_individual_mme_matches,
+    'matchmaker/search_mme_matches/(?P<submission_guid>[^/]+)/(?P<node>[^/]+)': search_individual_mme_matches,
+    'matchmaker/finalize_mme_search/(?P<submission_guid>[^/]+)': finalize_mme_search,
     'matchmaker/submission/create': update_mme_submission,
     'matchmaker/submission/(?P<submission_guid>[\w.|-]+)/update': update_mme_submission,
     'matchmaker/submission/(?P<submission_guid>[\w.|-]+)/delete': delete_mme_submission,
