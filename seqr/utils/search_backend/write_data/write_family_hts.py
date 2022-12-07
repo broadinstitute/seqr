1afbf0f2b19388bfa23aa568bb739cad28b4c501
Hana Snow <hana.snow@gmail.com>
2022-12-07 16:53:25 -0500

    write sv family tables

--- seqr/utils/search_backend/write_data/write_family_hts.py
+++ seqr/utils/search_backend/write_data/write_family_hts.py
@@ -1,21 +1,35 @@
 import argparse
 import hail as hl
 
+SV_TYPE = 'SV'
 ANNOTATIONS = {
-    'AB': lambda mt: hl.if_else(mt.AD.length() > 1, hl.float(mt.AD[1] / hl.sum(mt.AD)), hl.missing(hl.tfloat)),
+    'VARIANTS': {
+        'AB': lambda mt: hl.if_else(mt.AD.length() > 1, hl.float(mt.AD[1] / hl.sum(mt.AD)), hl.missing(hl.tfloat)),
+    },
+    SV_TYPE: {
+        'CN': lambda mt: mt.RD_CN,
+        'GQ_SV': lambda mt: mt.GQ,
+    },
 }
-ENTRY_FIELDS = ['AD', 'DP', 'GQ', 'GT', 'PL']
+ENTRY_FIELDS = {
+    'VARIANTS': ['AD', 'DP', 'GQ', 'PL'],
+}
 
 
-def write_family_hts(file, project):
+def write_family_hts(file, project, data_type):
     families_ht = hl.import_table(f'gs://seqr-project-subsets/{project}_fam.csv', key='s', delimiter=',')
     family_guids = families_ht.aggregate(hl.agg.collect_as_set(families_ht.familyGuid))
 
-    mt = hl.read_matrix_table(f'gs://hail-backend-datasets/{file}.mt').select_globals().select_rows()
+    mt = hl.read_matrix_table(f'gs://hail-backend-datasets/{file}.mt').select_globals()
+    if data_type == SV_TYPE:
+        mt = mt.rename({'rsid': 'variantId'})
+        mt = mt.key_rows_by('variantId')
+    mt = mt.select_rows()
     print(f'Total MT rows: {mt.rows().count()}')
     mt = mt.semi_join_cols(families_ht)
     mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
-    mt = mt.annotate_entries(**{k: v(mt) for k, v in ANNOTATIONS.items()})
+    annotations = ANNOTATIONS[data_type]
+    mt = mt.annotate_entries(**{k: v(mt) for k, v in annotations.items()})
 
     print(f'Exporting {len(family_guids)} families')
     totals = []
@@ -26,7 +40,7 @@ def write_family_hts(file, project):
 
         family_mt = mt.semi_join_cols(family_subset_ht)
         family_ht = family_mt.filter_rows(hl.agg.any(family_mt.GT.is_non_ref())).entries()
-        family_ht = family_ht.select(*ANNOTATIONS.keys(), *ENTRY_FIELDS)
+        family_ht = family_ht.select('GT', *annotations.keys(), *ENTRY_FIELDS.get(data_type, []))
 
         count = family_ht.count()
         print(f'{family_guid}: {family_subset_ht.count()} samples, {count} rows')
@@ -48,6 +62,7 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument('file')
     p.add_argument('project')
+    p.add_argument('data_type')
     args = p.parse_args()
 
-    write_family_hts(args.file, args.project)
+    write_family_hts(args.file, args.project, args.data_type)
--- seqr/utils/search_backend/write_data/write_project_mito_sample_hts.py
+++ seqr/utils/search_backend/write_data/write_project_mito_sample_hts.py
@@ -1,7 +1,16 @@
 import argparse
 import hail as hl
 
-from seqr.utils.search_backend.write_data.write_project_samples_utils import write_project_samples_hts
+def write_project_samples_hts(file, project, _read_table, _get_sample_table):
+    subset_ht = hl.import_table(f'gs://seqr-project-subsets/{project}_ids.txt', key='s')
+    sample_ids = subset_ht.aggregate(hl.agg.collect(subset_ht.s))
+
+    mt = _read_table(file, subset_ht=subset_ht)
+    print(f'Exporting {len(sample_ids)} samples')
+    for sample_id in sample_ids:
+        print(sample_id)
+        sample_ht = _get_sample_table(mt, sample_id)
+        sample_ht.write(f'gs://hail-backend-datasets/{file}__samples/{sample_id}.ht')
 
 
 def _read_table(file, subset_ht=None):
