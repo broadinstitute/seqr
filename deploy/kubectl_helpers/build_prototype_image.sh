d15a467f8630b891adcee736fdc7a265607cf451
Hana Snow <hana.snow@gmail.com>
2022-12-22 12:17:23 -0500

    fix dependencies

--- deploy/kubectl_helpers/build_prototype_image.sh
+++ deploy/kubectl_helpers/build_prototype_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build . -f deploy/docker/seqr/Dockerfile --platform=linux/amd64 -t gcr.io/seqr-project/seqr:prototype
-docker push gcr.io/seqr-project/seqr:prototype
+#docker push gcr.io/seqr-project/seqr:prototype
