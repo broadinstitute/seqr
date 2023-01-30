5dd048be0e5bd12a965607d1f67868b01e14bf8b
Shifa Zhang <zhangshifa07504@gmail.com>
2023-01-30 11:51:46 -0500

    Add back file utils unit tests.

--- seqr/utils/file_utils_tests.py
+++ seqr/utils/file_utils_tests.py
@@ -39,6 +39,19 @@ class FileUtilsTest(TestCase):
         self.assertEqual(str(ee.exception),  'A Google Storage path is expected.')
 
         process = mock_subproc.Popen.return_value
+        process.wait.return_value = -1
+        process.stdout = [b'-bash: gsutil: command not found.\n', b'Please check the path.\n']
+        with self.assertRaises(Exception) as ee:
+            get_gs_file_list('gs://bucket/target_path/', user=None)
+        self.assertEqual(str(ee.exception),
+                         'Run command failed: -bash: gsutil: command not found. Please check the path.')
+        mock_subproc.Popen.assert_called_with('gsutil ls gs://bucket/target_path', stdout=mock_subproc.PIPE,
+                                              stderr=mock_subproc.STDOUT, shell=True)
+        mock_logger.info.assert_called_with('==> gsutil ls gs://bucket/target_path', None)
+        process.wait.assert_called_with()
+
+        mock_subproc.reset_mock()
+        mock_logger.reset_mock()
         process.wait.return_value = 0
         process.stdout = [b'\n', b'\n', b'Updates are available for some Cloud SDK components.  To install them,\n',
                           b'please run:\n', b'  $ gcloud components update\n',
