46febbc018f628043c7274b1947d4607545072c5
Hana Snow <hana.snow@gmail.com>
2023-02-06 17:06:47 -0500

    debug

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -296,8 +296,9 @@ class BaseHailTableQuery(object):
                 families_ht = families_ht.join(family_ht, how='outer')
                 families_ht = families_ht.select(
                     genotypes=hl.bind(
-                        lambda g1, g2: g1.extend(g2),
-                        hl.or_else(families_ht.genotypes, hl.empty_array(families_ht.genotypes.dtype.element_type)),
+                        # lambda g1, g2: g1.extend(g2),
+                        # hl.or_else(families_ht.genotypes, hl.empty_array(families_ht.genotypes.dtype.element_type)),
+                        lambda g: g,
                         hl.or_else(families_ht.genotypes_1, hl.empty_array(families_ht.genotypes.dtype.element_type)),
                     ),
                     **{k: hl.bind(
