472bb982b49c21b1fdf52c0c0e656d71332ca304
Hana Snow <hana.snow@gmail.com>
2023-02-06 17:04:42 -0500

    fix multi family

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -295,10 +295,10 @@ class BaseHailTableQuery(object):
             if families_ht is not None:
                 families_ht = families_ht.join(family_ht, how='outer')
                 families_ht = families_ht.select(
-                    genotypes=hl.if_else(
-                        hl.is_defined(families_ht.genotypes),
-                        families_ht.genotypes.extend(families_ht.genotypes_1),
-                        families_ht.genotypes_1,
+                    genotypes=hl.bind(
+                        lambda g1, g2: g1.extend(g2),
+                        hl.or_else(families_ht.genotypes, hl.empty_array(families_ht.genotypes.dtype.element_type)),
+                        hl.or_else(families_ht.genotypes_1, hl.empty_array(families_ht.genotypes.dtype.element_type)),
                     ),
                     **{k: hl.bind(
                         lambda family_set: hl.if_else(families_ht[field], family_set.add(f.guid), family_set),
