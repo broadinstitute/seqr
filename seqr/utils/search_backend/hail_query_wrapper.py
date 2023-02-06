16d7edbf64e6108317910da8044fbff7d90dd6a8
Hana Snow <hana.snow@gmail.com>
2023-02-06 17:47:58 -0500

    fix multi family override quality

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -309,8 +309,8 @@ class BaseHailTableQuery(object):
                         hl.or_else(families_ht.genotypes_1, hl.empty_array(families_ht.genotypes.dtype.element_type)),
                     ),
                     **{k: hl.bind(
-                        lambda family_set: hl.if_else(families_ht[field], family_set.add(f.guid), family_set),
-                        hl.or_else(hl.is_defined(families_ht[field]) & families_ht[k], hl.empty_set(hl.tstr)),
+                        lambda family_set: hl.if_else(hl.is_defined(families_ht[field]) & families_ht[field], family_set.add(f.guid), family_set),
+                        hl.or_else(families_ht[k], hl.empty_set(hl.tstr)),
                     ) for k, field in family_set_fields.items()},
                     **{k: hl.bind(
                         lambda family_arr: hl.if_else(
