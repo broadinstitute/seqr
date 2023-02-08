6936eb7bcf519ed1ab4a7eefb857883c9a7f0b14
Hana Snow <hana.snow@gmail.com>
2023-02-08 15:48:51 -0500

    debug

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -788,13 +788,16 @@ class BaseHailTableQuery(object):
                 ch_ht.v2.compHetFamilyCarriers[family_guid]).size() == 0)
 
     def _format_results(self, ht):
+        logger.info(ht.agg.collect(ht.sortedTranscriptConsequences))  # TODO debug
         results = ht.annotate(
             genomeVersion=self._genome_version.replace('GRCh', ''),
             **{k: v(ht) for k, v in self.annotation_fields.items()},
         )
+        logger.info(results.agg.collect(results.transcripts))  # TODO debug
         results = results.annotate(
             **{k: v(self, results) for k, v in self.COMPUTED_ANNOTATION_FIELDS.items()},
         )
+        logger.info(results.agg.collect(results.transcripts))  # TODO debug
         return results.select(
             'genomeVersion', *self.CORE_FIELDS, *set(list(self.COMPUTED_ANNOTATION_FIELDS.keys()) + list(self.annotation_fields.keys())))
 
