8fa13b79619ddf75f3ecd585c5349cc1b3b667b1
Hana Snow <hsnow@broadinstitute.org>
2023-02-23 16:05:52 -0500

    fix query tables

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -320,7 +320,8 @@ class BaseHailTableQuery(object):
 
         logger.info(f'Prefiltered to {families_ht.count()} rows ({cls.__name__})')
 
-        annotation_ht_query_result = hl.query_table(f'/hail_datasets/{data_source}.ht', families_ht.key).first()
+        annotation_ht_query_result = hl.query_table(
+            f'/hail_datasets/{data_source}.ht', families_ht.key).first().drop('locus', 'alleles')
         ht = families_ht.annotate(**annotation_ht_query_result)
 
         if clinvar_path_terms and quality_filter:
