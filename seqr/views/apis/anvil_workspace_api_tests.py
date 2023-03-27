f64cfc54cf5dc2a6b551b062f954f80f67109b3b
Hana Snow <hsnow@broadinstitute.org>
2023-03-27 16:08:27 -0400

    fix test

--- seqr/views/apis/anvil_workspace_api_tests.py
+++ seqr/views/apis/anvil_workspace_api_tests.py
@@ -927,31 +927,31 @@ class LoadAnvilDataAPITest(AnvilAuthenticationTestCase):
             'Status': 'Loading Requested',
         }}]})
 
-    # @mock.patch('seqr.views.apis.anvil_workspace_api.ANVIL_LOADING_DELAY_EMAIL_START_DATE', '2021-06-01')
-    # @responses.activate
-    # def test_create_project_from_workspace_loading_delay_email(self):
-    #     url = reverse(create_project_from_workspace, args=[TEST_WORKSPACE_NAMESPACE, TEST_NO_PROJECT_WORKSPACE_NAME])
-    #     self.check_manager_login(url, login_redirect_url='/login/google-oauth2')
-    #
-    #     # make sure the task id including the newly created project to avoid infinitely pulling the tasks
-    #     responses.add(responses.GET,
-    #                   '{}/api/v1/dags/seqr_vcf_to_es_AnVIL_WES_v0.0.1/tasks'.format(MOCK_AIRFLOW_URL),
-    #                   headers={'Authorization': 'Bearer {}'.format(MOCK_TOKEN)},
-    #                   json={"tasks": [
-    #                         {"task_id": "pyspark_compute_project_R0006_anvil_no_project_workspace"},
-    #                         {"task_id": "pyspark_compute_project_R0007_anvil_no_project_workspace"},
-    #                         {"task_id": "pyspark_compute_project_R0008_anvil_no_project_workspace"}],
-    #                         "total_entries": 2},
-    #                   status=200)
-    #     self._test_not_yet_email_date(url, REQUEST_BODY)
-    #
-    #     # Remove created project to allow future requests
-    #     project = Project.objects.get(
-    #         workspace_namespace=TEST_WORKSPACE_NAMESPACE, workspace_name=TEST_NO_PROJECT_WORKSPACE_NAME)
-    #     project.workspace_name = None
-    #     project.save()
-    #
-    #     self._test_after_email_date(url, REQUEST_BODY)
+    @mock.patch('seqr.views.apis.anvil_workspace_api.ANVIL_LOADING_DELAY_EMAIL_START_DATE', '2021-06-01')
+    @responses.activate
+    def test_create_project_from_workspace_loading_delay_email(self):
+        url = reverse(create_project_from_workspace, args=[TEST_WORKSPACE_NAMESPACE, TEST_NO_PROJECT_WORKSPACE_NAME])
+        self.check_manager_login(url, login_redirect_url='/login/google-oauth2')
+
+        # make sure the task id including the newly created project to avoid infinitely pulling the tasks
+        responses.add(responses.GET,
+                      '{}/api/v1/dags/seqr_vcf_to_es_AnVIL_WES_v0.0.1/tasks'.format(MOCK_AIRFLOW_URL),
+                      headers={'Authorization': 'Bearer {}'.format(MOCK_TOKEN)},
+                      json={"tasks": [
+                            {"task_id": "pyspark_compute_project_R0006_anvil_no_project_workspace"},
+                            {"task_id": "pyspark_compute_project_R0007_anvil_no_project_workspace"},
+                            {"task_id": "pyspark_compute_project_R0008_anvil_no_project_workspace"}],
+                            "total_entries": 2},
+                      status=200)
+        self._test_not_yet_email_date(url, REQUEST_BODY)
+
+        # Remove created project to allow future requests
+        project = Project.objects.get(
+            workspace_namespace=TEST_WORKSPACE_NAMESPACE, workspace_name=TEST_NO_PROJECT_WORKSPACE_NAME)
+        project.workspace_name = None
+        project.save()
+
+        self._test_after_email_date(url, REQUEST_BODY)
 
     @mock.patch('seqr.views.apis.anvil_workspace_api.ANVIL_LOADING_DELAY_EMAIL_START_DATE', '2021-06-01')
     @responses.activate
