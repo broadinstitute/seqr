526590e91012e4625d0e6e26bccbc8d5681832d5
Hana Snow <hsnow@broadinstitute.org>
2023-04-03 16:38:43 -0400

    debug

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -905,7 +905,7 @@ class BaseHailTableQuery(object):
 
         if sort in PREDICTOR_SORTS:
             # TODO not working
-            loggger.info(f'applied predictor sort {"NONE" if sort not in self.PREDICTION_FIELDS_CONFIG else sort}')
+            logger.info(f'applied predictor sort {"NONE" if sort not in self.PREDICTION_FIELDS_CONFIG else sort}')
             return None if sort not in self.PREDICTION_FIELDS_CONFIG else lambda r: -r.predictions[sort]
 
         raise InvalidSearchException(f'Invalid sort "{sort}"')
