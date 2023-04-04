4b659eaebdd18fd353361a6a3c5629653268bb5d
Hana Snow <hsnow@broadinstitute.org>
2023-04-04 15:17:08 -0400

    fix syntax

--- seqr/utils/search_backend/hail_query_wrapper.py
+++ seqr/utils/search_backend/hail_query_wrapper.py
@@ -1430,18 +1430,18 @@ class AllVariantHailTableQuery(MultiDataTypeHailTableQuery, VariantHailTableQuer
     pass
 
 
+def merged_consequence_sort(ht):
+    rank_sort, additional_sort = BaseVariantHailTableQuery.SORTS[CONSEQUENCE_SORT_KEY](ht)
+    is_sv = hl.is_defined(ht.svType)
+    return [
+        hl.if_else(is_sv, SV_CONSEQUENCE_RANK_OFFSET, rank_sort),
+        hl.if_else(is_sv, rank_sort, additional_sort),
+    ]
+
+
 class AllDataTypeHailTableQuery(AllVariantHailTableQuery):
 
     DATA_TYPE_ANNOTATION_FIELDS = ['chrom', 'pos', 'end']
-
-    @staticmethod
-    def merged_consequence_sort(ht):
-        rank_sort, additional_sort = BaseVariantHailTableQuery.SORTS[CONSEQUENCE_SORT_KEY](ht)
-        is_sv = hl.is_defined(ht.svType)
-        return [
-            hl.if_else(is_sv, SV_CONSEQUENCE_RANK_OFFSET, rank_sort),
-            hl.if_else(is_sv, rank_sort, additional_sort),
-        ]
 
     SORT_OVERRIDES = {
         CONSEQUENCE_SORT_KEY: merged_consequence_sort,
