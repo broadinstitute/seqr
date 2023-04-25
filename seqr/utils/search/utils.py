b12bdcf2255f0487c804001428245257b00ee706
Hana Snow <hsnow@broadinstitute.org>
2023-04-25 16:21:41 -0400

    search helpers for middleware eerors

--- seqr/utils/search/utils.py
+++ seqr/utils/search/utils.py
@@ -18,6 +18,34 @@ class InvalidSearchException(Exception):
     pass
 
 
+SEARCH_EXCEPTION_ERROR_MAP = {
+    InvalidIndexException: 400,
+    InvalidSearchException: 400,
+    elasticsearch.exceptions.ConnectionError: 504,
+    elasticsearch.exceptions.TransportError: lambda e: int(e.status_code) if e.status_code != 'N/A' else 400,
+}
+SEARCH_EXCEPTION_MESSAGE_MAP = {
+    elasticsearch.exceptions.ConnectionError: str,
+    elasticsearch.exceptions.TransportError: lambda e: '{}: {} - {} - {}'.format(e.__class__.__name__, e.status_code, repr(e.error), _get_transport_error_type(e.info)),
+}
+ERROR_LOG_EXCEPTIONS = {InvalidIndexException}
+
+
+def _get_transport_error_type(error):
+    error_type = 'no detail'
+    if isinstance(error, dict):
+        root_cause = error.get('root_cause')
+        error_info = error.get('error')
+        if (not root_cause) and isinstance(error_info, dict):
+            root_cause = error_info.get('root_cause')
+
+        if root_cause:
+            error_type = root_cause[0].get('type') or root_cause[0].get('reason')
+        elif error_info and not isinstance(error_info, dict):
+            error_type = repr(error_info)
+    return error_type
+
+
 def get_es_client(timeout=60, **kwargs):
     client_kwargs = {
         'hosts': [{'host': ELASTICSEARCH_SERVICE_HOSTNAME, 'port': ELASTICSEARCH_SERVICE_PORT}],
