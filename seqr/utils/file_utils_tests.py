58dd338c3ba0427644cde1a5dd75c351945fea88
Hana Snow <hsnow@broadinstitute.org>
2023-05-16 13:56:43 -0400

    pr feedback

--- seqr/utils/file_utils_tests.py
+++ seqr/utils/file_utils_tests.py
@@ -39,7 +39,7 @@ class FileUtilsTest(TestCase):
         self.assertEqual(str(ee.exception),  'A Google Storage path is expected.')
 
         process = mock_subproc.Popen.return_value
-        process.communicate.return_value = None, b'-bash: gsutil: command not found.\nPlease check the path.\n'
+        process.communicate.return_value = b'', b'-bash: gsutil: command not found.\nPlease check the path.\n'
         with self.assertRaises(Exception) as ee:
             get_gs_file_list('gs://bucket/target_path/', user=None)
         self.assertEqual(str(ee.exception), 'Run command failed: -bash: gsutil: command not found. Please check the path.')
@@ -52,7 +52,7 @@ class FileUtilsTest(TestCase):
         mock_logger.reset_mock()
         process.communicate.return_value = b'\n\nUpdates are available for some Cloud SDK components.  To install them,\n' \
                                            b'please run:\n  $ gcloud components update\ngs://bucket/target_path/id_file.txt\n' \
-                                           b'gs://bucket/target_path/data.vcf.gz\n', None
+                                           b'gs://bucket/target_path/data.vcf.gz\n', b''
         file_list = get_gs_file_list('gs://bucket/target_path', user=None)
         mock_subproc.Popen.assert_called_with('gsutil ls gs://bucket/target_path/**', stdout=mock_subproc.PIPE,
                                               stderr=mock_subproc.PIPE, shell=True)
