2b4e5eab65b3e9aa74274c2d9169f3b0f03ffd40
Hana Snow <hsnow@broadinstitute.org>
2023-06-06 17:21:06 -0400

    add status endpoint

--- hail_search/__main__.py
+++ hail_search/__main__.py
@@ -10,15 +10,20 @@ async def search(request: web.Request) -> web.Response:
     return web.json_response({'results': hail_results, 'total': total_results})
 
 
+async def status(request: web.Request) -> web.Response:
+    return web.json_response({'success': True})
+
+
 def run():
     logging.basicConfig(level=logging.INFO)
     hl.init()
     app = web.Application()
-    app.add_routes([web.post("/search", search)])
+    app.add_routes([web.get('/status', status)])
+    app.add_routes([web.post('/search', search)])
     # TODO add gene_counts route
     web.run_app(
         app,
-        host="0.0.0.0",
+        host='0.0.0.0',
         port=5000,
         access_log_format='"%r" %s %b %Tfs',
     )
