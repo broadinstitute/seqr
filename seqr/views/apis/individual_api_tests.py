158205f432d13eb795378ec80e33a141239c7ee8
Hana Snow <hsnow@broadinstitute.org>
2023-06-16 15:32:32 -0400

    move datastat test to individual api test

--- seqr/views/apis/individual_api_tests.py
+++ seqr/views/apis/individual_api_tests.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 import json
 import mock
 
@@ -695,6 +696,125 @@ class IndividualAPITest(object):
             'affected': 'A', 'notes': None, 'sex': 'F', 'maternalId': None, 'paternalId': 'SCO_PED073B_GA0339_1',
              'primaryBiosample': 'BM', 'analyteType': 'D', 'tissueAffectedStatus': True, 'probandRelationship': 'S',
         })
+        self.assertEqual(indiv_2['paternalGuid'], indiv_1['individualGuid'])
+        self.assertEqual(indiv_3['paternalGuid'], indiv_1['individualGuid'])
+
+    @mock.patch('seqr.views.utils.pedigree_info_utils.date')
+    def test_individuals_datastat_table_handler(self, mock_date):
+        mock_date.today.return_value = datetime.date(2020, 1, 1)
+
+        receive_url = reverse(receive_individuals_table_handler, args=[PROJECT_GUID])
+        self.check_manager_login(receive_url)
+
+        data =  [['participant_guid', 'familyId', 'RELATIONSHIP', 'RELATIONSHIP_OTHER_DETAILS', 'WEBSITE', 'DESCRIPTION', 'CLINICAL_DIAGNOSES', 'CLINICAL_DIAGNOSES_DETAILS', 'GENETIC_DIAGNOSES', 'GENETIC_DIAGNOSES_DETAILS', 'FIND_OUT_DOCTOR_DETAILS', 'PATIENT_AGE', 'CONDITION_AGE', 'PATIENT_DECEASED', 'DECEASED_AGE', 'DECEASED_CAUSE', 'DECEASED_DNA', 'PATIENT_SEX', 'RACE', 'ETHNICITY', 'DOCTOR_TYPES', 'DOCTOR_TYPES_OTHER_DETAILS', 'TESTS', 'TESTS_MICROARRAY_YEAR', 'TESTS_MICROARRAY_LAB', 'TESTS_MICROARRAY_FAMILY', 'TESTS_MICROARRAY_FAMILY_OTHER_DETAILS',  'TESTS_WEXOME_YEAR', 'TESTS_WEXOME_LAB', 'TESTS_WEXOME_FAMILY', 'TESTS_WEXOME_FAMILY_OTHER_DETAILS', 'TESTS_WGENOME_YEAR', 'TESTS_WGENOME_LAB', 'TESTS_WGENOME_FAMILY', 'TESTS_WGENOME_FAMILY_OTHER_DETAILS', 'TESTS_OTHER_DETAILS', 'BIOPSY', 'BIOPSY_OTHER_DETAILS', 'OTHER_STUDIES', 'OTHER_STUDIES_DESCRIBE', 'EXPECT_RESULTS', 'MOTHER_SAME_CONDITION', 'MOTHER_CONDITION_AGE', 'MOTHER_RACE', 'MOTHER_ETHNICITY', 'MOTHER_CAN_PARTICIPATE', 'MOTHER_DECEASED', 'MOTHER_DECEASED_DNA', 'FATHER_SAME_CONDITION', 'FATHER_CONDITION_AGE', 'FATHER_RACE', 'FATHER_ETHNICITY', 'FATHER_CAN_PARTICIPATE', 'FATHER_DECEASED', 'FATHER_DECEASED_DNA', 'NO_SIBLINGS', 'SIBLING', 'NO_CHILDREN', 'CHILD', 'NO_RELATIVE_AFFECTED', 'RELATIVE', 'FAMILY_INFO'],
+            ['1518231365', '123', 'OTHER', 'Grandchild', 'wwww.myblog.com', 'I have a really debilitating probably genetic condition. I\xe2ve seen many specialists.', 'YES', 'SMA\xe2s', 'YES', 'Dwarfism\xe2', 'Dr John Smith', '34', '21', 'YES', '33', 'heart attack', 'NO', 'MALE', 'WHITE,ASIAN,PACIFIC', 'NOT_HISPANIC', 'CLIN_GEN,NEURO,CARDIO,OTHER', 'Pediatrician', 'SINGLE_GENE,GENE_PANEL,WEXOME,WGENOME,OTHER', '', '', '', '', '2018', 'UDN\xe2s lab', 'PARENT,AUNT_UNCLE,NIECE_NEPHEW,OTHER', 'Grandmother',  '', '', '', 'Grandmother', 'Blood work', 'MUSCLE,SKIN,OTHER', 'Bone\xe2s', 'YES', 'Undiagnosed Diseases Network', 'NO', 'YES', '19', 'WHITE,ASIAN', 'NOT_HISPANIC', 'YES', '', '', 'NO', '', '', 'BLACK', 'PREFER_NOT_ANSWER', 'YES', 'NO', '', '[{"SIBLING_SEX":"FEMALE","SIBLING_AGE":"21","SIBLING_RACE":"WHITE","SIBLING_ETHNICITY":"NOT_HISPANIC","SIBLING_SAME_CONDITION":"YES","SIBLING_CONDITION_AGE":null,"SIBLING_CAN_PARTICIPATE":"NO"},{"SIBLING_SEX":"","SIBLING_AGE":"17","SIBLING_RACE": "WHITE","SIBLING_ETHNICITY":"NOT_HISPANIC","SIBLING_SAME_CONDITION":"","SIBLING_CONDITION_AGE":"","SIBLING_CAN_PARTICIPATE":"YES"}]', 'YES', '', 'NO', '[{"RELATIVE_SEX":"MALE","RELATIVE_AGE":"44","RELATIVE_RACE": "WHITE", "RELATIVE_ETHNICITY":"NOT_HISPANIC","RELATIVE_CONDITION_AGE":null,"RELATIVE_CAN_PARTICIPATE":null}]', 'patient\xe2s uncle (dads brother) died from Fahrs disease at 70'],
+            ['b392fd78b440', '987', 'ADULT_CHILD', 'Grandchild', '', '', 'UNSURE', 'SMA', 'NO', 'Dwarfism', '', '47', '2', '', '33', 'heart attack', 'NO', 'PREFER_NOT_ANSWER', 'WHITE', 'UNKNOWN', '', 'Pediatrician', 'NOT_SURE,MICROARRAY,WEXOME', '', '', '', '', '2018', 'UDN', 'PARENT,AUNT_UNCLE,OTHER', 'Grandmother', '', '', '', 'Grandmother', 'Blood work', 'NONE', '', 'NO', 'Undiagnosed Diseases Network', 'NO', 'UNSURE', '19', '', 'UNKNOWN', 'NO', 'UNSURE', '', '', '', '', '', '', '', 'YES', 'YES', '[{"SIBLING_SEX":"FEMALE","SIBLING_AGE":"21","SIBLING_RACE":"WHITE","SIBLING_ETHNICITY":"NOT_HISPANIC","SIBLING_SAME_CONDITION":"YES","SIBLING_CONDITION_AGE":null,"SIBLING_CAN_PARTICIPATE":"NO"}]', 'NO', '[{"CHILD_SEX":"MALE","CHILD_AGE":"12","CHILD_RACE":"WHITE","CHILD_ETHNICITY":"NOT_HISPANIC","CHILD_SAME_CONDITION":"NO","CHILD_CONDITION_AGE":null,"CHILD_CAN_PARTICIPATE":"UNSURE"}]', 'YES', '', '']]
+        response = self.client.post(receive_url, {'f': SimpleUploadedFile(
+            'datstat.tsv', '\n'.join(['\t'.join(row) for row in data]).encode('utf-8')),
+        })
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertDictEqual(response_json, {'uploadedFileId': mock.ANY, 'warnings': [], 'errors': [], 'info': [
+            '2 families, 6 individuals parsed from datstat.tsv',
+            '2 new families, 6 new individuals will be added to the project',
+            '0 existing individuals will be updated',
+        ]})
+
+        url = reverse(save_individuals_table_handler, args=[PROJECT_GUID, response_json['uploadedFileId']])
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertSetEqual(set(response_json.keys()), {'individualsByGuid', 'familiesByGuid', 'familyNotesByGuid'})
+        self.assertEqual(len(response_json['familiesByGuid']), 2)
+        RGP_123_guid = next(f for f in response_json['familiesByGuid'].values() if f['familyId'] == 'RGP_123')['familyGuid']
+        RGP_987_guid = next(f for f in response_json['familiesByGuid'].values() if f['familyId'] == 'RGP_987')['familyGuid']
+        self.assertEqual(len(response_json['individualsByGuid']), 6)
+        individuals = sorted(response_json['individualsByGuid'].values(), key=lambda i: i['individualId'])
+        self.assertListEqual(
+            [i['individualId'] for i in individuals],
+            ['RGP_123_1', 'RGP_123_2', 'RGP_123_3', 'RGP_987_1', 'RGP_987_2', 'RGP_987_3'],
+        )
+        self.assertSetEqual({i['familyGuid'] for i in individuals[:3]}, {RGP_123_guid})
+        self.assertSetEqual({i['familyGuid'] for i in individuals[3:]}, {RGP_987_guid})
+        self.assertListEqual([i['sex'] for i in individuals], ['F', 'M', 'M', 'F', 'M', 'U'])
+        self.assertListEqual([i['affected'] for i in individuals], ['N', 'N', 'A', 'N', 'N', 'A'])
+        parents = individuals[:2] + individuals[3:5]
+        no_parent_fields = [
+            'maternalId', 'paternalId', 'maternalGuid', 'paternalGuid', 'maternalEthnicity', 'paternalEthnicity',
+            'birthYear', 'deathYear', 'onsetAge', 'affectedRelatives',
+        ]
+        for no_parent_field in no_parent_fields:
+            self.assertSetEqual({i[no_parent_field] for i in parents}, {None})
+
+        self.assertDictEqual({k: v for k, v in individuals[2].items() if k in no_parent_fields}, {
+            'maternalGuid': individuals[0]['individualGuid'], 'paternalGuid': individuals[1]['individualGuid'],
+            'maternalId': 'RGP_123_1', 'paternalId': 'RGP_123_2', 'paternalEthnicity': ['Black'],
+            'maternalEthnicity': ['White', 'Asian', 'Not Hispanic'], 'birthYear': 1986, 'deathYear': 2019,
+            'onsetAge': 'A', 'affectedRelatives': True,
+        })
+        self.assertDictEqual({k: v for k, v in individuals[5].items() if k in no_parent_fields}, {
+            'maternalGuid': individuals[3]['individualGuid'], 'paternalGuid': individuals[4]['individualGuid'],
+            'maternalId': 'RGP_987_1', 'paternalId': 'RGP_987_2', 'maternalEthnicity': None, 'paternalEthnicity': None,
+            'birthYear': 1973, 'deathYear': None, 'onsetAge': 'C', 'affectedRelatives': False,
+        })
+
+        self.assertEqual(len(response_json['familyNotesByGuid']), 2)
+        note_1 = next(n['note'] for n in response_json['familyNotesByGuid'].values() if n['familyGuid'] == RGP_123_guid)
+        self.assertEqual(note_1, """#### Clinical Information
+* __Patient is my:__ Grandchild (male)
+* __Current Age:__ Patient is deceased, age 33, due to heart attack, sample not available
+* __Age of Onset:__ 21
+* __Race/Ethnicity:__ White, Asian, Pacific; Not Hispanic
+* __Case Description:__ I have a really debilitating probably genetic condition. Ive seen many specialists.
+* __Clinical Diagnoses:__ Yes; SMAs
+* __Genetic Diagnoses:__ Yes; Dwarfism
+* __Website/Blog:__ Yes
+* __Additional Information:__ patients uncle (dads brother) died from Fahrs disease at 70
+#### Prior Testing
+* __Referring Physician:__ Dr John Smith
+* __Doctors Seen:__ Clinical geneticist, Neurologist, Cardiologist, Other: Pediatrician
+* __Previous Testing:__ Yes;
+* * Single gene testing
+* * Gene panel testing
+* * Whole exome sequencing. Year: 2018, Lab: UDNs lab, Relatives: Parent, Aunt or Uncle, Niece or Nephew, Other: Grandmother
+* * Whole genome sequencing. Year: unspecified, Lab: unspecified, Relatives: None Specified
+* * Other tests: Blood work
+* __Biopsies Available:__ Muscle Biopsy, Skin Biopsy, Other Tissue Biopsy: Bones
+* __Other Research Studies:__ Yes, Name of studies: Undiagnosed Diseases Network, Expecting results: No
+#### Family Information
+* __Mother:__ affected, onset age 19, available
+* __Father:__ unaffected, unavailable, deceased, sample not available
+* __Siblings:__ 
+* * Sister, age 21, affected, unavailable
+* * Sibling (unspecified sex), age 17, unspecified affected status, available
+* __Children:__ None
+* __Relatives:__ 
+* * Male, age 44, affected, unspecified availability""")
+
+        note_2 = next(n['note'] for n in response_json['familyNotesByGuid'].values() if n['familyGuid'] == RGP_987_guid)
+        self.assertEqual(note_2, """#### Clinical Information
+* __Patient is my:__ Adult Child (unspecified sex) - unable to provide consent
+* __Current Age:__ 47
+* __Age of Onset:__ 2
+* __Race/Ethnicity:__ White; Unknown
+* __Case Description:__ 
+* __Clinical Diagnoses:__ Unsure
+* __Genetic Diagnoses:__ No
+* __Website/Blog:__ No
+* __Additional Information:__ None specified
+#### Prior Testing
+* __Referring Physician:__ None
+* __Doctors Seen:__ 
+* __Previous Testing:__ Not sure
+* __Biopsies Available:__ None
+* __Other Research Studies:__ No
+#### Family Information
+* __Mother:__ unknown affected status, unavailable, unknown deceased status
+* __Father:__ unknown affected status, unavailable, unspecified deceased status
+* __Siblings:__ None
+* __Children:__ 
+* * Son, age 12, unaffected, unspecified availability
+* __Relatives:__ None""")
 
     def _is_expected_individuals_metadata_upload(self, response, expected_families=False):
         self.assertEqual(response.status_code, 200)
