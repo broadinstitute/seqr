9970e6170a74d06dd2adbf9f052dd77d5ee62e9d
Hana Snow <hsnow@broadinstitute.org>
2023-07-25 13:56:10 -0400

    initial variant sarch

---
+++ hail_search/hail_search_query.py
@@ -0,0 +1,339 @@
+from aiohttp.web import HTTPBadRequest
+from collections import defaultdict
+import hail as hl
+
+from hail_search.constants import AFFECTED, UNAFFECTED, AFFECTED_ID, UNAFFECTED_ID, VARIANT_DATASET, VARIANT_KEY_FIELD,\
+    GNOMAD_GENOMES_FIELD, XPOS_SORT_KEY, GENOME_VERSION_GRCh38_DISPLAY
+
+
+class BaseHailTableQuery(object):
+
+    GENOTYPE_FIELDS = {}
+    GENOTYPE_RESPONSE_KEYS = {}
+    POPULATIONS = {}
+    PREDICTION_FIELDS_CONFIG = {}
+    OMIT_TRANSCRIPT_FIELDS = []
+
+    GLOBALS = ['enums']
+    CORE_FIELDS = ['xpos']
+    BASE_ANNOTATION_FIELDS = {
+        'familyGuids': lambda r: r.genotypes.group_by(lambda x: x.familyGuid).keys(),
+        'genotypes': lambda r: r.genotypes.group_by(lambda x: x.individualGuid).map_values(lambda x: x[0]),
+    }
+    ENUM_ANNOTATION_FIELDS = {}
+    LIFTOVER_ANNOTATION_FIELDS = {
+        'liftedOverGenomeVersion': lambda r: hl.if_else(  # In production - format all rg37_locus fields in main HT?
+            hl.is_defined(r.rg37_locus), '37', hl.missing(hl.dtype('str')),
+        ),
+        'liftedOverChrom': lambda r: hl.if_else(
+            hl.is_defined(r.rg37_locus), r.rg37_locus.contig, hl.missing(hl.dtype('str')),
+        ),
+        'liftedOverPos': lambda r: hl.if_else(
+            hl.is_defined(r.rg37_locus), r.rg37_locus.position, hl.missing(hl.dtype('int32')),
+        ),
+    }
+
+    SORTS = {
+        XPOS_SORT_KEY: lambda r: [r.xpos],
+    }
+
+    @classmethod
+    def populations_configs(cls):
+        return {pop: cls._format_population_config(pop_config) for pop, pop_config in cls.POPULATIONS.items()}
+
+    @staticmethod
+    def _format_population_config(pop_config):
+        base_pop_config = {field.lower(): field for field in ['AF', 'AC', 'AN', 'Hom', 'Hemi', 'Het']}
+        base_pop_config.update(pop_config)
+        return base_pop_config
+
+    @property
+    def annotation_fields(self):
+        globals = {k: hl.eval(self._ht[k]) for k in self.GLOBALS}
+        enums = globals.pop('enums')
+
+        annotation_fields = {
+            'populations': lambda r: hl.struct(**{
+                population: self.population_expression(r, population) for population in self.POPULATIONS.keys()
+            }),
+            'predictions': lambda r: hl.struct(**{
+                prediction: hl.array(enums[path[0]][path[1]])[r[path[0]][f'{path[1]}_id']]
+                if enums.get(path[0], {}).get(path[1]) else r[path[0]][path[1]]
+                for prediction, path in self.PREDICTION_FIELDS_CONFIG.items()
+            }),
+            'transcripts': lambda r: hl.or_else(
+                r.sorted_transcript_consequences, hl.empty_array(r.sorted_transcript_consequences.dtype.element_type)
+            ).map(lambda t: self._enum_field(
+                t, enums['sorted_transcript_consequences'], drop_fields=self.OMIT_TRANSCRIPT_FIELDS,
+                format=lambda value: value.rename({k: _to_camel_case(k) for k in value.keys()}),
+                annotate=self._annotate_transcript,
+            )).group_by(lambda t: t.geneId),
+        }
+        annotation_fields.update(self.BASE_ANNOTATION_FIELDS)
+
+        format_enum = lambda k, enum_config: lambda r: self._enum_field(r[k], enums[k], globals=globals, **enum_config)
+        annotation_fields.update({
+            enum_config.get('response_key', k): format_enum(k, enum_config)
+            for k, enum_config in self.ENUM_ANNOTATION_FIELDS.items()
+        })
+
+        if self._genome_version == GENOME_VERSION_GRCh38_DISPLAY:
+            annotation_fields.update(self.LIFTOVER_ANNOTATION_FIELDS)
+        return annotation_fields
+
+    def population_expression(self, r, population):
+        pop_config = self._format_population_config(self.POPULATIONS[population])
+        pop_field = self.POPULATION_FIELDS.get(population, population)
+        return hl.struct(**{
+            response_key: hl.or_else(r[pop_field][field], '' if response_key == 'id' else 0)
+            for response_key, field in pop_config.items() if field is not None
+        })
+
+    @classmethod
+    def _annotate_transcript(cls, *args):
+        return {}
+
+    @staticmethod
+    def _enum_field(value, enum, globals=None, annotate=None, format=None, drop_fields=None, **kwargs):
+        annotations = {}
+        drop = [] + (drop_fields or [])
+        value_keys = value.keys()
+        for field, field_enum in enum.items():
+            is_array = f'{field}_ids' in value_keys
+            value_field = f"{field}_id{'s' if is_array else ''}"
+            drop.append(value_field)
+
+            enum_array = hl.array(field_enum)
+            if is_array:
+                annotations[f'{field}s'] = value[value_field].map(lambda v: enum_array[v])
+            else:
+                annotations[field] = enum_array[value[value_field]]
+
+        value = value.annotate(**annotations)
+        if annotate:
+            annotations = annotate(value, enum, globals)
+            value = value.annotate(**annotations)
+        value = value.drop(*drop)
+
+        if format:
+            value = format(value)
+
+        return value
+
+    def __init__(self, data_type, sample_data, genome_version, sort=None, num_results=100, **kwargs):
+        self._genome_version = genome_version
+        self._sort = sort
+        self._num_results = num_results
+
+        self._load_filtered_table(data_type, sample_data, **kwargs)
+
+    def _load_filtered_table(self, data_type, sample_data, **kwargs):
+        self._ht = self.import_filtered_table(
+            data_type, sample_data, genome_version=self._genome_version, **kwargs,
+        )
+
+    @classmethod
+    def import_filtered_table(cls, data_type, sample_data, genome_version=None, **kwargs):
+        tables_path = f'/hail_datasets/{genome_version}/{data_type}'
+
+        family_samples = defaultdict(list)
+        project_samples = defaultdict(list)
+        for s in sample_data:
+            family_samples[s['family_guid']].append(s)
+            project_samples[s['project_guid']].append(s)
+
+        logger.info(f'Loading data for {len(family_samples)} families in {len(project_samples)} projects ({cls.__name__})')
+        if len(family_samples) == 1:
+            family_guid, family_sample_data = list(family_samples.items())[0]
+            family_ht = hl.read_table(f'{tables_path}/families/{family_guid}.ht', **load_table_kwargs)
+            families_ht = cls._add_entry_sample_families(family_ht, family_sample_data)
+            families_ht = families_ht.select_globals()
+        else:
+            raise NotImplementedError
+
+        annotations_ht_path = f'{tables_path}/annotations.ht'
+        annotation_ht_query_result = hl.query_table(
+            annotations_ht_path, families_ht.key).first().drop(*families_ht.key)
+        ht = families_ht.annotate(**annotation_ht_query_result)
+        # Add globals
+        ht = ht.join(hl.read_table(annotations_ht_path).head(0).select().select_globals(*cls.GLOBALS), how='left')
+
+        ht = ht.transmute(
+            genotypes=ht.family_entries.flatmap(lambda x: x).filter(
+                lambda gt: hl.is_defined(gt.individualGuid)
+            ).map(lambda gt: gt.select(
+                'sampleId', 'individualGuid', 'familyGuid',
+                numAlt=hl.if_else(hl.is_defined(gt.GT), gt.GT.n_alt_alleles(), -1),
+                **{cls.GENOTYPE_RESPONSE_KEYS.get(k, k): gt[field] for k, field in cls.GENOTYPE_FIELDS.items()}
+            ))
+        )
+
+        return ht
+
+    @classmethod
+    def _add_entry_sample_families(cls, ht, sample_data):
+        sample_index_id_map = dict(enumerate(hl.eval(ht.sample_ids)))
+        sample_id_index_map = {v: k for k, v in sample_index_id_map.items()}
+        sample_index_id_map = hl.dict(sample_index_id_map)
+        sample_individual_map = {s['sample_id']: s['individual_guid'] for s in sample_data}
+        missing_samples = set(sample_individual_map.keys()) - set(sample_id_index_map.keys())
+        if missing_samples:
+            raise HTTPBadRequest(
+                text=f'The following samples are available in seqr but missing the loaded data: {", ".join(missing_samples)}'
+            )
+
+        affected_id_map = {AFFECTED: AFFECTED_ID, UNAFFECTED: UNAFFECTED_ID}
+        sample_index_affected_status = hl.dict({
+            sample_id_index_map[s['sample_id']]: affected_id_map.get(s['affected']) for s in sample_data
+        })
+        sample_index_individual_map = hl.dict({
+            sample_id_index_map[sample_id]: i_guid for sample_id, i_guid in sample_individual_map.items()
+        })
+        sample_id_family_map = {s['sample_id']: s['family_guid'] for s in sample_data}
+        sample_index_family_map = hl.dict({sample_id_index_map[k]: v for k, v in sample_id_family_map.items()})
+        family_index_map = {f: i for i, f in enumerate(sorted(set(sample_id_family_map.values())))}
+        family_sample_indices = [None] * len(family_index_map)
+        for sample_id, family_guid in sample_id_family_map.items():
+            sample_index = sample_id_index_map[sample_id]
+            family_index = family_index_map[family_guid]
+            if not family_sample_indices[family_index]:
+                family_sample_indices[family_index] = []
+            family_sample_indices[family_index].append(sample_index)
+        family_sample_indices = hl.array(family_sample_indices)
+
+        ht = ht.transmute(
+            family_entries=family_sample_indices.map(lambda sample_indices: sample_indices.map(
+                lambda i: hl.or_else(ht.entries[i], cls._missing_entry(ht.entries[i])).annotate(
+                    sampleId=sample_index_id_map.get(i),
+                    individualGuid=sample_index_individual_map.get(i),
+                    familyGuid=sample_index_family_map.get(i),
+                    affected_id=sample_index_affected_status.get(i),
+                )
+            ))
+        )
+
+        return ht
+
+    @classmethod
+    def _missing_entry(cls, entry):
+        entry_type = dict(**entry.dtype)
+        return hl.struct(**{k: hl.missing(v) for k, v in entry_type.items()})
+
+    def _format_results(self, ht):
+        annotations = {k: v(ht) for k, v in self.annotation_fields.items()}
+        annotations.update({
+            '_sort': self._sort_order(ht),
+            'genomeVersion': self._genome_version.replace('GRCh', ''),
+        })
+        results = ht.annotate(**annotations)
+        return results.select(*self.CORE_FIELDS, *list(annotations.keys()))
+
+    def search(self):
+        ht = self._format_results(self._ht)
+
+        (total_results, collected) = ht.aggregate((hl.agg.count(), hl.agg.take(ht.row, self._num_results, ordering=ht._sort)))
+        logger.info(f'Total hits: {total_results}. Fetched: {self._num_results}')
+
+        hail_results = [self._json_serialize(row) for row in collected]
+        return hail_results, total_results
+
+    def _sort_order(self, ht):
+        sort_expressions = self._get_sort_expressions(ht, XPOS_SORT_KEY)
+        if self._sort != XPOS_SORT_KEY:
+            sort_expressions = self._get_sort_expressions(ht, self._sort) + sort_expressions
+        return sort_expressions
+
+    def _get_sort_expressions(self, ht, sort):
+        return self.SORTS[sort](ht)
+
+    # TODO For production: should use custom json serializer
+    @classmethod
+    def _json_serialize(cls, result):
+        if isinstance(result, list):
+            return [cls._json_serialize(o) for o in result]
+
+        if isinstance(result, hl.Struct) or isinstance(result, hl.utils.frozendict):
+            result = dict(result)
+
+        if isinstance(result, dict):
+            return {k: cls._json_serialize(v) for k, v in result.items()}
+
+        return result
+
+
+class VariantHailTableQuery(BaseHailTableQuery):
+
+    GENOTYPE_FIELDS = {f.lower(): f for f in ['DP', 'GQ', 'AB']}
+    POPULATIONS = {
+        'seqr': {'hom': 'hom', 'hemi': None, 'het': None},
+        'topmed': {'hemi': None},
+        'exac': {
+            'filter_af': 'AF_POPMAX', 'ac': 'AC_Adj', 'an': 'AN_Adj', 'hom': 'AC_Hom', 'hemi': 'AC_Hemi',
+            'het': 'AC_Het',
+        },
+        'gnomad_exomes': {'filter_af': 'AF_POPMAX_OR_GLOBAL', 'het': None},
+        GNOMAD_GENOMES_FIELD: {'filter_af': 'AF_POPMAX_OR_GLOBAL', 'het': None},
+    }
+    POPULATION_FIELDS = {'seqr': 'gt_stats'}
+    PREDICTION_FIELDS_CONFIG = {
+        'cadd': ('cadd', 'PHRED'),
+        'eigen': ('eigen', 'Eigen_phred'),
+        'fathmm': ('dbnsfp', 'fathmm_MKL_coding_pred'),
+        'gnomad_noncoding': ('gnomad_non_coding_constraint', 'z_score'),
+        'mpc': ('mpc', 'MPC'),
+        'mut_pred': ('dbnsfp', 'MutPred_score'),
+        'primate_ai': ('primate_ai', 'score'),
+        'splice_ai': ('splice_ai', 'delta_score'),
+        'splice_ai_consequence': ('splice_ai', 'splice_consequence'),
+        'vest': ('dbnsfp', 'VEST4_score'),
+        'mut_taster': ('dbnsfp', 'MutationTaster_pred'),
+        'polyphen': ('dbnsfp', 'Polyphen2_HVAR_pred'),
+        'revel': ('dbnsfp', 'REVEL_score'),
+        'sift': ('dbnsfp', 'SIFT_pred'),
+    }
+    OMIT_TRANSCRIPT_FIELDS = ['consequence_terms']
+
+    GLOBALS = BaseHailTableQuery.GLOBALS + ['versions']
+    CORE_FIELDS = BaseHailTableQuery.CORE_FIELDS + ['rsid']
+    BASE_ANNOTATION_FIELDS = {
+        'chrom': lambda r: r.locus.contig.replace("^chr", ""),
+        'pos': lambda r: r.locus.position,
+        'ref': lambda r: r.alleles[0],
+        'alt': lambda r: r.alleles[1],
+        'genotypeFilters': lambda r: hl.str(' ,').join(r.filters),  # In production - format in main HT?
+        'mainTranscriptId': lambda r: r.sorted_transcript_consequences.first().transcript_id,
+        'selectedMainTranscriptId': lambda r: hl.or_missing(
+            r.selected_transcript != r.sorted_transcript_consequences.first(), r.selected_transcript.transcript_id,
+        ),
+    }
+    BASE_ANNOTATION_FIELDS.update(BaseHailTableQuery.BASE_ANNOTATION_FIELDS)
+    ENUM_ANNOTATION_FIELDS = {
+        'clinvar': {'annotate': lambda value, enum, globals: {
+            'conflictingPathogenicities': value.conflictingPathogenicities.map(
+                lambda p: p.annotate(pathogenicity=hl.array(enum['pathogenicity'])[p.pathogenicity_id]).drop(
+                    'pathogenicity_id')
+            ),
+            'version': globals['versions'].clinvar,
+        }},
+        'hgmd': {},
+        'screen': {
+            'response_key': 'screenRegionType',
+            'format': lambda value: value.region_types.first(),
+        },
+    }
+
+    @classmethod
+    def import_filtered_table(cls, data_type, sample_data, intervals=None, exclude_intervals=False, **kwargs):
+        ht = super(BaseVariantHailTableQuery, cls).import_filtered_table(
+            data_type, sample_data, intervals=None if exclude_intervals else intervals,
+            excluded_intervals=intervals if exclude_intervals else None, **kwargs)
+        return ht.key_by(VARIANT_KEY_FIELD)
+
+    @classmethod
+    def _annotate_transcript(cls, transcript, *args):
+        return {'major_consequence': transcript.consequence_terms.first()}
+
+
+QUERY_CLASS_MAP = {
+    VARIANT_DATASET: VariantHailTableQuery,
+}
--- hail_search/web_app.py
+++ hail_search/web_app.py
@@ -2,6 +2,13 @@ from aiohttp import web
 import hail as hl
 import logging
 
+from hail_search.search import search_hail_backend
+
+
+async def search(request: web.Request) -> web.Response:
+    hail_results, total_results = search_hail_backend(await request.json())
+    return web.json_response({'results': hail_results, 'total': total_results})
+
 
 async def status(request: web.Request) -> web.Response:
     return web.json_response({'success': True})
@@ -13,5 +20,6 @@ def init_web_app():
     app = web.Application()
     app.add_routes([
         web.get('/status', status),
+        web.post('/search', search),
     ])
     return app
