eacc42c7ab426aaff24a2d8c43255017e1346d33
Shifa Zhang <zhangshifa07504@gmail.com>
2023-08-11 09:43:19 -0400

    fix tests.

--- ui/shared/components/panel/variants/selectors.test.js
+++ ui/shared/components/panel/variants/selectors.test.js
@@ -10,7 +10,8 @@ import {
 
 test('getPairedSelectedSavedVariants', () => {
 
-  const savedAllVariants = getPairedSelectedSavedVariants(STATE_WITH_2_FAMILIES, { match: { params:  {} } })
+  const savedAllVariants = getPairedSelectedSavedVariants(STATE_WITH_2_FAMILIES,
+    { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  {} } })
   expect(savedAllVariants.length).toEqual(3)
   expect(savedAllVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
   expect(savedAllVariants[1].variantGuid).toEqual('SV0000002_1248367227_r0390_100')
@@ -19,21 +20,24 @@ test('getPairedSelectedSavedVariants', () => {
   expect(savedAllVariants[2][1].variantGuid).toEqual('SV0000005_2246859833_r0390_100')
 
   const savedReviewVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES, { match: { params:  { tag: 'Review' } } }
+    STATE_WITH_2_FAMILIES,
+    { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  { tag: 'Review' } } },
   )
   expect(savedReviewVariants.length).toEqual(2)
   expect(savedReviewVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
   expect(savedReviewVariants[1].variantGuid).toEqual('SV0000002_1248367227_r0390_100')
 
   const savedNotesVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES, { match: { params:  { tag: 'Has Notes' } } }
+    STATE_WITH_2_FAMILIES,
+    { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  { tag: 'Has Notes' } } },
   )
   expect(savedNotesVariants.length).toEqual(1)
   expect(savedNotesVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
 
 
   const savedFamilyVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES, { match: { params:  { familyGuid: 'F011652_1' } } }
+    STATE_WITH_2_FAMILIES,
+    { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  { familyGuid: 'F011652_1' } } },
   )
   expect(savedFamilyVariants.length).toEqual(3)
   expect(savedFamilyVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
@@ -41,7 +45,11 @@ test('getPairedSelectedSavedVariants', () => {
   expect(savedFamilyVariants[2].variantGuid).toEqual('SV0000002_SV48367227_r0390_100')
 
   const savedAnalysisGroupVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES, { match: { params:  { analysisGroupGuid: 'AG0000183_test_group' } } }
+    STATE_WITH_2_FAMILIES,
+    {
+      project: { projectGuid: 'R0237_1000_genomes_demo' },
+      match: { params:  { analysisGroupGuid: 'AG0000183_test_group' } },
+    },
   )
   expect(savedAnalysisGroupVariants.length).toEqual(3)
   expect(savedAnalysisGroupVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
@@ -50,7 +58,11 @@ test('getPairedSelectedSavedVariants', () => {
 
 
   const savedVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES, { match: { params:  { variantGuid: 'SV0000004_116042722_r0390_1000' } } }
+    STATE_WITH_2_FAMILIES,
+    {
+      project: { projectGuid: 'R0237_1000_genomes_demo' },
+      match: { params:  { variantGuid: 'SV0000004_116042722_r0390_1000' } },
+    },
   )
   expect(savedVariants.length).toEqual(1)
   expect(savedFamilyVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
@@ -58,7 +70,7 @@ test('getPairedSelectedSavedVariants', () => {
 
 test('getPairedFilteredSavedVariants', () => {
   const pairedSavedVariants = getPairedFilteredSavedVariants(
-    STATE_WITH_2_FAMILIES, { match: { params:  {} } }
+    STATE_WITH_2_FAMILIES, { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  {} } },
   )
   expect(pairedSavedVariants.length).toEqual(2)
   expect(pairedSavedVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
@@ -67,7 +79,7 @@ test('getPairedFilteredSavedVariants', () => {
 
 test('getVisibleSortedSavedVariants', () => {
   const savedVariants = getVisibleSortedSavedVariants(
-    STATE_WITH_2_FAMILIES, { match: { params:  {} } }
+    STATE_WITH_2_FAMILIES, { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  {} } },
   )
   expect(savedVariants.length).toEqual(1)
   expect(savedVariants[0].variantGuid).toEqual('SV0000002_1248367227_r0390_100')
