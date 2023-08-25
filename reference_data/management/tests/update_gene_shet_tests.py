6bceb0e58ef40d52fd74be6527023f6b2b4ec41a
Shifa Zhang <zhangshifa07504@gmail.com>
2023-08-25 11:07:28 -0400

    Update tests.

--- reference_data/management/tests/update_gene_shet_tests.py
+++ reference_data/management/tests/update_gene_shet_tests.py
@@ -2,12 +2,12 @@ from reference_data.models import GeneShet
 from reference_data.management.tests.test_utils import ReferenceDataCommandTestCase
 
 class UpdateGeneShetTest(ReferenceDataCommandTestCase):
-    URL = 'https://storage.googleapis.com/seqr-reference-data/Shet/Shet_Zeng_2023.tsv'
+    URL = 'https://storage.googleapis.com/seqr-reference-data/gene_constraint/shet_Zeng(2023).xlsx%20-%20All%20scores-for%20gene%20page.tsv'
     DATA = [
-        'ensg	hgnc	post_mean_shet	shet_constrained\n',
-        'ENSG00000223972	HGNC:37225	3.01E-05	0\n',
-        'ENSG00000227233	HGNC:26441	4.85E-05	0\n',
-        'ENSG00000243485	HGNC:4013	5.08E-05	1\n',
+        'ensg	hgnc	post_mean (Shet)\n',
+        'ENSG00000223972	HGNC:37225	3.01E-05\n',
+        'ENSG00000227233	HGNC:26441	4.85E-05\n',
+        'ENSG00000243485	HGNC:4013	5.08E-05\n',
     ]
 
     def test_update_gene_cn_sensitivity_command(self):
@@ -16,7 +16,5 @@ class UpdateGeneShetTest(ReferenceDataCommandTestCase):
         self.assertEqual(GeneShet.objects.count(), 2)
         record = GeneShet.objects.get(gene__gene_id='ENSG00000223972')
         self.assertEqual(record.shet, 3.01E-05)
-        self.assertEqual(record.shet_constrained, False)
         record = GeneShet.objects.get(gene__gene_id='ENSG00000243485')
         self.assertEqual(record.shet, 5.08E-05)
-        self.assertEqual(record.shet_constrained, True)
