fb288fe75414229991a1c6ed407cb20fca19a588
Hana Snow <hsnow@broadinstitute.org>
2023-09-06 11:55:35 -0400

    fix tests

--- ui/shared/components/panel/variants/selectors.test.js
+++ ui/shared/components/panel/variants/selectors.test.js
@@ -8,18 +8,11 @@ import {
   getIndividualGeneDataByFamilyGene,
 } from './selectors'
 
-const STATE_WITH_2_FAMILIES_AND_VARIANT_TAGS = {
-  ...STATE_WITH_2_FAMILIES,
-  savedVariantsByTag: {
-    Review: ['SV0000004_116042722_r0390_1000', 'SV0000002_1248367227_r0390_100'],
-    'Review;Tier 1 - Phenotype not delineated': ['SV0000002_1248367227_r0390_100'],
-  },
-}
+const NON_PROJECT_PAGE_STATE = { ...STATE_WITH_2_FAMILIES, currentProjectGuid: null }
 
 test('getPairedSelectedSavedVariants', () => {
 
-  const savedAllVariants = getPairedSelectedSavedVariants(STATE_WITH_2_FAMILIES,
-    { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  {} } })
+  const savedAllVariants = getPairedSelectedSavedVariants(STATE_WITH_2_FAMILIES, { match: { params:  {} } })
   expect(savedAllVariants.length).toEqual(3)
   expect(savedAllVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
   expect(savedAllVariants[1].variantGuid).toEqual('SV0000002_1248367227_r0390_100')
@@ -28,24 +21,20 @@ test('getPairedSelectedSavedVariants', () => {
   expect(savedAllVariants[2][1].variantGuid).toEqual('SV0000005_2246859833_r0390_100')
 
   const savedReviewVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES,
-    { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  { tag: 'Review' } } },
+    STATE_WITH_2_FAMILIES, { match: { params:  { tag: 'Review' } } },
   )
   expect(savedReviewVariants.length).toEqual(2)
   expect(savedReviewVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
   expect(savedReviewVariants[1].variantGuid).toEqual('SV0000002_1248367227_r0390_100')
 
   const savedNotesVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES,
-    { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  { tag: 'Has Notes' } } },
+    STATE_WITH_2_FAMILIES, { match: { params: { tag: 'Has Notes' } } },
   )
   expect(savedNotesVariants.length).toEqual(1)
   expect(savedNotesVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
 
-
   const savedFamilyVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES,
-    { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  { familyGuid: 'F011652_1' } } },
+    STATE_WITH_2_FAMILIES, { match: { params:  { familyGuid: 'F011652_1' } } },
   )
   expect(savedFamilyVariants.length).toEqual(3)
   expect(savedFamilyVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
@@ -53,38 +42,33 @@ test('getPairedSelectedSavedVariants', () => {
   expect(savedFamilyVariants[2].variantGuid).toEqual('SV0000002_SV48367227_r0390_100')
 
   const savedAnalysisGroupVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES,
-    {
-      project: { projectGuid: 'R0237_1000_genomes_demo' },
-      match: { params:  { analysisGroupGuid: 'AG0000183_test_group' } },
-    },
+    STATE_WITH_2_FAMILIES, { match: { params: { analysisGroupGuid: 'AG0000183_test_group' } } },
   )
   expect(savedAnalysisGroupVariants.length).toEqual(3)
   expect(savedAnalysisGroupVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
   expect(savedAnalysisGroupVariants[1].variantGuid).toEqual('SV0000002_1248367227_r0390_100')
   expect(savedFamilyVariants[2].variantGuid).toEqual('SV0000002_SV48367227_r0390_100')
 
-
   const savedVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES,
-    {
-      project: { projectGuid: 'R0237_1000_genomes_demo' },
-      match: { params:  { variantGuid: 'SV0000004_116042722_r0390_1000' } },
-    },
+    STATE_WITH_2_FAMILIES, { match: { params: { variantGuid: 'SV0000004_116042722_r0390_1000' } } },
   )
   expect(savedVariants.length).toEqual(1)
   expect(savedFamilyVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
 
   const tagSavedVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES_AND_VARIANT_TAGS,
-    { match: { params: { tag: 'Review' } } },
+    STATE_WITH_2_FAMILIES, { match: { params: { tag: 'Review' } } },
   )
   expect(tagSavedVariants.length).toEqual(2)
   expect(tagSavedVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
 
+  const summaryDataTagSavedVariants = getPairedSelectedSavedVariants(
+    NON_PROJECT_PAGE_STATE, { match: { params: { tag: 'Review' } } },
+  )
+  expect(summaryDataTagSavedVariants.length).toEqual(2)
+  expect(summaryDataTagSavedVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
+
   const multiTagSavedVariants = getPairedSelectedSavedVariants(
-    STATE_WITH_2_FAMILIES_AND_VARIANT_TAGS,
-    { match: { params: { tag: 'Review;Tier 1 - Phenotype not delineated' } } },
+    NON_PROJECT_PAGE_STATE, { match: { params: { tag: 'Review;Tier 1 - Phenotype not delineated' } } },
   )
   expect(multiTagSavedVariants.length).toEqual(1)
   expect(multiTagSavedVariants[0].variantGuid).toEqual('SV0000002_1248367227_r0390_100')
@@ -92,7 +76,7 @@ test('getPairedSelectedSavedVariants', () => {
 
 test('getPairedFilteredSavedVariants', () => {
   const pairedSavedVariants = getPairedFilteredSavedVariants(
-    STATE_WITH_2_FAMILIES, { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  {} } },
+    STATE_WITH_2_FAMILIES, { match: { params: {} } },
   )
   expect(pairedSavedVariants.length).toEqual(2)
   expect(pairedSavedVariants[0].variantGuid).toEqual('SV0000004_116042722_r0390_1000')
@@ -101,7 +85,7 @@ test('getPairedFilteredSavedVariants', () => {
 
 test('getVisibleSortedSavedVariants', () => {
   const savedVariants = getVisibleSortedSavedVariants(
-    STATE_WITH_2_FAMILIES, { project: { projectGuid: 'R0237_1000_genomes_demo' }, match: { params:  {} } },
+    STATE_WITH_2_FAMILIES, { match: { params: {} } },
   )
   expect(savedVariants.length).toEqual(1)
   expect(savedVariants[0].variantGuid).toEqual('SV0000002_1248367227_r0390_100')
