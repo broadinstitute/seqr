d74d29933d25c8630e43b46df762b490fe18c496
Hana Snow <hsnow@broadinstitute.org>
2023-09-19 14:14:05 -0400

    unshare report functinality

--- ui/pages/SummaryData/components/SampleMetadata.jsx
+++ ui/pages/SummaryData/components/SampleMetadata.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 
+import AwesomeBar from 'shared/components/page/AwesomeBar'
+import DataTable from 'shared/components/table/DataTable'
+import { HorizontalSpacer } from 'shared/components/Spacers'
+import DataLoader from 'shared/components/DataLoader'
+import { InlineHeader } from 'shared/components/StyledComponents'
 import FormWrapper from 'shared/components/form/FormWrapper'
 import { BaseSemanticInput, BooleanCheckbox } from 'shared/components/form/Inputs'
-import BaseReport from 'shared/components/table/BaseReport'
 import { ALL_PROJECTS_PATH, GREGOR_PROJECT_PATH } from '../constants'
 import { loadSampleMetadata } from '../reducers'
 import { getSampleMetadataLoading, getSampleMetadataLoadingError, getSampleMetadataRows, getSampleMetadataColumns } from '../selectors'
@@ -12,11 +17,6 @@ import { getSampleMetadataLoading, getSampleMetadataLoadingError, getSampleMetad
 const FILENAME_LOOKUP = {
   [ALL_PROJECTS_PATH]: 'All_AnVIL_Projects',
   [GREGOR_PROJECT_PATH]: 'All_GREGoR_Projects',
-}
-
-const getDownloadFilename = (projectGuid, data) => {
-  const projectName = FILENAME_LOOKUP[projectGuid] || (data.length && data[0].project_id.replace(/ /g, '_'))
-  return `${projectName}_${new Date().toISOString().slice(0, 10)}_Metadata`
 }
 
 const FIELDS = [
@@ -36,37 +36,66 @@ const FIELDS = [
   },
 ]
 
-const SampleMetadataFilters = React.memo(({ load }) => (
-  <FormWrapper
-    onSubmit={load}
-    fields={FIELDS}
-    noModal
-    inline
-    submitOnChange
-  />
-))
-
-SampleMetadataFilters.propTypes = {
-  load: PropTypes.func,
-}
-
 const VIEW_ALL_PAGES = [{ name: 'GREGoR', path: GREGOR_PROJECT_PATH }, { name: 'Broad', path: ALL_PROJECTS_PATH }]
 
-const SampleMetadata = React.memo(props => (
-  <BaseReport
-    page="sample_metadata"
-    viewAllPages={VIEW_ALL_PAGES}
-    idField="subject_id"
-    defaultSortColumn="family_id"
-    getDownloadFilename={getDownloadFilename}
-    filters={<SampleMetadataFilters {...props} />}
-    rowsPerPage={100}
-    {...props}
-  />
+const SEARCH_CATEGORIES = ['projects']
+
+const ACTIVE_LINK_STYLE = {
+  cursor: 'notAllowed',
+  color: 'grey',
+}
+
+const LOADING_PROPS = { inline: true }
+
+const getResultHref = result => `/summary_data/sample_metadata/${result.key}`
+
+const SampleMetadata = React.memo(({ match, data, columns, loading, load, loadingError }) => (
+  <DataLoader contentId={match.params.projectGuid} load={load} reloadOnIdUpdate content loading={false}>
+    <InlineHeader size="medium" content="Project:" />
+    <AwesomeBar
+      categories={SEARCH_CATEGORIES}
+      placeholder="Enter project name"
+      inputwidth="350px"
+      getResultHref={getResultHref}
+    />
+    {VIEW_ALL_PAGES.map(({ name, path }) => (
+      <span>
+        &nbsp; or &nbsp;
+        <NavLink to={`/summary_data/sample_metadata/${path}`} activeStyle={ACTIVE_LINK_STYLE}>{`view all ${name} projects`}</NavLink>
+      </span>
+    ))}
+    <HorizontalSpacer width={20} />
+    <FormWrapper
+      onSubmit={load}
+      fields={FIELDS}
+      noModal
+      inline
+      submitOnChange
+    />
+    <DataTable
+      striped
+      collapsing
+      horizontalScroll
+      downloadFileName={`${FILENAME_LOOKUP[match.params.projectGuid] || (data.length && data[0].project_id.replace(/ /g, '_'))}_${new Date().toISOString().slice(0, 10)}_Metadata`}
+      idField="subject_id"
+      defaultSortColumn="family_id"
+      emptyContent={loadingError || (match.params.projectGuid ? '0 cases found' : 'Select a project to view data')}
+      loading={loading}
+      data={data}
+      columns={columns}
+      loadingProps={LOADING_PROPS}
+      rowsPerPage={100}
+    />
+  </DataLoader>
 ))
 
 SampleMetadata.propTypes = {
   match: PropTypes.object,
+  data: PropTypes.arrayOf(PropTypes.object),
+  columns: PropTypes.arrayOf(PropTypes.object),
+  loading: PropTypes.bool,
+  loadingError: PropTypes.string,
+  load: PropTypes.func,
 }
 
 const mapStateToProps = (state, ownProps) => ({
