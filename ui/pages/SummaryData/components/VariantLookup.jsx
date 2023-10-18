43fcd389271d98b0fdc368e234ba495df22db17f
Hana Snow <hsnow@broadinstitute.org>
2023-10-18 13:53:28 -0400

    display variant

--- ui/pages/SummaryData/components/VariantLookup.jsx
+++ ui/pages/SummaryData/components/VariantLookup.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Grid, Segment, Header } from 'semantic-ui-react'
 
 import StateDataLoader from 'shared/components/StateDataLoader'
 import FormWrapper from 'shared/components/form/FormWrapper'
 import { BaseSemanticInput } from 'shared/components/form/Inputs'
+import { Variant } from 'shared/components/panel/variants/Variants'
 import { GENOME_VERSION_FIELD } from 'shared/utils/constants'
 
 const FIELDS = [
@@ -18,7 +20,11 @@ const FIELDS = [
   { required: true, ...GENOME_VERSION_FIELD },
 ]
 
-const VariantDisplay = ({ variant }) => JSON.stringify(variant) || 'none'
+const VariantDisplay = ({ variant }) => (variant ? <Variant variant={variant} /> : null)
+
+VariantDisplay.propTypes = {
+  variant: PropTypes.object,
+}
 
 class VariantLookup extends React.PureComponent {
 
