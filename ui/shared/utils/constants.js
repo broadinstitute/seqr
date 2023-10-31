58df1b874c25d5f30499a8b7a0e25e9a978b63fb
Hana Snow <hsnow@broadinstitute.org>
2023-10-31 16:09:09 -0400

    add ont data type to seqr

--- ui/shared/utils/constants.js
+++ ui/shared/utils/constants.js
@@ -118,6 +118,7 @@ export const DATASET_TYPE_MITO_CALLS = 'MITO'
 export const DATASET_TITLE_LOOKUP = {
   [DATASET_TYPE_SV_CALLS]: ' SV',
   [DATASET_TYPE_MITO_CALLS]: ' Mitochondria',
+  ONT_SNV_INDEL: ' ONT',
 }
 
 export const SAMPLE_TYPE_EXOME = 'WES'
