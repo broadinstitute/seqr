c3206f3da28db7694da536d2b2f5961756c66d9c
Hana Snow <hsnow@broadinstitute.org>
2023-11-15 14:18:34 -0500

    do not show option to edit aip tag

--- ui/shared/components/panel/variants/FamilyVariantTags.jsx
+++ ui/shared/components/panel/variants/FamilyVariantTags.jsx
@@ -58,6 +58,7 @@ const VARIANT_NOTE_FIELDS = [{
 }]
 
 const DEPRECATED_MME_TAG = 'seqr MME (old)'
+const AIP_TAG = 'AIP'
 
 const aipCategoryRow = ([key, { name, date }]) => (
   <Table.Row key={key}>
@@ -276,6 +277,7 @@ const FamilyVariantTags = React.memo(({
               tagOptions={projectTagTypes}
               displayMetadata
               disabledTagType={DEPRECATED_MME_TAG}
+              noEditTagType={AIP_TAG}
               onSubmit={dispatchUpdateFamilyVariantTags}
             />
             <HorizontalSpacer width={5} />
