48d9fb4d2de7fec763c6ff457f93084450f4b13b
Julia Klugherz <juliaklugherz@gmail.com>
2023-11-16 21:48:07 -0500

    dataclass and constants

--- seqr/views/apis/feature_updates_api.py
+++ seqr/views/apis/feature_updates_api.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass, asdict
-
 from feedparser import parse
 from markdownify import markdownify as md
 
@@ -7,17 +5,10 @@ import requests
 
 from seqr.views.utils.json_utils import create_json_response
 
-
-@dataclass
-class FeedEntry:
-    """Class representative of one Atom feed entry."""
-
-    author: str
-    author_link: str
-    link: str
-    markdown: str
-    published_datestr: str
-    title: str
+FEED_URL = (
+    "https://github.com/broadinstitute/seqr/discussions/categories/feature-updates.atom"
+)
+TIMEOUT = 5
 
 
 def get_feature_updates(request):
@@ -25,8 +16,7 @@ def get_feature_updates(request):
     Fetches the feature-updates GitHub Discussion Atom feed, converts feed entries into markdown, and returns
     markdown and information for each feed entry.
     """
-    url = "https://github.com/broadinstitute/seqr/discussions/categories/feature-updates.atom"
-    response = requests.get(url, timeout=5)
+    response = requests.get(FEED_URL, timeout=TIMEOUT)
     response.raise_for_status()
 
     feed = parse(response.content)
@@ -36,16 +26,14 @@ def get_feature_updates(request):
         # Atom feeds can have multiple content elements per feed entry
         markdown = "".join(md(content.value) for content in entry.content)
         entries.append(
-            asdict(
-                FeedEntry(
-                    author=entry.author,
-                    author_link=entry.href,
-                    link=entry.link,
-                    markdown=markdown,
-                    published_datestr=entry.published,
-                    title=entry.title,
-                )
-            )
+            {
+                "author": entry.author,
+                "author_link": entry.href,
+                "link": entry.link,
+                "markdown": markdown,
+                "published_datestr": entry.published,
+                "title": entry.title,
+            }
         )
 
     return create_json_response({"entries": entries})
