cd57ecd568668ea0c8c71ebcefa3cadb4adddc4a
Julia Klugherz <juliaklugherz@gmail.com>
2023-11-20 14:27:19 -0500

    clean up review comments

--- ui/pages/Public/components/FeatureUpdates.jsx
+++ ui/pages/Public/components/FeatureUpdates.jsx
@@ -5,51 +5,49 @@ import { Feed, Header } from 'semantic-ui-react'
 import TextFieldView from 'shared/components/panel/view-fields/TextFieldView'
 import StateDataLoader from 'shared/components/StateDataLoader'
 
-const FeatureUpdatesFeed = ({ entries }) => {
-  const getDateFromDateStr = dateStr => (
-    new Date(dateStr).toLocaleDateString(
-      'en-us', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' },
-    ))
+const getDateFromDateStr = dateStr => (
+  new Date(dateStr).toLocaleDateString(
+    'en-us', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' },
+  ))
 
-  return (
-    <div>
-      <Header key="header" dividing size="huge">
-        Feature Updates
-        <Header.Subheader>
-          This page serves as an announcement hub for new seqr functionality, sourced from this
-          {' '}
-          <a href="https://github.com/broadinstitute/seqr/discussions/categories/feature-updates">GitHub Discussion</a>
-          .
-        </Header.Subheader>
-      </Header>
-      <Feed key="feed" size="large">
-        {entries.map(entry => (
-          <Feed.Event key={entry.link}>
-            <Feed.Content>
-              <Feed.Summary>
-                <a href={entry.link}>{entry.title}</a>
-                {' '}
-                by
-                {' '}
-                <a href={entry.author_link}>{entry.author}</a>
-                <Feed.Date>
-                  <div>{getDateFromDateStr(entry.published_datestr)}</div>
-                </Feed.Date>
-              </Feed.Summary>
-              <Feed.Extra>
-                <TextFieldView
-                  field="markdown"
-                  isEditable={false}
-                  initialValues={(markdown => ({ markdown }))(entry.markdown)}
-                />
-              </Feed.Extra>
-            </Feed.Content>
-          </Feed.Event>
-        ))}
-      </Feed>
-    </div>
-  )
-}
+const FeatureUpdatesFeed = ({ entries }) => (
+  <div>
+    <Header key="header" dividing size="huge">
+      Feature Updates
+      <Header.Subheader>
+        This page serves as an announcement hub for new seqr functionality, sourced from this
+        &nbsp;
+        <a href="https://github.com/broadinstitute/seqr/discussions/categories/feature-updates">GitHub Discussion</a>
+        .
+      </Header.Subheader>
+    </Header>
+    <Feed key="feed" size="large">
+      {entries.map(entry => (
+        <Feed.Event key={entry.link}>
+          <Feed.Content>
+            <Feed.Summary>
+              <a href={entry.link}>{entry.title}</a>
+              &nbsp;
+              by
+              &nbsp;
+              <a href={entry.author_link}>{entry.author}</a>
+              <Feed.Date>
+                <div>{getDateFromDateStr(entry.published_datestr)}</div>
+              </Feed.Date>
+            </Feed.Summary>
+            <Feed.Extra>
+              <TextFieldView
+                field="markdown"
+                isEditable={false}
+                initialValues={entry}
+              />
+            </Feed.Extra>
+          </Feed.Content>
+        </Feed.Event>
+      ))}
+    </Feed>
+  </div>
+)
 
 FeatureUpdatesFeed.propTypes = {
   entries: PropTypes.arrayOf(PropTypes.object),
@@ -57,7 +55,7 @@ FeatureUpdatesFeed.propTypes = {
 
 const URL = '/api/feature_updates'
 
-const parseResponse = () => responseJson => (
+const parseResponse = responseJson => (
   { entries: responseJson.entries }
 )
 
@@ -65,7 +63,7 @@ const FeatureUpdates = () => (
   <StateDataLoader
     url={URL}
     childComponent={FeatureUpdatesFeed}
-    parseResponse={parseResponse()}
+    parseResponse={parseResponse}
     errorHeader="Unable to fetch."
   />
 )
