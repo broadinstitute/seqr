13358da8a09f1b330c5cc1212087314ca1ae9072
Hana Snow <hsnow@broadinstitute.org>
2023-12-18 13:13:16 -0500

    fix test

--- seqr/views/apis/saved_variant_api_tests.py
+++ seqr/views/apis/saved_variant_api_tests.py
@@ -992,7 +992,7 @@ class AnvilSavedVariantAPITest(AnvilAuthenticationTestCase, SavedVariantAPITest)
         self.mock_list_workspaces.assert_called_with(self.analyst_user)
         self.mock_get_ws_access_level.assert_called_with(
             mock.ANY, 'my-seqr-billing', 'anvil-1kg project n\u00e5me with uni\u00e7\u00f8de')
-        self.assertEqual(self.mock_get_ws_access_level.call_count, 14)
+        self.assertEqual(self.mock_get_ws_access_level.call_count, 15)
         self.mock_get_groups.assert_has_calls([mock.call(self.collaborator_user), mock.call(self.analyst_user)])
         self.assertEqual(self.mock_get_groups.call_count, 10)
         self.mock_get_ws_acl.assert_not_called()
