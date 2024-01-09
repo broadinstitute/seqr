e299247b765e3f67077f7618a3c968dce9a78699
Hana Snow <hsnow@broadinstitute.org>
2024-01-09 17:17:20 -0500

    add manage command to deactivate search samples

---
+++ seqr/management/commands/deactivate_project_search.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from seqr.models import Project, Sample
+from seqr.utils.search.utils import backend_specific_call
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('project')
+
+    def handle(self, *args, **options):
+        project = Project.objects.get(guid=options['project'])
+
+        if input(f'Are you sure you want to deactivate search for {project.name} (y/n)? ') != 'y':
+            raise CommandError('Error: user did not confirm')
+
+        updated = Sample.bulk_update(user=None, update_json={'is_active': False}, individual__family__project=project)
+
+        logger.info(f'Deactivated {len(updated)} samples')
