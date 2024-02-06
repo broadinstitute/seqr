ea3074019a2dcc5493768cc07519e0c8da6ce4fa
Michael Franklin <illusional@users.noreply.github.com>
2024-02-06 12:42:54 +1100

    Fix indeterministic test

--- seqr/views/utils/orm_to_json_utils_tests.py
+++ seqr/views/utils/orm_to_json_utils_tests.py
@@ -155,11 +155,13 @@ class JSONUtilsTest(object):
         self.assertSetEqual(set(var_2['noteGuids']), set(v2_note_guids))
 
         self.assertSetEqual(set(json['variantTagsByGuid'].keys()), v1_tag_guids | v2_tag_guids)
-        # mfranklin: AssertionError: Items in the first set but not the second:
-        #     'aipMetadata'
-        #     Items in the second set but not the first:
-        #     'metadata'
-        # self.assertSetEqual(set(next(iter(json['variantTagsByGuid'].values())).keys()), TAG_FIELDS)
+        # mfranklin: the original code here does a next(iter(<dictionary>)) has an
+        #   inderministic order, so this occasionally returns the variantTag for an
+        #   AIP variant, which gets transformed with a different key, causing this test
+        #   to intermittently fail. For this reason, we'll hard select the 
+        #   the variant VT1726961_2103343353_r0390_100 to look at the keys for
+        self.assertSetEqual(set(json['variantTagsByGuid']['VT1726961_2103343353_r0390_100'].keys()), TAG_FIELDS)
+        
         for tag_guid in v1_tag_guids:
             self.assertListEqual(json['variantTagsByGuid'][tag_guid]['variantGuids'], [variant_guid_1])
         for tag_guid in v2_tag_guids:
