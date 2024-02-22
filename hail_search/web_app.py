4ae8c6bf47e487646fc3b333d1103ca56391eb17
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2024-02-22 09:46:18 -0500

    Use helper func

--- hail_search/web_app.py
+++ hail_search/web_app.py
@@ -7,6 +7,7 @@ import os
 import hail as hl
 import logging
 import traceback
+from typing import Callable
 
 from hail_search.search import search_hail_backend, load_globals, lookup_variant
 
@@ -43,22 +44,23 @@ def _hl_json_default(o):
 def hl_json_dumps(obj):
     return json.dumps(obj, default=_hl_json_default)
 
+async def sync_to_async(request: web.Request, func: Callable, *args, **kwargs):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(request.app.pool, functools.partial(func, *args, **kwargs))
+
 
 async def gene_counts(request: web.Request) -> web.Response:
-    loop = asyncio.get_running_loop()
-    hail_results = await loop.run_in_executor(request.app.pool, functools.partial(search_hail_backend, await request.json(), gene_counts=True))
+    hail_results = await sync_to_async(search_hail_backend, await request.json(), gene_counts=True)
     return web.json_response(hail_results, dumps=hl_json_dumps)
 
 
 async def search(request: web.Request) -> web.Response:
-    loop = asyncio.get_running_loop()
-    hail_results, total_results = await loop.run_in_executor(request.app.pool, functools.partial(search_hail_backend, await request.json()))
+    hail_results, total_results = await sync_to_async(search_hail_backend, await request.json())
     return web.json_response({'results': hail_results, 'total': total_results}, dumps=hl_json_dumps)
 
 
 async def lookup(request: web.Request) -> web.Response:
-    loop = asyncio.get_running_loop()
-    result = await loop.run_in_executor(request.app.pool, functools.partial(lookup_variant, await request.json()))
+    result = await sync_to_async(lookup_variant, await request.json())
     return web.json_response(result, dumps=hl_json_dumps)
 
 
