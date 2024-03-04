829448a2a797149b843bfc875b9c4806aba41024
Hana Snow <hsnow@broadinstitute.org>
2024-03-04 15:06:41 -0500

    more stringent af refilter logic

--- hail_search/queries/snv_indel_37.py
+++ hail_search/queries/snv_indel_37.py
@@ -1,4 +1,6 @@
-from hail_search.constants import SCREEN_KEY, GENOME_VERSION_GRCh37
+from collections import OrderedDict
+
+from hail_search.constants import SCREEN_KEY, GENOME_VERSION_GRCh37, PREFILTER_FREQ_CUTOFF
 from hail_search.queries.snv_indel import SnvIndelHailTableQuery
 
 
@@ -8,6 +10,10 @@ class SnvIndelHailTableQuery37(SnvIndelHailTableQuery):
     PREDICTION_FIELDS_CONFIG = SnvIndelHailTableQuery.PREDICTION_FIELDS_CONFIG_ALL_BUILDS
     LIFTOVER_ANNOTATION_FIELDS = {}
     ANNOTATION_OVERRIDE_FIELDS = SnvIndelHailTableQuery.ANNOTATION_OVERRIDE_FIELDS[:-1]
+    FREQUENCY_PREFILTER_FIELDS = OrderedDict([
+        (True, PREFILTER_FREQ_CUTOFF),
+        ('is_gt_10_percent', 0.1),
+    ])
 
     def _should_add_chr_prefix(self):
         return False
