5480d1579f68f3d939615e0df1a2a0990f70504d
Julia Klugherz <juliaklugherz@gmail.com>
2024-03-07 11:23:20 -0500

    remove unnecessary config for ont

--- hail_search/queries/ont_snv_indel.py
+++ hail_search/queries/ont_snv_indel.py
@@ -8,12 +8,5 @@ class OntSnvIndelHailTableQuery(SnvIndelHailTableQuery):
 
     CORE_FIELDS = BaseHailTableQuery.CORE_FIELDS
 
-    PREDICTION_FIELDS_CONFIG = {
-        **SnvIndelHailTableQuery.PREDICTION_FIELDS_CONFIG,
-        'fathmm': PredictionPath('dbnsfp', 'fathmm_MKL_coding_pred'),
-        'polyphen': PredictionPath('dbnsfp', 'Polyphen2_HVAR_pred'),
-        'sift': PredictionPath('dbnsfp', 'SIFT_pred'),
-    }
-
     def _get_loaded_filter_ht(self, *args, **kwargs):
         return None
