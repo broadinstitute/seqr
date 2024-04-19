d804734dcd64379ba2640e440424bfadd2606b26
Hana Snow <hsnow@broadinstitute.org>
2024-04-19 15:06:53 -0400

    test analysis groups

--- seqr/views/apis/analysis_group_api_tests.py
+++ seqr/views/apis/analysis_group_api_tests.py
@@ -2,8 +2,9 @@ import json
 
 from django.urls.base import reverse
 
-from seqr.models import AnalysisGroup
-from seqr.views.apis.analysis_group_api import update_analysis_group_handler, delete_analysis_group_handler
+from seqr.models import AnalysisGroup, DynamicAnalysisGroup
+from seqr.views.apis.analysis_group_api import update_analysis_group_handler, delete_analysis_group_handler, \
+    update_dynamic_analysis_group_handler, delete_dynamic_analysis_group_handler
 from seqr.views.utils.test_utils import AuthenticationTestCase
 
 PROJECT_GUID = 'R0001_1kg'
@@ -81,4 +82,56 @@ class AnalysisGroupAPITest(AuthenticationTestCase):
 
         # check that analysis_group was deleted
         new_analysis_group = AnalysisGroup.objects.filter(guid=guid)
+        self.assertEqual(len(new_analysis_group), 0)
+
+    def test_create_update_and_delete_dynamic_analysis_group(self):
+        create_analysis_group_url = reverse(update_dynamic_analysis_group_handler, args=[PROJECT_GUID])
+        self.check_manager_login(create_analysis_group_url)
+
+        # send invalid requests to create analysis_group
+        response = self.client.post(create_analysis_group_url, content_type='application/json', data=json.dumps({}))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.reason_phrase, 'Missing required field(s): Name, Criteria')
+
+        # send valid request to create analysis_group
+        response = self.client.post(create_analysis_group_url, content_type='application/json', data=json.dumps({
+            'name': 'new_dynamic_group', 'criteria': {'analysisStatus': ['Q']},
+        }))
+        self.assertEqual(response.status_code, 200)
+        new_analysis_group_response = response.json()
+        self.assertEqual(len(new_analysis_group_response['analysisGroupsByGuid']), 1)
+        new_analysis_group = next(iter(new_analysis_group_response['analysisGroupsByGuid'].values()))
+        self.assertEqual(new_analysis_group['name'], 'new_dynamic_group')
+
+        guid = new_analysis_group['analysisGroupGuid']
+        new_analysis_group_model = DynamicAnalysisGroup.objects.filter(guid=guid).first()
+        self.assertIsNotNone(new_analysis_group_model)
+        self.assertEqual(new_analysis_group_model.name, new_analysis_group['name'])
+
+        # update the analysis_group
+        update_analysis_group_url = reverse(update_dynamic_analysis_group_handler, args=[PROJECT_GUID, guid])
+        response = self.client.post(update_analysis_group_url, content_type='application/json',  data=json.dumps(
+            {'name': 'updated_analysis_group', 'criteria': {'analysisStatus': ['I']}}))
+
+        self.assertEqual(response.status_code, 200)
+        updated_analysis_group_response = response.json()
+        self.assertEqual(len(updated_analysis_group_response['analysisGroupsByGuid']), 1)
+        updated_analysis_group = next(iter(updated_analysis_group_response['analysisGroupsByGuid'].values()))
+        self.assertEqual(updated_analysis_group['name'], 'updated_analysis_group')
+        self.assertDictEqual(updated_analysis_group['criteria'], {'analysisStatus': ['I']})
+
+        updated_analysis_group_model = DynamicAnalysisGroup.objects.filter(guid=guid).first()
+        self.assertIsNotNone(updated_analysis_group_model)
+        self.assertEqual(updated_analysis_group_model.name, updated_analysis_group['name'])
+        self.assertEqual(updated_analysis_group_model.criteria, updated_analysis_group['criteria'])
+
+        # delete the analysis_group
+        delete_analysis_group_url = reverse(delete_dynamic_analysis_group_handler, args=[PROJECT_GUID, guid])
+        response = self.client.post(delete_analysis_group_url, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(response.json(), {'analysisGroupsByGuid': {guid: None}})
+
+        # check that analysis_group was deleted
+        new_analysis_group = DynamicAnalysisGroup.objects.filter(guid=guid)
         self.assertEqual(len(new_analysis_group), 0)
