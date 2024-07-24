bf3b4066ffbd440541ea8e9d1afa9520b8058783
Hana Snow <hsnow@broadinstitute.org>
2024-07-24 10:47:22 -0400

    remove drs code

--- seqr/views/apis/igv_api.py
+++ seqr/views/apis/igv_api.py
@@ -158,16 +158,12 @@ def update_individual_igv_sample(request, individual_guid):
         if not file_path:
             raise ValueError('request must contain fields: filePath')
 
-        original_file_path = file_path
-        file_path = _get_valid_file_path(file_path, request.user)
-        if not file_path:
-            raise Exception('Error accessing "{}"'.format(original_file_path))
         sample_type = next((st for suffix, st in SAMPLE_TYPE_MAP if file_path.endswith(suffix)), None)
         if not sample_type:
             raise Exception('Invalid file extension for "{}" - valid extensions are {}'.format(
                 file_path, ', '.join([suffix for suffix, _ in SAMPLE_TYPE_MAP])))
-        if _is_drs_uri_path(file_path) and not request_json.get('indexFilePath'):
-            raise Exception('Index File Path is required for DRS URIs')
+        if not does_file_exist(file_path, user=request.user):
+            raise Exception('Error accessing "{}"'.format(file_path))
 
         sample, created = get_or_create_model_from_json(
             IgvSample, create_json={'individual': individual, 'sample_type': sample_type},
@@ -190,28 +186,6 @@ def update_individual_igv_sample(request, individual_guid):
         return create_json_response({'error': error}, status=400, reason=error)
 
 
-def _is_drs_uri_path(file_path):
-    return file_path.startswith('drs://')
-
-
-def _get_drs_info(file_path, user):
-    drs_path = file_path.split('/')
-    response = requests.get(
-        f'https://{drs_path[2]}/ga4gh/drs/v1/objects/{drs_path[3]}',
-        headers=_get_gs_auth_api_headers(user),
-    )
-
-    return response.json() if response.status_code == 200 else None
-
-
-def _get_valid_file_path(file_path, user):
-    if _is_drs_uri_path(file_path):
-        drs_info = _get_drs_info(file_path, user)
-        return None if drs_info is None else f"{file_path}/{drs_info['name']}"
-
-    return file_path if does_file_exist(file_path, user=user) else None
-
-
 @login_and_policies_required
 def fetch_igv_track(request, project_guid, igv_track_path):
 
@@ -223,45 +197,25 @@ def fetch_igv_track(request, project_guid, igv_track_path):
     if is_google_bucket_file_path(igv_track_path):
         return _stream_gs(request, igv_track_path)
 
-    if _is_drs_uri_path(igv_track_path):
-        return _stream_drs(request, igv_track_path)
-
     return _stream_file(request, igv_track_path)
 
 
 def _stream_gs(request, gs_path):
-    return _stream_response(
-        url=f"{GS_STORAGE_URL}/{gs_path.replace('gs://', '', 1)}",
-        headers=_get_gs_rest_api_headers(gs_path, user=request.user),
-        request=request)
+    headers = _get_gs_rest_api_headers(request.META.get('HTTP_RANGE'), gs_path, user=request.user)
 
-
-def _stream_drs(request, file_path):
-    drs_info = _get_drs_info(file_path, request.user)
-
-    https_access = next(a['access_url'] for a in drs_info['access_methods'] if a['type'] == 'https')
-    headers = dict([h.split(': ') for h in https_access['headers']])
-
-    return _stream_response(https_access['url'], headers, request)
-
-
-def _stream_response(url, headers, request):
-    range_header = request.META.get('HTTP_RANGE')
-    if range_header:
-        headers['Range'] = range_header
-
-    response = requests.get(url, headers=headers, stream=True, timeout=TIMEOUT)
+    response = requests.get(
+        f"{GS_STORAGE_URL}/{gs_path.replace('gs://', '', 1)}",
+        headers=headers,
+        stream=True, timeout=TIMEOUT)
 
     return StreamingHttpResponse(response.iter_content(chunk_size=65536), status=response.status_code,
                                  content_type='application/octet-stream')
 
 
-def _get_gs_auth_api_headers(user):
-    return {'Authorization': 'Bearer {}'.format(_get_access_token(user))}
-
-
-def _get_gs_rest_api_headers(gs_path, user):
-    headers = _get_gs_auth_api_headers(user)
+def _get_gs_rest_api_headers(range_header, gs_path, user=None):
+    headers = {'Authorization': 'Bearer {}'.format(_get_access_token(user))}
+    if range_header:
+        headers['Range'] = range_header
     google_project = get_google_project(gs_path)
     if google_project:
         headers['x-goog-user-project'] = get_google_project(gs_path)
