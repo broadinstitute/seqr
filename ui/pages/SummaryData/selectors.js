e6f2c465545d13e96d35efbe215735eb23368589
Hana Snow <hsnow@broadinstitute.org>
2024-09-03 17:35:31 -0400

    show vlm families grouped by contact email

--- ui/pages/SummaryData/selectors.js
+++ ui/pages/SummaryData/selectors.js
@@ -12,24 +12,28 @@ export const getMmeMetrics = state => state.mmeMetrics
 export const getMmeSubmissions = state => state.mmeSubmissions
 export const getExternalAnalysisUploadStats = state => state.externalAnalysisUploadStats
 
-export const geVlmDefaultContactEmailByFamily = createSelector(
+export const getVlmFamiliesByContactEmail = createSelector(
   getSortedIndividualsByFamily,
+  (state, ownProps) => ownProps.variant,
+  (individualsByFamily, variant) => (variant.lookupFamilyGuids || []).reduce((acc, familyGuid) => {
+    const individual = individualsByFamily[familyGuid]?.[0]
+    const contactEmail = individual?.projectGuid ? 'internal' : (individual?.vlmContactEmail || 'disabled')
+    return { ...acc, [contactEmail]: [...(acc[contactEmail] || []), familyGuid] }
+  }, {}),
+)
+
+export const getVlmDefaultContactEmails = createSelector(
+  getVlmFamiliesByContactEmail,
   getGenesById,
   getUser,
   (state, ownProps) => ownProps.variant,
-  (individualsByFamily, genesById, user, variant) => {
+  (familiesByContactEmail, genesById, user, variant) => {
     const gene = genesById[getVariantMainGeneId(variant)]?.geneSymbol
-    const defaultEmail = {
-      subject: `${gene || variant.variantId} variant match in seqr`,
-      //
-      body: `Dear researcher,\n\nWe are interested in learning more about your case in seqr harboring ${getVariantSummary(variant)} in ${gene || 'no genes'} (${window.location.href}).\n\nWe appreciate your assistance and look forward to hearing more from you.\n\nBest wishes,\n${user.displayName}`,
-    }
-    return (variant.lookupFamilyGuids || []).reduce((acc, familyGuid) => {
-      const individual = individualsByFamily[familyGuid]?.[0]
-      if (!individual || individual.projectGuid) {
-        return acc
-      }
-      return { ...acc, [familyGuid]: { ...defaultEmail, to: individual.vlmContactEmail } }
-    }, {})
+    const subject = `${gene || variant.variantId} variant match in seqr`
+    const defaultEmailContent = `harboring ${getVariantSummary(variant)} in ${gene || 'no genes'} (${window.location.href}).\n\nWe appreciate your assistance and look forward to hearing more from you.\n\nBest wishes,\n${user.displayName}`
+    return Object.entries(familiesByContactEmail).reduce((acc, [to, familyGuids]) => ({
+      ...acc,
+      [to]: { to, subject, body: `Dear researcher,\n\nWe are interested in learning more about your ${familyGuids.length} cases in seqr ${defaultEmailContent}` },
+    }), {})
   },
 )
