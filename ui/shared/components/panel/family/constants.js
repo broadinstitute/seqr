637474a8ea9a7781ce28622c634db435bf8d9b4a
Julia Klugherz <juliaklugherz@gmail.com>
2024-09-09 16:30:04 -0400

    igv fix - get cytobandURL from hgdownload.soe.ucsc.edu  (#4355)
    * try without proxy
    
    * try one first

--- ui/shared/components/panel/family/constants.js
+++ ui/shared/components/panel/family/constants.js
@@ -90,10 +90,10 @@ const REFERENCE_URLS = [
   },
   {
     key: 'cytobandURL',
-    baseUrl: `${BASE_REFERENCE_URL}/s3`,
+    baseUrl: 'https://hgdownload.soe.ucsc.edu/goldenPath',
     path: {
-      37: 'igv.broadinstitute.org/genomes/seq/hg19/cytoBand.txt',
-      38: 'igv.org.genomes/hg38/annotations/cytoBandIdeo.txt.gz',
+      37: 'hg19/database/cytoBand.txt.gz',
+      38: 'hg38/database/cytoBandIdeo.txt.gz',
     },
   },
   {
