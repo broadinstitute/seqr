a7aab22b350b9d1095ae83443048b2a6dc165d10
Julia Klugherz <juliaklugherz@gmail.com>
2024-09-10 11:42:02 -0400

    fix igv - aliasURL and Refseq (#4356)
    * try without proxy
    
    * try one first
    
    * aliasurl and refseq

--- ui/shared/components/panel/family/constants.js
+++ ui/shared/components/panel/family/constants.js
@@ -98,7 +98,7 @@ const REFERENCE_URLS = [
   },
   {
     key: 'aliasURL',
-    baseUrl: `${BASE_REFERENCE_URL}/s3/igv.org.genomes`,
+    baseUrl: 'https://igv-genepattern-org.s3.amazonaws.com/genomes',
     path: {
       37: 'hg19/hg19_alias.tab',
       38: 'hg38/hg38_alias.tab',
@@ -121,7 +121,7 @@ const REFERENCE_TRACKS = [
   {
     name: 'Refseq',
     indexPostfix: 'tbi',
-    baseUrl: `${BASE_REFERENCE_URL}/s3/igv.org.genomes`,
+    baseUrl: 'https://s3.amazonaws.com/igv.org.genomes',
     path: {
       37: 'hg19/refGene.sorted.txt.gz',
       38: 'hg38/refGene.sorted.txt.gz',
