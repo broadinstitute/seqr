b127d9327218e2ae9d08f4fcbec5ae45076a10bd
Hana Snow <hsnow@broadinstitute.org>
2024-11-07 14:44:23 -0500

    undo reference data update changes

--- reference_data/management/commands/update_all_reference_data.py
+++ reference_data/management/commands/update_all_reference_data.py
@@ -20,8 +20,6 @@ from reference_data.management.commands.update_refseq import RefseqReferenceData
 logger = logging.getLogger(__name__)
 
 REFERENCE_DATA_SOURCES = OrderedDict([
-    ("gencode", None),
-    ("omim", CachedOmimReferenceDataHandler),
     ("dbnsfp_gene", DbNSFPReferenceDataHandler),
     ("gene_constraint", GeneConstraintReferenceDataHandler),
     ("gene_cn_sensitivity", CNSensitivityReferenceDataHandler),
@@ -43,45 +41,42 @@ class Command(BaseCommand):
         omim_options.add_argument('--use-cached-omim', help='Use parsed OMIM from google storage', action='store_true')
         omim_options.add_argument('--skip-omim', help="Don't reload gene constraint", action="store_true")
 
+        parser.add_argument('--skip-gencode', help="Don't reload gencode", action="store_true")
+
         for source in REFERENCE_DATA_SOURCES.keys():
-            if source == 'omim':
-                continue
             parser.add_argument(
                 '--skip-{}'.format(source.replace('_', '-')), help="Don't reload {}".format(source), action="store_true"
             )
 
     def handle(self, *args, **options):
-        should_skip = lambda source, data_handler: options[f'skip_{source}']
-        data_handler_override = {}
-        if not (options['use_cached_omim'] or options['skip_omim']):
-            data_handler_override['omim'] = lambda: OmimReferenceDataHandler(options["omim_key"])
-
-        self._update_all_reference_data_sources(should_skip, data_handler_override)
-
-    @staticmethod
-    def _update_gencode():
-        # Download latest version first, and then add any genes from old releases not included in the latest release
-        # Old gene ids are used in the gene constraint table and other datasets, as well as older sequencing data
-        update_gencode(LATEST_GENCODE_RELEASE, reset=True)
-        for release in OLD_GENCODE_RELEASES:
-            update_gencode(release)
-
-    @classmethod
-    def _update_all_reference_data_sources(cls, should_skip, data_handler_override=None):
         updated = []
         update_failed = []
 
+        if not options["skip_gencode"]:
+            # Download latest version first, and then add any genes from old releases not included in the latest release
+            # Old gene ids are used in the gene constraint table and other datasets, as well as older sequencing data
+            update_gencode(LATEST_GENCODE_RELEASE, reset=True)
+            for release in OLD_GENCODE_RELEASES:
+                update_gencode(release)
+            updated.append('gencode')
+
+        if not options["skip_omim"]:
+            try:
+                omim_handler = CachedOmimReferenceDataHandler() if options['use_cached_omim'] else \
+                    OmimReferenceDataHandler(options["omim_key"])
+                update_records(omim_handler)
+                updated.append('omim')
+            except Exception as e:
+                logger.error("unable to update omim: {}".format(e))
+                update_failed.append('omim')
+
         for source, data_handler in REFERENCE_DATA_SOURCES.items():
-            if not should_skip(source, data_handler):
+            if not options["skip_{}".format(source)]:
                 try:
                     if data_handler:
-                        if data_handler_override and source in data_handler_override:
-                            data_handler = data_handler_override[source]
                         update_records(data_handler())
                     elif source == "hpo":
                         update_hpo()
-                    elif source == "gencode":
-                        cls._update_gencode()
                     updated.append(source)
                 except Exception as e:
                     logger.error("unable to update {}: {}".format(source, e))
@@ -92,5 +87,3 @@ class Command(BaseCommand):
             logger.info("Updated: {}".format(', '.join(updated)))
         if update_failed:
             logger.info("Failed to Update: {}".format(', '.join(update_failed)))
-
-update_all_reference_data_sources = Command._update_all_reference_data_sources
