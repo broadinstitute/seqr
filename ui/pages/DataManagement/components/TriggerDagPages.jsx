ba56f6fe5bd3ca54d09ff741535d71b5903a7234
Hana Snow <hsnow@broadinstitute.org>
2025-02-27 09:01:33 -0500

    dag trigger ui forms

--- ui/pages/DataManagement/components/TriggerDagPages.jsx
+++ ui/pages/DataManagement/components/TriggerDagPages.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { validators } from 'shared/components/form/FormHelpers'
+import { Select } from 'shared/components/form/Inputs'
 import { AwesomeBarFormInput } from 'shared/components/page/AwesomeBar'
 import SubmitFormPage from 'shared/components/page/SubmitFormPage'
+import {
+  DATASET_TYPE_SNV_INDEL_CALLS,
+  DATASET_TYPE_SV_CALLS,
+  DATASET_TYPE_MITO_CALLS,
+  GENOME_VERSION_FIELD,
+} from 'shared/utils/constants'
 
-const FIELDS = [
+const DATASET_TYPE_FIELD = {
+  name: 'datasetType',
+  label: 'Dataset Type',
+  labelHelp: 'Optionally specify one dataset type to run. If omitted, all dataset types will be run.',
+  component: Select,
+  options: [
+    DATASET_TYPE_SNV_INDEL_CALLS, DATASET_TYPE_MITO_CALLS, DATASET_TYPE_SV_CALLS,
+  ].map(value => ({ value, name: value })),
+}
+const PROJECT_FIELDS = [
   {
     name: 'project',
     label: 'Project',
@@ -15,31 +31,48 @@ const FIELDS = [
     placeholder: 'Search for a project',
     validate: validators.required,
   },
-  // TODO optional dataset type
+  DATASET_TYPE_FIELD,
+]
+const FAMILY_FIELDS = [
+  {
+    name: 'family',
+    label: 'Family',
+    control: AwesomeBarFormInput,
+    categories: ['families'],
+    fluid: true,
+    placeholder: 'Search for a family',
+    validate: validators.required,
+  },
+  DATASET_TYPE_FIELD,
+]
+const REFERENCE_DATASET_FIELDS = [
+  { ...GENOME_VERSION_FIELD, validate: validators.required },
+  { ...DATASET_TYPE_FIELD, validate: validators.required, labelHelp: null },
 ]
 
-const TriggerDagForm = ({ dagName }) => (
+const TriggerDagForm = ({ dagName, fields }) => (
   <SubmitFormPage
     header={`Trigger ${dagName} DAG`}
     url={`/api/data_management/trigger_dag/${dagName}`}
-    fields={FIELDS}
+    fields={fields}
   />
 )
 
 TriggerDagForm.propTypes = {
   dagName: PropTypes.string,
+  fields: PropTypes.arrayOf(PropTypes.object),
 }
 
 const TriggerDeleteProjectsDag = () => (
-  <TriggerDagForm dagName="DELETE_PROJECTS" />
+  <TriggerDagForm dagName="DELETE_PROJECTS" fields={PROJECT_FIELDS} />
 )
 
 const TriggerDeleteFamiliesDag = () => (
-  <TriggerDagForm dagName="DELETE_FAMILIES" />
+  <TriggerDagForm dagName="DELETE_FAMILIES" fields={FAMILY_FIELDS} />
 )
 
 const TriggerUpdateReferenceDatasetDag = () => (
-  <TriggerDagForm dagName="UPDATE_REFERENCE_DATASETS" />
+  <TriggerDagForm dagName="UPDATE_REFERENCE_DATASETS" fields={REFERENCE_DATASET_FIELDS} />
 )
 
 export default [
