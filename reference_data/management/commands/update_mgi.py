1541fb59b210c531ab5bf7145aa92fc7a4641431
Hana Snow <hsnow@broadinstitute.org>
2025-03-19 17:24:53 -0400

    delete single data source update commands

--- reference_data/management/commands/update_mgi.py
+++
@@ -1,9 +0,0 @@
-from reference_data.management.commands.utils.update_utils import GeneCommand, ReferenceDataHandler
-from reference_data.models import MGI
-
-class MGIReferenceDataHandler(ReferenceDataHandler):
-
-    model_cls = MGI
-
-class Command(GeneCommand):
-    reference_data_handler = MGIReferenceDataHandler
--- reference_data/management/commands/update_refseq.py
+++
@@ -1,9 +0,0 @@
-from reference_data.management.commands.utils.update_utils import GeneCommand, ReferenceDataHandler
-from reference_data.models import RefseqTranscript
-
-class RefseqReferenceDataHandler(ReferenceDataHandler):
-
-    model_cls = RefseqTranscript
-
-class Command(GeneCommand):
-    reference_data_handler = RefseqReferenceDataHandler
