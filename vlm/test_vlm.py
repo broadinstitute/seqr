6af4010622dd0738ccb4b83deda819ad252b7769
Hana Snow <hsnow@broadinstitute.org>
2025-03-21 17:37:32 -0400

    fix test

--- vlm/test_vlm.py
+++ vlm/test_vlm.py
@@ -97,9 +97,9 @@ class VlmTestCase(AioHTTPTestCase):
             f'https://vlm-auth.us.auth0.com/api/v2/clients/{REQUESTER_CLIENT_ID}',
             headers={'Authorization': 'Bearer test_token'},
         )
-        self.assertEqual(
-            self._caplog.messages[-2],
+        self.assertIn(
             'Received match request from Test Node: assemblyId=GRCh38&referenceName=1&start=38724419&referenceBases=T&alternateBases=G',
+            self._caplog.messages,
         )
 
         only_37_response = {
