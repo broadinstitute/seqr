f4e1da975760e11b522229b7284fd8147eb24b3b
Hana Snow <hsnow@broadinstitute.org>
2025-04-30 16:25:51 -0400

    fix clinvar field map

--- clickhouse_search/search.py
+++ clickhouse_search/search.py
@@ -22,8 +22,8 @@ ANNOTATION_VALUES = {
 }
 
 CLINVAR_FIELDS = OrderedDict({
-    f'key__clinvar__{field.name}': (field.db_column or field.name, field.formfield())
-    for field in Clinvar._meta.local_fields
+    f'key__clinvar__{field.name}': (field.db_column or field.name, field.clone())
+    for field in Clinvar._meta.local_fields if field.name not in CORE_ENTRIES_FIELDS
 })
 
 GENOTYPE_FIELDS = OrderedDict({
