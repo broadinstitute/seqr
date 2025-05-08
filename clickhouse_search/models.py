bbe8e98204e6808add7ac7feed98c6e85e78a025
Hana Snow <hsnow@broadinstitute.org>
2025-05-08 16:39:05 -0400

    debug

--- clickhouse_search/models.py
+++ clickhouse_search/models.py
@@ -206,6 +206,12 @@ class AnnotationsSnvIndel(BaseAnnotationsSnvIndel):
         db_table = 'GRCh38/SNV_INDEL/annotations_memory'
         engine = EmbeddedRocksDB(0, f'{CLICKHOUSE_IN_MEMORY_DIR}/GRCh38/SNV_INDEL/annotations', primary_key='key')
 
+    def _save_table(self, *args, **kwargs):
+        from django.db import connections
+        with connections['clickhouse'].cursor() as cursor:
+            print(cursor.execute('SHOW TABLE "GRCh38/SNV_INDEL/annotations_memory"'))
+        return super()._save_table(*args, **kwargs)
+
 
 # Future work: create an alias and manager to switch between disk/in-memory annotations
 class AnnotationsDiskSnvIndel(BaseAnnotationsSnvIndel):
