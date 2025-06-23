a4e0af9edbb4d2c5823e19a98e5e67158de9a7b9
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-06-23 11:19:03 -0400

    feat: add key lookup tables

--- clickhouse_search/models.py
+++ clickhouse_search/models.py
@@ -1165,3 +1165,48 @@ class TranscriptsSnvIndel(models.ClickhouseModel):
     class Meta:
         db_table = 'GRCh38/SNV_INDEL/transcripts'
         engine = EmbeddedRocksDB(0, f'{CLICKHOUSE_DATA_DIR}/GRCh38/SNV_INDEL/transcripts', primary_key='key', flatten_nested=0)
+
+class KeyLookup(models.ClickhouseModel):
+    variant_id = models.StringField(db_column='variantId')
+
+    class Meta:
+        abstract = True
+
+class KeyLookupGRCh37SnvIndel(KeyLookup):
+    key = OneToOneField('AnnotationsGRCh37SnvIndel', db_column='key', primary_key=True, on_delete=CASCADE)
+
+    class Meta:
+        db_table = 'GRCh37/SNV_INDEL/key_lookup'
+        engine = EmbeddedRocksDB(0, f'{CLICKHOUSE_DATA_DIR}/GRCh37/SNV_INDEL/key_lookup', primary_key='variantId', flatten_nested=0)
+
+class KeyLookupSnvIndel(KeyLookup):
+    key = OneToOneField('AnnotationsSnvIndel', db_column='key', primary_key=True, on_delete=CASCADE)
+
+    class Meta:
+        db_table = 'GRCh38/SNV_INDEL/key_lookup'
+        engine = EmbeddedRocksDB(0, f'{CLICKHOUSE_DATA_DIR}/GRCh38/SNV_INDEL/key_lookup', primary_key='variantId', flatten_nested=0)
+
+
+class KeyLookupMito(KeyLookup):
+    key = OneToOneField('AnnotationsMito', db_column='key', primary_key=True, on_delete=CASCADE)
+
+    class Meta:
+        db_table = 'GRCh38/MITO/key_lookup'
+        engine = EmbeddedRocksDB(0, f'{CLICKHOUSE_DATA_DIR}/GRCh38/MITO/key_lookup', primary_key='variantId', flatten_nested=0)
+
+
+class KeyLookupSv(KeyLookup):
+    key = OneToOneField('AnnotationsSv', db_column='key', primary_key=True, on_delete=CASCADE)
+
+    class Meta:
+        db_table = 'GRCh38/SV/key_lookup'
+        engine = EmbeddedRocksDB(0, f'{CLICKHOUSE_DATA_DIR}/GRCh38/SV/key_lookup', primary_key='variantId', flatten_nested=0)
+
+
+class KeyLookupGcnv(KeyLookup):
+    key = OneToOneField('AnnotationsGcnv', db_column='key', primary_key=True, on_delete=CASCADE)
+
+    class Meta:
+        db_table = 'GRCh38/GCNV/key_lookup'
+        engine = EmbeddedRocksDB(0, f'{CLICKHOUSE_DATA_DIR}/GRCh38/GCNV/key_lookup', primary_key='variantId', flatten_nested=0)
+
