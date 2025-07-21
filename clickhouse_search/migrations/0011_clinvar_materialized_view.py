363680ae616b897b93aef1844a99494e4ce5f03a
Benjamin Blankenmeister <b.p.blankenmeister@gmail.com>
2025-07-21 15:30:42 -0400

    feat: clinvar all variants job (#4886)
    * clinvar all variants
    
    * rework it
    
    * unused file
    
    * column selection improvement
    
    * starting to sprawl
    
    * Passing test
    
    * remove maxDiff
    
    * flip fixture
    
    * remove ttl
    
    * updates
    
    * simplify test
    
    * flesh out tests
    
    * some cleanup and new test
    
    * codacy
    
    * using clickhouse write
    
    * fix perms
    
    * improve coverage
    
    * Throw command error
    
    * ws
    
    * cleanup exception handling
    
    * coverage improvements
    
    * coverage
    
    * coverage
    
    * Update models.py
    
    * update regex
    
    * Remove support for no password
    
    * clean up parsing
    
    * handle edge case in log
    
    * more complete
    
    * missing colon
    
    * use a fixture
    
    * ws
    
    * change model name
    
    * PR comments
    
    * still working
    
    * finish coverage
    
    * remove conflicting data
    
    * finish test
    
    * cleanup
    
    * no max diff
    
    * ws
    
    * revert assertions change
    
    * test and ws
    
    * ws
    
    * ws
    
    * move fixture
    
    * rename to mv

---
+++ clickhouse_search/migrations/0011_clinvar_materialized_view.py
@@ -0,0 +1,56 @@
+# Generated manually by the seqr team.
+from string import Template
+
+from django.db import migrations
+
+
+CLINVAR_ALL_VARIANTS_TO_CLINVAR_MV = Template("""
+CREATE MATERIALIZED VIEW `$reference_genome/$dataset_type/clinvar_all_variants_to_clinvar_mv`
+REFRESH EVERY 10 YEAR
+TO `$reference_genome/$dataset_type/clinvar`
+AS 
+SELECT
+    DISTINCT ON (key)
+    kl.key as key, 
+    alleleId,
+    conflictingPathogenicities,
+    goldStars,
+    submitters,
+    conditions,
+    assertions,
+    pathogenicity
+FROM `$reference_genome/$dataset_type/clinvar_all_variants` c
+INNER JOIN `$reference_genome/$dataset_type/key_lookup` kl
+ON c.variantId = kl.variantId
+""")
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('clickhouse_search', '0010_clinvarallvariantsgrch37snvindel_and_more'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            CLINVAR_ALL_VARIANTS_TO_CLINVAR_MV.substitute(
+                reference_genome='GRCh37',
+                dataset_type='SNV_INDEL',
+            ),
+            hints={'clickhouse': True},
+        ),
+        migrations.RunSQL(
+            CLINVAR_ALL_VARIANTS_TO_CLINVAR_MV.substitute(
+                reference_genome='GRCh38',
+                dataset_type='SNV_INDEL',
+            ),
+            hints={'clickhouse': True},
+        ),
+        migrations.RunSQL(
+            CLINVAR_ALL_VARIANTS_TO_CLINVAR_MV.substitute(
+                reference_genome='GRCh38',
+                dataset_type='MITO',
+            ),
+            hints={'clickhouse': True},
+        ),
+    ]
+
