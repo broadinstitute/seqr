397fce19afcc5a227c857db1edd303d767c08308
Hana Snow <hsnow@broadinstitute.org>
2025-07-29 17:22:45 -0400

    fix test

--- seqr/views/apis/project_api_tests.py
+++ seqr/views/apis/project_api_tests.py
@@ -427,7 +427,7 @@ class ProjectAPITest(object):
             len(response_json['familiesByGuid'][family_guid]['discoveryTags']) for family_guid in no_discovery_families
         }, {0})
 
-        self.assertSetEqual(set(response_json['genesById'].keys()), {'ENSG00000135953'})
+        self.assertSetEqual(set(response_json['genesById'].keys()), {'ENSG00000135953', 'ENSG00000240361'})
 
         # Test empty project
         empty_url = reverse(project_families, args=[EMPTY_PROJECT_GUID])
