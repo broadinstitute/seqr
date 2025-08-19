b2f087abfd869e3377907d43138d0b89718766bf
Hana Snow <hsnow@broadinstitute.org>
2025-08-19 14:20:04 -0400

    correctly handle different genotype annotations for different dataset types

--- clickhouse_search/search.py
+++ clickhouse_search/search.py
@@ -178,27 +178,40 @@ def _get_comp_het_results_queryset(annotations_cls, primary_q, secondary_q, num_
         secondary_family_expr = 'secondary_familyGuids' if results.has_annotation('secondary_familyGuids') else ArrayMap(
             'secondary_familyGenotypes', mapped_expression='x.1',
         )
-        genotype_expressions = {
-            'primary_genotypes': ArrayFilter('primary_genotypes', conditions=[
-                {2: ('arrayIntersect(primary_familyGuids, secondary_familyGuids)', 'has({value}, {field})')},
-            ]),
-            'secondary_genotypes': ArrayFilter('secondary_genotypes', conditions=[
-                {2: ('arrayIntersect(primary_familyGuids, secondary_familyGuids)', 'has({value}, {field})')},
-            ]),
-            'primary_familyGenotypes': ArrayFilter('primary_familyGenotypes', conditions=[
-                {1: (None, 'arrayExists(g -> g.1 = {field}, secondary_familyGenotypes)')},
-            ]),
-            'secondary_familyGenotypes': ArrayFilter('secondary_familyGenotypes', conditions=[
-                {1: (None, 'arrayExists(g -> g.1 = {field}, primary_familyGenotypes)')},
-            ]),
-        }
+        if results.has_annotation('primary_genotypes'):
+            genotype_expressions = {
+                'primary_genotypes': ArrayFilter('primary_genotypes', conditions=[
+                    {2: ('arrayIntersect(primary_familyGuids, secondary_familyGuids)', 'has({value}, {field})')},
+                ]),
+                'secondary_genotypes': ArrayFilter('secondary_genotypes', conditions=[
+                    {2: ('arrayIntersect(primary_familyGuids, secondary_familyGuids)', 'has({value}, {field})')},
+                ]),
+            }
+        elif results.has_annotation('secondary_genotypes'):
+            genotype_expressions = {
+                'primary_familyGenotypes': ArrayFilter('primary_familyGenotypes', conditions=[
+                    {1: ('secondary_familyGuids', 'has({value}, {field})')},
+                ]),
+                'secondary_genotypes': ArrayFilter('secondary_genotypes', conditions=[
+                    {2: (None, 'arrayExists(g -> g.1 = {field}, primary_familyGenotypes)')},
+                ]),
+            }
+        else:
+            genotype_expressions = {
+                'primary_familyGenotypes': ArrayFilter('primary_familyGenotypes', conditions=[
+                    {1: (None, 'arrayExists(g -> g.1 = {field}, secondary_familyGenotypes)')},
+                ]),
+                'secondary_familyGenotypes': ArrayFilter('secondary_familyGenotypes', conditions=[
+                    {1: (None, 'arrayExists(g -> g.1 = {field}, primary_familyGenotypes)')},
+                ]),
+            }
         results = results.annotate(
             primary_familyGuids=ArrayIntersect(
                 primary_family_expr, secondary_family_expr, output_field=ArrayField(StringField()),
             ),
         ).filter(primary_familyGuids__not_empty=True).annotate(
             secondary_familyGuids=F('primary_familyGuids'),
-            **{k: v for k, v in genotype_expressions.items() if results.has_annotation(k)},
+            **genotype_expressions,
         )
 
     return results.annotate(
