e1dc970738370d562cd9c54ea63f0b97731fceee
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-10-17 10:35:04 -0400

    update joinGet

--- clickhouse_search/migrations/0017_projectpartitionssnvindel.py
+++ clickhouse_search/migrations/0017_projectpartitionssnvindel.py
@@ -20,17 +20,17 @@ def conditionally_recreate_repartitioned_snv_indel_entries(apps, schema_editor):
         if not row:
             return
         create_table_query = row[0]
-        if 'farmHash64' in create_table_query:
+        if 'project_partitions' in create_table_query:
             return
         cursor.execute(f"SELECT COUNT(*) FROM `GRCh38/SNV_INDEL/entries`;")
         if cursor.fetchone()[0] > 0:
             return 
-        new_partition_by = "(project_guid, farmHash64(family_guid) % coalesce(joinGet('seqr.project_partitions', 'n_partitions', project_guid), 1))"
+        new_partition_by = "(project_guid, farmHash64(family_guid) % coalesce(joinGet(concat(currentDatabase(), '.project_partitions'), 'n_partitions', project_guid), 1))"
         create_table_query = create_table_query.replace(
             'PARTITION BY project_guid',
             f'PARTITION BY {new_partition_by}',
         )
-        cursor.execute(f"DROP TABLE `GRCh38/SNV_INDEL/entries`")
+        cursor.execute('DROP TABLE `GRCh38/SNV_INDEL/entries`')
         cursor.execute(create_table_query)
 
 
