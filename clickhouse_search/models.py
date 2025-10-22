643b5f9061ed5c63e211850867afda902cb179d3
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-10-22 10:51:19 -0400

    pext

--- clickhouse_search/models.py
+++ clickhouse_search/models.py
@@ -961,6 +961,33 @@ class ProjectPartitionsSnvIndel(FixtureLoadableClickhouseModel):
         )
 
 
+class PextAllVariantsSnvIndel(models.ClickhouseModel):
+    chrom = Enum8Field(return_int=False, choices=BaseAnnotations.CHROMOSOME_CHOICES, primary_key=True)
+    pos = models.UInt32Field()
+    exp_prop_mean = models.DecimalField(max_digits=9, decimal_places=5, null=True, blank=True)
+
+    class Meta:
+        unique_together = (('chrom', 'pos'),)
+        db_table = 'GRCh38/SNV_INDEL/reference_data/pext/all'
+        engine = models.MergeTree(
+            primary_key=('chrom', 'pos'),
+            order_by=('chrom', 'pos'),
+        )
+
+class PextAllVariantsMito(models.ClickhouseModel):
+    chrom = Enum8Field(return_int=False, choices=[(1, 'M')], primary_key=True)
+    pos = models.UInt32Field()
+    exp_prop_mean = models.DecimalField(max_digits=9, decimal_places=5, null=True, blank=True)
+
+    class Meta:
+        unique_together = (('chrom', 'pos'),)
+        db_table = 'GRCh38/MITO/reference_data/pext/all'
+        engine = models.MergeTree(
+            primary_key=('chrom', 'pos'),
+            order_by=('chrom', 'pos'),
+        )
+
+
 ENTRY_CLASS_MAP = {
     GENOME_VERSION_GRCh37: {Sample.DATASET_TYPE_VARIANT_CALLS: EntriesGRCh37SnvIndel},
     GENOME_VERSION_GRCh38: {
