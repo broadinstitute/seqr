7cfbe3c61ce479a66a60dc3db6e051d5defbb372
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-10-23 13:36:56 -0400

    tmpfile

--- seqr/management/commands/reload_clinvar_all_variants.py
+++ seqr/management/commands/reload_clinvar_all_variants.py
@@ -251,7 +251,7 @@ class Command(BaseCommand):
         new_version = None
         with requests.get(WEEKLY_XML_RELEASE, stream=True, timeout=10) as r, tempfile.NamedTemporaryFile(delete=False) as tmpfile:
             r.raise_for_status()
-            shutil.copyfileobj(r.raw, tmp_file)
+            shutil.copyfileobj(r.raw, tmpfile)
         with gzip.open(tmpfile.name, 'rb') as gzipped_file:
             for event, elem in ET.iterparse(gzipped_file, events=('start', 'end')):
                 # Handle parsing the current date.
