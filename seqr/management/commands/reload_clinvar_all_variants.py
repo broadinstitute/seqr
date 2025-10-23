bbf0b635c1d7eb0c4f66a21ce24175d1942a6fc2
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-10-23 12:49:51 -0400

    download file, then parse

--- seqr/management/commands/reload_clinvar_all_variants.py
+++ seqr/management/commands/reload_clinvar_all_variants.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 from settings import SEQR_SLACK_DATA_ALERTS_NOTIFICATION_CHANNEL
 import re
 from string import Template
+import tempfile
 from typing import Optional, Union
 
 from clickhouse_backend import models
@@ -247,8 +248,11 @@ class Command(BaseCommand):
             ClinvarAllVariantsMito: [],
         }
         new_version = None
-        with requests.get(WEEKLY_XML_RELEASE, stream=True, timeout=10) as r:
+        with requests.get(WEEKLY_XML_RELEASE, stream=True, timeout=10) as r, tempfile.NamedTemporaryFile(delete=False) as tmpfile:
             r.raise_for_status()
+            for chunk in r.iter_content(chunk_size=8192):
+                tmpfile.write(chunk)
+        with gzip.open(tmpfile.name, 'rb') as gzipped_file:
             for event, elem in ET.iterparse(gzip.GzipFile(fileobj=r.raw), events=('start', 'end',)):
                 # Handle parsing the current date.
                 if event == 'start' and elem.tag == 'ClinVarVariationRelease':
