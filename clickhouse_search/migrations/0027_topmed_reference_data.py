4cbd61cc52e5433c8e381d1623ee880a5db337b0
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2025-12-02 22:46:16 -0500

    safe substitute

--- clickhouse_search/migrations/0027_topmed_reference_data.py
+++ clickhouse_search/migrations/0027_topmed_reference_data.py
@@ -10,7 +10,7 @@ import django.db.models.manager
 CLICKHOUSE_WRITER_PASSWORD = os.environ.get('CLICKHOUSE_WRITER_PASSWORD', 'clickhouse_test')
 CLICKHOUSE_WRITER_USER = os.environ.get('CLICKHOUSE_WRITER_USER', 'clickhouse')
 
-TOPMED_SEARCH = Template("""
+TOPMED_SEARCH = Template(Template("""
 CREATE DICTIONARY `$reference_genome/SNV_INDEL/reference_data/topmed`
 (
     `key` UInt32,
@@ -28,7 +28,7 @@ SOURCE(CLICKHOUSE(
 ))
 LIFETIME(MIN 0 MAX 0)
 LAYOUT(FLAT(MAX_ARRAY_SIZE 1000000000))
-""").substitute(
+""")).safe_substitute(
     # Note the nested Template-ing that allows
     # double substitution these shared values
     clickhouse_writer_user=CLICKHOUSE_WRITER_USER,
