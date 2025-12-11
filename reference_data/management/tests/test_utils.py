d195e69377f055ee8e94d72d69664a8f5119a3ff
Hana Snow <hsnow@broadinstitute.org>
2025-12-11 11:24:52 -0500

    update tests

--- reference_data/management/tests/test_utils.py
+++ reference_data/management/tests/test_utils.py
@@ -20,13 +20,6 @@ class ReferenceDataCommandTestCase(AuthenticationTestCase):
     def setUp(self):
         super().setUp()
 
-        patcher = mock.patch('reference_data.models.logger')
-        self.mock_logger = patcher.start()
-        self.addCleanup(patcher.stop)
-        patcher = mock.patch('reference_data.management.commands.update_all_reference_data.logger')
-        self.mock_command_logger = patcher.start()
-        self.addCleanup(patcher.stop)
-
         self.tmp_dir = tempfile.gettempdir()
         patcher = mock.patch('reference_data.utils.download_utils.tempfile')
         self.mock_tempfile = patcher.start()
@@ -57,42 +50,47 @@ class ReferenceDataCommandTestCase(AuthenticationTestCase):
         call_command('update_all_reference_data', *(command_args or []))
 
 
-    def _test_update_command(self, model_name, expected_version, existing_records=1, created_records=1, skipped_records=1, head_response=None, command_args=None):
+    def _test_update_command(self, model_name, expected_version, existing_records=1, created_records=1, skipped_records=1, head_response=None, command_args=None, version_check_download=False):
         DataVersions.objects.filter(data_model_name=model_name).delete()
 
         # test without a file_path parameter
         self._run_command(self.DATA, head_response=head_response, command_args=command_args)
 
-        self.mock_logger.error.assert_not_called()
         tmp_file = '{}/{}'.format(self.tmp_dir, self.URL.split('/')[-1])
+        download_log = (f'Downloading {self.URL} to {tmp_file}', None)
         log_calls = [
-            mock.call(f'Updating {model_name}'),
-            mock.call(f'Parsing file {tmp_file}'),
-            mock.call(f'Deleted {existing_records} {model_name} records'),
-            mock.call(f'Created {created_records} {model_name} records'),
-            mock.call('Done'),
-            mock.call(f'Loaded {created_records} {model_name} records'),
+            (f'Updating {model_name}', None),
+            download_log,
+            (f'Parsing file {tmp_file}', None),
+            (f'Deleted {existing_records} {model_name} records', None),
+            (f'Created {created_records} {model_name} records', None),
+            ('Done', None),
+            (f'Loaded {created_records} {model_name} records', None),
         ]
+        if version_check_download:
+            log_calls.insert(0, download_log)
         if skipped_records:
-            log_calls.append(mock.call(f'Skipped {skipped_records} records with unrecognized genes.'))
-        self.mock_logger.info.assert_has_calls(log_calls)
-        self.mock_command_logger.error.assert_not_called()
-        self.mock_command_logger.info.assert_has_calls([
-            mock.call('Done'),
-            mock.call(f'Updated: {model_name}'),
-        ])
+            log_calls.append((f'Skipped {skipped_records} records with unrecognized genes.', None))
+        log_calls += [
+            ('Done', None),
+            (f'Updated: {model_name}', None),
+        ]
+        self.assert_json_logs(user=None, expected=log_calls)
 
         dv = DataVersions.objects.get(data_model_name=model_name)
         self.assertEqual(dv.version, expected_version)
 
         # test with a locally cached file
         dv.delete()
-        self.mock_logger.reset_mock()
+        self.reset_logs()
         headers = head_response['headers'] if head_response else {}
         head_response = {
             **(head_response or {}),
             'headers': {**headers, "Content-Length": f"{os.path.getsize(tmp_file)}"},
         }
         self._run_command(data=None, head_response=head_response, command_args=command_args)
-        log_calls[2] = mock.call(f'Deleted {created_records} {model_name} records')
-        self.mock_logger.info.assert_has_calls(log_calls)
+        download_log_indices = [0, 2] if version_check_download else [1]
+        for index in download_log_indices:
+            log_calls[index] = (f'Re-using {tmp_file} previously downloaded from {self.URL}', None)
+        log_calls[download_log_indices[-1] + 2] = (f'Deleted {created_records} {model_name} records', None)
+        self.assert_json_logs(user=None, expected=log_calls)
