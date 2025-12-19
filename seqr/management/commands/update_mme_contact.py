559278274fe2db76ab713dcbccef0c80323410e5
Hana Snow <hsnow@broadinstitute.org>
2025-12-19 13:21:24 -0500

    fix update script

--- seqr/management/commands/update_mme_contact.py
+++ seqr/management/commands/update_mme_contact.py
@@ -10,17 +10,28 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('email', help='MME contact email to replace')
-        parser.add_argument('--replace-email', '-r', help='optional email to replace with')
+        parser.add_argument('--replace-email', '-re', help='optional email to replace with')
+        parser.add_argument('--replace-name', '-rn', help='optional name to replace with')
 
     def handle(self, *args, **options):
         email = options['email']
-        replace_email = options['replace_email'] or ''
-        submissions = MatchmakerSubmission.objects.filter(contact_href__contains=email)
-        logger.info('Updating {} submissions'.format(len(submissions)))
+        replace_email = options['replace_email']
+        submissions = MatchmakerSubmission.objects.all()
+        num_updated = 0
+        skipped = []
         for submission in tqdm(submissions):
-            contact = submission.contact_href
-            if not replace_email:
-                contact = contact.replace('{},'.format(email), replace_email)
-            submission.contact_href = contact.replace(email, replace_email).rstrip().rstrip(',')
+            contacts = [contact for contact in submission.contacts if contact['email'] != email]
+            if len(contacts) == len(submission.contacts):
+                continue
+            if replace_email:
+                contacts.append({'name': options['replace_name'] or '', 'email': replace_email})
+            if not any(c.get('name') for c in contacts):
+                skipped.append(submission.label)
+                continue
+            submission.contacts = contacts
             submission.save()
+            num_updated += 1
+        logger.info(f'Updated {num_updated} submissions')
+        if skipped:
+            logger.info(f'Skipped updating submissions with no remaining valid contacts: {", ".join(skipped)}')
         logger.info('Done')
