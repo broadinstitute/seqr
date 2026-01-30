1356951a93a67d8bcc2ec9d66618ec32ff314b38
Hana Snow <hsnow@broadinstitute.org>
2026-01-30 16:51:30 -0500

    Revert "maybe here instead"
    This reverts commit 241408ae106c935c121a3ffae4a671bd9093d3e0.

--- clickhouse_search/search_tests.py
+++ clickhouse_search/search_tests.py
@@ -39,8 +39,8 @@ from seqr.views.apis.variant_search_api import query_variants_handler
 
 class ClickhouseSearchTestCase(AnvilAuthenticationTestCase):
 
-    def _pre_setup(self):
-        super()._pre_setup()
+    def setUp(self):
+        super().setUp()
         AffectedDict.reload()
         SexDict.reload()
 
