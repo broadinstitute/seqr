02cd4d329848fcbf169966115295276b23b8129e
Hana Snow <hsnow@broadinstitute.org>
2026-04-06 14:51:25 -0400

    fix seqr stats

--- seqr/views/apis/report_api.py
+++ seqr/views/apis/report_api.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 from datetime import datetime, timedelta
-from django.db.models import Count, Q, F, Value
+from django.db.models import Count, Q, F, Sum, Value
 from django.contrib.postgres.aggregates import ArrayAgg
 import json
 import re
@@ -24,7 +24,7 @@ from seqr.views.utils.permissions_utils import user_is_analyst, get_project_and_
 from seqr.views.utils.terra_api_utils import anvil_enabled
 from seqr.views.utils.variant_utils import DISCOVERY_CATEGORY
 
-from seqr.models import Project, Family, FamilyAnalysedBy, Sample, Dataset, RnaSample, Individual
+from seqr.models import Project, Family, FamilyAnalysedBy, Dataset, RnaSample, Individual
 from settings import GREGOR_DATA_MODEL_URL
 
 
@@ -60,10 +60,13 @@ def seqr_stats(request):
 
     grouped_sample_counts = defaultdict(dict)
     for project_key, projects in project_models.items():
-        samples_counts = _get_sample_counts(Sample.objects.filter(individual__family__project__in=projects))
+        samples_counts = _get_sample_counts(
+            Dataset.objects.filter(active_individuals__family__project__in=projects),
+            count=Count('active_individuals'),
+        )
         samples_counts.update(_get_sample_counts(
-            RnaSample.objects.filter(individual__family__project__in=projects).annotate(sample_type=Value('RNA')),
-            data_type_key='data_type')
+            RnaSample.objects.filter(individual__family__project__in=projects, is_active=True).annotate(sample_type=Value('RNA')),
+            data_type_key='data_type', count=Count('*'))
         )
         for k, v in samples_counts.items():
             grouped_sample_counts[k][project_key] = v
@@ -80,8 +83,8 @@ def seqr_stats(request):
     })
 
 
-def _get_sample_counts(sample_q, data_type_key='dataset_type'):
-    samples_agg = sample_q.filter(is_active=True).values('sample_type', data_type_key).annotate(count=Count('*'))
+def _get_sample_counts(sample_q, count=None, data_type_key='dataset_type'):
+    samples_agg = sample_q.values('sample_type', data_type_key).annotate(count=count)
     return {
         f'{sample_agg["sample_type"]}__{sample_agg[data_type_key]}': sample_agg['count'] for sample_agg in samples_agg
     }
@@ -513,20 +516,18 @@ def _process_participant_row(participant, phenotype_rows, missing_participant_id
 
 
 def _get_individual_data_types(projects):
-    sample_types = Sample.objects.filter(individual__family__project__in=projects).values_list('individual_id', 'sample_type')
-    individual_data_types = defaultdict(set)
-    for individual_db_id, sample_type in sample_types:
-        individual_data_types[individual_db_id].add(sample_type)
-    for individual_db_id in RnaSample.objects.filter(individual__family__project__in=projects).values_list('individual_id', flat=True):
-        individual_data_types[individual_db_id].add('RNA')
-    individuals = Individual.objects.filter(id__in=individual_data_types).prefetch_related(
+    rna_individuals = set(RnaSample.objects.filter(individual__family__project__in=projects).values_list('individual_id', flat=True))
+    individuals = Individual.objects.filter(family__project__in=projects).annotate(
+        sample_types=ArrayAgg('active_datasets__sample_type', distinct=True),
+    ).prefetch_related(
         'family__project', 'mother', 'father')
 
     grouped_data_type_individuals = defaultdict(dict)
     for i in individuals:
         participant_id = _format_gregor_id(i.individual_id)
-        grouped_data_type_individuals[participant_id].update(
-            {data_type: i for data_type in individual_data_types[i.id]})
+        data_types = set(i.sample_types) | ({'RNA'} if i.id in rna_individuals else set())
+        if data_types:
+            grouped_data_type_individuals[participant_id].update({data_type: i for data_type in data_types})
     return grouped_data_type_individuals
 
 
