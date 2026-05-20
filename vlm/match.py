56c6b6718016aa9a5e1c9f7c8bfeae8890f20f51
Hana Snow <hsnow@broadinstitute.org>
2026-05-20 17:43:48 -0400

    only initialize liftover once

--- vlm/match.py
+++ vlm/match.py
@@ -58,13 +58,6 @@ def get_variant_match(query: dict) -> dict:
     return _format_results(ac+lift_ac, hom+lift_hom, url)
 
 
-def _liftover_variant(chrom: str, pos: int, genome_build: str) -> Optional[tuple[str, int, str]]:
-    liftover_genome_build = GENOME_VERSION_GRCh38 if genome_build == GENOME_VERSION_GRCh37 else GENOME_VERSION_GRCh37
-    lo = LiftOver(f'{LIFTOVER_DIR}/{genome_build.lower()}_to_{liftover_genome_build.lower()}.over.chain.gz')
-    lifted_coord = lo.convert_coordinate(f'chr{chrom}', pos)
-    return (lifted_coord[0][0].replace('chr', ''), lifted_coord[0][1], liftover_genome_build) if lifted_coord and lifted_coord[0] else None
-
-
 def _get_contact_url(chrom: str, pos: int, ref: str, alt: str, genome_build: str, liftover: Optional[tuple[str, int, str]]) -> str:
     if not SEQR_BASE_URL:
         return SEQR_BASE_URL
@@ -136,3 +129,19 @@ def _format_results(ac: int, hom: int, url: str) -> dict:
             ]
         }
     }
+
+
+LIFTOVERS = {
+    GENOME_VERSION_GRCh38: None,
+    GENOME_VERSION_GRCh37: None,
+}
+
+
+def _liftover_variant(chrom: str, pos: int, genome_build: str) -> Optional[tuple[str, int, str]]:
+    liftover_genome_build = GENOME_VERSION_GRCh38 if genome_build == GENOME_VERSION_GRCh37 else GENOME_VERSION_GRCh37
+    if not LIFTOVERS[genome_version]:
+        LIFTOVERS[genome_version] = LiftOver(
+            f'{LIFTOVER_DIR}/{genome_build.lower()}_to_{liftover_genome_build.lower()}.over.chain.gz'
+        )
+    lifted_coord = LIFTOVERS[genome_version].convert_coordinate(f'chr{chrom}', pos)
+    return (lifted_coord[0][0].replace('chr', ''), lifted_coord[0][1], liftover_genome_build) if lifted_coord and lifted_coord[0] else None
