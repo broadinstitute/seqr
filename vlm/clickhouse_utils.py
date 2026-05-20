645d368cea747b89243f8640a8ba9459a9aa65cd
Hana Snow <hsnow@broadinstitute.org>
2026-05-20 16:36:52 -0400

    use pyliftover

--- vlm/clickhouse_utils.py
+++ vlm/clickhouse_utils.py
@@ -1,5 +1,4 @@
 import clickhouse_connect
-import hail as hl
 import os
 
 CLICKHOUSE_CONNECTION_PARAMS = {
@@ -11,13 +10,12 @@ CLICKHOUSE_CONNECTION_PARAMS = {
 }
 
 
-def get_clickhouse_variant_counts(locus: hl.LocusExpression, ref: str, alt: str, genome_build: str) -> hl.Struct:
-    locus = hl.eval(locus)
+def get_clickhouse_variant_counts(chrom: str, pos: int, genome_build: str, ref: str, alt: str) -> tuple[int, int]:
     client = clickhouse_connect.get_client(**CLICKHOUSE_CONNECTION_PARAMS)
     results = client.query(
          "SELECT plus(gt_stats.1, gt_stats.2), plus(gt_stats.3, gt_stats.4) FROM (SELECT dictGet(%(dict_name)s, ('ac_wes', 'ac_wgs', 'hom_wes', 'hom_wgs'), key) AS gt_stats FROM %(table_name)s WHERE variantId=%(variant_id)s)",
         parameters={
-            'variant_id': f'{locus.contig.replace("chr", "")}-{locus.position}-{ref}-{alt}',
+            'variant_id': f'{chrom}-{pos}-{ref}-{alt}',
             'table_name': f'{genome_build}/SNV_INDEL/key_lookup',
             'dict_name': f'{genome_build}/SNV_INDEL/gt_stats_dict',
         },
--- vlm/match.py
+++ vlm/match.py
@@ -1,6 +1,6 @@
 from aiohttp.web import HTTPBadRequest
-import hail as hl
 import os
+from pyliftover.liftover import LiftOver
 import re
 
 from vlm.clickhouse_utils import get_clickhouse_variant_counts
@@ -8,7 +8,7 @@ from vlm.clickhouse_utils import get_clickhouse_variant_counts
 SEQR_BASE_URL = os.environ.get('SEQR_BASE_URL')
 VLM_DEFAULT_CONTACT_EMAIL = os.environ.get('VLM_DEFAULT_CONTACT_EMAIL')
 NODE_ID = os.environ.get('NODE_ID')
-
+LIFTOVER_DIR = f'{os.path.dirname(os.path.abspath(__file__))}/liftover_references'
 
 BEACON_HANDOVER_TYPE = {
     'id': NODE_ID,
@@ -39,29 +39,31 @@ ASSEMBLY_LOOKUP = {
 
 def get_variant_match(query: dict) -> dict:
     chrom, pos, ref, alt, genome_build = _parse_match_query(query)
-    locus = hl.locus(chrom, pos, reference_genome=genome_build)
 
-    ac, hom = get_clickhouse_variant_counts(locus, ref, alt, genome_build)
+    ac, hom = get_clickhouse_variant_counts(chrom, pos, genome_build, ref, alt)
 
-    liftover_genome_build = GENOME_VERSION_GRCh38 if genome_build == GENOME_VERSION_GRCh37 else GENOME_VERSION_GRCh37
-    liftover_locus = hl.liftover(locus, liftover_genome_build)
-    lift_ac, lift_hom = get_clickhouse_variant_counts(liftover_locus, ref, alt, liftover_genome_build)
+    liftover = _liftover_variant(chrom, pos, genome_build)
+    lift_ac, lift_hom = get_clickhouse_variant_counts(*liftover, ref, alt) if liftover else (0, 0)
 
     url = _get_contact_url(
-        chrom, pos, ref, alt, genome_build, liftover_genome_build, liftover_locus if lift_ac and not ac else None,
+        chrom, pos, ref, alt, genome_build, liftover if lift_ac and not ac else None,
     )
     return _format_results(ac+lift_ac, hom+lift_hom, url)
 
 
-def _get_contact_url(chrom: str, pos: int, ref: str, alt: str, genome_build: str, liftover_genome_build: str, liftover_locus: hl.LocusExpression) -> str:
+def _liftover_variant(chrom: str, pos: int, genome_build: str) -> tuple[str, int, str]:
+    liftover_genome_build = GENOME_VERSION_GRCh38 if genome_build == GENOME_VERSION_GRCh37 else GENOME_VERSION_GRCh37
+    lo = LiftOver(f'{LIFTOVER_DIR}/{genome_build.lower()}_to_{liftover_genome_build.lower()}.over.chain.gz')
+    lifted_coord = lo.convert_coordinate(f'chr{chrom}', pos)
+    return (lifted_coord[0][0].lstrip('chr'), lifted_coord[0][1], liftover_genome_build) if lifted_coord and lifted_coord[0] else None
+
+
+def _get_contact_url(chrom: str, pos: int, ref: str, alt: str, genome_build: str, liftover: tuple[str, int, str]) -> str:
     if not SEQR_BASE_URL:
         return SEQR_BASE_URL
 
     if liftover_locus is not None:
-        lifted = hl.eval(liftover_locus)
-        chrom = lifted.contig
-        pos = lifted.position
-        genome_build = liftover_genome_build
+        chrom, pos, genome_build = liftover
     genome_build = genome_build.replace('GRCh', '')
     return f'{SEQR_BASE_URL}variant_lookup?genomeVersion={genome_build}&variantId={chrom}-{pos}-{ref}-{alt}'
 
@@ -93,7 +95,7 @@ def _parse_match_query(query: dict) -> tuple[str, int, str, str, str]:
         if not re.fullmatch(r'[ATCG]', allele):
             raise HTTPBadRequest(reason=f'Invalid {allele_field}: {allele}')
 
-    return chrom, start, query['referenceBases'], query['alternateBases'], genome_build
+    return chrom.replace('chr', ''), start, query['referenceBases'], query['alternateBases'], genome_build
 
 
 def _format_results(ac: int, hom: int, url: str) -> dict:
