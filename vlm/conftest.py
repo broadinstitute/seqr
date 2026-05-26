d34aacee3c5d0089e0f7e57ef92fab14ea3b3013
Hana Snow <hsnow@broadinstitute.org>
2026-05-26 16:51:42 -0400

    better fixture

--- vlm/conftest.py
+++ vlm/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 
 @pytest.fixture(autouse=True)
-def enable_db_globally(django_db_blocker):
-    """Globally unblocks database access for all tests and fixtures."""
-    django_db_blocker.unblock()
+def enable_db_access_for_all_tests(db):
+    pass
