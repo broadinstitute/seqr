5d60cc9dc2c473380e3096ce4c9f17c1e21dd690
Hana Snow <hsnow@broadinstitute.org>
2026-05-26 17:08:50 -0400

    custom db setup

--- vlm/conftest.py
+++ vlm/conftest.py
@@ -3,3 +3,38 @@ import pytest
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
     pass
+
+@pytest.fixture(scope='session')
+def django_db_setup(
+    request: pytest.FixtureRequest,
+    django_db_blocker: DjangoDbBlocker,
+    django_db_keepdb: bool,
+    django_db_createdb: bool,
+):
+    """Top level fixture to ensure test databases are available"""
+    from django.test.utils import setup_databases, teardown_databases
+
+    setup_databases_args = {}
+
+    if django_db_keepdb and not django_db_createdb:
+        setup_databases_args["keepdb"] = True
+
+    with django_db_blocker.unblock():
+        db_cfg = setup_databases(
+            verbosity=request.config.option.verbose,
+            interactive=False,
+            aliases=['clickhouse_write'],
+            **setup_databases_args,
+        )
+        #     TODO loaddata
+
+    yield
+
+    if not django_db_keepdb:
+        with django_db_blocker.unblock():
+            try:
+                teardown_databases(db_cfg, verbosity=request.config.option.verbose)
+            except Exception as exc:  # noqa: BLE001
+                request.node.warn(
+                    pytest.PytestWarning(f"Error when trying to teardown test databases: {exc!r}")
+                )
