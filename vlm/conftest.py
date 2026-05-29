0508de1f3860be3518ab74f55ed0d9e807c557c6
Hana Snow <hsnow@broadinstitute.org>
2026-05-29 11:39:02 -0400

    remove unneeded fixture

--- vlm/conftest.py
+++ vlm/conftest.py
@@ -1,8 +1,5 @@
 import pytest
 
-@pytest.fixture(autouse=True)
-def enable_db_access_for_all_tests(db):
-    pass
 
 @pytest.fixture(scope='session')
 def django_db_setup(request, django_db_blocker,django_db_keepdb):
