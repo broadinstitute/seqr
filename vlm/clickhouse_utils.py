611352aa479011dc8bd4a48dc5dc5145397f3ab1
Hana Snow <hsnow@broadinstitute.org>
2026-06-02 15:13:08 -0400

    better query construction

--- vlm/clickhouse_utils.py
+++ vlm/clickhouse_utils.py
@@ -13,26 +13,26 @@ CLICKHOUSE_CONNECTION_PARAMS = {
 
 
 def get_clickhouse_variant_counts(chrom: str, pos: int, genome_build: str, ref: str, alt: str) -> Optional[Tuple[int, int]]:
-    query = "SELECT plus(gt_stats.1, gt_stats.2), plus(gt_stats.3, gt_stats.4) FROM (SELECT dictGet(%(dict_name)s, ('ac_wes', 'ac_wgs', 'hom_wes', 'hom_wgs'), key) AS gt_stats"
-    params = {'dict_name': f'{genome_build}/SNV_INDEL/gt_stats_dict'}
+    query = "SELECT plus(gt_stats.1, gt_stats.2), plus(gt_stats.3, gt_stats.4) FROM (SELECT dictGet(%(gt_stats_dict)s, ('ac_wes', 'ac_wgs', 'hom_wes', 'hom_wgs'), key) AS gt_stats FROM %(key_lookup_table)s WHERE variantId=%(variant_id)s)"
+    params = {'gt_stats_dict': f'{genome_build}/SNV_INDEL/gt_stats_dict'}
     results = _get_clickhouse_variant_query_result(chrom, pos, genome_build, ref, alt, query, params)
     return results[0] if results else None
 
 
 def get_clickhouse_variant_details(chrom: str, pos: int, genome_build: str, ref: str, alt: str) -> list[tuple]:
     # TODO real query
-    query = "SELECT plus(gt_stats.1, gt_stats.2), plus(gt_stats.3, gt_stats.4) FROM (SELECT dictGet(%(dict_name)s, ('ac_wes', 'ac_wgs', 'hom_wes', 'hom_wgs'), key) AS gt_stats"
-    params = {'dict_name': f'{genome_build}/SNV_INDEL/gt_stats_dict'}
+    query = "SELECT family_guid, calls FROM %(entries_table_name)s WHERE key in SELECT key"
+    params = {'entries_table_name': f'{genome_build}/SNV_INDEL/entries'}
     return _get_clickhouse_variant_query_result(chrom, pos, genome_build, ref, alt, query, params)
 
 
 def _get_clickhouse_variant_query_result(chrom: str, pos: int, genome_build: str, ref: str, alt: str, query: str, params: dict) -> list[tuple]:
     client = clickhouse_connect.get_client(**CLICKHOUSE_CONNECTION_PARAMS)
     return client.query(
-        query + ' FROM %(table_name)s WHERE variantId=%(variant_id)s)',
+        query,
         parameters={
             'variant_id': f'{chrom}-{pos}-{ref}-{alt}',
-            'table_name': f'{genome_build}/SNV_INDEL/key_lookup',
+            'key_lookup_table': f'{genome_build}/SNV_INDEL/key_lookup',
             **params,
         },
     ).result_set
