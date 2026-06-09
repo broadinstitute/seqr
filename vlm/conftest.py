c998c83dc960a056903938235f153594bead2a56
Hana Snow <hsnow@broadinstitute.org>
2026-06-09 14:54:29 -0400

    additional fixture data

--- vlm/conftest.py
+++ vlm/conftest.py
@@ -10,6 +10,8 @@ def django_db_setup(request, django_db_blocker,django_db_keepdb):
     from django.test.utils import setup_databases, teardown_databases
     from clickhouse_search.models.gt_stats_models import ProjectsToGtStatsGRCh37SnvIndel, ProjectsToGtStatsSnvIndel, \
         GtStatsDictGRCh37SnvIndel, GtStatsDictSnvIndel
+    from clickhouse_search.models.postgres_dicts import AffectedDict, SexDict, IndividualMetadataDict, \
+        DiscoveryVariantDict, ExcludedVariantDict, OmimDict
 
     with django_db_blocker.unblock():
         db_cfg = setup_databases(
@@ -19,10 +21,15 @@ def django_db_setup(request, django_db_blocker,django_db_keepdb):
             keepdb=django_db_keepdb,
         )
         call_command('loaddata', 'clickhouse_search', '--database=clickhouse_write')
+        call_command('loaddata', '1kg_project')
+        AffectedDict.reload()
         ProjectsToGtStatsGRCh37SnvIndel.refresh()
         ProjectsToGtStatsSnvIndel.refresh()
-        GtStatsDictGRCh37SnvIndel.reload()
-        GtStatsDictSnvIndel.reload()
+        for d in [
+            GtStatsDictGRCh37SnvIndel, GtStatsDictSnvIndel, SexDict, IndividualMetadataDict, DiscoveryVariantDict,
+            ExcludedVariantDict, OmimDict,
+        ]:
+            d.reload()
 
     yield
 
