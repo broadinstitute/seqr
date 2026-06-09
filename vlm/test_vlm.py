f42c79476fe7360f465096110bd67fe91c78a14f
Hana Snow <hsnow@broadinstitute.org>
2026-06-09 14:39:47 -0400

    abstract match tests

--- vlm/test_vlm.py
+++ vlm/test_vlm.py
@@ -30,22 +30,7 @@ class VlmTestCase(AioHTTPTestCase):
 
     @aioresponses(passthrough=['http://127.0.0.1'])
     async def test_match(self, mocked_responses):
-
-        mocked_responses.post(
-            'https://vlm-auth.us.auth0.com/oauth/token', payload={'access_token': 'test_token'}, repeat=True,  # nosec
-        )
-        mocked_responses.get(
-            f'https://vlm-auth.us.auth0.com/api/v2/clients/{REQUESTER_CLIENT_ID}',
-            payload={'tenant': 'vlm-auth', 'name': 'Test Node'},
-            repeat=True,
-        )
-        jwt_body = {'iss': 'https://vlm-auth.us.auth0.com/', 'azp': REQUESTER_CLIENT_ID}
-        headers = {'Authorization': f'Bearer {jwt.encode(jwt_body, "")}'}
-
-        async with self.client.request('GET', '/vlm/match?assemblyId=GRCh38&referenceName=1&start=38724419&referenceBases=T&alternateBases=G', headers=headers) as resp:
-            self.assertEqual(resp.status, 200)
-            resp_json = await resp.json()
-        self.assertDictEqual(resp_json, {
+        response = {
             'beaconHandovers': [
                 {
                     'handoverType': {
@@ -102,21 +87,7 @@ class VlmTestCase(AioHTTPTestCase):
                     },
                 ],
             }
-        })
-        mocked_responses.assert_called_with(
-            'https://vlm-auth.us.auth0.com/oauth/token',
-            method='POST',
-            json={'client_id': 'unknown_client_id', 'client_secret': 'unknown_client_secret', 'audience': 'https://vlm-auth.us.auth0.com/api/v2/', 'grant_type': 'client_credentials'},  # nosec
-        )
-        mocked_responses.assert_called_with(
-            f'https://vlm-auth.us.auth0.com/api/v2/clients/{REQUESTER_CLIENT_ID}',
-            headers={'Authorization': 'Bearer test_token'},
-        )
-        self.assertIn(
-            'Received match request from Test Node: assemblyId=GRCh38&referenceName=1&start=38724419&referenceBases=T&alternateBases=G',
-            self._caplog.messages,
-        )
-
+        }
         only_37_response = {
             'beaconHandovers': [
                 {
@@ -175,20 +146,7 @@ class VlmTestCase(AioHTTPTestCase):
                 ],
             }
         }
-        async with self.client.request('GET', '/vlm/match?assemblyId=hg19&referenceName=chr7&start=143270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
-            self.assertEqual(resp.status, 200)
-            resp_json = await resp.json()
-        self.assertDictEqual(resp_json, only_37_response)
-
-        async with self.client.request('GET', '/vlm/match?assemblyId=GRCh38&referenceName=chr7&start=143573079&referenceBases=A&alternateBases=G', headers=headers) as resp:
-            self.assertEqual(resp.status, 200)
-            resp_json = await resp.json()
-        self.assertDictEqual(resp_json, only_37_response)
-
-        async with self.client.request('GET', '/vlm/match?assemblyId=hg38&referenceName=chr7&start=143270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
-            self.assertEqual(resp.status, 200)
-            resp_json = await resp.json()
-        self.assertDictEqual(resp_json, {
+        empty_response = {
             'beaconHandovers': [
                 {
                     'handoverType': {
@@ -245,55 +203,114 @@ class VlmTestCase(AioHTTPTestCase):
                     },
                 ],
             }
-        })
+        }
+        await self._test_match_endpoint('match', mocked_responses, response, only_37_response, empty_response)
+
+    @aioresponses(passthrough=['http://127.0.0.1'])
+    async def test_match_details(self, mocked_responses):
+        await self._test_match_endpoint('match_details', mocked_responses, {}, {}, {})
+
+    async def _test_match_endpoint(self, path, mocked_responses, response, only_37_response, empty_response):
+
+        mocked_responses.post(
+            'https://vlm-auth.us.auth0.com/oauth/token', payload={'access_token': 'test_token'}, repeat=True,  # nosec
+        )
+        mocked_responses.get(
+            f'https://vlm-auth.us.auth0.com/api/v2/clients/{REQUESTER_CLIENT_ID}',
+            payload={'tenant': 'vlm-auth', 'name': 'Test Node'},
+            repeat=True,
+        )
+        jwt_body = {'iss': 'https://vlm-auth.us.auth0.com/', 'azp': REQUESTER_CLIENT_ID}
+        headers = {'Authorization': f'Bearer {jwt.encode(jwt_body, "")}'}
+
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=GRCh38&referenceName=1&start=38724419&referenceBases=T&alternateBases=G', headers=headers) as resp:
+            self.assertEqual(resp.status, 200)
+            resp_json = await resp.json()
+        self.assertDictEqual(resp_json, response)
+        mocked_responses.assert_called_with(
+            'https://vlm-auth.us.auth0.com/oauth/token',
+            method='POST',
+            json={'client_id': 'unknown_client_id', 'client_secret': 'unknown_client_secret', 'audience': 'https://vlm-auth.us.auth0.com/api/v2/', 'grant_type': 'client_credentials'},  # nosec
+        )
+        mocked_responses.assert_called_with(
+            f'https://vlm-auth.us.auth0.com/api/v2/clients/{REQUESTER_CLIENT_ID}',
+            headers={'Authorization': 'Bearer test_token'},
+        )
+        self.assertIn(
+            'Received match request from Test Node: assemblyId=GRCh38&referenceName=1&start=38724419&referenceBases=T&alternateBases=G',
+            self._caplog.messages,
+        )
+
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=hg19&referenceName=chr7&start=143270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
+            self.assertEqual(resp.status, 200)
+            resp_json = await resp.json()
+        self.assertDictEqual(resp_json, only_37_response)
+
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=GRCh38&referenceName=chr7&start=143573079&referenceBases=A&alternateBases=G', headers=headers) as resp:
+            self.assertEqual(resp.status, 200)
+            resp_json = await resp.json()
+        self.assertDictEqual(resp_json, only_37_response)
+
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=hg38&referenceName=chr7&start=143270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
+            self.assertEqual(resp.status, 200)
+            resp_json = await resp.json()
+        self.assertDictEqual(resp_json, empty_response)
 
     @aioresponses(passthrough=['http://127.0.0.1'])
     async def test_match_error(self, mocked_responses):
+        await self._test_match_endpoint_error('match', mocked_responses)
+
+    @aioresponses(passthrough=['http://127.0.0.1'])
+    async def test_match_details_error(self, mocked_responses):
+        await self._test_match_endpoint_error('match_details', mocked_responses)
+
+    async def _test_match_endpoint_error(self, path, mocked_responses):
+
         mocked_responses.get(
             f'https://vlm-auth.us.auth0.com/api/v2/clients/{REQUESTER_CLIENT_ID}', status=404,
         )
 
-        async with self.client.request('GET', '/vlm/match') as resp:
+        async with self.client.request('GET', f'/vlm/{path}') as resp:
             self.assertEqual(resp.status, 403)
             self.assertEqual(resp.reason, 'Invalid authorization header')
 
         headers = {'Authorization': 'token'}
-        async with self.client.request('GET', '/vlm/match', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}', headers=headers) as resp:
             self.assertEqual(resp.status, 403)
             self.assertEqual(resp.reason, 'Invalid authorization header')
 
         headers['Authorization'] =  'JWT token'
-        async with self.client.request('GET', '/vlm/match', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}', headers=headers) as resp:
             self.assertEqual(resp.status, 403)
             self.assertEqual(resp.reason, 'Invalid token scheme')
 
         headers['Authorization'] = 'Bearer token'
-        async with self.client.request('GET', '/vlm/match', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}', headers=headers) as resp:
             self.assertEqual(resp.status, 403)
             self.assertEqual(resp.reason, 'Invalid token: Not enough segments')
 
         jwt_body = {'iss': 'invalid_issuer'}
         headers['Authorization'] = f'Bearer {jwt.encode(jwt_body, "")}'
-        async with self.client.request('GET', '/vlm/match', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}', headers=headers) as resp:
             self.assertEqual(resp.status, 403)
             self.assertEqual(resp.reason, 'Invalid token: Token is missing the "azp" claim')
 
         jwt_body['azp'] = REQUESTER_CLIENT_ID
         headers['Authorization'] = f'Bearer {jwt.encode(jwt_body, "")}'
-        async with self.client.request('GET', '/vlm/match', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}', headers=headers) as resp:
             self.assertEqual(resp.status, 403)
             self.assertEqual(resp.reason, 'Invalid token: Invalid issuer')
 
         mocked_responses.post('https://vlm-auth.us.auth0.com/oauth/token', status=400, payload={'error': 'invalid_request'})
         jwt_body['iss'] = 'https://vlm-auth.us.auth0.com/'
         headers['Authorization'] = f'Bearer {jwt.encode(jwt_body, "")}'
-        async with self.client.request('GET', '/vlm/match', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}', headers=headers) as resp:
             self.assertEqual(resp.status, 403)
             self.assertEqual(resp.reason, 'Credential Check Error')
         self.assertEqual(self._caplog.messages[-3], "Credential Check Error: 400 - {'error': 'invalid_request'}")
 
         mocked_responses.post('https://vlm-auth.us.auth0.com/oauth/token', payload={'access_token': 'test_token'}, repeat=True)  # nosec
-        async with self.client.request('GET', '/vlm/match', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}', headers=headers) as resp:
             self.assertEqual(resp.status, 403)
             self.assertEqual(resp.reason, 'Invalid Client ID abc123')
         self.assertEqual(self._caplog.messages[-3], "Invalid Client ID abc123: 404 - Not Found")
@@ -303,33 +320,33 @@ class VlmTestCase(AioHTTPTestCase):
             payload={'tenant': 'vlm-auth'},
             repeat=True,
         )
-        async with self.client.request('GET', '/vlm/match', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}', headers=headers) as resp:
             self.assertEqual(resp.status, 400)
             self.assertEqual(
                 resp.reason,
                 'Missing required parameters: assemblyId, referenceName, start, referenceBases, alternateBases',
             )
 
-        async with self.client.request('GET', '/vlm/match?assemblyId=38&referenceName=chr7&start=143270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=38&referenceName=chr7&start=143270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
             self.assertEqual(resp.status, 400)
             self.assertEqual(resp.reason,'Invalid assemblyId: 38')
 
-        async with self.client.request('GET', '/vlm/match?assemblyId=hg38&referenceName=27&start=143270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=hg38&referenceName=27&start=143270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
             self.assertEqual(resp.status, 400)
             self.assertEqual(resp.reason,'Invalid referenceName: 27')
 
-        async with self.client.request('GET', '/vlm/match?assemblyId=hg38&referenceName=7&start=1x43270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=hg38&referenceName=7&start=1x43270172&referenceBases=A&alternateBases=G', headers=headers) as resp:
             self.assertEqual(resp.status, 400)
             self.assertEqual(resp.reason,'Invalid start: 1x43270172')
 
-        async with self.client.request('GET', '/vlm/match?assemblyId=hg38&referenceName=7&start=999999999&referenceBases=A&alternateBases=G', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=hg38&referenceName=7&start=999999999&referenceBases=A&alternateBases=G', headers=headers) as resp:
             self.assertEqual(resp.status, 400)
             self.assertEqual(resp.reason,'Invalid start: 999999999')
 
-        async with self.client.request('GET', '/vlm/match?assemblyId=hg38&referenceName=7&start=143270172&referenceBases=ATC&alternateBases=G', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=hg38&referenceName=7&start=143270172&referenceBases=ATC&alternateBases=G', headers=headers) as resp:
             self.assertEqual(resp.status, 400)
             self.assertEqual(resp.reason,'Invalid referenceBases: ATC')
 
-        async with self.client.request('GET', '/vlm/match?assemblyId=hg38&referenceName=7&start=143270172&referenceBases=A&alternateBases=GAG', headers=headers) as resp:
+        async with self.client.request('GET', f'/vlm/{path}?assemblyId=hg38&referenceName=7&start=143270172&referenceBases=A&alternateBases=GAG', headers=headers) as resp:
             self.assertEqual(resp.status, 400)
             self.assertEqual(resp.reason,'Invalid alternateBases: GAG')
