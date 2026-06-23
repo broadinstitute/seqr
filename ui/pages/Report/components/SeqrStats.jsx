e43776c38d391494ad2bea4936e8fc067881e166
Hana Snow <hsnow@broadinstitute.org>
2026-06-23 14:36:05 -0400

    show user stats

--- ui/pages/Report/components/SeqrStats.jsx
+++ ui/pages/Report/components/SeqrStats.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { Header, Table } from 'semantic-ui-react'
+import { Header, Table, Grid } from 'semantic-ui-react'
 
 import { getUser } from 'redux/selectors'
 import DataLoader from 'shared/components/DataLoader'
@@ -22,38 +22,67 @@ const COLUMN_MAP = {
     DEMO_COLUMN,
   ],
 }
+const USER_ROWS = [
+  { title: 'Total', key: 'total' },
+  { title: 'Logged In More Than Once', key: 'multipleLogins' },
+  { title: 'Logged In Within 30 Days', key: 'lastMonth' },
+  { title: 'Logged In Within 365 Days', key: 'lastYear' },
+  { title: 'Logged In This Year', key: 'thisYear' },
+]
 
 const SeqrStats = React.memo(({ stats, error, loading, load, user }) => (
   <div>
     <Header size="large" content="Seqr Stats:" />
     <DataLoader load={load} content={Object.keys(stats).length} loading={loading} errorMessage={error}>
-      <Table collapsing basic="very" textAlign="right">
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell />
-            {COLUMN_MAP[user.isAnvil].map(({ title }) => <Table.HeaderCell key={title} content={title} />)}
-          </Table.Row>
-        </Table.Header>
-        {['Projects', 'Families', 'Individuals'].map(field => (
-          <Table.Row key={field}>
-            <Table.HeaderCell textAlign="right" content={field} />
-            {COLUMN_MAP[user.isAnvil].map(({ key }) => (
-              <Table.Cell key={key} content={(stats[`${field.toLowerCase()}Count`] || {})[key]} />
-            ))}
-          </Table.Row>
-        ))}
-        {Object.keys(stats.sampleCountsByType || {}).sort().map(sampleTypes => (
-          <Table.Row key={sampleTypes}>
-            <Table.HeaderCell
-              textAlign="right"
-              content={`${sampleTypes.split('__')[0]}${DATASET_TITLE_LOOKUP[sampleTypes.split('__')[1]] || ''} samples`}
-            />
-            {COLUMN_MAP[user.isAnvil].map(({ key }) => (
-              <Table.Cell key={key} content={stats.sampleCountsByType[sampleTypes][key]} />
-            ))}
-          </Table.Row>
-        ))}
-      </Table>
+      <Grid columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            <Table collapsing basic="very" textAlign="right">
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell />
+                  {COLUMN_MAP[user.isAnvil].map(({ title }) => <Table.HeaderCell key={title} content={title} />)}
+                </Table.Row>
+              </Table.Header>
+              {['Projects', 'Families', 'Individuals'].map(field => (
+                <Table.Row key={field}>
+                  <Table.HeaderCell textAlign="right" content={field} />
+                  {COLUMN_MAP[user.isAnvil].map(({ key }) => (
+                    <Table.Cell key={key} content={(stats[`${field.toLowerCase()}Count`] || {})[key]} />
+                  ))}
+                </Table.Row>
+              ))}
+              {Object.keys(stats.sampleCountsByType || {}).sort().map(sampleTypes => (
+                <Table.Row key={sampleTypes}>
+                  <Table.HeaderCell
+                    textAlign="right"
+                    content={`${sampleTypes.split('__')[0]}${DATASET_TITLE_LOOKUP[sampleTypes.split('__')[1]] || ''} samples`}
+                  />
+                  {COLUMN_MAP[user.isAnvil].map(({ key }) => (
+                    <Table.Cell key={key} content={stats.sampleCountsByType[sampleTypes][key]} />
+                  ))}
+                </Table.Row>
+              ))}
+            </Table>
+          </Grid.Column>
+          <Grid.Column>
+            <Table collapsing basic="very" textAlign="right">
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell />
+                  <Table.HeaderCell content="Users" />
+                </Table.Row>
+                {USER_ROWS.map(({ title, key }) => (
+                  <Table.Row key={key}>
+                    <Table.HeaderCell textAlign="right" content={title} />
+                    <Table.Cell content={(stats.usersCounts || {})[key]} />
+                  </Table.Row>
+                ))}
+              </Table.Header>
+            </Table>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </DataLoader>
   </div>
 ))
