abfa3dfd34094b14bdc27dc34989a50537f60f60
Hana Snow <hsnow@broadinstitute.org>
2026-07-02 14:10:40 -0400

    more clean up

--- seqr/views/apis/variant_search_api.py
+++ seqr/views/apis/variant_search_api.py
@@ -94,12 +94,11 @@ def _all_project_family_search_genome(search_context):
 
 
 def _all_genome_version_families(genome_version, user):
-    omit_projects = [p.guid for p in Project.objects.filter(is_demo=True).only('guid')]
-    project_guids = [
-        project_guid for project_guid in get_project_analysis_group_guids_user_can_view(user, limit_data_manager=True)
-        if project_guid not in omit_projects
-    ]
-    return Family.objects.filter(project__guid__in=project_guids, project__genome_version=genome_version)
+    project_guids, analysis_group_guids = get_project_analysis_group_guids_user_can_view(request.user, limit_data_manager=True)
+    access_filter = Q(project__guid__in=project_guids)
+    if analysis_group_guids:
+        access_filter |= Q(analysisgroup_guid__in=analysis_group_guids)
+    return Family.objects.filter(project__genome_version=genome_version).filter(access_filter).exclude(project__is_demo=True)
 
 
 def _get_or_create_results_model(search_hash, search_context, user):
