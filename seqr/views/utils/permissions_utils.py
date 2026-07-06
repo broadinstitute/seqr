55b6a21b329cce19a9481f95678fe64b5b56636e
Hana Snow <hsnow@broadinstitute.org>
2026-07-06 13:23:17 -0400

    actual any family access projects

--- seqr/views/utils/permissions_utils.py
+++ seqr/views/utils/permissions_utils.py
@@ -269,8 +269,9 @@ def check_locus_list_permissions(locus_list, user):
 
 
 def get_project_guids_any_family_user_can_view(user):
-    # TODO add projects based on analysis groups
-    return get_project_guids_user_can_view(user)
+    project_guids, analysis_group_guids = get_project_analysis_group_guids_user_can_view(user)
+    additional_project_guids = AnalysisGroup.objects.filter(guid__in=analysis_group_guids).values_list('project__guid', flat=True)
+    return project_guids + list(additional_project_guids)
 
 
 def get_project_analysis_group_guids_user_can_view(user, limit_data_manager=True):
