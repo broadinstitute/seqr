87527c8ca7bd910d6539a2de62476ffc8a9bcd13
Hana Snow <hsnow@broadinstitute.org>
2026-07-08 12:17:12 -0400

    fix null ag access

--- seqr/views/utils/permissions_utils.py
+++ seqr/views/utils/permissions_utils.py
@@ -282,7 +282,10 @@ def check_user_created_object_permissions(obj, user):
 
 def check_families_view_permission(families, user):
     project_guids, analysis_group_guids = get_project_analysis_group_guids_user_can_view(user, limit_data_manager=False)
-    no_access_families = families.exclude(Q(project__guid__in=project_guids) | Q(analysisgroup__guid__in=analysis_group_guids))
+    access_q = Q(project__guid__in=project_guids)
+    if analysis_group_guids:
+        access_q |= Q(analysis_group_guid__in=analysis_group_guids)
+    no_access_families = families.exclude(access_q)
     if no_access_families:
         raise PermissionDenied(f"{user} does not have sufficient permissions for {','.join(no_access_families.values_list('guid', flat=True))}")
 
@@ -299,7 +302,8 @@ def check_locus_list_permissions(locus_list, user):
 
 def get_project_guids_any_family_user_can_view(user):
     project_guids, analysis_group_guids = get_project_analysis_group_guids_user_can_view(user)
-    additional_project_guids = AnalysisGroup.objects.filter(guid__in=analysis_group_guids).values_list('project__guid', flat=True)
+    additional_project_guids = AnalysisGroup.objects.filter(guid__in=analysis_group_guids).values_list('project__guid', flat=True) \
+        if analysis_group_guids else []
     return project_guids + list(additional_project_guids)
 
 
