60824a638ba73cbad2a374e08ad184294a5be8d9
Hana Snow <hsnow@broadinstitute.org>
2026-07-08 16:58:06 -0400

    conditional perm check

--- seqr/views/apis/individual_api_tests.py
+++ seqr/views/apis/individual_api_tests.py
@@ -1422,7 +1422,8 @@ class IndividualAPITest(object):
 
     def test_get_individual_rna_seq_data(self):
         url = reverse(get_individual_rna_seq_data, args=[INDIVIDUAL_GUID])
-        self.check_collaborator_login(url)
+        check_access = self.check_require_login if self.HAS_EXTERNAL_PROJECT_ACCESS else self.check_collaborator_login
+        check_access(url)
 
         response = self.client.get(url, content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -1466,7 +1467,8 @@ class IndividualAPITest(object):
 
     def test_get_individual_rna_seq_data_is_significant(self):
         url = reverse(get_individual_rna_seq_data, args=[INDIVIDUAL_GUID])
-        self.check_collaborator_login(url)
+        check_access = self.check_require_login if self.HAS_EXTERNAL_PROJECT_ACCESS else self.check_collaborator_login
+        check_access(url)
 
         response = self.client.get(url, content_type='application/json')
         self.assertEqual(response.status_code, 200)
