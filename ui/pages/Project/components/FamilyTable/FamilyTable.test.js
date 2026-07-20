1031d3213c78200f15300afa39a5c27c4c9b4bb6
Hana Snow <hsnow@broadinstitute.org>
2026-07-20 17:49:02 -0400

    actually test the rendered content for collapasable layout usages

--- ui/pages/Project/components/FamilyTable/FamilyTable.test.js
+++ ui/pages/Project/components/FamilyTable/FamilyTable.test.js
@@ -1,23 +1,54 @@
 import React from 'react'
-import { shallow, configure } from 'enzyme'
+import { shallow, mount, configure } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-import { FamilyTableComponent } from './FamilyTable'
-import { getVisibleFamiliesInSortedOrder } from '../../selectors'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router-dom'
+import { FAMILY_MAIN_FIELDS, FAMILY_DETAIL_FIELDS } from 'shared/utils/constants'
 
-import { STATE1 } from '../../fixtures'
+import FamilyTable, { FamilyTableComponent } from './FamilyTable'
+import { STATE_WITH_2_FAMILIES } from '../../fixtures'
 
 configure({ adapter: new Adapter() })
 
-test('shallow-render without crashing', () => {
-  /*
-    visibleFamilies: PropTypes.array.isRequired,
-    familyGuidToIndividuals: PropTypes.object.isRequired,
-   */
+const configureStore = configureMockStore([thunk])
 
-  const props = {
-    visibleFamilies: Object.values(STATE1.familiesByGuid),
-    match: { params: {} },
-  }
+test('toggles compact/full family details', () => {
+  const store = configureStore(STATE_WITH_2_FAMILIES)
 
-  shallow(<FamilyTableComponent {...props} />)
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter>
+        <FamilyTable
+          detailFields={FAMILY_DETAIL_FIELDS}
+          noDetailFields={FAMILY_MAIN_FIELDS}
+        />
+      </MemoryRouter>
+    </Provider>,
+  )
+
+  // the family name field is always rendered in addition to the toggled field set
+  const NAME_FIELD_COUNT = 1
+  const CORE_FIELD_COUNT = FAMILY_MAIN_FIELDS.length + NAME_FIELD_COUNT
+  const DETAIL_FIELD_COUNT = FAMILY_DETAIL_FIELDS.length + NAME_FIELD_COUNT
+
+  // starts collapsed - only the compact noDetailFields are rendered
+  const rows = wrapper.find('FamilyTableRow')
+  expect(rows.length).toEqual(2)
+  expect(rows.find({ familyGuid: 'F011652_1' }).exists()).toBe(true)
+  expect(rows.find({ familyGuid: 'F011652_1' }).find('BaseFieldView').length).toEqual(CORE_FIELD_COUNT)
+  expect(rows.find({ familyGuid: 'F011652_2' }).find('BaseFieldView').length).toEqual(CORE_FIELD_COUNT)
+
+  const toggle = rows.find({ familyGuid: 'F011652_1' }).find('CollapsableLayout').find('Icon[name="dropdown"]')
+  expect(toggle.exists()).toBe(true)
+
+  toggle.first().simulate('click')
+  wrapper.update()
+
+  // after toggling, the full detailFields are rendered instead for the toggled row
+  expect(wrapper.find('FamilyTableRow').find({ familyGuid: 'F011652_2' }).find('BaseFieldView').length).toEqual(CORE_FIELD_COUNT)
+  const toggledRow = wrapper.find('FamilyTableRow').find({ familyGuid: 'F011652_1' }).first()
+  expect(toggledRow.find('Family').find('BaseFieldView').length).toEqual(DETAIL_FIELD_COUNT)
+  expect(toggledRow.find('IndividualRow').length).toEqual(3)
 })
--- ui/pages/Project/components/FamilyTable/IndividualRow.test.js
+++ ui/pages/Project/components/FamilyTable/IndividualRow.test.js
@@ -1,19 +1,43 @@
 import React from 'react'
-import { shallow, configure } from 'enzyme'
+import { shallow, mount, configure } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-import configureStore from 'redux-mock-store'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router-dom'
+import { CASE_REVIEW_TABLE_NAME } from '../../constants'
 
 import IndividualRow from './IndividualRow'
 import { STATE_WITH_2_FAMILIES } from '../../fixtures'
 
 configure({ adapter: new Adapter() })
 
-test('shallow-render without crashing', () => {
-  const store = configureStore()(STATE_WITH_2_FAMILIES)
-  shallow(
-    <IndividualRow
-      store={store}
-      family={STATE_WITH_2_FAMILIES.familiesByGuid.F011652_1}
-      individual={STATE_WITH_2_FAMILIES.individualsByGuid.I021475_na19675_1}
-    />)
+const configureStore = configureMockStore([thunk])
+
+test('toggles compact/full individual details via CollapsableLayout when deeply rendered', () => {
+  const store = configureStore(STATE_WITH_2_FAMILIES)
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter>
+        <IndividualRow
+          family={STATE_WITH_2_FAMILIES.familiesByGuid.F011652_1}
+          individual={STATE_WITH_2_FAMILIES.individualsByGuid.I021475_na19675_1}
+          tableName={CASE_REVIEW_TABLE_NAME}
+        />
+      </MemoryRouter>
+    </Provider>,
+  )
+
+  // the pedigree/IGV edit buttons are always rendered, regardless of collapsed state
+  const baseFieldCount = wrapper.find('BaseFieldView').length
+
+  const toggle = wrapper.find('CollapsableLayout').find('Icon[name="dropdown"]')
+  expect(toggle.exists()).toBe(true)
+
+  toggle.first().simulate('click')
+  wrapper.update()
+
+  // after toggling, the full set of case review detail fields is rendered in addition
+  expect(wrapper.find('BaseFieldView').length).toBeGreaterThan(baseFieldCount)
 })
