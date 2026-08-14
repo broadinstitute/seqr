58eb9dd88220603cf07f51d77a72f53440c997d4
Hana Snow <hsnow@broadinstitute.org>
2026-08-14 13:46:29 -0400

    fix test

--- loading_pipeline/lib/misc/clickhouse_test.py
+++ loading_pipeline/lib/misc/clickhouse_test.py
@@ -881,12 +881,12 @@ class ClickhouseTest(MockedDatarootTestCase, ClickhouseSchemaTestCase):
         self.assertEqual(
             new_gt_stats_post_reload,
             [
-                (10, 2, 0, 0, 1, 0, 0),
                 (1, 1, 1, 0, 0, 0, 0),
                 (2, 0, 2, 0, 0, 1, 0),
                 (3, 2, 0, 0, 1, 0, 0),
                 (4, 5, 0, 0, 2, 0, 0),
                 (5, 2, 0, 0, 1, 0, 0),
+                (10, 2, 0, 0, 1, 0, 0),
             ],
         )
 
