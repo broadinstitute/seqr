463e4e60df18cfbc6381d53a3d3b4c06a4f5ffb3
Hana Snow <hsnow@broadinstitute.org>
2026-08-18 16:18:44 -0400

    remove annotations table dependency from SV info field parsing

--- loading_pipeline/lib/misc/callsets.py
+++ loading_pipeline/lib/misc/callsets.py
@@ -1,13 +1,9 @@
 import functools
 
 import hail as hl
-import hailtop.fs as hfs
 
 from loading_pipeline.lib.core import DatasetType, ReferenceGenome
-from loading_pipeline.lib.paths import (
-    remapped_and_subsetted_callset_path,
-    variant_annotations_table_path,
-)
+from loading_pipeline.lib.paths import remapped_and_subsetted_callset_path
 
 
 def get_callset_ht(
@@ -107,23 +103,6 @@ def get_additional_row_fields(
         **(
             {'info.SEQR_INTERNAL_TRUTH_VID': hl.tstr}
             if dataset_type.re_key_by_seqr_internal_truth_vid
-            and hfs.exists(
-                variant_annotations_table_path(
-                    reference_genome,
-                    dataset_type,
-                ),
-            )
-            and hl.eval(
-                hl.len(
-                    hl.read_table(
-                        variant_annotations_table_path(
-                            reference_genome,
-                            dataset_type,
-                        ),
-                    ).globals.updates,
-                )
-                > 0,
-            )
             else {}
         ),
     }
