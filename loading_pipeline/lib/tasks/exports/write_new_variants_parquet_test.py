54e3d80271e1a72d63218cc5d2ce4b541c954ba6
Hana Snow <hsnow@broadinstitute.org>
2026-08-21 15:35:32 -0400

    undo debug

--- loading_pipeline/lib/tasks/exports/write_new_variants_parquet_test.py
+++ loading_pipeline/lib/tasks/exports/write_new_variants_parquet_test.py
@@ -322,7 +322,7 @@ class WriteNewVariantsParquetTest(MockedReferenceDatasetsTestCase):
                 TEST_RUN_ID,
             ),
         )
-        export_json = convert_ndarray_to_list(df.head(10).to_dict('records')) # TODO debug
+        export_json = convert_ndarray_to_list(df.head(1).to_dict('records'))
         export_json[0]['sortedTranscriptConsequences'] = [
             export_json[0]['sortedTranscriptConsequences'][0],
         ]
