cea65620be90db917717f82ba7b3d8c1c0703b08
Hana Snow <hsnow@broadinstitute.org>
2026-09-11 17:05:20 -0400

    clean up export fields

--- loading_pipeline/lib/tasks/exports/fields.py
+++ loading_pipeline/lib/tasks/exports/fields.py
@@ -92,11 +92,12 @@ def get_dataset_type_specific_variants_annotations(
 
 
 def get_existing_variants_export_field(dataset_type: DatasetType) -> str:
+    # TODO should really be querying from key lookup table
     dt_fields = {
-        DatasetType.SNV_INDEL: ', transcripts.geneId AS geneIds',
+        DatasetType.SNV_INDEL: '',
         DatasetType.MITO: '',
-        DatasetType.SV: ', CAST(xpos AS Int64) AS xpos, end, endChrom, sortedGeneConsequences.geneId AS geneIds',
-        DatasetType.GCNV: ', CAST(xpos AS Int64) AS xpos, pos AS start, end, numExon as num_exon, sortedGeneConsequences.geneId AS gene_ids',
+        DatasetType.SV: ', end, endChrom',
+        DatasetType.GCNV: '',
     }[dataset_type]
     return f'key AS key_, variantId AS variant_id {dt_fields}'
 
