c2df84b2571abdd18c6b125943a8bf304e6e7402
Hana Snow <hsnow@broadinstitute.org>
2026-09-15 12:11:07 -0400

    test existing variants parquets with hail

--- loading_pipeline/lib/tasks/write_existing_variants_parquet_test.py
+++ loading_pipeline/lib/tasks/write_existing_variants_parquet_test.py
@@ -1,10 +1,10 @@
-import gzip
 from typing import ClassVar
 
+import hail as hl
 import luigi.worker
-import pandas as pd
 
 from loading_pipeline.lib.core import DatasetType, ReferenceGenome, SampleType
+from loading_pipeline.lib.misc.io import import_parquet
 from loading_pipeline.lib.paths import existing_variants_parquet_path
 from loading_pipeline.lib.tasks.write_existing_variants_parquet import (
     WriteExistingVariantsParquetTask,
@@ -12,7 +12,6 @@ from loading_pipeline.lib.tasks.write_existing_variants_parquet import (
 from loading_pipeline.lib.test.clickhouse_schema_testcase import (
     ClickhouseSchemaTestCase,
 )
-from loading_pipeline.lib.test.misc import convert_ndarray_to_list
 from loading_pipeline.lib.test.mocked_dataroot_testcase import MockedDatarootTestCase
 
 TEST_RUN_ID = 'manual__2024-04-03'
@@ -28,7 +27,7 @@ class WriteExistingVariantsParquetTest(
         self,
         dataset_type: DatasetType,
         reference_genome: ReferenceGenome = ReferenceGenome.GRCh38,
-    ) -> pd.DataFrame:
+    ) -> hl.Table:
         worker = luigi.worker.Worker()
         task = WriteExistingVariantsParquetTask(
             reference_genome=reference_genome,
@@ -41,51 +40,67 @@ class WriteExistingVariantsParquetTest(
         worker.run()
         self.assertTrue(task.output().exists())
         self.assertTrue(task.complete())
-        with gzip.open(
+        return import_parquet(
             existing_variants_parquet_path(reference_genome, dataset_type, TEST_RUN_ID),
-        ) as f:
-            return pd.read_parquet(f)
+            reference_genome,
+            dataset_type,
+        )
 
     def test_snv_indel(self):
-        df = self._run_task(DatasetType.SNV_INDEL)
-        self.assertEqual(list(df.columns), ['key_', 'variant_id'])
-        df = df.sort_values('key_').reset_index(drop=True)
+        ht = self._run_task(DatasetType.SNV_INDEL)
+        self.assertEqual(list(ht.row), ['key_', 'locus', 'alleles'])
         self.assertEqual(
-            convert_ndarray_to_list(
-                df[['key_', 'variant_id']].to_dict('records'),
-            ),
+            ht.collect(),
             [
-                {'key_': 1, 'variant_id': '1-878314-G-C'},
-                {'key_': 7, 'variant_id': '7-1234567-AGT-A'},
-                {'key_': 10, 'variant_id': '10-987654-G-A'},
+                hl.Struct(
+                    key_=1,
+                    locus=hl.Locus(
+                        contig='chr1',
+                        position=878314,
+                        reference_genome='GRCh38',
+                    ),
+                    alleles=['G', 'C'],
+                ),
+                hl.Struct(
+                    key_=7,
+                    locus=hl.Locus(
+                        contig='chr7',
+                        position=1234567,
+                        reference_genome='GRCh38',
+                    ),
+                    alleles=['AGT', 'A'],
+                ),
+                hl.Struct(
+                    key_=10,
+                    locus=hl.Locus(
+                        contig='chr10',
+                        position=987654,
+                        reference_genome='GRCh38',
+                    ),
+                    alleles=['G', 'A'],
+                ),
             ],
         )
 
     def test_grch37_snv_indel(self):
-        df = self._run_task(
+        ht = self._run_task(
             DatasetType.SNV_INDEL,
             reference_genome=ReferenceGenome.GRCh37,
         )
-        self.assertEqual(list(df.columns), ['key_', 'variant_id'])
-        self.assertEqual(len(df), 0)
+        self.assertEqual(list(ht.row), ['key_', 'locus', 'alleles'])
+        self.assertEqual(ht.count(), 0)
 
     def test_mito(self):
-        df = self._run_task(DatasetType.MITO)
-        self.assertEqual(list(df.columns), ['key_', 'variant_id'])
-        self.assertEqual(len(df), 0)
+        ht = self._run_task(DatasetType.MITO)
+        self.assertEqual(list(ht.row), ['key_', 'locus', 'alleles'])
+        self.assertEqual(ht.count(), 0)
 
     def test_sv(self):
-        df = self._run_task(DatasetType.SV)
-        self.assertEqual(
-            list(df.columns),
-            ['key_', 'variant_id', 'end', 'endChrom'],
-        )
-        self.assertEqual(len(df), 0)
+        ht = self._run_task(DatasetType.SV)
+        self.assertEqual(list(ht.row), ['key_', 'variant_id', 'end', 'endChrom'])
+        self.assertEqual(ht.count(), 0)
 
     def test_gcnv(self):
-        df = self._run_task(DatasetType.GCNV)
-        self.assertEqual(
-            list(df.columns),
-            ['key_', 'variant_id'],
-        )
-        self.assertEqual(len(df), 0)
+        ht = self._run_task(DatasetType.GCNV)
+        self.assertEqual(list(ht.row), ['key_', 'variant_id'])
+        self.assertEqual(ht.count(), 0)
