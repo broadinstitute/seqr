38a22d676edc33d87a32decdd44182d2d124847a
Hana Snow <hsnow@broadinstitute.org>
2026-09-15 16:09:48 -0400

    actual update existing variants test

--- loading_pipeline/lib/tasks/write_existing_variants_parquet_test.py
+++ loading_pipeline/lib/tasks/write_existing_variants_parquet_test.py
@@ -46,20 +46,16 @@ class WriteExistingVariantsParquetTest(
 
     def test_snv_indel(self):
         df = self._run_task(DatasetType.SNV_INDEL)
-        self.assertEqual(list(df.columns), ['key_', 'variant_id', 'geneIds'])
+        self.assertEqual(list(df.columns), ['key_', 'variant_id'])
         df = df.sort_values('key_').reset_index(drop=True)
         self.assertEqual(
             convert_ndarray_to_list(
-                df[['key_', 'variant_id', 'geneIds']].to_dict('records'),
+                df[['key_', 'variant_id']].to_dict('records'),
             ),
             [
-                {
-                    'key_': 1,
-                    'variant_id': '1-878314-G-C',
-                    'geneIds': ['ENSG00000177000'],
-                },
-                {'key_': 7, 'variant_id': '7-1234567-AGT-A', 'geneIds': []},
-                {'key_': 10, 'variant_id': '10-987654-G-A', 'geneIds': []},
+                {'key_': 1, 'variant_id': '1-878314-G-C'},
+                {'key_': 7, 'variant_id': '7-1234567-AGT-A'},
+                {'key_': 10, 'variant_id': '10-987654-G-A'},
             ],
         )
 
@@ -68,7 +64,7 @@ class WriteExistingVariantsParquetTest(
             DatasetType.SNV_INDEL,
             reference_genome=ReferenceGenome.GRCh37,
         )
-        self.assertEqual(list(df.columns), ['key_', 'variant_id', 'geneIds'])
+        self.assertEqual(list(df.columns), ['key_', 'variant_id'])
         self.assertEqual(len(df), 0)
 
     def test_mito(self):
@@ -80,7 +76,7 @@ class WriteExistingVariantsParquetTest(
         df = self._run_task(DatasetType.SV)
         self.assertEqual(
             list(df.columns),
-            ['key_', 'variant_id', 'xpos', 'end', 'endChrom', 'geneIds'],
+            ['key_', 'variant_id', 'end', 'endChrom'],
         )
         self.assertEqual(len(df), 0)
 
